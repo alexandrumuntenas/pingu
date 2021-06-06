6372118330a6fbb4d91b607a32a2d94464e6ee41
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-06 20:26:10 +0200

    Insider >> Actualizar cmd nuevo servidor

--- tools_insider/nuevoservidor.js
+++ tools_insider/nuevoservidor.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'nuevoservidor',
     execute(client, versionbot, build, con, Math, Jimp, downloader, webp, fs, pdf, moment, msi, emojiStrip, message, args, contenido, result, Intents, MessageEmbed, MessageReaction, MessageCollector, MessageAttachment, global) {
-        message.reply(' puedes añadirme a tu servidor utilizando el siguiente link: https://discord.com/oauth2/authorize?client_id=827199539185975417&permissions=1933044831&scope=bot%20applications.commands')
+        message.reply(' puedes añadirme a tu servidor utilizando el siguiente link: https://discord.com/oauth2/authorize?client_id=847517541080367124&permissions=1933044831&scope=bot%20applications.commands')
     }
 }
