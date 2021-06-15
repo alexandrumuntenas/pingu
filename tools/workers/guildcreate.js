4e75e5ad621731b395e01623f2fe7c65a9c77a85
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-15 13:22:27 +0200

    Detectado fallos en el SQL "duplicatedentry". Solucionado.

--- tools/workers/guildcreate.js
+++ tools/workers/guildcreate.js
@@ -2,11 +2,11 @@ module.exports = function (con, guild) {
     var id = guild.id;
     var chx = guild.channels.cache.filter(chx => chx.type === "text").find(x => x.position === 0);
     var sql = "INSERT INTO `servidores` (`guild`, `prefix`,`bienvenida_canal_id`,`bienvenida_mensaje`,`salida_canal`,`salida_mensaje`,`niveles_canal_id`,`niveles_canal_mensaje`) VALUES (" + id + ", '/','" + chx.id + "','Bienvenido {user} a {server}','" + chx.id + "','¡Adiós {user}!','" + chx.id + "','GG! {user} ha subido al nivel {nivel-nuevo}');";
-    var duplicatedentry = "SELECT * FROM `servidores` WHERE 'guild' LIKE .id";
+    var duplicatedentry = "SELECT * FROM `servidores` WHERE 'guild' LIKE ".id;
     con.query(duplicatedentry, function (err, result) {
         if (!result[0]) {
             con.query(sql, function (err, result) {
-                if (err) throw err;
+                if (err) console.log(err);
             });
         }
     });
