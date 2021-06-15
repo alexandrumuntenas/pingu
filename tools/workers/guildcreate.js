85dc7eaaaf0f2d1d69d06c07b6adba94fa13aa85
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-15 13:30:12 +0200

    SQL Update

--- tools/workers/guildcreate.js
+++ tools/workers/guildcreate.js
@@ -4,6 +4,7 @@ module.exports = function (con, guild) {
     var sql = "INSERT INTO `servidores` (`guild`, `prefix`,`bienvenida_canal_id`,`bienvenida_mensaje`,`salida_canal`,`salida_mensaje`,`niveles_canal_id`,`niveles_canal_mensaje`) VALUES (" + id + ", '/','" + chx.id + "','Bienvenido {user} a {server}','" + chx.id + "','¡Adiós {user}!','" + chx.id + "','GG! {user} ha subido al nivel {nivel-nuevo}');";
     var duplicatedentry = "SELECT * FROM `servidores` WHERE `guild` LIKE ".id;
     con.query(duplicatedentry, function (err, result) {
+        if (err) throw err;
         if (!result[0]) {
             con.query(sql, function (err, result) {
                 if (err) throw err;
