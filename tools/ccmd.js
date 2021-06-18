007e5b758bda2ff895e217b6683ded219476bb6c
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-18 10:27:14 +0200

    Sistema de creación de comandos personalizados

---
+++ tools/ccmd.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: 'ccmd',
+    execut8e(client, versionbot, build, con, Math, Jimp, downloader, webp, fs, pdf, moment, msi, emojiStrip, message, args, contenido, result, Intents, MessageEmbed, MessageReaction, MessageCollector, MessageAttachment, global) {
+        if (args[1]) {
+            switch (args[1]) {
+                case 'crear':
+                    var motivo = message.content.replace(global.prefix + 'ccmd crear ', '');
+                    var crearccmd = "INSERT INTO `comandos_custom` (`guild`, `cmd`, `returns`) VALUES ('" + global.id + "', '" + args[2] + "', '" + motivo + "')";
+                    con.query(crearcmd, function (err) {
+                        if (err) throw err;
+                        message.channel.send(':white_check_mark: Se ha creado correctamente el comando personalizado `' + args[2] + '`. Este devolverá el valor especificado `' + motivo + '`');
+                    })
+                    break;
+                case 'eliminar':
+                    break;
+                case 'listar':
+                    break;
+            }
+        }
+    }
+}
