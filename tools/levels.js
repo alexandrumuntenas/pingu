1fe59d4ed228df7809ce39021470fa0f2e8798a9
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-21 10:29:36 +0200

    Comando de levels

--- tools/levels.js
+++ tools/levels.js
@@ -2,7 +2,24 @@ module.exports = {
     name: 'levels',
     execute(client, versionbot, build, con, Math, Jimp, downloader, webp, fs, pdf, moment, msi, emojiStrip, message, args, contenido, result, Intents, MessageEmbed, MessageReaction, MessageCollector, MessageAttachment, global) {
         if (result[0].niveles_activado != 0) {
-            message.reply(' este comando sigue en construcción... Disculpe las molestias');
+            var dif = result[0].niveles_dificultad;
+            var lookupfortop10 = "SELECT * FROM leveling WHERE guild = " + global.id + " ORDER BY nivel DESC LIMIT 10";
+            con.query(lookupfortop10, function (err, rows, result) {
+                console.log(rows);
+                if (result) {
+                    if (typeof result[0] !== 'undefined') {
+                        var embed = new MessageEmbed();
+                        embed.setTitle('Ranking')
+                        embed.setAuthor(global.name);
+                        rows.forEach(function (row) {
+                            var usuario = client.users.cache.find(user => user.id === row.user);
+                            embed.addFields({ name: usuario.username, value: "Nivel: " + row.nivel + " | Experiencia: " + ((parseInt(row.nivel) * (dif * 100)) + parseInt(row.experiencia)) })
+                        });
+                    };
+                    embed.setFooter('Para obtener el TOP 10, Wired Penguin ordena de forma descendente los datos de nivel registrados.')
+                    message.channel.send(embed);
+                }
+            });
         }
     }
 }
