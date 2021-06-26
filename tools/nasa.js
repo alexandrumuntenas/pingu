1d92afacd14088ee83524394244897cccdc70a8e
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-26 11:07:10 +0200

    Nasa APOD command

---
+++ tools/nasa.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'nasa',
+    execute(args, client, con, contenido, downloader, emojiStrip, fetch, fs, global, Intents, Jimp, Math, message, MessageAttachment, MessageCollector, MessageEmbed, MessageReaction, moment, msi, pdf, result, webp) {
+        function embed(quote) {
+            var embed = new MessageEmbed();
+            embed.setAuthor(quote.anime);
+            embed.setTitle(quote.title);
+            embed.setDescription(quote.explanation);
+            embed.setImage(quote.hdurl);
+            embed.setFooter("Imagen por " + quote.copyright);
+            message.channel.send(embed);
+        }
+        fetch('https://api.nasa.gov/planetary/apod?api_key=ezowSxroDnhKvjzojV9SXx7LmZ6P7OndGYLGXuE9')
+            .then(response => response.json())
+            .then(quote => embed(quote));
+    }
+}
