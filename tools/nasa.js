c89be3048630797440022c88257ec3a84831f2a9
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-26 12:18:40 +0200

    Mostrar mensaje intermedio para avisar la carga

--- tools/nasa.js
+++ tools/nasa.js
@@ -3,14 +3,19 @@ module.exports = {
     execute(args, client, con, contenido, downloader, emojiStrip, fetch, fs, global, Intents, Jimp, Math, message, MessageAttachment, MessageCollector, MessageEmbed, MessageReaction, moment, msi, pdf, result, translate, webp) {
 
         async function embed(quote) {
-            translate(quote.explanation, { to: "es" }).then(res => {
-                var embed = new MessageEmbed();
-                embed.setTitle(quote.title);
-                embed.setDescription(":flag_es: **Traducción:**" + res.text);
-                embed.setImage(quote.hdurl);
-                embed.setFooter("Imagen por " + quote.copyright);
-                message.channel.send(embed);
-            });
+            message.reply(' cargando imagen del día... ')
+                .then(msg => {
+                    translate(quote.explanation, { to: "es" }).then(res => {
+                        var embed = new MessageEmbed();
+                        embed.setTitle(quote.title);
+                        embed.setDescription(":flag_es: **Traducción: **" + res.text);
+                        embed.setImage(quote.hdurl);
+                        embed.setFooter("Imagen por " + quote.copyright);
+                        message.channel.send(embed);
+                        msg.delete();
+                    });
+                })
+
 
         }
         fetch('https://api.nasa.gov/planetary/apod?api_key=ezowSxroDnhKvjzojV9SXx7LmZ6P7OndGYLGXuE9')
