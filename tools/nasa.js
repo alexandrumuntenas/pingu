5d22f29cec093fdf30368add969218b3bcd06391
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-26 12:22:20 +0200

    Carga de mensaje del día con delay, corregido

--- tools/nasa.js
+++ tools/nasa.js
@@ -2,24 +2,23 @@ module.exports = {
     name: 'nasa',
     execute(args, client, con, contenido, downloader, emojiStrip, fetch, fs, global, Intents, Jimp, Math, message, MessageAttachment, MessageCollector, MessageEmbed, MessageReaction, moment, msi, pdf, result, translate, webp) {
 
-        async function embed(quote) {
-            message.reply(' cargando imagen del día... ')
-                .then(msg => {
-                    translate(quote.explanation, { to: "es" }).then(res => {
-                        var embed = new MessageEmbed();
-                        embed.setTitle(quote.title);
-                        embed.setDescription(":flag_es: **Traducción: **" + res.text);
-                        embed.setImage(quote.hdurl);
-                        embed.setFooter("Imagen por " + quote.copyright);
-                        message.channel.send(embed);
-                        msg.delete();
+        message.reply(' cargando imagen del día... ')
+            .then(msg => {
+                fetch('https://api.nasa.gov/planetary/apod?api_key=ezowSxroDnhKvjzojV9SXx7LmZ6P7OndGYLGXuE9')
+                    .then(response => response.json())
+                    .then(quote => {
+                        translate(quote.explanation, { to: "es" }).then(res => {
+                            var embed = new MessageEmbed();
+                            embed.setTitle(quote.title);
+                            embed.setDescription(":flag_es: **Traducción: **" + res.text);
+                            embed.setImage(quote.hdurl);
+                            embed.setFooter("Imagen por " + quote.copyright);
+                            message.channel.send(embed);
+                            msg.delete();
+                        });
                     });
-                })
+            })
 
 
-        }
-        fetch('https://api.nasa.gov/planetary/apod?api_key=ezowSxroDnhKvjzojV9SXx7LmZ6P7OndGYLGXuE9')
-            .then(response => response.json())
-            .then(quote => embed(quote));
     }
 }
