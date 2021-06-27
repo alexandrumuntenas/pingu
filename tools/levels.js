d0d3c300be41a32d9b2f21dbab7d2c581769627b
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-27 17:32:00 +0200

    Corregir SQL

--- tools/levels.js
+++ tools/levels.js
@@ -2,24 +2,20 @@ module.exports = {
     name: 'levels',
     execute(args, client, con, contenido, downloader, emojiStrip, fetch, fs, global, Intents, Jimp, Math, message, MessageAttachment, MessageCollector, MessageEmbed, MessageReaction, moment, msi, pdf, result, translate, webp) {
         if (result[0].niveles_activado != 0) {
-            function ordenarDesc(p_array_json, p_key) {
-                ordenarAsc(p_array_json, p_key); p_array_json.reverse();
-            }
             var dif = result[0].niveles_dificultad;
-            var lookupfortop10 = "SELECT * FROM leveling WHERE guild = " + global.id + " ORDER BY nivel DESC, experiencia DESC LIMIT 10";
+            var lookupfortop10 = "SELECT * FROM leveling WHERE guild = " + global.id + " ORDER BY nivel DESC, experiencia ASC LIMIT 10";
             con.query(lookupfortop10, function (err, rows, result) {
                 if (result) {
                     if (typeof result[0] !== 'undefined') {
                         var embed = new MessageEmbed();
                         embed.setTitle('Ranking')
                         embed.setAuthor(global.name);
-                        ordenarDesc(rows, experiecia);
                         rows.forEach(function (row) {
                             var usuario = client.users.cache.find(user => user.id === row.user);
                             embed.addFields({ name: usuario.username, value: "Nivel: " + row.nivel + " | Experiencia: " + ((parseInt(row.nivel) * (dif * 100)) + parseInt(row.experiencia)) })
                         });
                     };
-                    embed.setFooter('Para obtener el TOP 10, Wired Penguin ordena de forma descendente los datos de nivel registrados en función del nivel.')
+                    embed.setFooter('Para obtener el TOP 10, Pingu ordena de forma descendente los datos de nivel registrados en función del nivel.')
                     message.channel.send(embed);
                 }
             });
