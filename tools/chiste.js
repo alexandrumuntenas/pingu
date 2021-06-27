149823b9aa8c7a41d326495b2030f634d2d9726f
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-27 21:38:33 +0200

    Comando chiste

---
+++ tools/chiste.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: 'nasa',
+    execute(args, client, con, contenido, downloader, emojiStrip, fetch, fs, global, Intents, Jimp, Math, message, MessageAttachment, MessageCollector, MessageEmbed, MessageReaction, moment, msi, pdf, result, translate, webp) {
+
+        message.reply(' cargando chiste... ')
+            .then(msg => {
+                fetch('https://official-joke-api.appspot.com/jokes/random')
+                    .then(response => response.json())
+                    .then(quote => {
+                        translate(quote.setup, { to: "es" }).then(res => {
+                            var inicip = res.text;
+                            translate(quote.setup, { to: "es" }).then(res => {
+                                var embed = new MessageEmbed();
+                                embed.setDescription(inicip);
+                                embed.setColor('#F7CB46');
+                                embed.setFooter(res.text);
+                                message.channel.send(embed);
+                                msg.delete();
+                            });
+                        });
+                    });
+            })
+
+
+    }
+}
