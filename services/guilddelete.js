0186057ca6251a5185c9b4ec63e5bb468259f352
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-27 18:11:12 +0200

    Eliminar datos de servidores cuando retiran a Pingu

--- services/guilddelete.js
+++ services/guilddelete.js
@@ -3,9 +3,14 @@ module.exports = function (con, guild) {
     var sql = "DELETE FROM `servidores` WHERE guild = '" + id + "'";
     var sql1 = "DELETE FROM `leveling` WHERE guild = '" + id + "'";
     var sql2 = "DELETE FROM `infracciones` WHERE guild = '" + id + "'";
+    var sql3 = "DELETE FROM `comandos_custom` WHERE guild = '" + id + "'";
+    var sql4 = "DELETE FROM `respuestas_custom` WHERE guild = '" + id + "'";
     con.query(sql, function (err, result) {
         if (err) throw err;
         con.query(sql1);
         con.query(sql2);
+        con.query(sql2);
+        con.query(sql3);
+        con.query(sql4);
     });
 }
