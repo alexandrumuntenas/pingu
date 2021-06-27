5b016ebefe4fcd80af113a85c8dfc70c2a35db28
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-27 17:11:24 +0200

    Order with Json

--- tools/levels.js
+++ tools/levels.js
@@ -2,6 +2,9 @@ module.exports = {
     name: 'levels',
     execute(args, client, con, contenido, downloader, emojiStrip, fetch, fs, global, Intents, Jimp, Math, message, MessageAttachment, MessageCollector, MessageEmbed, MessageReaction, moment, msi, pdf, result, translate, webp) {
         if (result[0].niveles_activado != 0) {
+            function ordenarDesc(p_array_json, p_key) {
+                ordenarAsc(p_array_json, p_key); p_array_json.reverse();
+            }
             var dif = result[0].niveles_dificultad;
             var lookupfortop10 = "SELECT * FROM leveling WHERE guild = " + global.id + " ORDER BY nivel DESC, experiencia DESC LIMIT 10";
             con.query(lookupfortop10, function (err, rows, result) {
@@ -10,6 +13,7 @@ module.exports = {
                         var embed = new MessageEmbed();
                         embed.setTitle('Ranking')
                         embed.setAuthor(global.name);
+                        ordenarDesc(row, experiecia);
                         rows.forEach(function (row) {
                             var usuario = client.users.cache.find(user => user.id === row.user);
                             embed.addFields({ name: usuario.username, value: "Nivel: " + row.nivel + " | Experiencia: " + ((parseInt(row.nivel) * (dif * 100)) + parseInt(row.experiencia)) })
