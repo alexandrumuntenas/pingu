3bead99fb6b40518735baa0e61b972b8cb29dd8c
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-27 21:40:29 +0200

    Corregir export name, corregir orden del chiste

--- tools/chiste.js
+++ tools/chiste.js
@@ -1,15 +1,15 @@
 module.exports = {
-    name: 'nasa',
+    name: 'chiste',
     execute(args, client, con, contenido, downloader, emojiStrip, fetch, fs, global, Intents, Jimp, Math, message, MessageAttachment, MessageCollector, MessageEmbed, MessageReaction, moment, msi, pdf, result, translate, webp) {
 
-        message.reply(' cargando chiste... ')
+        message.reply(' cargando chiste...')
             .then(msg => {
                 fetch('https://official-joke-api.appspot.com/jokes/random')
                     .then(response => response.json())
                     .then(quote => {
                         translate(quote.setup, { to: "es" }).then(res => {
                             var inicip = res.text;
-                            translate(quote.setup, { to: "es" }).then(res => {
+                            translate(quote.punchline, { to: "es" }).then(res => {
                                 var embed = new MessageEmbed();
                                 embed.setDescription(inicip);
                                 embed.setColor('#F7CB46');
