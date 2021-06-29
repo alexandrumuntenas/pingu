d4fc0b25dd2ebea23fd148506213cd7f0e5fe515
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-29 20:51:09 +0200

    Revertir rank cli

--- tools/rank.js
+++ tools/rank.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'rank',
-    execute(args, client, con, contenido, downloader, emojiStrip, fetch, fs, global, Intents, Jimp, Math, message, MessageAttachment, MessageCollector, MessageEmbed, MessageReaction, moment, msi, pdf, result, translate, webp) {
+    execute(args, boxen, client, con, contenido, downloader, emojiStrip, fetch, fs, global, Intents, Jimp, Math, message, MessageAttachment, MessageCollector, MessageEmbed, MessageReaction, moment, msi, pdf, progressbar, result, translate, webp) {
         if (result[0].niveles_activado != 0) {
             if (message.mentions.users.first()) {
                 var user = message.mentions.users.first();
@@ -16,12 +16,44 @@ module.exports = {
                     if (result[0]) {
                         var exp = parseInt(result[0].experiencia);
                         var niv = parseInt(result[0].nivel);
-                        var required = ((niv ^ 2) * dif) * 100;
-                        var progress = progressbar.filledBar(required, exp);
-                        var restante = required - exp;
-                        var card = boxen(user.username + '#' + user.discriminator + '\n**Nivel** ' + niv + '\n**Experiencia** ' + exp + '\n**Progreso** ' + exp + '/' + required + ' \n' + progress, { padding: 1 });
-                        message.channel.send(" <@" + user.id + "> se encuentra en el nivel `" + niv + "` y dispone de `" + ((niv * 100) + exp) + "` puntos de experiencia");
-                        message.channel.send(card);
+                        async function paso1() {
+                            const avatar = new downloader({
+                                url: user.avatarURL(),
+                                directory: "./usuarios/avatares/",
+                                fileName: user.id + '.webp',
+                                cloneFiles: false,
+                            });
+                            try {
+                                await avatar.download();
+                            } catch (error) {
+                            }
+                        }
+                        async function paso2() {
+                            await webp.dwebp("./usuarios/avatares/" + user.id + ".webp", "./usuarios/avatares/" + user.id + ".jpg", "-o", logging = "-v");
+                        }
+                        async function paso3() {
+                            const top = await Jimp.read("./usuarios/avatares/" + user.id + ".jpg");
+                            top.circle();
+                            top.resize(220, 220);
+                            const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+                            await Jimp.read('./recursos/carteles/' + cache.aspecto + '.png', function (err, image) {
+                                image.composite(top, 39, 32);
+                                image.print(font, 300, 55, "Nivel: " + niv);
+                                image.print(font, 300, 155, "XP: " + ((niv * 100) + exp));
+                                image.writeAsync('./usuarios/leveling/' + user.id + '_' + global.id + '_rank.jpg')
+                                enviar();
+                            });
+                        }
+                        function enviar() {
+                            var attachament = new MessageAttachment('./usuarios/leveling/' + user.id + '_' + global.id + '_rank.jpg');
+                            message.channel.send(" <@" + user.id + "> se encuentra en el nivel `" + niv + "` y dispone de `" + ((niv * 100) + exp) + "` puntos de experiencia", attachament);
+                        }
+                        async function cocina() {
+                            await paso1();
+                            await paso2();
+                            paso3();
+                        }
+                        cocina();
                     } else {
                         message.reply(" no te he podido localizar en la base de datos. Escribe unos cuantos mensajes y vuelve a intentarlo.")
                     }
