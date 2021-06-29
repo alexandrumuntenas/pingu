3a5ef083502d3021595a35e28c58ad3c793efb03
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-29 23:59:58 +0200

    Eiminar comandos lock unlock, para posterior

--- tools/lock.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'lock',
-    execute(args, client, con, contenido, downloader, emojiStrip, fetch, fs, global, Intents, Jimp, Math, message, MessageAttachment, MessageCollector, MessageEmbed, MessageReaction, moment, msi, pdf, result, translate, webp) {
-        if (message.member.hasPermission('MANAGE_MESSAGES') && message.member.hasPermission('KICK_MEMBERS') && message.member.hasPermission('BAN_MEMBERS') || message.member.hasPermission('ADMINISTRATOR')) {
-            message.channel.overwritePermissions(role, { 'SEND_MESSAGES': false })
-            message.channel.send(':white_check_mark: El canal ha sido bloqueado. Ya no se pueden enviar mensajes.')
-        }
-    }
-}
