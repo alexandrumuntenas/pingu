d69abab9db18c0e7155d874c3e7b604da1e01690
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-06-30 23:21:40 +0200

    Remover if (result[0].bienvenida_activado != 0), bienvenida_activado es una función obsoleta.

--- services/guildmemberadd.js
+++ services/guildmemberadd.js
@@ -3,82 +3,80 @@ module.exports = function (client, con, Jimp, downloader, webp, fs, MessageAttac
     var sql = "SELECT * FROM `servidores` WHERE guild = '" + id + "'";
     //Conectamos con el servidor
     con.query(sql, function (err, result) {
-        if (result[0].bienvenida_activado != 0) {
-            if (result[0].bienvenida_mensaje_activado != 0) {
-                cache = { "canal_id": result[0].bienvenida_canal_id, "canal_msg": result[0].bienvenida_mensaje, "aspecto": result[0].bienvenida_fondo };
-                const mensaje = client.channels.cache.find(channel => channel.id === cache.canal_id);
-                if (result[0].bienvenida_cartel != 0) {
-                    var user = member.user;
-                    async function paso1() {
-                        const avatar = new downloader({
-                            url: user.avatarURL(),
-                            directory: "./usuarios/avatares/",
-                            fileName: user.id + '_join.webp',
-                            cloneFiles: false,
-                        });
-                        try {
-                            await avatar.download();
-                            paso2();
-                        } catch (e) {
-                            console.log(e);
-                        }
+        if (result[0].bienvenida_mensaje_activado != 0) {
+            cache = { "canal_id": result[0].bienvenida_canal_id, "canal_msg": result[0].bienvenida_mensaje, "aspecto": result[0].bienvenida_fondo };
+            const mensaje = client.channels.cache.find(channel => channel.id === cache.canal_id);
+            if (result[0].bienvenida_cartel != 0) {
+                var user = member.user;
+                async function paso1() {
+                    const avatar = new downloader({
+                        url: user.avatarURL(),
+                        directory: "./usuarios/avatares/",
+                        fileName: user.id + '_join.webp',
+                        cloneFiles: false,
+                    });
+                    try {
+                        await avatar.download();
+                        paso2();
+                    } catch (e) {
+                        console.log(e);
                     }
-                    async function paso2() {
-                        await webp.dwebp("./usuarios/avatares/" + user.id + "_join.webp", "./usuarios/avatares/" + user.id + "_join.jpg", "-o", logging = "-v");
-                        paso3();
-                    }
-                    async function paso3() {
-                        const top = await Jimp.read("./usuarios/avatares/" + user.id + "_join.jpg");
-                        top.circle();
+                }
+                async function paso2() {
+                    await webp.dwebp("./usuarios/avatares/" + user.id + "_join.webp", "./usuarios/avatares/" + user.id + "_join.jpg", "-o", logging = "-v");
+                    paso3();
+                }
+                async function paso3() {
+                    const top = await Jimp.read("./usuarios/avatares/" + user.id + "_join.jpg");
+                    top.circle();
 
-                        top.resize(220, 220);
-                        const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
-                        await Jimp.read('./recursos/carteles/' + cache.aspecto + '.png', function (err, image) {
-                            image.composite(top, 39, 32);
-                            image.print(font, 300, 109, "Hola " + user.tag);
-                            image.print(font, 300, 141, "Miembro #" + member.guild.memberCount);
-                            image.writeAsync('./usuarios/bienvenidas/' + user.id + '_' + id + '_join.jpg')
-                            enviar();
-                        });
-                    }
-                    function enviar() {
-                        //Reemplazamos valores como {user} o {server} para que nodejs pueda interpretarlo
-                        const fromdb = cache.canal_msg;
-                        const userreplace = fromdb.replace('{user}', `<@${user.id}>`);
-                        const toexport = userreplace.replace('{server}', `${member.guild.name}`);
-                        var attachament = new MessageAttachment('./usuarios/bienvenidas/' + member.id + '_' + id + '_join.jpg');
-                        mensaje.send(toexport, attachament);
-                    }
-                    async function cocina() {
-                        paso1();
-                        if (result[0].burbuja_activado == 1) {
-                            if (member) {
-                                member
-                                    .kick('El servidor al que intentas acceder dispone del modo burbuja activado.');
-                            }
-                        }
-                    }
-                    cocina();
-                } else {
+                    top.resize(220, 220);
+                    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+                    await Jimp.read('./recursos/carteles/' + cache.aspecto + '.png', function (err, image) {
+                        image.composite(top, 39, 32);
+                        image.print(font, 300, 109, "Hola " + user.tag);
+                        image.print(font, 300, 141, "Miembro #" + member.guild.memberCount);
+                        image.writeAsync('./usuarios/bienvenidas/' + user.id + '_' + id + '_join.jpg')
+                        enviar();
+                    });
+                }
+                function enviar() {
                     //Reemplazamos valores como {user} o {server} para que nodejs pueda interpretarlo
                     const fromdb = cache.canal_msg;
-                    const userreplace = fromdb.replace('{user}', `<@${member.user.id}>`);
+                    const userreplace = fromdb.replace('{user}', `<@${user.id}>`);
                     const toexport = userreplace.replace('{server}', `${member.guild.name}`);
-                    mensaje.send(toexport);
+                    var attachament = new MessageAttachment('./usuarios/bienvenidas/' + member.id + '_' + id + '_join.jpg');
+                    mensaje.send(toexport, attachament);
                 }
-                if (result[0].burbuja_activado == 1) {
-                    if (member) {
-                        member
-                            .kick('El servidor al que intentas acceder dispone del modo burbuja activado.');
+                async function cocina() {
+                    paso1();
+                    if (result[0].burbuja_activado == 1) {
+                        if (member) {
+                            member
+                                .kick('El servidor al que intentas acceder dispone del modo burbuja activado.');
+                        }
                     }
                 }
+                cocina();
+            } else {
+                //Reemplazamos valores como {user} o {server} para que nodejs pueda interpretarlo
+                const fromdb = cache.canal_msg;
+                const userreplace = fromdb.replace('{user}', `<@${member.user.id}>`);
+                const toexport = userreplace.replace('{server}', `${member.guild.name}`);
+                mensaje.send(toexport);
             }
-            if (result[0].bienvenida_roles_user) {
-                var role = JSON.parse(result[0].bienvenida_roles_user);
-                role.forEach(element => {
-                    member.roles.add(member.guild.roles.cache.find(role => role.id === element));
-                })
+            if (result[0].burbuja_activado == 1) {
+                if (member) {
+                    member
+                        .kick('El servidor al que intentas acceder dispone del modo burbuja activado.');
+                }
             }
+        }
+        if (result[0].bienvenida_roles_user) {
+            var role = JSON.parse(result[0].bienvenida_roles_user);
+            role.forEach(element => {
+                member.roles.add(member.guild.roles.cache.find(role => role.id === element));
+            })
         }
     });
 }
