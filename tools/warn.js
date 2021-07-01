2b83dea0a4d6723b94e3d8aa0309fb5fa0e16fac
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-01 20:45:02 +0200

    Repetición de warn

--- tools/warn.js
+++ tools/warn.js
@@ -4,48 +4,46 @@ module.exports = {
         if (message.member.hasPermission('MANAGE_MESSAGES') && message.member.hasPermission('KICK_MEMBERS') && message.member.hasPermission('BAN_MEMBERS') || message.member.hasPermission('ADMINISTRATOR')) {
             if (result[0].moderador_activado != 0) {
                 var array = message.mentions.users.array();
+                var warn = message.content;
+                warn.replace(global.prefix + 'warn ', '');
+                await array.forEach(user => {
+                    warn = warn.replace('<@' + user.id + '>', '');
+                })
                 message.mentions.users.array().forEach(user => {
                     var cache = { "activado": result[0].moderador_warn_expulsion_activado, "cantidad": result[0].moderador_warn_expulsion_cantidad, "accion": result[0].moderador_warn_expulsion_accion };
-                    var warn = message.content;
-                    warn.replace(global.prefix + 'warn ', '');
-                    array.forEach(user => {
-                        warn = warn.replace('<@' + user.id + '>', '');
-                    })
-                    array.forEach(user => {
-                        var consultarcantidad = "SELECT COUNT(*) AS itotal FROM `infracciones` WHERE user = '" + user.id + "' AND guild = '" + global.id + "'";
-                        con.query(consultarcantidad, function (err, result) {
-                            var nuevainfraccion = "INSERT INTO `infracciones` (`user`, `guild`,`motivo`) VALUES ('" + user.id + "', '" + global.id + "','" + warn + "')";
-                            con.query(nuevainfraccion);
-                            var embed = new MessageEmbed().setAuthor(user.tag + " usted ha sido advertido", user.displayAvatarURL()).setTitle('Detalles de infracción').setDescription(warn);
-                            message.channel.send(embed);
-                            const member = message.guild.member(user);
-                            if (cache.activado != 0) {
-                                if (parseInt(result[0].itotal) + 1 >= cache.cantidad) {
-                                    if (cache.accion != 0) {
-                                        member
-                                            .ban({
-                                                reason: 'Acumulación de infracciones.',
-                                            })
-                                            .then(() => {
-                                                message.channel.send(`:police_officer: El usuario ${user.tag} ha sido baneado del servidor por acumular más de ` + cache.cantidad + " infracciones.");
-                                            })
-                                            .catch(err => {
-                                                message.channel.send(`:x: Se ha intentado banear el usuario ${user.tag} por acumular más de ` + cache.cantidad + " infracciones, pero se ha producido un error...");
-                                            });
-                                    } else {
-                                        member
-                                            .kick('Acumulación de infracciones.')
-                                            .then(() => {
-                                                message.channel.send(`:police_officer: El usuario ${user.tag} ha sido expulsado del servidor por acumular más de ` + cache.cantidad + " infracciones.");
-                                            })
-                                            .catch(err => {
-                                                message.channel.send(`:x: Se ha intentado expulsar el usuario ${user.tag} por acumular más de ` + cache.cantidad + " infracciones, pero se ha producido un error...");
-                                            });
-                                    }
+                    var consultarcantidad = "SELECT COUNT(*) AS itotal FROM `infracciones` WHERE user = '" + user.id + "' AND guild = '" + global.id + "'";
+                    con.query(consultarcantidad, function (err, result) {
+                        var nuevainfraccion = "INSERT INTO `infracciones` (`user`, `guild`,`motivo`) VALUES ('" + user.id + "', '" + global.id + "','" + warn + "')";
+                        con.query(nuevainfraccion);
+                        var embed = new MessageEmbed().setAuthor(user.tag + " usted ha sido advertido", user.displayAvatarURL()).setTitle('Detalles de infracción').setDescription(warn);
+                        message.channel.send(embed);
+                        const member = message.guild.member(user);
+                        if (cache.activado != 0) {
+                            if (parseInt(result[0].itotal) + 1 >= cache.cantidad) {
+                                if (cache.accion != 0) {
+                                    member
+                                        .ban({
+                                            reason: 'Acumulación de infracciones.',
+                                        })
+                                        .then(() => {
+                                            message.channel.send(`:police_officer: El usuario ${user.tag} ha sido baneado del servidor por acumular más de ` + cache.cantidad + " infracciones.");
+                                        })
+                                        .catch(err => {
+                                            message.channel.send(`:x: Se ha intentado banear el usuario ${user.tag} por acumular más de ` + cache.cantidad + " infracciones, pero se ha producido un error...");
+                                        });
+                                } else {
+                                    member
+                                        .kick('Acumulación de infracciones.')
+                                        .then(() => {
+                                            message.channel.send(`:police_officer: El usuario ${user.tag} ha sido expulsado del servidor por acumular más de ` + cache.cantidad + " infracciones.");
+                                        })
+                                        .catch(err => {
+                                            message.channel.send(`:x: Se ha intentado expulsar el usuario ${user.tag} por acumular más de ` + cache.cantidad + " infracciones, pero se ha producido un error...");
+                                        });
                                 }
                             }
-                        })
-                    });
+                        }
+                    })
                 });
             }
         } else {
