8db77b4ec3bed5b55f0c180feda68715ae1d1669
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-01 19:48:19 +0200

    Panel de configuración en despedidas

--- tools/despedidas.js
+++ tools/despedidas.js
@@ -30,17 +30,93 @@ module.exports = {
                         break;
                 }
             } else {
-                var valor = result[0].salida_activado;
-                if (valor == 1) {
-                    var fin = 0;
-                    var response = 'desactivadas';
-                } else {
-                    var fin = 1;
-                    var response = 'activadas';
+                ftime = 0;
+                function purga() {
+                    if (ftime == 0) {
+                        ++ftime;
+                    } else {
+                        message.channel.bulkDelete(2);
+                    }
                 }
-                var sql = "UPDATE `servidores` SET `salida_activado` = '" + fin + "' WHERE `servidores`.`guild` = " + global.id;
-                message.channel.send(':white_check_mark: Las despedidas han sido ' + response + '.');
-                con.query(sql);
+
+                function indice() {
+                    message.channel.send('Para ejecutar una opción, indica el número de la opción. \n \n ****Opciones Disponibles** \n **1.** ¿Enviar mensaje cuando alguien abandona el servidor? \n **2.** Establecer mensaje de despedidas \n **3.** Establecer canal de despedida \n **4.** Salir');
+                    message.channel.awaitMessages(m => m.author.id == message.author.id,
+                        { max: 1, time: 30000, errors: ['time'] }).then(collected => {
+                            switch (collected.first().content) {
+                                case '1':
+                                    purga()
+                                    t_mensaje();
+                                    break;
+                                case '2':
+                                    purga()
+                                    u_mensaje();
+                                    break;
+                                case '3':
+                                    purga()
+                                    u_canal();
+                                    break;
+                                case '4':
+                                    message.channel.send(':information_source: Se ha cerrado la configuración del módulo de despedidas')
+                                    break;
+                                default:
+                                    purga();
+                                    indice();
+                                    break;
+                            }
+                        }).catch(collected => {
+                            message.channel.send(':information_source: Se ha cerrado la configuración del módulo de despedidas');
+                        });
+                }
+
+                function t_mensaje() {
+                    message.channel.send(':arrow_right: ¿Enviar mensaje de bienvenida cuando se una un nuevo miembro? Respuestas disponibles: y(es) / n(o)');
+                    message.channel.awaitMessages(m => m.author.id == message.author.id,
+                        { max: 1 }).then(collected => {
+                            if (collected.first().content === "y" || collected.first().content === "yes") {
+                                var yes = "UPDATE `servidores` SET `salida_mensaje_activado` = '1' WHERE `servidores`.`guild` = " + global.id;
+                                con.query(yes);
+                                message.channel.send(':white_check_mark: A partir de ahora se enviarán mensajes de despedidas...');
+                                indice();
+                            } else {
+                                var no = "UPDATE `servidores` SET `salida_mensaje_activado` = '0' WHERE `servidores`.`guild` = " + global.id;
+                                con.query(no);
+                                message.channel.send(':white_check_mark: No se enviarán mensajes de despedida...');
+                                indice();
+                            }
+                        });
+                }
+
+                function u_canal() {
+                    message.channel.send(':arrow_right: ¿Dónde deseas que se envíen los mensajes y carteles de bienvenida? ¡Menciona el canal!')
+                    message.channel.awaitMessages(m => m.author.id == message.author.id,
+                        { max: 1 }).then(collected => {
+                            if (collected.first().mentions.channels.first()) {
+                                var channel = collected.first().mentions.channels.first();
+                                var updatechannel = "UPDATE `servidores` SET `bienvenida_canal_id` = '" + channel.id + "' WHERE `servidores`.`guild` = " + global.id;
+                                con.query(updatechannel);
+                                message.channel.send(':white_check_mark: Se ha actualizado el canal de bienvenida. Anunciaré allí los nuevos miembros :thumbsup:');
+                                indice();
+                            } else {
+                                message.channel.send(':x: No has mencionado un canal válido.')
+                                u_canal();
+                            }
+                        });
+                }
+
+                function u_mensaje() {
+                    message.channel.send(':arrow_right: ¿Qué mensaje deseas que envíe para recibir a los nuevos miembros? <:warn:858736919432527942> El mensaje de bienvenida no soporta emojis. Si se incluyen, serán retirados automáticamente del texto.')
+                    message.channel.awaitMessages(m => m.author.id == message.author.id,
+                        { max: 1 }).then(collected => {
+                            var updatemsg = "UPDATE `servidores` SET `bienvenida_mensaje` = '" + emojiStrip(collected.first().content) + "' WHERE `servidores`.`guild` = " + global.id;
+                            con.query(updatemsg);
+                            message.channel.send(':white_check_mark: Se ha actualizado el mensaje de bienvenida correctamente.');
+                            indice();
+                        });
+                }
+
+                message.channel.send('<:info:858737080950718484> Configuración del módulo de **DESPEDIDAS / SALIDAS**');
+                indice();
             }
         } else {
             message.channel.send(':x: No dispones de permisos suficientes para ejecutar este comando')
