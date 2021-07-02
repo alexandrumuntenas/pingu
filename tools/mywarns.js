9c53031cca6217d257ddb153ea277c81de550a90
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-03 00:16:22 +0200

    Actualizar mensajes mywarns

--- tools/mywarns.js
+++ tools/mywarns.js
@@ -9,14 +9,14 @@ module.exports = {
             con.query(verinfraccionescantidad, function (err, result) {
                 var ultimas = result[0].total;
                 con.query(verinfracciones5, function (err, result) {
-                    var embed = new MessageEmbed().setAuthor("Infracciones de " + user.tag, user.displayAvatarURL()).setTitle('Infracciones totales: ' + ultimas);
+                    var embed = new MessageEmbed().setAuthor("Advertencias de " + user.tag, user.displayAvatarURL()).setTitle('Advertencias totales: ' + ultimas);
                     async function infraccionestotales() {
                         var i = 0;
                         for (var i = 0; i < result.length; i++) {
                             var timeStamp = JSON.stringify(result[i].timestamp);
                             var s = timeStamp;
                             var m = moment(s, 'YYYY MM dd').format('MM-DD-YYYY');
-                            embed.addFields({ name: 'Infracción #' + (i + 1), value: "**" + result[i].motivo + "** • " + s.slice(0, 11) + "\"" })
+                            embed.addFields({ name: 'Advertencia #' + (i + 1), value: "**" + result[i].motivo + "** • " + s.slice(0, 11) + "\"" })
                         }
                         message.channel.send(embed);
                     }
