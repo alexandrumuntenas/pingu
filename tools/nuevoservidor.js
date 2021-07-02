85c0e9c16f135a00e4ace700722d716d2b62ef76
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-03 00:16:51 +0200

    Actualizar mensaje nuevoservidor

--- tools/nuevoservidor.js
+++ tools/nuevoservidor.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'nuevoservidor',
     execute(args, canvacord, client, con, contenido, downloader, emojiStrip, fetch, fs, global, Intents, Jimp, Math, message, MessageAttachment, MessageCollector, MessageEmbed, MessageReaction, moment, msi, pdf, result, translate, webp) {
-        message.reply(' puedes añadirme a tu servidor utilizando el siguiente link: https://discord.com/oauth2/authorize?client_id=827199539185975417&permissions=1933044831&scope=bot%20applications.commands')
+        message.reply(' puede añadirme a su servidor utilizando el siguiente link: https://discord.com/oauth2/authorize?client_id=827199539185975417&permissions=1933044831&scope=bot%20applications.commands')
     }
 }
