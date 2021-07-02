1c961f4e67316a0cffa84c01b9131493055138db
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-02 23:54:17 +0200

    Cambiar URL de la docu

--- tools/ayuda.js
+++ tools/ayuda.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'ayuda',
     execute(args, canvacord, client, con, contenido, downloader, emojiStrip, fetch, fs, global, Intents, Jimp, Math, message, MessageAttachment, MessageCollector, MessageEmbed, MessageReaction, moment, msi, pdf, result, translate, webp) {
-        message.channel.send(':information_source: Puedes consultar la documentación en https://pingu.duoestudios.es/v/fairy-tail/')
+        message.channel.send(':information_source: Puede consultar la documentación en https://pingu.duoestudios.es/')
     }
 }
