9da61a2d1c044088f725986dfe483bd28e1efe12
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-03 10:35:21 +0200

    Require libreries in module

--- tools/all-infractions.js
+++ tools/all-infractions.js
@@ -1,13 +1,18 @@
+const { MessageAttachment } = require('discord.js');
+const moment = require('moment');
+const pdf = require('pdfkit');
+const fs = require('fs')
+
 module.exports = {
     name: 'all-infractions',
-    execute(args, canvacord, client, con, contenido, downloader, emojiStrip, fetch, fs, global, Intents, Jimp, Math, message, MessageAttachment, MessageCollector, MessageEmbed, MessageReaction, moment, msi, pdf, result, translate, webp) {
+    execute(args, client, con, contenido, global, message, result) {
         if (message.member.hasPermission('MANAGE_MESSAGES') && message.member.hasPermission('KICK_MEMBERS') && message.member.hasPermission('BAN_MEMBERS') || message.member.hasPermission('ADMINISTRATOR')) {
             if (result[0].moderador_activado != 0) {
                 if (message.mentions.users.first()) {
                     var user = message.mentions.users.first()
                     async function step1() {
                         con.query('SELECT * FROM `infracciones` WHERE user = \'' + user.id + '\' AND guild = \'' + global.id + '\'', function (err, result) {
-                            console.log(err)
+                            if (err) console.log(err)
                             var pdfDoc = new pdf;
                             pdfDoc.pipe(fs.createWriteStream('./usuarios/moderacion/' + user.id + '_' + global.id + '.pdf'));
                             pdfDoc.font('./recursos/typography/Roboto-Bold.ttf', 20).text('Servicios de Moderación · Pingu ').moveDown(1);
@@ -24,7 +29,7 @@ module.exports = {
                             }
                             pdfDoc.end();
                             pdfDoc.on('end', function (err) {
-                                console.log(err)
+                                if (err) console.log(err)
                                 var attachament = new MessageAttachment('./usuarios/moderacion/' + user.id + '_' + global.id + '.pdf');
                                 message.author.send('**Reporte de advertencias**\n Usuario: `' + user.tag + '`\n Servidor: `' + global.name + '` \n Fecha de Generación: `' + moment().format('MMMM Do YYYY, h:mm:ss a') + '`', attachament);
                             })
