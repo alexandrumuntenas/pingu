2d25ba81db28886ad88de596fb90927408902587
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-21 10:11:14 +0200

    Panel de configuración del módulo niveles

--- tools/niveles.js
+++ tools/niveles.js
@@ -61,13 +61,42 @@ module.exports = {
                     }
                 }
                 function indice() {
-                    message.channel.send('Para ejecutar una opción, indica el número de la opción. \n \n ****Opciones Disponibles** \n **1.** ¿Habilitar sistema de niveles? (Alterna entre sí y no) \n **2.** Establecer mensaje de avance de nivel \n **3.** Establecer canal de bienvenida \n **4.** ¿Enviar cartel de notificación de avance de nivel? \n **5.** Cambiar fondo de los carteles de ranking \n **6.** Cambia la dificultad de avanzar de nivel \n **7.** Dar X rol/es a los usuarios cuando alcancen X nivel :construction:\n **8.** Salir');
+                    message.channel.send('Para ejecutar una opción, indica el número de la opción. \n \n ****Opciones Disponibles** \n **1.** ¿Habilitar sistema de niveles? \n **2.** Establecer mensaje de avance de nivel \n **3.** ¿Establecer canal de avance de nivel? \n **4.** ¿Enviar cartel de notificación de avance de nivel? \n **5.** Cambiar fondo de los carteles de ranking \n **6.** Cambia la dificultad de avanzar de nivel \n **7.** Dar X rol/es a los usuarios cuando alcancen X nivel :construction:\n **8.** Salir');
                     message.channel.awaitMessages(m => m.author.id == message.author.id,
                         { max: 1, time: 30000, errors: ['time'] }).then(collected => {
                             if (isInteger(collected.first().content)) {
                                 switch (collected.first().content) {
                                     case '1':
+                                        purga();
                                         t_niveles();
+                                        break;
+                                    case '2':
+                                        purga();
+                                        u_mensaje();
+                                        break;
+                                    case '3':
+                                        purga();
+                                        u_canal();
+                                        break;
+                                    case '4':
+                                        purga();
+                                        t_cartel();
+                                        break;
+                                    case '5':
+                                        purga();
+                                        u_fondo();
+                                        break;
+                                    case '6':
+                                        purga();
+                                        u_dificultad();
+                                        break;
+                                    case '7':
+                                        purga();
+                                        message.channel.send('Configuración en desarrollo...');
+                                        indice();
+                                        break;
+                                    case '8':
+                                        message.channel.send(':information_source: Se ha cerrado la configuración del módulo de **NIVELES**');
                                         break;
                                     default:
                                         purga();
@@ -78,24 +107,115 @@ module.exports = {
                                 message.channel.send(':information_source: El valor especificado debe ser alfanumérico');
                                 indice();
                             }
-                        }).catch(collected => {
-                            message.channel.send(':information_source: Se ha cerrado la configuración del módulo de niveles');
+                        }).catch(error => {
+                            message.channel.send(':information_source: Se ha cerrado la configuración del módulo de **NIVELES**');
                         });
                 }
+
                 function t_niveles() {
-                    var valor = result[0].niveles_activado;
-                    if (valor == 1) {
-                        var fin = 0;
-                        var response = 'desactivado';
-                        result[0].niveles_activado = 0;
-                    } else {
-                        var fin = 1;
-                        var response = 'activado';
-                        result[0].niveles_activado = 1;
-                    }
-                    var sql = "UPDATE `servidores` SET `niveles_activado` = '" + fin + "' WHERE `servidores`.`guild` = " + global.id;
-                    message.channel.send(':white_check_mark: El sistema de niveles ha sido ' + response + ' correctamente');
-                    con.query(sql);
+                    message.channel.send(':arrow_right: ¿Activar módulo de niveles? Respuestas disponibles: y(es) / n(o)');
+                    message.channel.awaitMessages(m => m.author.id == message.author.id,
+                        { max: 1 }).then(collected => {
+                            if (collected.first().content === "y" || collected.first().content === "yes") {
+                                var yes = "UPDATE `servidores` SET `niveles_activado` = '1' WHERE `servidores`.`guild` = " + global.id;
+                                con.query(yes);
+                                message.channel.send(':white_check_mark: Se ha activado el módulo de niveles...');
+                                indice();
+                            } else {
+                                var no = "UPDATE `servidores` SET `niveles_activado` = '0' WHERE `servidores`.`guild` = " + global.id;
+                                con.query(no);
+                                message.channel.send(':white_check_mark: Se ha desactivado el módulo de niveles...');
+                                indice();
+                            }
+                        });
+                }
+
+                function t_cartel() {
+                    message.channel.send(':arrow_right: ¿Enviar cartel de avance de nivel? Respuestas disponibles: y(es) / n(o)');
+                    message.channel.awaitMessages(m => m.author.id == message.author.id,
+                        { max: 1 }).then(collected => {
+                            if (collected.first().content === "y" || collected.first().content === "yes") {
+                                var yes = "UPDATE `servidores` SET `niveles_cartel` = '1' WHERE `servidores`.`guild` = " + global.id;
+                                con.query(yes);
+                                message.channel.send(':white_check_mark: Se enviará el cartel de avance de nivel...');
+                                indice();
+                            } else {
+                                var no = "UPDATE `servidores` SET `niveles_cartel` = '0' WHERE `servidores`.`guild` = " + global.id;
+                                con.query(no);
+                                message.channel.send(':white_check_mark: No se enviará el cartel de avance de nivel...');
+                                indice();
+                            }
+                        });
+                }
+
+                function u_mensaje() {
+                    message.channel.send(':arrow_right: ¿Qué mensaje desea que se envíe para notificar el avance de nivel? <:warn:858736919432527942> El mensaje de avance de nivel no soporta emojis. Si se incluyen, serán retirados automáticamente del texto.')
+                    message.channel.awaitMessages(m => m.author.id == message.author.id,
+                        { max: 1 }).then(collected => {
+                            var updatemsg = "UPDATE `servidores` SET `niveles_canal_mensaje` = '" + emojiStrip(collected.first().content) + "' WHERE `servidores`.`guild` = " + global.id;
+                            con.query(updatemsg);
+                            message.channel.send(':white_check_mark: Se ha actualizado el mensaje de avance de nivel');
+                            indice();
+                        });
+                }
+
+                function u_canal() {
+                    message.channel.send(':arrow_right: ¿Dónde desea que se envíen los mensajes de avance de nivel? ¡Mencione el canal!')
+                    message.channel.awaitMessages(m => m.author.id == message.author.id,
+                        { max: 1 }).then(collected => {
+                            if (collected.first().mentions.channels.first()) {
+                                var channel = collected.first().mentions.channels.first();
+                                var updatechannel = "UPDATE `servidores` SET `niveles_canal_id` = '" + channel.id + "' WHERE `servidores`.`guild` = " + global.id;
+                                con.query(updatechannel);
+                                message.channel.send(':white_check_mark: Se ha actualizado el canal de avance de nivel');
+                                indice();
+                            } else {
+                                message.channel.send(':x: No ha mencionado un canal válido')
+                                u_canal();
+                            }
+                        });
+                }
+
+                function u_fondo() {
+                    message.channel.send(':arrow_right: Introduzca el ID del fondo que desea establecer. Puede consultar los fondos disponibles en este enlace: https://pingu.duoestudios.es/personalizacion/fondos')
+                    message.channel.awaitMessages(m => m.author.id == message.author.id,
+                        { max: 1 }).then(collected => {
+                            if (isInteger(collected.first().content)) {
+                                if (parseInt(collected.first().content) <= 20 && parseInt(collected.first().content) >= 1) {
+                                    var updatemsg = "UPDATE `servidores` SET `niveles_fondo` = '" + collected.first().content + "' WHERE `servidores`.`guild` = " + global.id;
+                                    con.query(updatemsg);
+                                    message.channel.send(':white_check_mark: Se ha actualizado el fondo de los carteles de rank y avance de nivel');
+                                    indice();
+                                } else {
+                                    message.channel.send(':x: Ese fondo no existe, por favor, introduzca un ID válido.')
+                                    u_fondo();
+                                }
+                            } else {
+                                message.channel.send(':x: El valor introducido debe ser alfanumérico.')
+                                u_fondo();
+                            }
+                        });
+                }
+
+                function u_dificultad() {
+                    message.channel.send(':arrow_right: Indique la dificultad de subir de nivel')
+                    message.channel.awaitMessages(m => m.author.id == message.author.id,
+                        { max: 1 }).then(collected => {
+                            if (isInteger(collected.first().content)) {
+                                if (parseInt(collected.first().content) < 5 && parseInt(collected.first().content) > 0) {
+                                    var updatemsg = "UPDATE `servidores` SET `niveles_dificultad` = '" + collected.first().content + "' WHERE `servidores`.`guild` = " + global.id;
+                                    con.query(updatemsg);
+                                    message.channel.send(':white_check_mark: Se ha actualizado la dificultad correctamente');
+                                    indice();
+                                } else {
+                                    message.channel.send(':information_source: Debe introducir un valor entre 1-5. No se pueden emplear decimales...');
+                                    u_dificultad();
+                                }
+                            } else {
+                                message.channel.send(':x: El valor introducido debe ser alfanumérico.')
+                                u_fondo();
+                            }
+                        });
                 }
 
                 message.channel.send('<:info:858737080950718484> Configuración del módulo de **NIVELES**');
