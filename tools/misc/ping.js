346e3b401e06635c208587a81d8f294cf176d71c
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-22 21:18:32 +0200

    Actualizar el comando ping

--- tools/misc/ping.js
+++ tools/misc/ping.js
@@ -1,8 +1,11 @@
+const { MessageEmbed } = require('discord.js');
 const Math = require('mathjs');
 
 module.exports = {
     name: 'ping',
     execute(args, client, con, contenido, global, message, result) {
-        message.channel.send(`🏓 Pong! \n🕑 Comando: **${Date.now() - message.createdTimestamp}ms** \n📨 API: **${Math.round(client.ws.ping)}ms**`);
+        var embed = new MessageEmbed();
+        embed.setColor('#9DF63F').setTitle('🏓 Pong!').setDescription(`🕑 Comando: **${Date.now() - message.createdTimestamp}ms** \n📨 API: **${Math.round(client.ws.ping)}ms**\n ⌛ Total: **${Math.round(client.ws.ping + (Date.now() - message.createdTimestamp))}ms**`).setTimestamp().setFooter('Hey!')
+        message.channel.send(embed);
     }
 }
