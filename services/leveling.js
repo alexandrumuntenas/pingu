8a21fbfe0ad42aee020ad1c9f52a6df976b6140e
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-23 22:02:00 +0200

    Parchear error de experiencia

--- services/leveling.js
+++ services/leveling.js
@@ -2,7 +2,6 @@ const { MessageAttachment } = require('discord.js');
 const fs = require('fs');
 const Jimp = require('jimp');
 const downloader = require('nodejs-file-downloader');
-
 module.exports = function (result, client, con, message, global) {
     var cache = { "canal_id": result[0].niveles_canal_id, "canal_msg": result[0].niveles_canal_mensaje, "aspecto": result[0].niveles_fondo, "cartelactivado": result[0].niveles_cartel };
     var dif = result[0].niveles_dificultad;
@@ -10,8 +9,10 @@ module.exports = function (result, client, con, message, global) {
         if (result[0]) {
             var exp = parseInt(result[0].experiencia);
             var niv = parseInt(result[0].nivel);
-            var exp = exp + Math.round(Math.random(15, 25));
-
+            function randomNumber(min, max) {
+                return Math.random() * (max - min) + min;
+            }
+            var exp = exp + Math.round(randomNumber(15, 25));
             if (exp >= (((niv * niv) * dif) * 100)) {
                 var exp = 0;
                 var niv = niv + 1;
