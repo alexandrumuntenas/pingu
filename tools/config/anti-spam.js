2440ba57c1c4eebd262564e16e8b1b7894931d91
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-28 09:20:10 +0200

    Añadir traducciones módulo anti-spam

--- tools/config/anti-spam.js
+++ tools/config/anti-spam.js
@@ -1,20 +1,23 @@
+
 module.exports = {
     name: 'anti-spam',
     execute(args, client, con, contenido, global, message, result) {
+        var lan = require(`../../languages/${result[0].idioma}.json`);
+        lan = lan.tools.config.antispam;
         if (message.guild.ownerID == message.author.id || message.member.hasPermission('ADMINISTRATOR')) {
             var valor = result[0].aspam_activado;
             if (valor == 1) {
                 var fin = 0;
-                var response = 'desactivado';
+                var response = lan.response_a;
             } else {
                 var fin = 1;
-                var response = 'activado';
+                var response = lan.response_b;
             }
             var sql = "UPDATE `servidores` SET `aspam_activado` = '" + fin + "' WHERE `servidores`.`guild` = " + global.id;
-            message.channel.send(':white_check_mark: Se ha ' + response + ' correctamente el filtro anti-spam.');
+            message.channel.send(`:white_check_mark: ${response}`);
             con.query(sql);
         } else {
-            message.channel.send(':x: No dispone de permisos suficientes para ejecutar este comando')
+            message.channel.send(`:x: ${lan.permerror}`)
         }
     }
 }
