7d0bea8c5503372de8c777ff14e6f36e75d391ff
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-28 09:23:15 +0200

    Preparar código para traducciones en despedidas

--- tools/config/despedidas.js
+++ tools/config/despedidas.js
@@ -3,125 +3,98 @@ const emojiStrip = require('emoji-strip');
 module.exports = {
     name: 'despedidas',
     execute(args, client, con, contenido, global, message, result) {
+        var lan = require(`../../languages/${result[0].idioma}.json`);
+        lan = lan.tools.config.despedidas;
         if (message.guild.ownerID == message.author.id || message.member.hasPermission('ADMINISTRATOR')) {
-            if (args[1]) {
-                switch (args[1]) {
-                    case 'canal':
-                        if (message.mentions.channels.first()) {
-                            var channel = message.mentions.channels.first();
-                            var sql = "UPDATE `servidores` SET `salida_canal` = '" + channel.id + "' WHERE `servidores`.`guild` = " + global.id;
-                            message.channel.send(':white_check_mark: Se ha actualizado el canal de despedidas');
-                            con.query(sql);
-                        } else {
-                            message.channel.send(':information_source: Falta un argumento en el comando. Uso: `' + global.prefix + 'despedidas canal <canal>`');
-                        }
-                        break;
-                    case 'mensaje':
-                        if (args[2]) {
-                            var mensaje = message.content.replace(global.prefix + 'despedidas mensaje ', '');
-                            var mensaje = emojiStrip(mensaje);
-                            var sql = "UPDATE `servidores` SET `salida_mensaje` = '" + mensaje + "' WHERE `servidores`.`guild` = " + global.id;
-                            message.channel.send(':white_check_mark: Se ha actualizado el mensaje correctamente');
-                            con.query(sql);
-                        } else {
-                            message.channel.send(':information_source: Falta un argumento en el comando. Uso: `' + global.prefix + 'despedidas mensaje <message>"`');
-                        }
-                        break;
-                    default:
-                        message.channel.send(':information_source: No se ha especificado una configuración válida del módulo de despedidas :arrow_right:  https://pingu.duoestudios.es/gestion-del-servidor/despedidas');
-                        break;
+            ftime = 0;
+            function purga() {
+                if (ftime == 0) {
+                    ++ftime;
+                } else {
+                    message.channel.bulkDelete(2);
                 }
-            } else {
-                ftime = 0;
-                function purga() {
-                    if (ftime == 0) {
-                        ++ftime;
-                    } else {
-                        message.channel.bulkDelete(2);
-                    }
-                }
-
-                function indice() {
-                    message.channel.send('Para ejecutar una opción, indique el número de la opción. \n \n ****Opciones Disponibles** \n **1.** ¿Enviar mensaje cuando alguien abandona el servidor? \n **2.** Establecer mensaje de despedidas \n **3.** Establecer canal de despedida \n **4.** Salir');
-                    message.channel.awaitMessages(m => m.author.id == message.author.id,
-                        { max: 1, time: 30000, errors: ['time'] }).then(collected => {
-                            switch (collected.first().content) {
-                                case '1':
-                                    purga()
-                                    t_mensaje();
-                                    break;
-                                case '2':
-                                    purga()
-                                    u_mensaje();
-                                    break;
-                                case '3':
-                                    purga()
-                                    u_canal();
-                                    break;
-                                case '4':
-                                    message.channel.send(':information_source: Se ha cerrado la configuración del módulo de despedidas')
-                                    break;
-                                default:
-                                    purga();
-                                    indice();
-                                    break;
-                            }
-                        }).catch(collected => {
-                            message.channel.send(':information_source: Se ha cerrado la configuración del módulo de despedidas');
-                        });
-                }
-
-                function t_mensaje() {
-                    message.channel.send(':arrow_right: ¿Enviar mensaje de despedida cuando se una miembro abandone el servidor? Respuestas disponibles: y(es) / n(o)');
-                    message.channel.awaitMessages(m => m.author.id == message.author.id,
-                        { max: 1 }).then(collected => {
-                            if (collected.first().content === "y" || collected.first().content === "yes") {
-                                var yes = "UPDATE `servidores` SET `salida_mensaje_activado` = '1' WHERE `servidores`.`guild` = " + global.id;
-                                con.query(yes);
-                                message.channel.send(':white_check_mark: A partir de ahora se enviarán mensajes de despedidas...');
-                                indice();
-                            } else {
-                                var no = "UPDATE `servidores` SET `salida_mensaje_activado` = '0' WHERE `servidores`.`guild` = " + global.id;
-                                con.query(no);
-                                message.channel.send(':white_check_mark: No se enviarán mensajes de despedida...');
-                                indice();
-                            }
-                        });
-                }
-
-                function u_canal() {
-                    message.channel.send(':arrow_right: ¿Dónde desea que se envíen los mensajes de despedidas? ¡Mencione el canal!')
-                    message.channel.awaitMessages(m => m.author.id == message.author.id,
-                        { max: 1 }).then(collected => {
-                            if (collected.first().mentions.channels.first()) {
-                                var channel = collected.first().mentions.channels.first();
-                                var updatechannel = "UPDATE `servidores` SET `salida_canal` = '" + channel.id + "' WHERE `servidores`.`guild` = " + global.id;
-                                con.query(updatechannel);
-                                message.channel.send(':white_check_mark: Se ha actualizado el canal de despedidas');
-                                indice();
-                            } else {
-                                message.channel.send(':x: No ha mencionado un canal válido.')
-                                u_canal();
-                            }
-                        });
-                }
-
-                function u_mensaje() {
-                    message.channel.send(':arrow_right: ¿Qué mensaje desea que se envíe para recibir a los despedir a los miembros? <:warn:858736919432527942> El mensaje de despedidas no soporta emojis. Si se incluyen, serán retirados automáticamente del texto.')
-                    message.channel.awaitMessages(m => m.author.id == message.author.id,
-                        { max: 1 }).then(collected => {
-                            var updatemsg = "UPDATE `servidores` SET `bienvenida_mensaje` = '" + emojiStrip(collected.first().content) + "' WHERE `servidores`.`guild` = " + global.id;
-                            con.query(updatemsg);
-                            message.channel.send(':white_check_mark: Se ha actualizado el mensaje de despedida');
-                            indice();
-                        });
-                }
-
-                message.channel.send('<:info:858737080950718484> Configuración del módulo de **DESPEDIDAS / SALIDAS**');
-                indice();
             }
+
+            function indice() {
+                message.channel.send(` ${lan.index.before} \n \n **${lan.index.avaliable}** \n **1.** ${lan.index.options.first} \n **2.** ${lan.index.options.second}  \n **3.** ${lan.index.options.third} \n **4.** ${lan.index.options.fourth}`);
+                message.channel.awaitMessages(m => m.author.id == message.author.id,
+                    { max: 1, time: 30000, errors: ['time'] }).then(collected => {
+                        switch (collected.first().content) {
+                            case '1':
+                                purga()
+                                t_mensaje();
+                                break;
+                            case '2':
+                                purga()
+                                u_mensaje();
+                                break;
+                            case '3':
+                                purga()
+                                u_canal();
+                                break;
+                            case '4':
+                                message.channel.send(`:information_source: ${lan.time_error}`)
+                                break;
+                            default:
+                                purga();
+                                indice();
+                                break;
+                        }
+                    }).catch(collected => {
+                        message.channel.send(`:information_source: ${lan.time_error}`)
+                    });
+            }
+
+            function t_mensaje() {
+                message.channel.send(`:arrow_right: ${lan.toggle_message.question} ${lan.toggle_message.avaliable_responses}: y(es) / n(o)`);
+                message.channel.awaitMessages(m => m.author.id == message.author.id,
+                    { max: 1 }).then(collected => {
+                        if (collected.first().content === "y" || collected.first().content === "yes") {
+                            var yes = "UPDATE `servidores` SET `salida_mensaje_activado` = '1' WHERE `servidores`.`guild` = " + global.id;
+                            con.query(yes);
+                            message.channel.send(`:white_check_mark: ${lan.toggle_message.response_b}`);
+                            indice();
+                        } else {
+                            var no = "UPDATE `servidores` SET `salida_mensaje_activado` = '0' WHERE `servidores`.`guild` = " + global.id;
+                            con.query(no);
+                            message.channel.send(`:white_check_mark: ${lan.toggle_message.response_a}`);
+                            indice();
+                        }
+                    });
+            }
+
+            function u_canal() {
+                message.channel.send(`:arrow_right: ${lan.update_channel.question}`)
+                message.channel.awaitMessages(m => m.author.id == message.author.id,
+                    { max: 1 }).then(collected => {
+                        if (collected.first().mentions.channels.first()) {
+                            var channel = collected.first().mentions.channels.first();
+                            var updatechannel = "UPDATE `servidores` SET `salida_canal` = '" + channel.id + "' WHERE `servidores`.`guild` = " + global.id;
+                            con.query(updatechannel);
+                            message.channel.send(`:white_check_mark: ${lan.update_channel.success}`);
+                            indice();
+                        } else {
+                            message.channel.send(`:x: ${lan.update_channel.invalid}`)
+                            u_canal();
+                        }
+                    });
+            }
+
+            function u_mensaje() {
+                message.channel.send(`:arrow_right: ${lan.update_message.question} <:warn:858736919432527942> ${lan.update_message.emoji_remover}`)
+                message.channel.awaitMessages(m => m.author.id == message.author.id,
+                    { max: 1 }).then(collected => {
+                        var updatemsg = "UPDATE `servidores` SET `bienvenida_mensaje` = '" + emojiStrip(collected.first().content) + "' WHERE `servidores`.`guild` = " + global.id;
+                        con.query(updatemsg);
+                        message.channel.send(`:white_check_mark: ${lan.update_message.success}`);
+                        indice();
+                    });
+            }
+
+            message.channel.send(`<:info: 858737080950718484 > ${lan.startup}`);
+            indice();
         } else {
-            message.channel.send(':x: No dispone de permisos suficientes para ejecutar este comando')
+            message.channel.send(`:x: ${lan.permerror}`);
         }
     }
 }
