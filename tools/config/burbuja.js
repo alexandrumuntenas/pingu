7f7a75426b0ee83d672bf7a2e7b4c3ce017524c7
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-28 09:21:06 +0200

    Preparar código para traducciones en burbuja

--- tools/config/burbuja.js
+++ tools/config/burbuja.js
@@ -1,21 +1,22 @@
 module.exports = {
     name: 'burbuja',
     execute(args, client, con, contenido, global, message, result) {
+        var lan = require(`../../languages/${result[0].idioma}.json`);
+        lan = lan.tools.config.burbuja;
         if (message.guild.ownerID == message.author.id || message.member.hasPermission('ADMINISTRATOR')) {
-
             var valor = result[0].burbuja_activado;
             if (valor == 1) {
                 var fin = 0;
-                var response = 'desactivado';
+                var response = lan.response_a;
             } else {
                 var fin = 1;
-                var response = 'activado';
+                var response = lan.response_b;
             }
             var sql = "UPDATE `servidores` SET `burbuja_activado` = '" + fin + "' WHERE `servidores`.`guild` = " + global.id;
-            message.channel.send(':white_check_mark: Se ha ' + response + ' correctamente el modo burbuja.');
+            message.channel.send(`:white_check_mark: ${response}`);
             con.query(sql);
         } else {
-            message.channel.send(':x: No dispone de permisos suficientes para ejecutar este comando')
+            message.channel.send(`:x: ${lan.permerror}`);
         }
     }
 }
