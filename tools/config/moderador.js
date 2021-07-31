0a77dac07ccff66804e0d1f5e908b380261cfc2a
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-31 16:36:39 +0200

    Traducciones configuración moderación

--- tools/config/moderador.js
+++ tools/config/moderador.js
@@ -3,6 +3,8 @@ const { isInteger } = require("mathjs");
 module.exports = {
     name: 'moderador',
     execute(args, client, con, contenido, global, message, result) {
+        var lan = require(`../../languages/${result[0].idioma}.json`);
+        lan = lan.tools.config.moderador;
         if (message.guild.ownerID == message.author.id || message.member.hasPermission('ADMINISTRATOR')) {
             ftime = 0;
             function purga() {
@@ -13,9 +15,9 @@ module.exports = {
                 }
             }
             function indice() {
-                message.channel.send('Para ejecutar una opción, indique el número de la opción. \n \n ****Opciones Disponibles** \n **1.** ¿Activar comandos de moderación?  (Alterna entre sí y no) \n **2.** Expulsar/banear usuario al alcanzar cierta cantidad de infracciones \n **3.** Salir');
+                message.channel.send(`${lan.index.before} \n \n **${lan.index.avaliable}** \n **1.** ${lan.index.options.first} \n **2.** ${lan.index.options.second} \n **3.** ${lan.index.options.third}`);
                 message.channel.awaitMessages(m => m.author.id == message.author.id,
-                    { max: 1 }).then(collected => {
+                    { max: 1, time: 30000, errors: ['time'] }).then(collected => {
                         switch (collected.first().content) {
                             case '1':
                                 purga()
@@ -26,31 +28,32 @@ module.exports = {
                                 e_modwarn();
                                 break;
                             case '3':
-                                message.channel.send(':information_source: Se ha cerrado la configuración del módulo de **MODERACIÓN**')
+                                message.channel.send(`:information_source: ${lan.index.time_error}`);
                                 break;
                             default:
                                 purga()
                                 indice();
                                 break;
                         }
-                    });
+                    }).catch(collected => {
+                        message.channel.send(`:information_source: ${lan.index.time_error}`);
+                    });;
             }
 
             function t_mod() {
                 var valor = result[0].moderador_activado;
                 if (valor == 1) {
                     var fin = 0;
+                    var response = lan.toggle_moderator.response_a;
+                    result[0].moderador_activado = 0;
                 } else {
                     var fin = 1;
+                    var response = lan.toggle_moderator.response_b;
+                    result[0].moderador_activado = 1;
                 }
                 var sql = "UPDATE `servidores` SET `moderador_activado` = '" + fin + "' WHERE `servidores`.`guild` = " + global.id;
-                if (fin == 0) {
-                    var response = 'desactivado';
-                } else {
-                    var response = 'activado';
-                }
                 con.query(sql);
-                message.channel.send(':white_check_mark: Se ha ' + response + ' correctamente los comandos de moderación.');
+                message.channel.send(`:white_check_mark: ${response}`);
                 indice();
             }
 
@@ -58,15 +61,15 @@ module.exports = {
                 var valor = result[0].moderador_warn_expulsion_activado;
                 if (valor == 1) {
                     var fin = 0;
-                    var response = 'desactivado';
+                    var response = lan.toggle_modwarn.response_a;
                     result[0].moderador_warn_expulsion_activado = 0;
                 } else {
                     var fin = 1;
-                    var response = 'activado';
+                    var response = lan.toggle_moderator.response_b;
                     result[0].moderador_warn_expulsion_activado = 1;
                 }
                 var sql = "UPDATE `servidores` SET `moderador_warn_expulsion_activado` = '" + fin + "' WHERE `servidores`.`guild` = " + message.guild.id;
-                message.channel.send(':white_check_mark: He ' + response + ' correctamente el `auto-ban` y el `auto-kick` del comando `warn`.');
+                message.channel.send(`:white_check_mark: ${response}`);
                 con.query(sql, function () {
                     e_modwarn();
 
@@ -74,42 +77,40 @@ module.exports = {
             }
 
             function e_limite() {
-                message.channel.send(':arrow_right: ¿Cuántos warns debe tener un usuario para tomar medidas de forma automática?');
+                message.channel.send(`:arrow_right: ${lan.e_limite.question}`);
                 message.channel.awaitMessages(m => m.author.id == message.author.id,
                     { max: 1 }).then(collected => {
                         if (isInteger(parseInt(collected.first().content))) {
                             var cantidad = parseInt(collected.first().content);
                             var sql = "UPDATE `servidores` SET `moderador_warn_expulsion_cantidad` = '" + cantidad + "' WHERE `servidores`.`guild` = " + global.id;
                             con.query(sql);
-                            message.channel.send(':white_check_mark: Se ha actualizado la cantidad correctamente.');
+                            message.channel.send(`:white_check_mark: ${lan.e_limite.success}`);
                             e_modwarn();
                         } else {
-                            message.channel.send(':information_source: Se debe introducir un valor alfanumérico');
+                            message.channel.send(`:information_source:  ${lan.e_limite.notinteger}`);
                             e_limite();
                         }
                     })
-
-
             }
 
             function t_medida() {
                 var valor = result[0].moderador_warn_expulsion_accion;
                 if (valor == 1) {
                     var fin = 0;
-                    var response = 'expulsar';
+                    var response = lan.toggle_medida.response_a;
                     result[0].moderador_warn_expulsion_accion = 0;
                 } else {
                     var fin = 1;
-                    var response = 'banear';
+                    var response = lan.toggle_medida.response_b;
                     result[0].moderador_warn_expulsion_accion = 1;
                 }
                 var sql = "UPDATE `servidores` SET `moderador_warn_expulsion_accion` = '" + fin + "' WHERE `servidores`.`guild` = " + global.id;
-                message.channel.send(':information_source: Se ha cambiado correctamente la acción a tomar a `' + response + '`');
+                message.channel.send(`:white_check_mark: ${response}`);
                 con.query(sql);
                 e_modwarn();
             }
             function e_modwarn() {
-                message.channel.send('Para ejecutar una opción, indique el número de la opción. \n \n ****Opciones Disponibles** \n **1.** ¿Activar función?  \n **2.** Configurar medida que se tomará si un usuario alcanza el límite de warns.  \n **3.** Modificar límite de warns (predeterminado => 5) \n **4.** Volver');
+                message.channel.send(`${lan.e_modwarn.before} \n \n **${lan.e_modwarn.avaliable}** \n **1.** ${lan.e_modwarn.options.first} \n **2.** ${lan.e_modwarn.options.second} \n **3.** ${lan.e_modwarn.options.third} \n **4.** ${lan.e_modwarn.options.fourth}`);
                 message.channel.awaitMessages(m => m.author.id == message.author.id,
                     { max: 1 }).then(collected => {
                         switch (collected.first().content) {
@@ -135,12 +136,12 @@ module.exports = {
                         }
                     });
             }
-            message.channel.send('<:info:858737080950718484> Configuración del módulo de **MODERACIÓN**');
+            message.channel.send(`<:info:858737080950718484> ${lan.startup}`);
             indice();
 
 
         } else {
-            message.channel.send(':x: No dispone de permisos suficientes para ejecutar este comando')
+            message.channel.send(`:x: ${lan.permerror}`)
         }
     }
 }
