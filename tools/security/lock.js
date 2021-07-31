baa879b804923e3ec785472610ab48339eb931ea
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-31 14:03:31 +0200

    Añadir traducciones comando Lock

--- tools/security/lock.js
+++ tools/security/lock.js
@@ -1,12 +1,16 @@
 module.exports = {
     name: 'lock',
     execute(args, client, con, contenido, global, message, result) {
+        var lan = require(`../../languages/${result[0].idioma}.json`);
+        lan = lan.tools.security.lock;
         if (message.member.hasPermission('MANAGE_MESSAGES') && message.member.hasPermission('KICK_MEMBERS') && message.member.hasPermission('BAN_MEMBERS') || message.member.hasPermission('ADMINISTRATOR')) {
             message.channel.updateOverwrite(message.channel.guild.roles.everyone, {
                 SEND_MESSAGES: false
             }).then(() => {
-                message.channel.send(':white_check_mark: Se ha bloqueado el canal correctamente. Para desbloquearlo, ejecute `' + global.prefix + 'unlock`');
+                message.channel.send(`:white_check_mark: ${lan.success} \`${global.prefix}unlock\``);
             });
+        } else {
+            message.channel.send(`:x: ${lan.permerror}`)
         }
     }
 }
