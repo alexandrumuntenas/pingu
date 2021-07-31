374bf20288d11871e9059497f448461aadd802d2
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-31 14:22:01 +0200

    Tiempo de procesamiento comando => Tiempo de respuesta bot

--- tools/misc/ping.js
+++ tools/misc/ping.js
@@ -5,7 +5,7 @@ module.exports = {
     name: 'ping',
     execute(args, client, con, contenido, global, message, result) {
         var embed = new MessageEmbed();
-        embed.setColor('#9DF63F').setTitle('🏓 Pong!').setDescription(`🕑 Comando: **${Date.now() - message.createdTimestamp}ms** \n📨 API: **${Math.round(client.ws.ping)}ms**\n ⌛ Total: **${Math.round(client.ws.ping + (Date.now() - message.createdTimestamp))}ms**`).setTimestamp().setFooter('Hey!')
+        embed.setColor('#9DF63F').setTitle('🏓 Pong!').setDescription(`🕑 Bot: **${Date.now() - message.createdTimestamp}ms** \n📨 API: **${Math.round(client.ws.ping)}ms**\n ⌛ Total: **${Math.round(client.ws.ping + (Date.now() - message.createdTimestamp))}ms**`).setTimestamp().setFooter('Hey!')
         message.channel.send(embed);
     }
 }
