829522fbfe1fecc8f7b43f29f3b888388932cd44
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-31 14:15:17 +0200

    Añadir traducciones al comando user-info // Remover campo "Author" del mensaje enriquecido y añadir thumbnail. // Remover datos de "roles que dispone".

--- tools/security/user-info.js
+++ tools/security/user-info.js
@@ -3,47 +3,51 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
     name: 'user-info',
     execute(args, client, con, contenido, global, message, result) {
+        var lan = require(`../../languages/${result[0].idioma}.json`);
+        lan = lan.tools.security.userinfo;
         if (message.member.hasPermission('MANAGE_MESSAGES') && message.member.hasPermission('KICK_MEMBERS') && message.member.hasPermission('BAN_MEMBERS') || message.member.hasPermission('ADMINISTRATOR')) {
             if (result[0].moderador_activado != 0) {
                 const user = message.mentions.users.first() || message.member.user
                 const member = message.guild.members.cache.get(user.id)
                 const embed = new MessageEmbed()
-                    .setAuthor(`Información de usuario de ${user.username}`, user.displayAvatarURL())
+                    .setThumbnail(user.displayAvatarURL())
                     .addFields(
                         {
-                            name: 'User tag',
+                            name: lan.usertag,
                             value: user.tag,
+                            inline: true
                         },
                         {
-                            name: 'Id del usuario',
+                            name: lan.id,
                             value: user.id,
+                            inline: true
                         },
                         {
-                            name: '¿Es un bot?',
+                            name: lan.isabot,
                             value: user.bot,
+                            inline: true
                         },
                         {
-                            name: 'Nickname',
+                            name: lan.hasnick,
                             value: member.nickname || 'No tiene',
+                            inline: true
                         },
                         {
-                            name: 'Se unió al servidor el',
+                            name: lan.joinedguild,
                             value: new Date(member.joinedTimestamp).toLocaleDateString(),
+                            inline: true
                         },
                         {
-                            name: 'Se unió a Discord el',
+                            name: lan.joineddiscord,
                             value: new Date(user.createdTimestamp).toLocaleDateString(),
+                            inline: true
                         },
-                        {
-                            name: 'Cantidad de roles que tiene',
-                            value: member.roles.cache.size - 1,
-                        }
                     )
 
                 message.channel.send(embed);
             }
         } else {
-            message.channel.send(':x: No dispone de permisos suficientes para ejecutar este comando')
+            message.channel.send(`:x: ${lan.permerror}`)
         }
     }
 }
