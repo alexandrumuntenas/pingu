d053d710591f1293a711a782f6172ec6c5756eb1
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-31 14:04:21 +0200

    Cambiar comando "del" a "clear" en las traducciones

--- tools/security/clear.js
+++ tools/security/clear.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'clear',
     execute(args, client, con, contenido, global, message, result) {
         var lan = require(`../../languages/${result[0].idioma}.json`);
-        lan = lan.tools.security.del;
+        lan = lan.tools.security.clear;
         if (message.member.hasPermission('MANAGE_MESSAGES') && message.member.hasPermission('KICK_MEMBERS') && message.member.hasPermission('BAN_MEMBERS') || message.member.hasPermission('ADMINISTRATOR')) {
             if (result[0].moderador_activado != 0) {
                 if (message.mentions.users.first()) {
