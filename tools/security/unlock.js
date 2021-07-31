1130c975b9c3576f82d94063a107efd0b0316559
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-07-31 14:17:56 +0200

    Adaptar código para las traduc. comando unlock

--- tools/security/unlock.js
+++ tools/security/unlock.js
@@ -1,12 +1,16 @@
 module.exports = {
     name: 'unlock',
     execute(args, client, con, contenido, global, message, result) {
+        var lan = require(`../../languages/${result[0].idioma}.json`);
+        lan = lan.tools.security.unlock;
         if (message.member.hasPermission('MANAGE_MESSAGES') && message.member.hasPermission('KICK_MEMBERS') && message.member.hasPermission('BAN_MEMBERS') || message.member.hasPermission('ADMINISTRATOR')) {
             message.channel.updateOverwrite(message.channel.guild.roles.everyone, {
                 SEND_MESSAGES: true
             }).then(() => {
-                message.channel.send(':white_check_mark: Se ha desbloqueado el canal correctamente');
+                message.channel.send(`:white_check_mark: ${lan.success}`);
             });
+        } else {
+            message.channel.send(`:x: ${lan.permerror}`)
         }
     }
 }
