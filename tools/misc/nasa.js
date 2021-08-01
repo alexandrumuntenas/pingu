265d6b7a6b3099d03b731a8fb69bbfaf859c5512
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-08-01 16:00:24 +0200

    Actualizar cmd nasa par aincluir el cargador y editar mensaje del bot

--- tools/misc/nasa.js
+++ tools/misc/nasa.js
@@ -6,20 +6,19 @@ module.exports = {
     name: 'nasa',
     execute(args, client, con, contenido, message, result) {
 
-        message.reply(' cargando imagen del día... ')
+        message.channel.send('<a:loader:871389840904695838>')
             .then(msg => {
                 fetch('https://api.nasa.gov/planetary/apod?api_key=ezowSxroDnhKvjzojV9SXx7LmZ6P7OndGYLGXuE9')
                     .then(response => response.json())
                     .then(quote => {
-                        translate(quote.explanation, { to: "es" }).then(res => {
+                        translate(quote.explanation, { to: result[0].idioma }).then(res => {
                             var embed = new MessageEmbed();
                             embed.setTitle(quote.title);
-                            embed.setDescription(":flag_es: **Traducción: **" + res.text);
+                            embed.setDescription(res.text);
                             embed.setImage(quote.hdurl);
-                            embed.setColor('#dd361c');
-                            embed.setFooter("Imagen por " + quote.copyright);
-                            message.channel.send(embed);
-                            msg.delete();
+                            embed.setColor('#0B3D91');
+                            embed.setFooter("Nasa.gov");
+                            msg.edit('', embed);
                         });
                     });
             })
