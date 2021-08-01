47d139e2039bd805e62d7e41806d00be8485b35a
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-08-01 20:19:23 +0200

    Añadir traducciones para slowmode

--- tools/security/slowmode.js
+++ tools/security/slowmode.js
@@ -3,15 +3,21 @@ var parse = require('parse-duration')
 module.exports = {
     name: 'slowmode',
     execute(args, client, con, contenido, message, result) {
+        var lan = require(`../../languages/${result[0].idioma}.json`);
+        lan = lan.tools.security.slowmode;
         if (message.member.hasPermission('MANAGE_MESSAGES') && message.member.hasPermission('KICK_MEMBERS') && message.member.hasPermission('BAN_MEMBERS') || message.member.hasPermission('ADMINISTRATOR')) {
             if (result[0].moderador_activado != 0) {
-                var timeslowmo1 = message.content.replace(`${result[0].prefix}slowmode `, '');
-                timeslowmo = parse(timeslowmo1, 's');
-                message.channel.setRateLimitPerUser(timeslowmo, "Slowmode");
-                message.reply(' se ha establecido un slowmode de `' + timeslowmo1 + '` :clock1:');
+                if (args[1]) {
+                    var timeslowmo1 = message.content.replace(`${result[0].prefix}slowmode `, '');
+                    timeslowmo = parse(timeslowmo1, 's');
+                    message.channel.setRateLimitPerUser(timeslowmo, "Slowmode");
+                    message.channel.send(`:clock1: ${lan.success} **${timeslowmo1}**`);
+                }
+            } else {
+                message.channel.send(`:information_source: ${lan.missing_args}`);
             }
         } else {
-            message.channel.send(':x: No dispone de permisos suficientes para ejecutar este comando')
+            message.channel.send(`:x: ${lan.permerror}`)
         }
     }
 }
