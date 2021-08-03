dca6f4752a5622e47cb447649524990a60f70f61
59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2021-08-03 13:49:28 +0200

    Reestructuración del código

--- app.js
+++ app.js
@@ -7,91 +7,38 @@ require('dotenv').config()
 const { Client, Collection } = require('discord.js');
 const mysql = require('mysql2');
 const fs = require('fs');
-const winston = require('winston');
-const Sentry = require("winston-transport-sentry-node").default;
-
-function makeId(length) {
-  var result = '';
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() *
-      charactersLength));
-  }
-  return result;
-}
+const makeId = require('./gen/makeId');
 
 const log = require('simple-node-logger').createRollingFileLogger({
-  logDirectory: `./log`,
+  logDirectory: `./logs`,
   fileNamePattern: `<date>_${makeId(5)}.log`,
   dateFormat: 'YYYY.MM.DD',
 });
 
 // Redireccionar console.log a @package/simple-node-logger
-/*console.log = function (d) { //
+console.log = function (d) {
+  process.stdout.write(`${d}\n`);
   log.info(d);
-};*/
+};
 
 process.on('uncaughtException', function (err) {
   log.warn((err && err.stack) ? err.stack : err);
 });
 
-const logger = winston.createLogger({
-  level: { error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6 },
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-});
-logger.add(new Sentry({
-  sentry: {
-    dsn: 'https://428ebb1b4ccc4d4d81806506a6064bdb@o923346.ingest.sentry.io/5870573',
-  },
-  level: { error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6 }
-}));
 const talkedRecently = new Set();
 const client = new Client();
 
-
-//Services Workers
-const guildcreate = require('./services/guildcreate');
-const guilddelete = require('./services/guilddelete');
-const guildmemberadd = require('./services/guildmemberadd');
-const guildmemberremove = require('./services/guildmemberremove');
+const guildcreate = require('./events/guildCreate');
+const guilddelete = require('./events/guildDelete');
+const guildmemberadd = require('./events/guildMemberAdd');
+const guildmemberremove = require('./events/guildMemberRemove');
 const leveling = require('./services/leveling');
 const antispamworker = require('./services/antispam');
-log.info('[OK] Services Workers Cargados');
+console.log('[OK] Eventos Cargados');
 
 // Funciones globales
 async function comprobarcarpetas() {
-  log.info('[··] Comprobando carpetas');
-  if (!fs.existsSync('./usuarios')) {
-    fs.mkdirSync('./usuarios/');
-    log.info('[··] Carpeta usuarios no existe >> creando...');
-    fs.mkdirSync('./usuarios/moderacion');
-    log.info('[··] Carpeta moderacion no existe >> creando...');
-    fs.mkdirSync('./usuarios/avatares');
-    log.info('[··] Carpeta avatares no existe >> creando...');
-    fs.mkdirSync('./usuarios/leveling');
-    log.info('[··] Carpeta leveling no existe >> creando...');
-    fs.mkdirSync('./usuarios/bievenidas');
-    log.info('[··] Carpeta bienvenidas no existe >> creando...');
-  }
-  if (!fs.existsSync('./usuarios/moderacion')) {
-    log.info('[··] Carpeta moderacion no existe >> creando...');
-    fs.mkdirSync('./usuarios/moderacion');
-  }
-  if (!fs.existsSync('./usuarios/avatares')) {
-    log.info('[··] Carpeta avatares no existe >> creando...');
-    fs.mkdirSync('./usuarios/avatares');
-  }
-  if (!fs.existsSync('./usuarios/leveling')) {
-    log.info('[··] Carpeta leveling no existe >> creando...');
-    fs.mkdirSync('./usuarios/leveling');
-  }
-  if (!fs.existsSync('./usuarios/bienvenidas')) {
-    log.info('[··] Carpeta bienvenidas no existe >> creando...');
-    fs.mkdirSync('./usuarios/bienvenidas');
-  }
-  log.info('[OK] Existen todas las carpetas necesarias');
+
 }
 
 // Bot
@@ -99,9 +46,9 @@ if (process.env.ENTORNO !== "desarrollo") {
   console.log(process.env.TOPGG);
   const { AutoPoster } = require('topgg-autoposter')
   const ap = AutoPoster(process.env.TOPGG, client)
-  log.info('[··] Publicando Estadísticas a Top.GG')
+  console.log('[··] Publicando Estadísticas a Top.GG')
   ap.on('posted', () => {
-    log.info('[OK] Estadísticas publicadas en Top.GG')
+    console.log('[OK] Estadísticas publicadas en Top.GG')
   })
   client.login(process.env.PUBLIC_TOKEN);
 } else {
@@ -109,7 +56,7 @@ if (process.env.ENTORNO !== "desarrollo") {
 }
 
 //Cargar comandos
-log.info('--Cargando comandos--');
+console.log('--Cargando comandos--');
 
 client.commands = new Collection();
 
@@ -123,9 +70,9 @@ function loadCommands(collection, directory) {
 
     if (file.endsWith('.js')) {
       const command = require(path);
-      log.info(`[··] Cargando ${command.name}`);
+      console.log(`[··] Cargando ${command.name}`);
       collection.set(command.name, command);
-      log.info(`[OK] Cargado ${command.name}`);
+      console.log(`[OK] Cargado ${command.name}`);
     }
     else if (fs.lstatSync(path).isDirectory()) {
       loadCommands(collection, path);
@@ -141,17 +88,17 @@ var con = mysql.createConnection({
   charset: "utf8_unicode_ci",
 });
 con.connect(function (err) {
-  log.info('[··] Conectando a MariaDB');
+  console.log('[··] Conectando a MariaDB');
   if (err) {
     log.warn(err)
   } else {
-    log.info('[OK] Conexión establecida con MariaDB');
+    console.log('[OK] Conexión establecida con MariaDB');
   }
 });
 
 client.on('ready', () => {
   comprobarcarpetas()
-  log.info('[OK] Bot inicializado...');
+  console.log('[OK] Bot inicializado...');
   client.user.setPresence({
     status: 'idle',
     activity: {
