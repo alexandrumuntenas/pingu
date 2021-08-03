fde658934c5936b80c33ff4c872a103cc6753c56
59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2021-08-03 13:46:13 +0200

    Preparar código para las traducciones > unbanall

--- tools/security/unbanall.js
+++ tools/security/unbanall.js
@@ -1,15 +1,22 @@
 module.exports = {
     name: 'unbanall',
     execute(args, client, con, contenido, message, result) {
+        var lan = require(`../../languages/${result[0].idioma}.json`);
+        var noavaliable = lan.tools.noavaliable;
+        lan = lan.tools.security.unbanall;
         if (result[0].moderador_activado != 0) {
             if (message.member.hasPermission("ADMINISTRATOR")) {
                 message.guild.fetchBans().then(bans => {
-                    if (bans.size == 0) { message.channel.send(":neutral_face: No hay usuarios baneados."); };
+                    if (bans.size == 0) { message.channel.send(`:neutral_face: ${lan.nousers}`); };
                     bans.forEach(ban => {
                         message.guild.members.unban(ban.user.id);
                     })
-                }).then(() => message.channel.send(":white_check_mark: Se han desbaneado a todos los usuarios de este servidor.")).catch(e => console.log(e))
-            } else { message.channel.send(":x: No dispone de permisos suficientes para ejecutar este comando") }
-        } else { message.channel.send(":x: El módulo de moderación no está activado") }
+                }).then(() => message.channel.send(`:white_check_mark: ${lan.success}`)).catch(e => console.log(e))
+            } else {
+                message.channel.send(`:x: ${lan.permerror}`);
+            }
+        } else {
+            message.channel.send(`:x: ${noavaliable}`);
+        }
     }
 }
