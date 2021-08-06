57dc52bd07b3ddb19a46bd947858fe61f8b1e3c7
59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2021-08-06 23:07:26 +0200

    Añadir nuevo servicio web de Pingu

---
+++ services/freshping.js
@@ -0,0 +1,13 @@
+module.exports = function (pwd) {
+    const express = require('express')
+    const app = express()
+    const port = pwd;
+
+    app.get('/', (req, res) => {
+        res.send('Pingu is online!')
+    })
+
+    app.listen(port, () => {
+        console.log(`[OK] Running web-server`);
+    })
+}
