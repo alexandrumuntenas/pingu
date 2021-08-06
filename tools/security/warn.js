902600ef79c4ece49479b2953733960f1f4c8bd9
59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2021-08-07 00:10:25 +0200

    Adaptar código para traducciones "warn". Mejorar respuestas y limpiar "warn" de espacios al principio y al final del string.

--- tools/security/warn.js
+++ tools/security/warn.js
@@ -1,24 +1,15 @@
 const { MessageEmbed } = require('discord.js')
+const makeId = require('../../gen/makeId');
 module.exports = {
     name: 'warn',
     execute(args, client, con, contenido, message, result) {
         if (message.member.hasPermission('MANAGE_MESSAGES') && message.member.hasPermission('KICK_MEMBERS') && message.member.hasPermission('BAN_MEMBERS') || message.member.hasPermission('ADMINISTRATOR')) {
             if (result[0].moderador_activado != 0) {
-                function makeId(length) {
-                    var result = '';
-                    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-                    var charactersLength = characters.length;
-                    for (var i = 0; i < length; i++) {
-                        result += characters.charAt(Math.floor(Math.random() *
-                            charactersLength));
-                    }
-                    return result;
-                }
+                var lan = require(`../../languages/${result[0].idioma}.json`);
+                lan = lan.tools.security.warn;
                 var warn = message.content;
                 message.mentions.users.array().forEach(user => {
-                    warn = warn.replace('<@!' + user.id + '>', '');
-                    warn = warn.replace('<@' + user.id + '>', '');
-                    warn = warn.replace(`${result[0].prefix}warn`, '');
+                    warn = warn.replace('<@!' + user.id + '>', '').replace('<@' + user.id + '>', '').replace(`${result[0].prefix}warn`, '');
                 })
                 message.mentions.users.array().forEach(user => {
                     var cache = { "activado": result[0].moderador_warn_expulsion_activado, "cantidad": result[0].moderador_warn_expulsion_cantidad, "accion": result[0].moderador_warn_expulsion_accion };
@@ -26,29 +17,35 @@ module.exports = {
                     con.query(consultarcantidad, function (err, result) {
                         var nuevainfraccion = "INSERT INTO `infracciones` (`identificador`,`user`, `guild`,`motivo`) VALUES ('" + makeId(7) + "', '" + user.id + "', '" + message.guild.id + "','" + warn + "')";
                         con.query(nuevainfraccion);
-                        message.channel.send(`:warning: ${user} ha sido advertido \nRazón: \`${warn}\``);
+                        if (warn.trim().length > 0) {
+                            message.channel.send(`:warning: ${user} ${lan.success} \n${lan.reason}: \`${warn.trim()}\``);
+                        } else {
+                            message.channel.send(`:warning: ${user} ${lan.success}`);
+                        }
                         const member = message.guild.member(user);
                         if (cache.activado != 0) {
                             if (parseInt(result[0].itotal) + 1 >= cache.cantidad) {
                                 if (cache.accion != 0) {
                                     member
                                         .ban({
-                                            reason: 'Acumulación de infracciones.',
+                                            reason: lan.automod.reason,
                                         })
                                         .then(() => {
-                                            message.channel.send(`:police_officer: El usuario ${user.tag} ha sido baneado del servidor por acumular más de ` + cache.cantidad + " infracciones.");
+                                            message.channel.send(`:police_officer: ${lan.automod.success.a} ${user.tag} ${lan.automod.success.b} ${lan.automod.ban} ${lan.automod.success.c} \`${cache.cantidad}\` ${lan.automod.success.d}`);
                                         })
                                         .catch(err => {
-                                            message.channel.send(`:x: Se ha intentado banear el usuario ${user.tag} por acumular más de ` + cache.cantidad + " infracciones, pero se ha producido un error...");
+                                            console.log(err);
+                                            message.channel.send(`:x: ${lan.automod.error.a} ${lan.automod.ban} ${lan.automod.error.b} (${user.tag}) ${lan.automod.error.c} \`${cache.cantidad}\` ${lan.automod.error.d}`);
                                         });
                                 } else {
                                     member
-                                        .kick('Acumulación de infracciones.')
+                                        .kick(lan.automod.reason)
                                         .then(() => {
-                                            message.channel.send(`:police_officer: El usuario ${user.tag} ha sido expulsado del servidor por acumular más de ` + cache.cantidad + " infracciones.");
+                                            message.channel.send(`:police_officer: ${lan.automod.success.a} ${user.tag} ${lan.automod.success.b} ${lan.automod.kick} ${lan.automod.success.c} \`${cache.cantidad}\` ${lan.automod.success.d}`);
                                         })
                                         .catch(err => {
-                                            message.channel.send(`:x: Se ha intentado expulsar el usuario ${user.tag} por acumular más de ` + cache.cantidad + " infracciones, pero se ha producido un error...");
+                                            console.log(err);
+                                            message.channel.send(`:x: ${lan.automod.error.a} ${lan.automod.kick} ${lan.automod.error.b} (${user.tag}) ${lan.automod.error.c} \`${cache.cantidad}\` ${lan.automod.error.d}`);
                                         });
                                 }
                             }
@@ -57,7 +54,7 @@ module.exports = {
                 });
             }
         } else {
-            message.channel.send(':x: No dispone de permisos suficientes para ejecutar este comando')
+            message.channel.send(`:x: ${lan.permerror}`)
         }
     }
 }
