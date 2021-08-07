90304b99993eac53ed4810a4cd344f5b32c530d7
59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2021-08-07 23:45:23 +0200

    Añadir traducciones de clear-server-warns

--- tools/security/clear-server-warns.js
+++ tools/security/clear-server-warns.js
@@ -1,16 +1,18 @@
 module.exports = {
     name: 'clear-server-warns',
     execute(args, client, con, contenido, message, result) {
+        var lan = require(`../../languages/${result[0].idioma}.json`);
+        lan = lan.tools.security.clearserverwarns;
         if (message.guild.ownerID == message.author.id || message.member.hasPermission('ADMINISTRATOR')) {
             if (result[0].moderador_activado != 0) {
                 var sql = "DELETE FROM `infracciones` WHERE guild = '" + message.guild.id + "'";
                 con.query(sql, function (err) {
                     console.log(err)
-                    message.channel.send(':white_check_mark: Se han eliminado las advertencias de todos los usuarios de este servidor');
+                    message.channel.send(`:white_check_mark: ${lan.success}`);
                 })
             }
         } else {
-            message.channel.send(':x: No dispone de permisos suficientes para ejecutar este comando')
+            message.channel.send(`:x: ${lan.permerror}`);
         }
     }
 }
