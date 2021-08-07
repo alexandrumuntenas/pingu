853776eabb98a9889e0ad75c8693a38114e1c31e
59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2021-08-07 23:42:43 +0200

    Actualizar userinfo para cumplir los estándares mínimos de 2108

--- tools/security/user-info.js
+++ tools/security/user-info.js
@@ -29,7 +29,7 @@ module.exports = {
                         },
                         {
                             name: lan.hasnick,
-                            value: member.nickname || 'No tiene',
+                            value: member.nickname || lan.nonick,
                             inline: true
                         },
                         {
