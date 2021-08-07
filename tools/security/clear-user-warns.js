2721f60a68524d34c713e515fcedcc426215c8ee
59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2021-08-07 23:52:37 +0200

    Clear User Warns añadir código para traducciones

--- tools/security/clear-user-warns.js
+++ tools/security/clear-user-warns.js
@@ -1,6 +1,8 @@
 module.exports = {
     name: 'clear-user-warns',
     execute(args, client, con, contenido, message, result) {
+        var lan = require(`../../languages/${result[0].idioma}.json`);
+        lan = lan.tools.security.clearuserwarns;
         if (message.member.hasPermission('MANAGE_MESSAGES') && message.member.hasPermission('KICK_MEMBERS') && message.member.hasPermission('BAN_MEMBERS') || message.member.hasPermission('ADMINISTRATOR')) {
             if (result[0].moderador_activado != 0) {
                 if (message.mentions.users.first()) {
@@ -8,14 +10,14 @@ module.exports = {
                     var sql = "DELETE FROM `infracciones` WHERE user = '" + user.id + "' AND guild = '" + message.guild.id + "'";
                     con.query(sql, function (err) {
                         console.log(err)
-                        message.channel.send(':white_check_mark: Se han eliminado todas las infracciones de <@!' + user.id + '> correctamente');
+                        message.channel.send(`:white_check_mark: ${lan.success} ${user}`);
                     })
                 } else {
-                    message.channel.send(':white_check_mark: Debe mencionar un usuario para poder retirarle todas las infracciones');
+                    message.channel.send(`:information_source: ${lan.atleastoneuser}`);
                 }
             }
         } else {
-            message.channel.send(':x: No dispone de permisos suficientes para ejecutar este comando')
+            message.channel.send(`:x: ${lan.permerror}`)
         }
     }
 }
