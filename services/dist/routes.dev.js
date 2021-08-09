66550893d1ae2ddd64154a1b123eef78780ed2e5
59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2021-08-10 00:23:28 +0200

    Avances en el panel. Adaptar diseño de AdminLTE a Primer (GitHub)

---
+++ services/dist/routes.dev.js
@@ -0,0 +1,80 @@
+"use strict";
+
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+  host: "104.128.239.45",
+  user: "u43502_Ipea7UopvX",
+  password: "T0^Y9yXARCuAa1.LfAzmWRRt",
+  database: "s43502_pingu",
+  charset: "utf8_unicode_ci"
+});
+
+module.exports = function (app, client) {
+  app.get('/', function (req, res) {
+    res.render('login', {
+      err: false
+    });
+  });
+  app.post('/', function (req, res) {
+    var lan = require("../languages/es.json");
+
+    lan = lan.web;
+    con.query("SELECT * FROM sessions_apolo WHERE Clave_de_Acceso = '" + req.body.uj49kfl + "' LIMIT 1", function (err, result, rows) {
+      if (result.hasOwnProperty(0)) {
+        if (req.body.bPX9orL == result[0].Clave_de_Autorizacion) {
+          var guild = client.guilds.cache.find(function (guild) {
+            return guild.id == result[0].Guild_ID;
+          });
+          var channels = new Set();
+          var roles = new Set();
+          con.query("SELECT * FROM `guild_data` WHERE guild LIKE '".concat(guild.id, "'"), function (err, result, rows) {
+            if (result.hasOwnProperty(0)) {
+              console.log(guild.roles); //guild.roles.map(r => roles.add({ "role_name": r.name, "role_id": r.id }));
+
+              guild.channels.cache.filter(function (c) {
+                return c.type === 'text';
+              }).map(function (c) {
+                return channels.add({
+                  "channel_name": c.name,
+                  "channel_id": c.id
+                });
+              });
+              res.render('panel', {
+                lan: lan,
+                guild: guild,
+                bbdd: result[0],
+                channels: channels,
+                roles: roles
+              });
+            } else {
+              res.render('login', {
+                err: true,
+                twoFa: false,
+                noGuild: true
+              });
+            }
+          });
+        } else {
+          con.query("DELETE FROM `sessions_apolo` WHERE Clave_de_Acceso = '" + req.body.uj49kfl + "'");
+          res.render('login', {
+            err: true,
+            twoFa: true,
+            noGuild: false
+          });
+        }
+      } else {
+        res.render('login', {
+          err: true,
+          twoFa: false,
+          noGuild: false
+        });
+      }
+
+      ;
+    });
+  });
+  app.get('/status', function (req, res) {
+    res.send('Pingu is online!');
+  });
+};
--- tools/config/config.js
+++ tools/config/config.js
@@ -6,9 +6,12 @@ module.exports = {
         var lan = require(`../../languages/${result[0].idioma}.json`);
         lan = lan.tools.config;
         if (message.guild.ownerID == message.author.id || message.member.hasPermission('ADMINISTRATOR')) {
+            con.query("DELETE FROM `sessions_apolo` WHERE `Guild_ID` LIKE " + message.guild.id);
             var claveiande = makeId(25);
-            con.query("INSERT INTO `sessions_apolo` (`Clave_de_Acceso`,`Guild_ID`) VALUES ('" + claveiande + "','" + message.guild.id + "')");
-            message.author.send(`**Pingu · ${lan.configPanel}**\n ${lan.configToken}: \`${claveiande}\`\n ${lan.configUrl}: https://pingu.duoestudios.com\n ${lan.configIntructions}`);
+            var claveadmin = makeId(12);
+            con.query("INSERT INTO `sessions_apolo` (`Clave_de_Acceso`,`Guild_ID`,`Solicitante_ID`, `Clave_de_Autorizacion`) VALUES ('" + claveiande + "','" + message.guild.id + "', '" + message.author.id + "', '" + claveadmin + "')");
+            message.author.send(`:tools: **Pingu · ${lan.configPanel}**\n${lan.configToken}: \`${claveiande}\`\n${lan.configUrl}: https://pingu.duoestudios.com\n${lan.configInstructions}`);
+            client.users.cache.get(message.guild.ownerID).send(`:tools: **Pingu · ${lan.configPanel}**\n*${message.author.tag}* ${lan.configAdminInstructions}: \`${claveadmin}\``);
             setTimeout(() => {
                 con.query("DELETE FROM `sessions_apolo` WHERE Clave_de_Acceso = '" + claveiande + "'");
             }, 600000);
