33114eea0c260ded72288b086ceb10fd7d403fce
59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2021-08-11 00:50:52 +0200

    Eliminar últimos trozos de bot roles

--- tools/config/bienvenidas.js
+++ tools/config/bienvenidas.js
@@ -19,6 +19,7 @@ module.exports = {
 
             var roles_user = new Set();
             var usersss = result[0].bienvenida_roles_user;
+            var botssss = result[0].bienvenida_roles_bot;
             if (usersss) {
                 var role_user = usersss.split(',');
                 role_user.forEach(element => {
