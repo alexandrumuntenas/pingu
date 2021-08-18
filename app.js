4f709521e1b7bf3f285c3237d515f7a922b32d4a
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-08-18 18:00:28 +0200

    null no identificado en los registros. Sanitizar entradas SQL.

--- app.js
+++ app.js
@@ -225,7 +225,7 @@ client.on('message', (message) => {
       });
     }
     else {
-      guildcreate(con, message.guild);
+      guildcreate(con, message);
     }
   }
   )
--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,12 +1,9 @@
 module.exports = function (con, guild) {
-    var id = guild.id;
-    var chx = guild.channels.cache.filter(chx => chx.type === "text").find(x => x.position === 0);
-    var sql = "INSERT INTO `guild_data` (`guild`, `guild_prefix`,`guild_language`,`welcome_channel`,`welcome_message`,`farewell_channel`,`farewell_message`,`leveling_rankup_channel`,`leveling_rankup_message`) VALUES (" + id + ", '/','en','" + chx.id + "','Bienvenido {user} a {server}','" + chx.id + "','¡Adiós {user}!','" + chx.id + "','GG! {user} ha subido al nivel {nivel-nuevo}');";
-    var desql = "SELECT * FROM `guild_data` WHERE `guild` LIKE " + id;
-    con.query(desql, function (err, result) {
-        console.log(err)
+    var chx = message.guild.channels.cache.filter(chx => chx.type === "text").find(x => x.position === 0);
+    con.query("SELECT * FROM `guild_data` WHERE `guild` LIKE ?", [message.guild.id], function (err, result) {
+        if(err) console.log(err);
         if (!result[0]) {
-            con.query(sql, function (err, result) {
+            con.query("INSERT INTO `guild_data` (`guild`, `guild_prefix`,`guild_language`,`welcome_channel`,`welcome_message`,`farewell_channel`,`farewell_message`,`leveling_rankup_channel`,`leveling_rankup_message`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",[message.guild.id, '/' ,'en', chx.id, 'Bienvenido {user} a {server}', chx.id, '¡Adiós {user}!', chx.id, 'GG! {user} ha subido al nivel {nivel-nuevo}'], function (err, result) {
                 console.log(err)
             });
         }
