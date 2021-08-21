b4e7431d50e71ffbea8668449b51e1969749bb17
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-08-21 17:07:16 +0200

    Hacer comando `ayuda` obsoleto

--- tools/functions/ayuda.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: 'ayuda',
-    execute(args, client, con, contenido, message, result) {
-        message.channel.send('<:win_information:876119543968305233> Puede consultar la documentación en https://pingu.duoestudios.es/')
-    }
-}
