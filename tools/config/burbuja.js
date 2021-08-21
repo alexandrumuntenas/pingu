5f5a085c0c279c2e80ffeaf29dcae2811970a2bb
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-08-21 17:06:50 +0200

    Mejorar y sanear códigos de configuración

--- tools/config/burbuja.js
+++ tools/config/burbuja.js
@@ -1,22 +1,19 @@
 module.exports = {
-    name: 'burbuja',
-    execute(args, client, con, contenido, message, result) {
-        var lan = require(`../../languages/${result[0].guild_language}.json`);
-        lan = lan.tools.config.burbuja;
-        if (message.guild.ownerID == message.author.id || message.member.hasPermission('ADMINISTRATOR')) {
-            var valor = result[0].burbuja_activado;
-            if (valor == 1) {
-                var fin = 0;
-                var response = lan.response_a;
-            } else {
-                var fin = 1;
-                var response = lan.response_b;
-            }
-            var sql = "UPDATE `guild_data` SET `burbuja_activado` = '" + fin + "' WHERE `guild_data`.`guild` = " + message.guild.id;
-            message.channel.send(`<:pingu_check:876104161794596964> ${response}`);
-            con.query(sql);
-        } else {
-            message.channel.send(`<:pingu_cross:876104109256769546> ${lan.permerror}`);
-        }
+  name: 'burbuja',
+  execute (args, client, con, contenido, message, result) {
+    let lan = require(`../../languages/${result[0].guild_language}.json`)
+    lan = lan.tools.config.burbuja
+    if (message.guild.ownerID === message.author.id || message.member.hasPermission('ADMINISTRATOR')) {
+      const valor = result[0].burbuja_activado
+      if (valor === 1) {
+        con.query("UPDATE `guild_data` SET `burbuja_activado` = '0' WHERE `guild_data`.`guild` = ?", [message.guild.id])
+        message.channel.send(`<:pingu_check:876104161794596964> ${lan.response_a}`)
+      } else {
+        con.query("UPDATE `guild_data` SET `burbuja_activado` = '1' WHERE `guild_data`.`guild` = ?", [message.guild.id])
+        message.channel.send(`<:pingu_check:876104161794596964> ${lan.response_b}`)
+      }
+    } else {
+      message.channel.send(`<:pingu_cross:876104109256769546> ${lan.permerror}`)
     }
+  }
 }
