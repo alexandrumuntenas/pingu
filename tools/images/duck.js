62865e03021adf698191592fd944388d312264e1
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-01 16:43:57 +0200

    Comandos: Añadir comando `duck`.

---
+++ tools/images/duck.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch')
+const getLocales = require('../../modules/getLocales')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+  name: 'duck',
+  execute (args, client, con, locale, message, result) {
+    function sendImageEmbed (url, provider) {
+      const embed = new MessageEmbed()
+        .setImage(url.url).setDescription(`<a:ultimahora:876105976573472778> ${getLocales(locale, 'ANIME_IMAGE_API', { API_PROVIDER: provider })}`)
+      message.reply({ embeds: [embed] })
+    }
+    fetch('https://random-d.uk/api/v2/random')
+      .then((response) => response.json())
+      .then((fetched) => sendImageEmbed(fetched, 'Random-d.uk'))
+  }
+}
