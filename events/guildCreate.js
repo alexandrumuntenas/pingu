5653c4b86cee6f35a2f07619aa5fa283ddc360ce
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-03 00:43:39 +0200

    Eventos: Resolver en guildCreate error `Cannot read property 'id' of undefined`.
    La constante chx filtraba los canales en busca de canales tipo `text`, cuando en Discord.js 13, el nombre del canal ha sido reemplazado por `GUILD_TEXT`.

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,11 +1,16 @@
-module.exports = (con, guild) => {
-  const chx = guild.channels.cache.filter(chx => chx.type === 'text').find(x => x.position === 0)
+module.exports = (con, guild, client) => {
+  const gC = client.Sentry.startTransaction({
+    op: 'guildCreate',
+    name: 'Guild Create'
+  })
+  const chx = guild.channels.cache.filter(chx => chx.type === 'GUILD_TEXT').find(x => x.position === 0)
   con.query('SELECT * FROM `guildData` WHERE `guild` LIKE ?', [guild.id], (err, result) => {
-    if (err) console.log(err)
+    if (err) client.Sentry.captureException(err)
     if (!Object.prototype.hasOwnProperty.call(result, 0)) {
       con.query('INSERT INTO `guildData` (`guild`, `guild_prefix`,`guild_language`,`welcome_channel`,`welcome_message`,`farewell_channel`,`farewell_message`,`leveling_rankup_channel`,`leveling_rankup_message`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [guild.id, '/', 'en', chx.id, 'Bienvenido {user} a {server}', chx.id, '¡Adiós {user}!', chx.id, 'GG! {user} ha subido al nivel {nivel-nuevo}'], (err, result) => {
-        if (err) console.log(err)
+        if (err) client.Sentry.captureException(err)
       })
     }
   })
+  gC.finish()
 }
