f0832b4bdf1482a460ce78b4fb69daa8e78a166a
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-07 15:45:21 +0200

    Script Inicial: Simplificar y elimiar declaraciones IF para la ejecución de comandos.

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ loadCommands(client.commands, './tools')
  * @param {collection} collection Discord Collection for Commands
  * @param {directory} directory The Directory Where Commands are stored
  */
-function loadCommands (collection, directory) {
+function loadCommands(collection, directory) {
   const files = fs.readdirSync(directory)
 
   for (const file of files) {
@@ -152,42 +152,40 @@ client.on('messageCreate', (message) => {
         message.args = message.content.slice(result[0].guild_prefix.length).trim().split(/ +/)
       }
       const contenido = message.content.toLowerCase()
-      if (message.content.startsWith(result[0].guild_prefix)) {
+      if (message.content.startsWith(result[0].guild_prefix) && message.args) {
         const command = message.args[0]
         message.args.shift()
-        if (message.args) {
-          if (client.commands.has(command)) {
-            const mCeIC = Sentry.startTransaction({
-              op: 'messageCreate/executeInternalCommand',
-              name: `Execute Internal Command (${command})`
-            })
-            try {
-              client.commands.get(command).execute(client, result[0].guild_language || 'en', message, result)
-            } catch (err) {
-              Sentry.captureException(err)
-              console.log(err)
-              message.reply('Se ha producido un error cuando ha intentado ejecutar este comando...')
-            } finally {
-              mCeIC.finish()
+        if (client.commands.has(command)) {
+          const mCeIC = Sentry.startTransaction({
+            op: 'messageCreate/executeInternalCommand',
+            name: `Execute Internal Command (${command})`
+          })
+          try {
+            client.commands.get(command).execute(client, result[0].guild_language || 'en', message, result)
+          } catch (err) {
+            Sentry.captureException(err)
+            console.log(err)
+            message.reply('Se ha producido un error cuando ha intentado ejecutar este comando...')
+          } finally {
+            mCeIC.finish()
+          }
+        } else {
+          const mCeEC = Sentry.startTransaction({
+            op: 'messageCreate/executeExternalCommand',
+            name: 'Execute External Command'
+          })
+          client.pool.query('SELECT * FROM `guildCustomCommands` WHERE `guild` = ?', [message.guild.id], (err, result) => {
+            if (err) Sentry.captureException(err)
+            if (Object.prototype.hasOwnProperty.call(result, 0)) {
+              client.pool.query('SELECT * FROM `guildCustomCommands` WHERE `guild` = ? AND `cmd` = ?', [message.guild.id, command], (err, result) => {
+                if (err) Sentry.captureException(err)
+                if (Object.prototype.hasOwnProperty.call(result, 0)) {
+                  message.channel.send('<:comandoscustom:858671400424046602>' + result[0].returns).catch((err) => Sentry.captureException(err)).finally(mCeEC.finish())
+                }
+              })
             }
-          } else {
-            const mCeEC = Sentry.startTransaction({
-              op: 'messageCreate/executeExternalCommand',
-              name: 'Execute External Command'
-            })
-            client.pool.query('SELECT * FROM `guildCustomCommands` WHERE `guild` = ?', [message.guild.id], (err, result) => {
-              if (err) Sentry.captureException(err)
-              if (Object.prototype.hasOwnProperty.call(result, 0)) {
-                client.pool.query('SELECT * FROM `guildCustomCommands` WHERE `guild` = ? AND `cmd` = ?', [message.guild.id, command], (err, result) => {
-                  if (err) Sentry.captureException(err)
-                  if (Object.prototype.hasOwnProperty.call(result, 0)) {
-                    message.channel.send('<:comandoscustom:858671400424046602>' + result[0].returns).catch((err) => Sentry.captureException(err)).finally(mCeEC.finish())
-                  }
-                })
-              }
-            })
-          };
-        }
+          })
+        };
       }
 
       if (result[0].moderator_noMoreInvites_enabled !== 0) {
