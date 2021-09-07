e74ec0fe06ce3eb970c0a9f49570d4896ee7b5e3
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-07 15:31:07 +0200

    Comando: Actualizar a mensajes enriquecidos las respuestas de `eval`.

--- tools/admin/eval.js
+++ tools/admin/eval.js
@@ -1,16 +1,24 @@
+const { MessageEmbed } = require('discord.js')
 const { codeBlock } = require('@discordjs/builders')
 const genericMessages = require('../../modules/genericMessages')
+
 module.exports = {
   name: 'eval',
   execute: async (client, locale, message, result) => { // eslint-disable-line no-unused-vars
     if (message.author.id === '722810818823192629') {
-      const code = message.args.join(' ')
-      try {
-        const evaled = eval(code)
-        const clean = await client.clean(client, evaled)
-        message.channel.send(codeBlock('js', clean))
-      } catch (err) {
-        message.channel.send(codeBlock('xl', `ERROR ${await client.clean(client, err)}`))
+      if (Object.prototype.hasOwnProperty.call(message.args, 0)) {
+        const code = message.args.join(' ')
+        const messageSent = new MessageEmbed().setTitle('Evaluado correctamente').addField(':inbox_tray: Entrada', codeBlock('js', code))
+        try {
+          const evaled = eval(code)
+          messageSent.addField(':outbox_tray: Salida', codeBlock('js', evaled || 'No se ha devuelto nada...')).setColor('GREEN')
+        } catch (err) {
+          messageSent.addField(':outbox_tray: Salida', codeBlock('js', err || 'No se ha devuelto nada...')).setColor('RED')
+        } finally {
+          message.reply({ embeds: [messageSent] })
+        }
+      } else {
+        message.reply(':information_source: Te hace falta mencionar un código para ejecutar')
       }
     } else {
       genericMessages.Error.permerror(message, locale)
