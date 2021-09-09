0006f930260bb80c62ac952495cbf9608d4710d3
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-09 23:18:10 +0200

    Eventos: Cambiar parámetros preestablecidos en `guildCreate`.

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -7,7 +7,7 @@ module.exports = (client, guild) => {
   client.pool.query('SELECT * FROM `guildData` WHERE `guild` LIKE ?', [guild.id], (err, result) => {
     if (err) client.Sentry.captureException(err)
     if (!Object.prototype.hasOwnProperty.call(result, 0)) {
-      client.pool.query('INSERT INTO `guildData` (`guild`, `guild_prefix`,`guild_language`,`welcome_channel`,`welcome_message`,`farewell_channel`,`farewell_message`,`leveling_rankup_channel`,`leveling_rankup_message`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [guild.id, '/', 'en', chx.id, 'Bienvenido {user} a {server}', chx.id, '¡Adiós {user}!', chx.id, 'GG! {user} ha subido al nivel {nivel-nuevo}'], (err, result) => {
+      client.pool.query('INSERT INTO `guildData` (`guild`, `guild_prefix`,`guild_language`,`welcome_channel`,`welcome_message`,`farewell_channel`,`farewell_message`,`leveling_rankup_channel`,`leveling_rankup_message`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [guild.id, '!', 'es', chx.id, 'Bienvenido {user} a {server}', chx.id, '¡Adiós {user}!', chx.id, 'GG! {user} ha subido al nivel {nivel-nuevo}'], (err, result) => {
         if (err) client.Sentry.captureException(err)
       })
     }
