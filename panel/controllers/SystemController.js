a1d14e2fc738462846b35c4a0438cd09bff7df89
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-09 23:31:36 +0200

    Panel: Eliminar archivos de la rama principal

--- panel/controllers/SystemController.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  main: (req, res, pool) => {
-    if (Object.prototype.hasOwnProperty.call(req.body, 'EEScEqQw')) {
-      pool.query('UPDATE `guildData` SET `guild_prefix` = ? WHERE `guild` = ?', [req.body.EEScEqQw, req.user.Guild_ID])
-    } else {
-      pool.query('UPDATE `guildData` SET `guild_prefix` = ? WHERE `guild` = ?', ['!', req.user.Guild_ID])
-    }
-  }
-}
