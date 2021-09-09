735f3335b8ecae364e97d46ed20ec1564e9af363
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-09 20:13:14 +0200

    Comandos: Modificar `levels` para mostrar la experiencia del usuario en base a su nivel.

--- tools/leveling/levels.js
+++ tools/leveling/levels.js
@@ -6,8 +6,7 @@ module.exports = {
   name: 'levels',
   execute (client, locale, message, result) {
     if (result[0].leveling_enabled !== 0) {
-      const dif = result[0].leveling_rankup_difficulty
-      client.pool.query('SELECT * FROM `guildLevels` WHERE guild = ? ORDER BY nivel DESC, experiencia DESC LIMIT 10', [message.guild.id], (err, rows, result) => {
+¡      client.pool.query('SELECT * FROM `guildLevels` WHERE guild = ? ORDER BY nivel DESC, experiencia DESC LIMIT 10', [message.guild.id], (err, rows, result) => {
         if (err) console.log(err)
         if (result) {
           if (Object.prototype.hasOwnProperty.call(result, 0)) {
@@ -17,9 +16,7 @@ module.exports = {
               .setColor('#FFD700')
             rows.forEach(function (row) {
               const usuario = message.guild.members.cache.get(row.user)
-              const nivel = parseInt(row.nivel)
-              const experiencia = parseInt(row.experiencia)
-              embed.addFields({ name: usuario.nickname || usuario.displayName || 'Lost User#0000', value: `${getLocales(locale, 'LEVELS_ENTRY', { LEVEL: row.nivel, XP: (((((nivel - 1) * (nivel - 1)) * dif) * 100) + experiencia) })}` })
+              embed.addFields({ name: usuario.nickname || usuario.displayName || 'Lost User#0000', value: `${getLocales(locale, 'LEVELS_ENTRY', { LEVEL: row.nivel, XP: row.experiencia })}` })
             })
             message.channel.send({ embeds: [embed] })
           } else {
