c7ff0fdd596b7252cd95c108cd07b73e2fb63803
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-10 12:19:00 +0200

    Comandos: El comando `unlock` no desbloqueaba el canal.

--- tools/security/unlock.js
+++ tools/security/unlock.js
@@ -8,7 +8,7 @@ module.exports = {
     if (message.database.moderator_enabled !== 0) {
       if (message.member.permissions.has([Permissions.FLAGS.MANAGE_MESSAGES, Permissions.FLAGS.KICK_MEMBERS, Permissions.FLAGS.BAN_MEMBERS]) || message.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
         message.channel.permissionOverwrites.create(message.channel.guild.roles.everyone, {
-          SEND_MESSAGES: false
+          SEND_MESSAGES: true
         }).then(() => {
           const sent = new MessageEmbed()
             .setColor('#28A745')
