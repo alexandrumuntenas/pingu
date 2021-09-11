4456feb7332ebda0865b2eba60a76711be31cff9
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-11 21:09:40 +0200

    Eventos: Eliminar script de generación de imágenes a través de JIMP.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,4 @@
 const { MessageAttachment } = require('discord.js')
-const Downloader = require('nodejs-file-downloader')
-const Jimp = require('jimp')
 
 module.exports = (client, member) => {
   const gMA = client.Sentry.startTransaction({
@@ -16,38 +14,6 @@ module.exports = (client, member) => {
       if (result[0].welcome_enabled !== 0) {
         const mensaje = client.channels.cache.get(result[0].welcome_channel)
         if (mensaje) {
-          /* if (result[0].welcome_image !== 0) {
-            const avatar = new Downloader({
-              url: member.user.avatarURL({ format: 'jpg' }),
-              directory: './usuarios/avatares/',
-              fileName: `${member.user.id}_join.jpg`,
-              cloneFiles: false
-            })
-            try {
-              avatar.download()
-              const top = Jimp.read(`./usuarios/avatares/${member.user.id}_join.jpg`)
-              top.circle()
-
-              top.resize(220, 220)
-              const font = Jimp.loadFont(Jimp.FONT_SANS_32_WHITE)
-              Jimp.read(`./recursos/carteles/${result[0].welcome_image_background}.png`, (err, image) => {
-                if (err) {
-                  client.Sentry.captureException(err)
-                  client.log.error(err)
-                }
-                image.composite(top, 39, 32)
-                image.print(font, 300, 109, `Hola ${member.user.tag}`)
-                image.print(font, 300, 141, `Miembro #${member.guild.memberCount}`)
-                image.writeAsync(`./usuarios/bienvenidas/${member.user.id}_${member.guild.id}_join.jpg`)
-                const attachament = new MessageAttachment('./usuarios/bienvenidas/' + member.id + '_' + member.guild.id + '_join.jpg')
-                mensaje.send(result[0].welcome_message.replace('{user}', `<@${member.user.id}>`).replace('{server}', `${member.guild.name}`), attachament)
-              })
-            } catch (err) {
-              client.Sentry.captureException(err)
-              client.log.error(err)
-            }
-          } else {
-          } */
           mensaje.send(result[0].welcome_message.replace('{user}', `<@${member.user.id}>`).replace('{server}', `${member.guild.name}`))
         }
       }
