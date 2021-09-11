4641645486ac132efbfa382c118b696068bcc3de
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-11 14:03:00 +0200

    Eventos: Modificar registros para reportar también errores en la consola.

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -4,18 +4,33 @@ module.exports = (client, guild) => {
     name: 'Guild Delete'
   })
   client.pool.query('DELETE FROM `guildData` WHERE guild = ?', [guild.id], (err, result) => {
-    if (err) client.Sentry.captureException(err)
+    if (err) {
+      client.Sentry.captureException(err)
+      client.log.error(err)
+    }
     client.pool.query('DELETE FROM `guildLevels` WHERE guild = ?', [guild.id], (err) => {
-      if (err) client.Sentry.captureException(err)
+      if (err) {
+        client.Sentry.captureException(err)
+        client.log.error(err)
+      }
     })
     client.pool.query('DELETE FROM `guildWarns` WHERE guild = ?', [guild.id], (err) => {
-      if (err) client.Sentry.captureException(err)
+      if (err) {
+        client.Sentry.captureException(err)
+        client.log.error(err)
+      }
     })
     client.pool.query('DELETE FROM `guildCustomCommands` WHERE guild = ?', [guild.id], (err) => {
-      if (err) client.Sentry.captureException(err)
+      if (err) {
+        client.Sentry.captureException(err)
+        client.log.error(err)
+      }
     })
     client.pool.query('DELETE FROM `guildAutoResponder` WHERE guild = ?', [guild.id], (err) => {
-      if (err) client.Sentry.captureException(err)
+      if (err) {
+        client.Sentry.captureException(err)
+        client.log.error(err)
+      }
     })
     gD.finish()
   })
