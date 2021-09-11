87256272baf59aad999d31d4028f20bc40f750d2
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-11 14:03:34 +0200

    Módulos: Añadir logger personalizado.

--- modules/checkFolders.js
+++ modules/checkFolders.js
@@ -1,38 +1,23 @@
 const fs = require('fs')
 
 module.exports = function () {
-  console.log('[··] Comprobando carpetas')
   if (!fs.existsSync('./usuarios')) {
     fs.mkdirSync('./usuarios/')
-    console.log('[··] Carpeta usuarios no existe >> creando...')
     fs.mkdirSync('./usuarios/moderacion')
-    console.log('[··] Carpeta moderacion no existe >> creando...')
     fs.mkdirSync('./usuarios/avatares')
-    console.log('[··] Carpeta avatares no existe >> creando...')
     fs.mkdirSync('./usuarios/leveling')
-    console.log('[··] Carpeta leveling no existe >> creando...')
     fs.mkdirSync('./usuarios/bievenidas')
-    console.log('[··] Carpeta bienvenidas no existe >> creando...')
   }
   if (!fs.existsSync('./usuarios/moderacion')) {
-    console.log('[··] Carpeta moderacion no existe >> creando...')
     fs.mkdirSync('./usuarios/moderacion')
   }
   if (!fs.existsSync('./usuarios/avatares')) {
-    console.log('[··] Carpeta avatares no existe >> creando...')
     fs.mkdirSync('./usuarios/avatares')
   }
   if (!fs.existsSync('./usuarios/leveling')) {
-    console.log('[··] Carpeta leveling no existe >> creando...')
     fs.mkdirSync('./usuarios/leveling')
   }
   if (!fs.existsSync('./usuarios/bienvenidas')) {
-    console.log('[··] Carpeta bienvenidas no existe >> creando...')
     fs.mkdirSync('./usuarios/bienvenidas')
   }
-  if (!fs.existsSync('./logs')) {
-    console.log('[··] Carpeta de registros no existe >> creando...')
-    fs.mkdirSync('./logs')
-  }
-  console.log('[OK] Existen todas las carpetas necesarias')
 }
--- modules/third-party/topggSDK.js
+++ modules/third-party/topggSDK.js
@@ -2,9 +2,9 @@ const topgg = require('topgg-autoposter')
 
 module.exports = (client) => {
   const ap = topgg.AutoPoster(process.env.TOPGG, client)
-  console.log('[··] Publicando Estadísticas a Top.GG')
+  client.log.info('Publicando Estadísticas a Top.GG')
   ap.on('posted', (err) => {
-    if (err.status === 503) console.log('[ERR] TopGG: 503 Servicio no disponible')
-    console.log('[OK] Estadísticas publicadas en Top.GG')
+    if (err.status === 503) client.log.warn('TopGG: 503 Servicio no disponible')
+    client.log.success('Estadísticas publicadas en Top.GG')
   })
 }
