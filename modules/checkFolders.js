38aa5fab9f4d45c0cc9870ab06bc1d909a77632c
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-11 23:13:03 +0200

    Módulos: Actualizar `checkFolders` para comprobar la existencia de la carpeta `./modules/temp`.
    Se eliminan las carpetas creadas a raíz de `./usuarios/` en favor de `./modules/temp/`

--- modules/checkFolders.js
+++ modules/checkFolders.js
@@ -1,23 +1,9 @@
-const fs = require('fs')
+const { existsSync, mkdirSync, rmdirSync } = require('fs')
 
 module.exports = function () {
-  if (!fs.existsSync('./usuarios')) {
-    fs.mkdirSync('./usuarios/')
-    fs.mkdirSync('./usuarios/moderacion')
-    fs.mkdirSync('./usuarios/avatares')
-    fs.mkdirSync('./usuarios/leveling')
-    fs.mkdirSync('./usuarios/bievenidas')
-  }
-  if (!fs.existsSync('./usuarios/moderacion')) {
-    fs.mkdirSync('./usuarios/moderacion')
-  }
-  if (!fs.existsSync('./usuarios/avatares')) {
-    fs.mkdirSync('./usuarios/avatares')
-  }
-  if (!fs.existsSync('./usuarios/leveling')) {
-    fs.mkdirSync('./usuarios/leveling')
-  }
-  if (!fs.existsSync('./usuarios/bienvenidas')) {
-    fs.mkdirSync('./usuarios/bienvenidas')
+  if (!existsSync('./modules/temp')) {
+    rmdirSync('./modules/temp', { recursive: true })
+    mkdirSync('./modules/temp')
   }
 }
+
