b07d238f7970c45ad2f069e6e27b312ecc0a6821
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-12 11:34:50 +0200

    Módulos: Añadir texto de `miembro #<>` y comentar la opción de generar círculos en avatar y avatar backdrop. Cambiar método de carga de imágenes para el fondo.

--- modules/canvasProcessing.js
+++ modules/canvasProcessing.js
@@ -20,39 +20,49 @@ module.exports = {
     const ctx = canvas.getContext('2d')
 
     // Establecer fondo del canvas
-    const background = new Image()
-    background.onload = function () {
-      const scale = Math.max(canvas.width / background.width, canvas.height / background.height)
-      ctx.drawImage(background, (canvas.width / 2) - (background.width / 2) * scale, (canvas.height / 2) - (background.height / 2) * scale, background.width * scale, background.height * scale)
-    }
-    background.src = './modules/sources/defaultBackgrounds/21.png'
+    const background = await loadImage('./modules/sources/defaultBackgrounds/7.png')
+    const scale = Math.max(canvas.width / background.width, canvas.height / background.height)
+    ctx.drawImage(background, (canvas.width / 2) - (background.width / 2) * scale, (canvas.height / 2) - (background.height / 2) * scale, background.width * scale, background.height * scale)
+    // background.src = './modules/sources/defaultBackgrounds/7.png'
 
     // Establecer blured overlay
     ctx.fillStyle = 'rgba(0, 0, 0, 0.2)'
-    ctx.fillRect(25, 25, 1050, 450)
     StackBlur.canvasRGBA(canvas, 25, 25, 1050, 450, 50)
 
     const joinText = getLocales(locale, 'GUILDMEMBERADD_USER_HAS_JOINED_THE_GUILD', { USER: member.user.tag })
+    const memberCountText = getLocales(locale, 'GUILDMEMBERADD_MEMBER_COUNT', { COUNT: member.guild.memberCount })
 
     ctx.font = applyText(canvas, joinText)
     ctx.fillStyle = '#ffffff'
     ctx.textAlign = 'center'
     ctx.fillText(joinText, canvas.width / 2, 350)
 
+    ctx.font = '30px "Montserrat SemiBold"'
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.8)'
+    ctx.fillText(memberCountText, canvas.width / 2, 425)
+
     // Añadir backdrop en avatar de usuario
+
+    // Type 1 with Circles
+    /*
     ctx.beginPath()
     ctx.arc(canvas.width / 2, 160, 110, 0, Math.PI * 2, true) // 110 es el radio de la figura
     ctx.closePath()
     ctx.clip()
+    */
 
     ctx.fillStyle = 'rgb(255,255,255)'
-    ctx.fillRect(canvas.width / 2 - 110, 10, canvas.width, canvas.height) // canvas.width - 110 para obtener el centrado de la figura
+    ctx.fillRect(canvas.width / 2 - 110, 50, 220, 220) // canvas.width - 110 para obtener el centrado de la figura
 
     // Añadir avatar de usuario
+
+    // Type 1 with Circles
+    /*
     ctx.beginPath()
     ctx.arc(canvas.width / 2, 160, 100, 0, Math.PI * 2, true)
     ctx.closePath()
     ctx.clip()
+    */
 
     const avatar = await loadImage(member.user.displayAvatarURL({ format: 'png', size: 512 }))
     ctx.drawImage(avatar, canvas.width / 2 - 100, 60, 200, 200)
