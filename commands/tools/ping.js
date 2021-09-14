c59208ae724432cf0d54e24429f14079d62c1725
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-14 11:57:26 +0200

    Comandos: `ping` no comprobaba si el comando es una interacción.

--- commands/tools/ping.js
+++ commands/tools/ping.js
@@ -9,7 +9,7 @@ module.exports = {
     .setDescription('Comprueba el tiempo de respuesta de Pingu hacia Discord'),
   execute (client, locale, message, isInteraction) {
     const embed = new MessageEmbed().setColor('#9DF63F').setTitle('🏓 Pong!').setDescription(`🕑 Bot: **${Date.now() - message.createdTimestamp}ms** \n📨 API: **${Math.round(client.ws.ping)}ms**\n ⌛ Total: **${Math.round(client.ws.ping + (Date.now() - message.createdTimestamp))}ms**`).setTimestamp().setFooter('Hey!')
-    if (isInteraction === false) {
+    if (isInteraction) {
       message.channel.send({ embeds: [embed] })
     } else {
       message.editReply({ embeds: [embed] })
