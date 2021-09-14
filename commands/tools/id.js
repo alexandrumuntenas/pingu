5bef20e6402cb7216e27becb6df30aeaf7e75fec
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-14 12:07:51 +0200

    Comandos: Añadir comando `id`.

--- commands/tools/id.js
+++ commands/tools/id.js
@@ -1,10 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
 const { MessageEmbed } = require('discord.js')
 const getLocales = require('../../modules/getLocales')
 
 module.exports = {
   name: 'id',
+  data: new SlashCommandBuilder()
+    .setName('id')
+    .setDescription('Returns guild ID'),
   execute (client, locale, message, isInteraction) {
     const messageSent = new MessageEmbed().setColor('#3984BD').setDescription(`<:win_information:876119543968305233> ${getLocales(locale, 'ID', { ID: message.guild.id })}`)
-    message.reply({ embeds: [messageSent] })
+    if (!isInteraction) {
+      message.channel.send({ embeds: [messageSent] })
+    } else {
+      message.editReply({ embeds: [messageSent] })
+    }
   }
 }
