3f6ced30df4ea2b7a84adb74b7938c740952672b
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-14 11:37:42 +0200

    Comandos: Preparar Ping para ser un slashCommand.

--- commands/tools/ping.js
+++ commands/tools/ping.js
@@ -1,9 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
 const { MessageEmbed } = require('discord.js')
 const Math = require('mathjs')
 
 module.exports = {
   name: 'ping',
-  execute(client, locale, message, isInteraction) {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Comprueba el tiempo de respuesta de Pingu hacia Discord'),
+  execute (client, locale, message, isInteraction) {
     const embed = new MessageEmbed().setColor('#9DF63F').setTitle('🏓 Pong!').setDescription(`🕑 Bot: **${Date.now() - message.createdTimestamp}ms** \n📨 API: **${Math.round(client.ws.ping)}ms**\n ⌛ Total: **${Math.round(client.ws.ping + (Date.now() - message.createdTimestamp))}ms**`).setTimestamp().setFooter('Hey!')
     if (isInteraction === false) {
       message.channel.send({ embeds: [embed] })
