5418dcf7e00f838891ae4f92ece4b761de270b3c
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-16 22:00:02 +0200

    DB: Actualizar script para utilizar varibles de entorno

--- db/updaters/2109140920211532_guildData-to-guildWelcomeConfig.js
+++ db/updaters/2109140920211532_guildData-to-guildWelcomeConfig.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const mysql = require('mysql2')
 
 const con = mysql.createConnection({
-  host: '#',
-  user: '#',
-  password: '#',
-  database: '#',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATA,
   charset: 'utf8_unicode_ci'
 })
 
