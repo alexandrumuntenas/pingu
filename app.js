c94341ff0d89eb3e0a8851f8d2693bf684f0a9d7
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-17 10:15:46 +0200

    Eventos: Separar del script inicial el evento de messageCreate. (#14)
    * Eventos: Separar del script inicial el evento de messageCreate.
    
    * Eventos: Modificar en `messageCreate` las rutas de los módulos locales.

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const { Client, Intents } = require('discord.js')
 const Sentry = require('@sentry/node')
 const mysql = require('mysql2')
 
-const talkedRecently = new Set()
-
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES] })
 
 client.log = require('./modules/customLogger')
@@ -31,11 +29,10 @@ const guildCreate = require('./events/guildCreate')
 const guildDelete = require('./events/guildDelete')
 const guildMemberAdd = require('./events/guildMemberAdd')
 const guildMemberRemove = require('./events/guildMemberRemove')
+const messageCreate = require('./events/messageCreate')
 client.log.success('Eventos Cargados')
 
 client.log.info('Cargando Módulos')
-const levelingRankUp = require('./modules/levelingRankUp')
-const noMoreInvites = require('./modules/noMoreInvites')
 const checkFolder = require('./modules/checkFolders')
 client.log.success('Módulos Cargados')
 
@@ -110,125 +107,5 @@ client.on('interactionCreate', async interaction => {
 })
 
 client.on('messageCreate', (message) => {
-  if (
-    message.channel.type === 'dm' ||
-    message.author.bot ||
-    message.author === client.user
-  ) return
-  client.pool.query('SELECT * FROM `guildData` WHERE guild = ?', [message.guild.id], (err, result, rows) => {
-    if (err) throw client.log.error(err)
-    if (Object.prototype.hasOwnProperty.call(result, 0)) {
-      message.database = result[0]
-      if (message.content.startsWith(message.database.guild_prefix) && message.content !== message.database.guild_prefix) {
-        message.args = message.content.slice(message.database.guild_prefix.length).trim().split(/ +/)
-      }
-      const contenido = message.content.toLowerCase()
-      if (message.content.startsWith(message.database.guild_prefix) && message.args) {
-        const command = message.args[0]
-        message.args.shift()
-        if (client.commands.has(command)) {
-          const mCeIC = Sentry.startTransaction({
-            op: 'messageCreate/executeInternalCommand',
-            name: `Execute Internal Command (${command})`
-          })
-          try {
-            client.commands.get(command).execute(client, message.database.guild_language || 'en', message, result)
-          } catch (err) {
-            Sentry.captureException(err)
-            client.log.error(err)
-            message.reply('Se ha producido un error cuando ha intentado ejecutar este comando...')
-          } finally {
-            mCeIC.finish()
-          }
-        } else {
-          const mCeEC = Sentry.startTransaction({
-            op: 'messageCreate/executeExternalCommand',
-            name: 'Execute External Command'
-          })
-          client.pool.query('SELECT * FROM `guildCustomCommands` WHERE `guild` = ?', [message.guild.id], (err, result) => {
-            if (err) {
-              Sentry.captureException(err)
-              client.log.error(err)
-            }
-            if (Object.prototype.hasOwnProperty.call(result, 0)) {
-              client.pool.query('SELECT * FROM `guildCustomCommands` WHERE `guild` = ? AND `cmd` = ?', [message.guild.id, command], (err, result) => {
-                if (err) {
-                  Sentry.captureException(err)
-                  client.log.error(err)
-                }
-                if (Object.prototype.hasOwnProperty.call(result, 0)) {
-                  message.channel.send('<:comandoscustom:858671400424046602>' + result[0].returns).catch((err) => {
-                    client.log.error(err)
-                    Sentry.captureException(err)
-                  }).finally(mCeEC.finish())
-                }
-              })
-            }
-          })
-        };
-      }
-
-      if (message.database.moderator_noMoreInvites_enabled !== 0) {
-        noMoreInvites(client, message, result)
-      }
-      if (message.database.leveling_enabled !== 0) {
-        const mClRU = Sentry.startTransaction({
-          op: 'messageCreate/levelingRankUp',
-          name: 'Leveling Rank Up'
-        })
-        try {
-          if (!contenido.startsWith(message.database.guild_prefix)) {
-            if (!talkedRecently.has(`${message.author.id}_${message.guild.id}`)) {
-              talkedRecently.add(`${message.author.id}_${message.guild.id}`)
-              setTimeout(() => {
-                talkedRecently.delete(`${message.author.id}_${message.guild.id}`)
-              }, 60000)
-              levelingRankUp(client, message)
-            }
-          }
-        } catch (err) {
-          Sentry.captureException(err)
-          client.log.error(err)
-        } finally {
-          mClRU.finish()
-        }
-      }
-
-      const mCgAR = Sentry.startTransaction({
-        op: 'messageCreate/guildAutoResponder',
-        name: 'Auto Responder'
-      })
-      client.pool.query('SELECT * FROM `guildAutoResponder` WHERE `guild` = ?', [message.guild.id], (err, result) => {
-        if (err) {
-          Sentry.captureException(err)
-          client.log.error(err)
-        }
-        if (result) {
-          try {
-            if (Object.prototype.hasOwnProperty.call(result, 0)) {
-              client.pool.query('SELECT * FROM `guildAutoResponder` WHERE `guild` = ? AND `action` = ?', [message.guild.id, contenido], (err, result) => {
-                if (err) {
-                  Sentry.captureException(err)
-                  client.log.error(err)
-                }
-                if (result) {
-                  if (Object.prototype.hasOwnProperty.call(result, 0)) {
-                    message.channel.send('<:respuestacustom:858671300024074240> ' + result[0].returns)
-                  }
-                }
-              })
-            }
-          } catch (err) {
-            Sentry.captureException(err)
-            client.log.error(err)
-          } finally {
-            mCgAR.finish()
-          }
-        }
-      })
-    } else {
-      guildCreate(client, message.guild)
-    }
-  }
-  )
+  messageCreate(client, message)
 })
