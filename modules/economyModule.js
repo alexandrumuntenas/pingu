dbfdd8ee137695679529200168f06d0cd5ec7ff5
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-17 23:38:20 +0200

    Módulos: `economyModule.getMoney` registrar movimientos en `guildEconomyBankTransferBook` para las ganancias.

--- modules/economyModule.js
+++ modules/economyModule.js
@@ -6,22 +6,29 @@ module.exports = {
       op: 'economy.getMoney',
       name: 'Economy (getMoney)'
     })
+    const plusNumber = Math.round(Math.random() * (25 - 15) + 15)
     client.pool.query('SELECT * FROM `guildEconomyUserBank` WHERE guild = ? AND member = ?', [message.guild.id, message.author.id], (err, result) => {
       if (err) {
         client.Sentry.captureException(err)
         client.log.error(err)
       }
       if (Object.prototype.hasOwnProperty.call(result, 0)) {
-        let money = parseInt(result[0].amount)
-        money = money + Math.round(Math.random() * (25 - 15) + 15)
+        const money = parseInt(result[0].amount) + plusNumber
         client.pool.query('UPDATE `guildEconomyUserBank` SET `amount` = ? WHERE `member` = ? AND `guild` = ?', [money, message.author.id, message.guild.id], (err) => {
           if (err) {
             client.Sentry.captureException(err)
             client.log.error(err)
           }
+          client.pool.query('INSERT INTO `guildEconomyBankTransferBook` (`transactionID`, `emisor`, `member`, `previousQuantity`, `newQuantity`, `type`) VALUES (?, ?, ?, ?, ?, 1)', [makeId(64), message.guild.id, message.author.id, parseInt(result[0].amount), money])
         })
       } else {
-        client.pool.query('INSERT INTO `guildEconomyUserBank` (`eban`, `member`, `guild`, `amount`) VALUES (?, ?, ?, ?)', [makeId(27), message.author.id, message.guild.id, Math.round(Math.random() * (25 - 15) + 15)])
+        client.pool.query('INSERT INTO `guildEconomyUserBank` (`eban`, `member`, `guild`, `amount`) VALUES (?, ?, ?, ?)', [makeId(27), message.author.id, message.guild.id, plusNumber], (err) => {
+          if (err) {
+            client.Sentry.captureException(err)
+            client.log.error(err)
+          }
+          client.pool.query('INSERT INTO `guildEconomyBankTransferBook` (`transactionID`, `emisor`, `member`, `newQuantity`, `type`) VALUES (?, ?, ?, ?, 1)', [makeId(64), message.guild.id, message.author.id, plusNumber])
+        })
       }
     })
     EgM.finish()
