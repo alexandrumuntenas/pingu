483eca62ae8a98e50d962db3837da2d4feab1479
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-28 19:08:58 +0200

    Comandos: `welcomer` Añadir opción viewconfig para poder ver la configuración establecida.

--- commands/config/welcomer.js
+++ commands/config/welcomer.js
@@ -1,8 +1,10 @@
-const { Permissions } = require('discord.js')
+const { Permissions, MessageEmbed } = require('discord.js')
 const genericMessages = require('../../modules/genericMessages')
 const getLocales = require('../../modules/getLocales')
 const { fetchConfig } = require('../../modules/welcomerModule')
 const emojiStrip = require('emoji-strip')
+
+const emojiRelationship = { 0: '<:discord_offline:876102753821278238>', 1: '<:discord_online:876102925129236481>' }
 
 module.exports = {
   name: 'welcomer',
@@ -12,6 +14,33 @@ module.exports = {
         if (message.guild.ownerId === message.author.id || message.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
           if (message.args[0]) {
             switch (message.args[0]) {
+              case 'viewconfig': {
+                message.channel.send('<a:loading:880765834774073344> Fetching data... Please wait.').then((_message) => {
+                  const roles = new Set()
+
+                  let rolset = ''
+
+                  data.welcomeRoles.split(',').forEach(element => {
+                    roles.add(element)
+                  })
+
+                  roles.forEach(element => {
+                    rolset = rolset + '<@&' + element + '> '
+                  })
+
+                  const sentEmbed = new MessageEmbed()
+                    .setColor('#17A2B8')
+                    .setTitle(getLocales(locale, 'WELCOMER_VIEWCONFIG_TITLE'))
+                    .setDescription(getLocales(locale, 'WELCOMER_VIEWCONFIG_DESCRIPTION'))
+                    .addField(`<:blurple_announcements:892441292909469726> ${getLocales(locale, 'WELCOMER_VIEWCONFIG_CHANNEL')}`, `${message.guild.channels.cache.find(c => c.id === data.welcomeChannel) || getLocales(locale, 'WELCOMER_VIEWCONFIG_NOCHANNEL')}`, true)
+                    .addField(`<:blurple_chat:892441341827616859> ${getLocales(locale, 'WELCOMER_VIEWCONFIG_MESSAGE')}`, `${data.welcomeMessage || getLocales(locale, 'WELCOMER_VIEWCONFIG_NOMESSAGE')}`, true)
+                    .addField(`<:blurple_image:892443053359517696> ${getLocales(locale, 'WELCOMER_VIEWCONFIG_WELCOMECARD_TITLE')}`, `${getLocales(locale, 'WELCOMER_VIEWCONFIG_WELCOMECARD_ENABLED', { WELCOMER_BACKGROUND_STATUS: emojiRelationship[data.welcomeImage] })}\n${getLocales(locale, 'WELCOMER_VIEWCONFIG_WELCOMECARD_DEFAULTBACKGROUND', { WELCOMER_DEFAULTBACKGROUND: data.welcomeImageBackground })}\n${getLocales(locale, 'WELCOMER_VIEWCONFIG_WELCOMECARD_CUSTOMBACKGROUND', { WELCOMER_CUSTOMBACKGROUND: `[Ver imagen](${data.welcomeImageCustomBackground})` || getLocales(locale, 'WELCOMER_VIEWCONFIG_WELCOMECARD_NOCUSTOMBACKGROUND') })}\n${getLocales(locale, 'WELCOMER_VIEWCONFIG_WELCOMECARD_OVERLAYBLUR', { WELCOMER_OVELAYBLUR: data.welcomeImageCustomBlur })}\n${getLocales(locale, 'WELCOMER_VIEWCONFIG_WELCOMECARD_OVERLAYOPACITY', { WELCOMER_OVERLAYOPACITY: data.welcomeImageCustomOpacity })}\n${getLocales(locale, 'WELCOMER_VIEWCONFIG_WELCOMECARD_ROUNDAVATAR', { WELCOMER_ROUNDAVATAR: emojiRelationship[data.welcomeImageRoundAvatar] })}`, false)
+                    .addField(`<:blurple_gift:892455451004911676> ${getLocales(locale, 'WELCOMER_VIEWCONFIG_WELCOMECARD_ROLES')}`, rolset)
+
+                  _message.edit({ content: 'Done', embeds: [sentEmbed] })
+                })
+                break
+              }
               case 'channel': {
                 if (message.mentions.channels.first()) {
                   client.pool.query('UPDATE `guildWelcomerConfig` SET `welcomeChannel` = ? WHERE `guild` = ?', [message.mentions.channels.first().id, message.guild.id], (err) => {
@@ -25,14 +54,12 @@ module.exports = {
               }
               case 'message': {
                 const filter = m => m.author.id === message.author.id
-                message.channel.awaitMessages({ filter, max: 1, time: 15000 }).then(collected => {
+                message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
                   client.pool.query('UPDATE `guildWelcomerConfig` SET `welcomeMessage` = ? WHERE `guild` = ?', [emojiStrip(collected.first().content), message.guild.id], (err) => {
                     if (err) client.Sentry.captureException(err)
                     genericMessages.Success(message, getLocales(locale, 'WELCOMER_MESSAGE_SUCCESS', { WELCOMER_MESSAGE: `\`${emojiStrip(collected.first().content)}\`` }))
                   })
                 })
-
-                // Buscar forma de poder mostrar el mensaje actual
                 break
               }
             }
