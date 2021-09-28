bd2fa06d35f2bbccf5ac169ff31cd6b6438a32cd
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-28 07:09:44 +0200

    Eventos: Emplear en guildCreate `welcomerModule` para inicializar la configuración del módulo de bienvenidas.

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,8 +1,11 @@
+const welcomerModule = require('../modules/welcomerModule')
+
 module.exports = (client, guild) => {
   const gC = client.Sentry.startTransaction({
     op: 'guildCreate',
     name: 'Guild Create'
   })
+  welcomerModule.fetchConfig(client, guild, (data) => {})
   const chx = guild.channels.cache.filter(chx => chx.type === 'GUILD_TEXT').find(x => x.position === 0)
   client.pool.query('SELECT * FROM `guildData` WHERE `guild` LIKE ?', [guild.id], (err, result) => {
     if (err) {
@@ -10,7 +13,7 @@ module.exports = (client, guild) => {
       client.log.error(err)
     }
     if (!Object.prototype.hasOwnProperty.call(result, 0)) {
-      client.pool.query('INSERT INTO `guildData` (`guild`, `guild_prefix`,`guild_language`,`welcome_channel`,`welcome_message`,`farewell_channel`,`farewell_message`,`leveling_rankup_channel`,`leveling_rankup_message`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [guild.id, '!', 'es', chx.id, 'Bienvenido {user} a {server}', chx.id, '¡Adiós {user}!', chx.id, 'GG! {user} ha subido al nivel {nivel-nuevo}'], (err, result) => {
+      client.pool.query('INSERT INTO `guildData` (`guild`, `guild_prefix`,`guild_language`, `farewell_channel`,`farewell_message`,`leveling_rankup_channel`,`leveling_rankup_message`) VALUES (?, ?, ?, ?, ?, ?, ?  )', [guild.id, '!', 'es', chx.id, '¡Adiós {user}!', chx.id, 'GG! {user} ha subido al nivel {nivel-nuevo}'], (err, result) => {
         if (err) {
           client.Sentry.captureException(err)
           client.log.error(err)
