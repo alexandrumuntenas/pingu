c0c44a2f06b585bfdedabd7c909a02979af0496a
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-09-30 19:20:18 +0200

    DB: Crear actualizador para transferir datos de `guildData` a `guildFarewellConfig`.

---
+++ db/updaters/2109300920211915_guildData-to-guildFarewellConfig.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const mysql = require('mysql2')
+
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATA,
+  charset: 'utf8_unicode_ci'
+})
+
+con.connect(function (err) {
+  if (err) throw err
+  console.log('Connected!')
+})
+
+con.config.namedPlaceholders = true
+
+con.query('SELECT * FROM `guildData`', async (err, rows) => {
+  if (err) console.log(err)
+  rows.forEach(element => {
+    console.log('Inserted ' + element.guild)
+    con.query('INSERT INTO `guildWelcomerConfig` (`guild`, `farewellEnabled`, `farewellChannel`, `farewellMessage`) VALUES (?, ?, ?, ?)', [element.guild, element.farewell_enabled, element.farewell_channel, element.farewell_message])
+  })
+})
+
+process.exit()
