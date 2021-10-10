5aff776ae4763fa2ed52c29fad4368d944b4041e
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-10-10 21:30:56 +0200

    Eventos: Simplificar query SQL para insertar datos en `guildData`.
    Se han establecido los valores predeterminados directamente en MariaDB.

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -10,14 +10,13 @@ module.exports = (client, guild) => {
   welcomerModule.fetchConfig(client, guild, (data) => {})
   farewellModule.fetchConfig(client, guild, (data) => {})
   levelsModule.fetchConfig(client, guild, (data) => {})
-  const chx = guild.channels.cache.filter(chx => chx.type === 'GUILD_TEXT').find(x => x.position === 0)
   client.pool.query('SELECT * FROM `guildData` WHERE `guild` LIKE ?', [guild.id], (err, result) => {
     if (err) {
       client.Sentry.captureException(err)
       client.log.error(err)
     }
     if (!Object.prototype.hasOwnProperty.call(result, 0)) {
-      client.pool.query('INSERT INTO `guildData` (`guild`, `guild_prefix`,`guild_language`, `leveling_rankup_channel`,`leveling_rankup_message`) VALUES (?, ?, ?, ?, ?)', [guild.id, '!', 'es', chx.id, '¡Adiós {user}!', chx.id, 'GG! {user} ha subido al nivel {nivel-nuevo}'], (err, result) => {
+      client.pool.query('INSERT INTO `guildData` (`guild`) VALUES (?)', [guild.id], (err, result) => {
         if (err) {
           client.Sentry.captureException(err)
           client.log.error(err)
