833c2a1c0b75c9a2fd4338fe8cb8d82db7d12247
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-10-11 23:25:15 +0200

    Comandos: Finalizar desarrollo del comando `levels` para la configuración del módulo de niveles.

--- commands/config/levels.js
+++ commands/config/levels.js
@@ -3,11 +3,12 @@ const genericMessages = require('../../modules/genericMessages')
 const getLocales = require('../../modules/getLocales')
 const { fetchConfig } = require('../../modules/levelsModule')
 const emojiStrip = require('emoji-strip')
+const { isInteger } = require('mathjs')
 
 module.exports = {
   name: 'levels',
   execute (client, locale, message) {
-    /*fetchConfig(client, message.guild, (data) => {
+    fetchConfig(client, message.guild, (data) => {
       if (data) {
         if (message.guild.ownerId === message.author.id || message.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
           if (message.args[0]) {
@@ -16,39 +17,74 @@ module.exports = {
                 message.channel.send('<a:loading:880765834774073344> Fetching data... Please wait.').then((_message) => {
                   const sentEmbed = new MessageEmbed()
                     .setColor('BLURPLE')
-                    .setTitle(getLocales(locale, 'FAREWELL_VIEWCONFIG_TITLE'))
-                    .setDescription(getLocales(locale, 'FAREWELL_VIEWCONFIG_DESCRIPTION'))
-                    .addField(`<:blurple_announcements:892441292909469726> ${getLocales(locale, 'WELCOMER_VIEWCONFIG_CHANNEL')}`, `${message.guild.channels.cache.find(c => c.id === data.farewellChannel) || getLocales(locale, 'WELCOMER_VIEWCONFIG_NOCHANNEL')}`, true)
-                    .addField(`<:blurple_chat:892441341827616859> ${getLocales(locale, 'WELCOMER_VIEWCONFIG_MESSAGE')}`, `${data.farewellMessage || getLocales(locale, 'WELCOMER_VIEWCONFIG_NOMESSAGE')}`, true)
+                    .setTitle(getLocales(locale, 'LEVELS_VIEWCONFIG_TITLE'))
+                    .setDescription(getLocales(locale, 'LEVELS_VIEWCONFIG_DESCRIPTION'))
+                    .addField(`<:blurple_announcements:892441292909469726> ${getLocales(locale, 'WELCOMER_VIEWCONFIG_CHANNEL')}`, `${message.guild.channels.cache.find(c => c.id === data.levelsChannel) || 'r/softwaregore'}`, true)
+                    .addField(`<:blurple_chat:892441341827616859> ${getLocales(locale, 'WELCOMER_VIEWCONFIG_MESSAGE')}`, `${data.levelsMessage || getLocales(locale, 'WELCOMER_VIEWCONFIG_NOMESSAGE')}`, true)
+                    .addField(`:trophy: ${getLocales(locale, 'LEVELS_VIEWCONFIG_DIFFICULTY')}`, `${data.levelsDifficulty}`, true)
 
                   _message.edit({ content: 'Done', embeds: [sentEmbed] })
                 })
                 break
               }
-              case 'channel': {
+              case 'rankUpChannel': {
                 if (message.mentions.channels.first()) {
-                  client.pool.query('UPDATE `guildFarewellConfig` SET `farewellChannel` = ? WHERE `guild` = ?', [message.mentions.channels.first().id, message.guild.id], (err) => {
+                  client.pool.query('UPDATE `guildLevelsConfig` SET `levelsChannel` = ? WHERE `guild` = ?', [message.mentions.channels.first().id, message.guild.id], (err) => {
                     if (err) client.Sentry.captureException(err)
-                    genericMessages.Success(message, getLocales(locale, 'FAREWELL_CHANNEL_SUCCESS', { FAREWELL_CHANNEL: message.mentions.channels.first() }))
+                    genericMessages.Success(message, getLocales(locale, 'LEVELS_CHANNEL_SUCCESS', { LEVELS_CHANNEL: message.mentions.channels.first() }))
                   })
                 } else {
-                  genericMessages.Info.status(message, getLocales(locale, 'FAREWELL_CHANNEL_MISSING_ARGS', { FAREWELL_CHANNEL: message.guild.channels.cache.find(c => c.id === data.farewellChannel) }))
+                  if (Object.prototype.hasOwnProperty.call(message.args, '1')) {
+                    switch (message.args[1]) {
+                      case 'none': {
+                        client.pool.query('UPDATE `guildLevelsConfig` SET `levelsChannel` = ? WHERE `guild` = ?', ['0', message.guild.id], (err) => {
+                          if (err) client.Sentry.captureException(err)
+                          genericMessages.Success(message, getLocales(locale, 'LEVELS_CHANNEL_SUCCESS', { LEVELS_CHANNEL: 'none' }))
+                        })
+                        break
+                      }
+                      case 'same': {
+                        client.pool.query('UPDATE `guildLevelsConfig` SET `levelsChannel` = ? WHERE `guild` = ?', ['1', message.guild.id], (err) => {
+                          if (err) client.Sentry.captureException(err)
+                          genericMessages.Success(message, getLocales(locale, 'LEVELS_CHANNEL_SUCCESS', { LEVELS_CHANNEL: 'same' }))
+                        })
+                        break
+                      }
+                      default: {
+                        genericMessages.Info.status(message, getLocales(locale, 'LEVELS_CHANNEL_MISSING_ARGS', { LEVELS_CHANNEL: 'r/softwaregore' }))
+                        break
+                      }
+                    }
+                  } else {
+                    genericMessages.Info.status(message, getLocales(locale, 'LEVELS_CHANNEL_MISSING_ARGS', { LEVELS_CHANNEL: 'r/softwaregore' }))
+                  }
                 }
                 break
               }
-              case 'message': {
+              case 'rankUpMessage': {
                 const filter = m => m.author.id === message.author.id
-                genericMessages.Info.status(message, getLocales(locale, 'FAREWELL_MESSAGE_PREUPDATE'))
+                genericMessages.Info.status(message, getLocales(locale, 'LEVELS_MESSAGE_PREUPDATE'))
                 message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
-                  client.pool.query('UPDATE `guildFarewellConfig` SET `farewellMessage` = ? WHERE `guild` = ?', [emojiStrip(collected.first().content), message.guild.id], (err) => {
+                  client.pool.query('UPDATE `guildLevelsConfig` SET `levelsMessage` = ? WHERE `guild` = ?', [emojiStrip(collected.first().content), message.guild.id], (err) => {
                     if (err) client.Sentry.captureException(err)
-                    genericMessages.Success(message, getLocales(locale, 'FAREWELL_MESSAGE_SUCCESS', { FAREWELL_MESSAGE: `\`${emojiStrip(collected.first().content)}\`` }))
+                    genericMessages.Success(message, getLocales(locale, 'LEVELS_MESSAGE_SUCCESS', { LEVELS_MESSAGE: `\`${emojiStrip(collected.first().content)}\`` }))
                   })
                 })
                 break
               }
-              case 'simulate': {
-                genericMessages.Info.status(message, getLocales(locale, 'FAREWELL_SIMULATE_SUCCESS'))
+              case 'difficulty': {
+                if (Object.prototype.hasOwnProperty.call(message.args, '1')) {
+                  if (isInteger(parseInt(message.args[1]))) {
+                    client.pool.query('UPDATE `guildLevelsConfig` SET `levelsDifficulty` = ? WHERE `guild` = ?', [parseInt(message.args[1]), message.guild.id], (err) => {
+                      if (err) client.Sentry.captureException(err)
+                      genericMessages.Success(message, getLocales(locale, 'LEVELS_DIFFICULTY_SUCCESS', { LEVELS_DIFFICULTY: message.args[1] }))
+                    })
+                  } else {
+                    genericMessages.Info.status(message, getLocales(locale, 'LEVELS_DIFFICULTY_NOT_INT'))
+                  }
+                } else {
+                  genericMessages.Info.status(message, getLocales(locale, 'LEVELS_DIFFICULTY_MISSING_ARGS', { LEVELS_DIFFICULTY: data.levelsDifficulty }))
+                }
                 break
               }
             }
@@ -59,13 +95,12 @@ module.exports = {
           genericMessages.Error.permerror(message, locale)
         }
       } else {
-        genericMessages.Info.status(message, getLocales(locale, 'FAREWELL_INITIALIZE'))
+        genericMessages.Info.status(message, getLocales(locale, 'LEVELS_INITIALIZE'))
       }
-    })*/
-    message.channel.send('If you want to see the leveling leaderboard, please use `levelstop`.')
+    })
   }
 }
 
 const helpTray = (message, locale) => {
-  genericMessages.Info.help(message, locale, `\`${message.database.guild_prefix}farewell <option>\``, ['viewconfig', 'channel <channel>', 'message', 'simulate'])
+  genericMessages.Info.help(message, locale, `\`${message.database.guild_prefix}levels <option>\``, ['viewconfig', 'rankUpChannel <channel>', 'rankUpMessage', 'difficulty <difficulty>'])
 }
