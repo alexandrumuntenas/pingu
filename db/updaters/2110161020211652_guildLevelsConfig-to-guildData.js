9d681bcef8fb35465bf28476667b02c03a3e2175
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-10-16 16:56:39 +0200

    DB: Añadir script para trasferir los datos de `guildLevelsConfig` a `guildData`.

---
+++ db/updaters/2110161020211652_guildLevelsConfig-to-guildData.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const mysql = require('mysql2')
+
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATA,
+  charset: 'utf8_unicode_ci'
+})
+
+con.connect(function (err) {
+  if (err) throw err
+  console.log('Connected!')
+})
+
+con.config.namedPlaceholders = true
+
+con.query('SELECT * FROM `guildLevelsConfig`', async (err, rows) => {
+  if (err) console.log(err)
+  rows.forEach(element => {
+    con.query('UPDATE `guildData` SET `levelsChannel` = ?, `levelsMessage` = ?, `levelsDifficulty` = ? WHERE `guildData`.`guild` = ?', [element.levelsChannel, element.levelsMessage, element.levelsDifficulty, element.guild], (err) => {
+      if (err) console.log(err)
+      console.log('Updated ' + element.guild)
+    })
+  })
+})
