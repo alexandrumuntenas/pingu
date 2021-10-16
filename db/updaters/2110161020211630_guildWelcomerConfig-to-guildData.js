c00251d8769b58d4559945ac82b074fd15503eed
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-10-16 16:34:32 +0200

    DB: Crear traspaso de datos de `guildWelcomerConfig` a `guildData`.

---
+++ db/updaters/2110161020211630_guildWelcomerConfig-to-guildData.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const mysql = require('mysql2')
+
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATA,
+  charset: 'utf8_unicode_ci'
+})
+
+con.connect(function (err) {
+  if (err) throw err
+  console.log('Connected!')
+})
+
+con.config.namedPlaceholders = true
+
+con.query('SELECT * FROM `guildWelcomerConfig`', async (err, rows) => {
+  if (err) console.log(err)
+  rows.forEach(element => {
+    con.query('UPDATE `guildData` SET `welcomeEnabled` = ?, `welcomeChannel` = ?, `welcomeMessage` = ?, `welcomeImage` = ?, `welcomeImageBackground` = ?, `welcomeImageRoundAvatar` = ?, `welcomeImageCustomBackground` = ?, `welcomeImageCustomOpacity` = ?, `welcomeImageCustomBlur` = ?, `welcomeRoles` = ? WHERE `guildData`.`guild` = ?', [element.welcomeEnabled, element.welcomeChannel, element.welcomeMessage, element.welcomeImage, element.welcomeImageBackground, element.welcomeImageRoundAvatar, element.welcomeImageCustomBackground, element.welcomeImageCustomOpacity, element.welcomeImageCustomBlur, element.welcomeRoles, element.guild], (err) => {
+      if (err) console.log(err)
+      console.log('Updated ' + element.guild)
+    })
+  })
+})
