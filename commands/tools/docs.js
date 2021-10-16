13e5d8a4a76221237dd79e688abcdefa97d24eb9
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-10-16 23:15:27 +0200

    Comando: Actualizar URL de la documentación.

--- commands/tools/docs.js
+++ commands/tools/docs.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require('discord.js')
 module.exports = {
   name: 'docs',
   execute (client, locale, message) {
-    const messageToSend = new MessageEmbed().setColor('#F8F9FA').setDescription(':point_right: https://alexandrutheodor.gitbook.io/pingu/')
+    const messageToSend = new MessageEmbed().setColor('#F8F9FA').setDescription(':point_right: https://pingu.duoestudios.com')
     message.reply({ embeds: [messageToSend] })
   }
 }
