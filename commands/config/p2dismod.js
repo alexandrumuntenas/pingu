7d149abb56570a3ae659d7d145505d950690678a
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-10-16 16:10:28 +0200

    Comandos: Habilitar p2dismod y p2enmod.

---
+++ commands/config/p2dismod.js
@@ -0,0 +1,52 @@
+const { Permissions } = require('discord.js')
+const genericMessages = require('../../modules/genericMessages')
+const getLocales = require('../../modules/getLocales')
+
+module.exports = {
+  name: 'p2dismod',
+  execute (client, locale, message) {
+    if (message.guild.ownerId === message.author.id || message.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
+      if (message.args[0]) {
+        switch (message.args[0]) {
+          case 'welcomer': {
+            client.pool.query('UPDATE `guildWelcomerConfig` SET `welcomerEnabled` = 0 WHERE `guild` = ?', [message.guild.id], (err) => {
+              if (err) client.Sentry.captureException(err)
+            })
+            break
+          }
+          case 'farewell': {
+            client.pool.query('UPDATE `guildWelcomerConfig` SET `farewellEnabled` = 0 WHERE `guild` = ?', [message.guild.id], (err) => {
+              if (err) client.Sentry.captureException(err)
+            })
+            break
+          }
+          case 'moderation': {
+            client.pool.query('UPDATE `guildData` SET `moderator_enabled` = 0 WHERE `guild` = ?', [message.guild.id], (err) => {
+              if (err) client.Sentry.captureException(err)
+            })
+            break
+          }
+          case 'levels': {
+            client.pool.query('UPDATE `guildData` SET `levelsEnabled` = 0 WHERE `guild` = ?', [message.guild.id], (err) => {
+              if (err) client.Sentry.captureException(err)
+            })
+            break
+          }
+          default: {
+            helpTray(message, locale)
+            return
+          }
+        }
+        genericMessages.Success(message, getLocales(locale, 'P2ENMOD', { PMODULE: `\`${message.args[0]}\`` }))
+      } else {
+        helpTray(message, locale)
+      }
+    } else {
+      genericMessages.Error.permerror(message, locale)
+    }
+  }
+}
+
+function helpTray (message, locale) {
+  genericMessages.Info.help(message, locale, `${message.database.guild_prefix}p2enmod <module>`, ['welcomer', 'farewell', 'moderation', 'levels'])
+}
