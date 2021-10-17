475d490f1d7020b1276b0b1ac675b4776ef41a9d
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-10-17 14:29:34 +0200

    Comandos: Añadir comando `balance` para consultar el saldo rápidamente

---
+++ commands/economy/balance.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require('discord.js')
+const { fetchUserAccount } = require('../../modules/economyModule')
+const genericMessages = require('../../modules/genericMessages')
+const getLocales = require('../../modules/getLocales')
+
+module.exports = {
+  name: 'balance',
+  execute (client, locale, message) {
+    if (message.database.economyEnabled !== 0) {
+      fetchUserAccount(client, message, (user) => {
+        const firstMessageSent = new MessageEmbed()
+          .setFooter(`${message.author.username} · ${message.database.economyBankName}`)
+          .setColor('#009FE3')
+          .addField(getLocales(locale, 'MYBANK_ACCOUNT_MONEY'), `\`${user.amount || 0} ${message.database.economyCurrency}\``, true)
+
+        message.reply({ embeds: [firstMessageSent] })
+      })
+    } else {
+      genericMessages.Error.no_avaliable(message, locale)
+    }
+  }
+}
