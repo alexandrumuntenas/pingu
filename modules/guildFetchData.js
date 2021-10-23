f081177243233e228261738459d558373bf05a15
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-10-24 00:20:03 +0200

    Issue #43: Solucionar `Column count doesn't match value count at row 1`.

--- modules/guildFetchData.js
+++ modules/guildFetchData.js
@@ -5,7 +5,7 @@ module.exports = (client, guild, callback) => {
       callback(result[0])
     } else {
       const chx = guild.channels.cache.filter(chx => chx.type === 'GUILD_TEXT').find(x => x.position === 0)
-      client.pool.query('INSERT INTO `guildData` (`guild`, `welcomeChannel`, `farewellChannel`, `levelsChannel`) VALUES (?)', [guild.id, chx.id, chx.id, chx.id, chx.id], (err) => {
+      client.pool.query('INSERT INTO `guildData` (`guild`, `welcomeChannel`, `farewellChannel`, `levelsChannel`) VALUES (?, ?, ?, ?)', [guild.id, chx.id, chx.id, chx.id], (err) => {
         if (err) {
           client.Sentry.captureException(err)
           client.log.error(err)
