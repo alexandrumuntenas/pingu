625634770fa59fccd38a3d490c1ed75e7babc32c
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-10-23 11:33:50 +0200

    Issue #42: Solucionar `TypeError: Cannot read property 'welcomeEnabled' of undefined`

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -9,33 +9,35 @@ module.exports = async (client, member) => {
     name: 'Guild Member Add'
   })
   guildFetchData(client, member.guild, (data) => {
-    if (data.welcomeEnabled !== 0) {
-      const mensaje = client.channels.cache.get(data.welcomeChannel)
-      if (mensaje) {
-        if (data.welcomeImage !== 0) {
-          welcomeCard(client, member, data.guildLanguage || 'en', data).then((paths) => {
-            const attachmentSent = new MessageAttachment(paths.attachmentSent)
-            mensaje.send({ content: data.welcomeMessage.replace('{member}', `<@${member.user.id}>`).replace('{guild}', `${member.guild.name}`), files: [attachmentSent] }).then(() => {
-              tempFileRemover(paths)
+    if (data) {
+      if (data.welcomeEnabled !== 0) {
+        const mensaje = client.channels.cache.get(data.welcomeChannel)
+        if (mensaje) {
+          if (data.welcomeImage !== 0) {
+            welcomeCard(client, member, data.guildLanguage || 'en', data).then((paths) => {
+              const attachmentSent = new MessageAttachment(paths.attachmentSent)
+              mensaje.send({ content: data.welcomeMessage.replace('{member}', `<@${member.user.id}>`).replace('{guild}', `${member.guild.name}`), files: [attachmentSent] }).then(() => {
+                tempFileRemover(paths)
+              })
             })
-          })
+          }
         }
       }
-    }
-    if (data.moderator_noMoreUsers_enabled === 1) {
-      if (member) {
-        member
-          .kick(data.moderator_noMoreUsers_message || 'noMoreUsers enabled on this guild · Powered by Pingu')
-      }
-    }
-    if (data.welcome_roles) {
-      const welcomeRoles = data.welcome_roles
-      const roleArray = welcomeRoles.split(',')
-      roleArray.forEach(element => {
-        if (member.guild.roles.cache.get(element)) {
-          member.roles.add(member.guild.roles.cache.get(element))
+      if (data.moderator_noMoreUsers_enabled === 1) {
+        if (member) {
+          member
+            .kick(data.moderator_noMoreUsers_message || 'noMoreUsers enabled on this guild · Powered by Pingu')
         }
-      })
+      }
+      if (data.welcome_roles) {
+        const welcomeRoles = data.welcome_roles
+        const roleArray = welcomeRoles.split(',')
+        roleArray.forEach(element => {
+          if (member.guild.roles.cache.get(element)) {
+            member.roles.add(member.guild.roles.cache.get(element))
+          }
+        })
+      }
     }
   })
   gMA.finish()
