115c62ed14615c84992eee208e93f733d640e326
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-10-30 22:59:04 +0200

    Issue #15: Solucionar Comando: `embed` no se pueden añadir más campos. (#50)

--- commands/utils/embed.js
+++ commands/utils/embed.js
@@ -14,7 +14,7 @@ module.exports = {
         embedCreated.setColor('#000000'.replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16) }))
         let addfields = 0
         function indice () {
-          embedMenu.edit({ content: 'Bienvenido al constructor visual de mensajes enriquecidos. Le iremos haciendo una serie de preguntas para ir construyendo su mensaje enriquecido a medida.\nPara ejecutar una opción, indique el número de la opción. \n \n ****Opciones Disponibles** \n **1.** Establecer Título \n **2.** Establecer Descripción \n **3.** Establecer Thumbnail \n **4.** Añadir Nuevo Campo (hasta 25) \n **5.** Establecer Imagen \n **6.** Establecer color del borde \n **7.** Enviar mensaje \n **9.** Descartar mensaje', embeds: [embedCreated] })
+          embedMenu.edit({ content: 'Bienvenido al constructor visual de mensajes enriquecidos. Le iremos haciendo una serie de preguntas para ir construyendo su mensaje enriquecido a medida.\nPara ejecutar una opción, indique el número de la opción. \n \n ****Opciones Disponibles** \n **1.** Establecer Título \n **2.** Establecer Descripción \n **3.** Establecer Thumbnail \n **4.** Añadir Nuevo Campo (hasta 25) \n **5.** Establecer Imagen \n **6.** Establecer color del borde \n **7.** Enviar mensaje \n **8.** Descartar mensaje', embeds: [embedCreated] })
           message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
             switch (collected.first().content) {
               case '1':
@@ -69,18 +69,15 @@ module.exports = {
           ++addfields
           if (addfields === 0 || addfields <= 25) {
             embedMenu.edit({ content: ':arrow_right: Introduzca el título del nuevo campo #' + addfields })
-            message.channel.awaitMessages(m => m.author.id === message.author.id,
-              { max: 1 }).then(collected => {
+            message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
               const titulo = collected.first().content
               collected.first().delete()
               embedMenu.edit({ content: ':arrow_right: Introduzca la descripción del nuevo campo #' + addfields })
-              message.channel.awaitMessages(m => m.author.id === message.author.id,
-                { max: 1 }).then(collected => {
+              message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
                 const descrip = collected.first().content
                 collected.first().delete()
                 embedMenu.edit({ content: ':arrow_right: Mostrar campo #' + addfields + ' en línea. Respuestas disponibles: y(es) / n(o)' })
-                message.channel.awaitMessages(m => m.author.id === message.author.id,
-                  { max: 1 }).then(collected => {
+                message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
                   if (collected.first().content === 'y' || collected.first().content === 'yes') {
                     embedCreated.addField(titulo, descrip, true)
                     collected.first().delete().then(() => indice())
