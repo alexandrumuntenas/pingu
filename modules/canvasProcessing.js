16639fbd514d7434d8c2093e2f6b5b1ba2c9dc0f
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-10-30 22:01:06 +0200

    Módulos: Actualizar propiedades de `canvasProcessing.rankCard` para apuntar a las nuevas columnas de personalización.

--- modules/canvasProcessing.js
+++ modules/canvasProcessing.js
@@ -8,7 +8,7 @@ const { millify } = require('millify')
 
 registerFont('./modules/sources/fonts/Montserrat/Montserrat-SemiBold.ttf', { family: 'Montserrat' })
 module.exports = {
-  welcomeCard: async (client, member, locale, database) => {
+  levelsCard: async (client, member, locale, database) => {
     const uniqueIdentifiers = {
       userAvatar: randomstring.generate({ charset: 'alphabetic' }),
       attachmentSent: randomstring.generate({ charset: 'alphabetic' })
@@ -23,19 +23,19 @@ module.exports = {
 
     // Establecer fondo del canvas
     let imgPath = ''
-    if (database.welcomeImageCustomBackground && isValidUrl(database.welcomeImageCustomBackground)) {
-      imgPath = database.welcomeImageCustomBackground
+    if (database.levelsImageCustomBackground && isValidUrl(database.levelsImageCustomBackground)) {
+      imgPath = database.levelsImageCustomBackground
     } else {
-      imgPath = `./modules/sources/defaultBackgrounds/${database.welcomeImageBackground || 1}.png`
+      imgPath = `./modules/sources/defaultBackgrounds/${database.levelsImageBackground || 1}.png`
     }
     const background = await loadImage(imgPath)
     const scale = Math.max(canvas.width / background.width, canvas.height / background.height)
     ctx.drawImage(background, (canvas.width / 2) - (background.width / 2) * scale, (canvas.height / 2) - (background.height / 2) * scale, background.width * scale, background.height * scale)
 
     // Establecer blured overlay
-    ctx.fillStyle = `rgba(0, 0, 0, ${database.welcomeImageCustomOpacity / 100})`
+    ctx.fillStyle = `rgba(0, 0, 0, ${database.levelsImageCustomOpacity / 100})`
     ctx.fillRect(25, 25, 1050, 450)
-    StackBlur.canvasRGBA(canvas, 25, 25, 1050, 450, database.welcomeImageCustomBlur)
+    StackBlur.canvasRGBA(canvas, 25, 25, 1050, 450, database.levelsImageCustomBlur)
 
     const joinText = getLocales(locale, 'GUILDMEMBERADD_USER_HAS_JOINED_THE_GUILD', { USER: member.user.tag })
     const memberCountText = getLocales(locale, 'GUILDMEMBERADD_MEMBER_COUNT', { COUNT: member.guild.memberCount })
@@ -51,7 +51,7 @@ module.exports = {
 
     // Añadir backdrop en avatar de usuario
 
-    if (database.welcomeImageRoundAvatar === 1) {
+    if (database.levelsImageRoundAvatar === 1) {
       ctx.beginPath()
       ctx.arc(canvas.width / 2, 160, 110, 0, Math.PI * 2, true) // 110 es el radio de la figura
       ctx.closePath()
@@ -63,7 +63,7 @@ module.exports = {
 
     // Añadir avatar de usuario
 
-    if (database.welcomeImageRoundAvatar === 1) {
+    if (database.levelsImageRoundAvatar === 1) {
       ctx.beginPath()
       ctx.arc(canvas.width / 2, 160, 100, 0, Math.PI * 2, true)
       ctx.closePath()
@@ -93,19 +93,19 @@ module.exports = {
 
     // Establecer fondo del canvas
     let imgPath = ''
-    if (database.welcomeImageCustomBackground && isValidUrl(database.welcomeImageCustomBackground)) {
-      imgPath = database.welcomeImageCustomBackground
+    if (database.levelsImageCustomBackground && isValidUrl(database.levelsImageCustomBackground)) {
+      imgPath = database.levelsImageCustomBackground
     } else {
-      imgPath = `./modules/sources/defaultBackgrounds/${database.welcomeImageBackground || 1}.png`
+      imgPath = `./modules/sources/defaultBackgrounds/${database.levelsImageBackground || 1}.png`
     }
     const background = await loadImage(imgPath)
     const scale = Math.max(canvas.width / background.width, canvas.height / background.height)
     ctx.drawImage(background, (canvas.width / 2) - (background.width / 2) * scale, (canvas.height / 2) - (background.height / 2) * scale, background.width * scale, background.height * scale)
 
     // Establecer blured overlay
-    ctx.fillStyle = `rgba(0, 0, 0, ${database.welcomeImageCustomOpacity / 100})`
+    ctx.fillStyle = `rgba(0, 0, 0, ${database.levelsImageCustomOpacity / 100})`
     ctx.fillRect(25, 25, 1050, 270)
-    StackBlur.canvasRGBA(canvas, 25, 25, 1050, 270, database.welcomeImageCustomBlur)
+    StackBlur.canvasRGBA(canvas, 25, 25, 1050, 270, database.levelsImageCustomBlur)
 
     // Escribir usuario
 
