4f749aef3e16a6ce533b680e90bb005dc8d591ea
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-09 20:14:53 +0100

    Issue #74: Revertir f996177

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -11,11 +11,7 @@ module.exports = {
         const firstMessageSent = new MessageEmbed()
           .setFooter(`${message.author.username} · ${message.database.economyBankName}`)
           .setColor('#009FE3')
-        if (user) {
-          firstMessageSent.addField(getLocales(locale, 'MYBANK_ACCOUNT_MONEY'), `\`${user.amount || 0} ${message.database.economyCurrency}\``, true)
-        } else {
-          firstMessageSent.addField(getLocales(locale, 'MYBANK_ACCOUNT_MONEY'), `\`0 ${message.database.economyCurrency}\``, true)
-        }
+          .addField(getLocales(locale, 'MYBANK_ACCOUNT_MONEY'), `\`${user.amount || 0} ${message.database.economyCurrency}\``, true)
         message.reply({ embeds: [firstMessageSent] })
       })
     } else {
