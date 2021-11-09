4fdcda0e46380053dc22679fb5c50ad3e1e4c687
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-09 21:42:40 +0100

    Módulos: Mejorar presentación en la consola de la carga de comandos...

--- modules/commandHandler.js
+++ modules/commandHandler.js
@@ -1,5 +1,9 @@
 const { Collection } = require('discord.js')
 const fs = require('fs')
+const Ascii = require('ascii-table')
+
+const table = new Ascii('Commands')
+table.setHeading('Command', 'Load status')
 
 module.exports = {
   loadCommands: (client) => {
@@ -20,14 +24,20 @@ module.exports = {
 
         if (file.endsWith('.js')) {
           const command = require(`.${path}`)
-          client.log.info(`Cargando ${command.name}`)
-          commands.set(command.name, command)
-          client.log.success(`Cargado ${command.name}`)
+          if (command.name) {
+            commands.set(command.name, command)
+            table.addRow(file, '✅')
+          } else {
+            table.addRow(file, '❌  -> missing a help.name, or help.name is not a string.')
+            continue
+          }
         } else if (fs.lstatSync(path).isDirectory()) {
           load(path)
         }
       }
     }
+
+    console.log(table.toString())
 
     return commands
   }
