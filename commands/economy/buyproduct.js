2b59af39dac2af148e253268f8aa3902ec67a270
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-11 18:45:20 +0100

    Comandos: Añadir comprobación de activación del módulo de economía en los comandos `buyproduct` y `shop`.

--- commands/economy/buyproduct.js
+++ commands/economy/buyproduct.js
@@ -5,20 +5,24 @@ const getLocales = require('../../modules/getLocales')
 module.exports = {
   name: 'buyproduct',
   execute (client, locale, message) {
-    if (message.args && Object.prototype.hasOwnProperty.call(message.args, 0)) {
-      fetchShopProduct(client, message.args[0], (productData) => {
-        if (productData) {
-          buyItem(client, message.member, message.guild, productData, (status) => {
-            if (status) {
-              genericMessages.Success(message, getLocales(locale, 'BUYPRODUCT_SUCCESS', { PRODUCT_NAME: productData.productName }))
-            } else {
-              genericMessages.Error.customerror(message, getLocales(locale, 'BUYPRODUCT_NOMONEY', { PRODUCT_NAME: productData.productName }))
-            }
-          })
-        } else {
-          genericMessages.Error.customerror(message, getLocales(locale, 'BUYPRODUCT_NOTFOUND', { PRODUCT_NAME: message.args[0] }))
-        }
-      })
+    if (message.database.economyEnabled !== 0) {
+      if (message.args && Object.prototype.hasOwnProperty.call(message.args, 0)) {
+        fetchShopProduct(client, message.args[0], (productData) => {
+          if (productData) {
+            buyItem(client, message.member, message.guild, productData, (status) => {
+              if (status) {
+                genericMessages.Success(message, getLocales(locale, 'BUYPRODUCT_SUCCESS', { PRODUCT_NAME: productData.productName }))
+              } else {
+                genericMessages.Error.customerror(message, getLocales(locale, 'BUYPRODUCT_NOMONEY', { PRODUCT_NAME: productData.productName }))
+              }
+            })
+          } else {
+            genericMessages.Error.customerror(message, getLocales(locale, 'BUYPRODUCT_NOTFOUND', { PRODUCT_NAME: message.args[0] }))
+          }
+        })
+      }
+    } else {
+      genericMessages.Error.no_avaliable(message, locale)
     }
   }
 }
--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -6,70 +6,74 @@ const getLocales = require('../../modules/getLocales')
 module.exports = {
   name: 'shop',
   execute (client, locale, message) {
-    if (Object.prototype.hasOwnProperty.call(message.args, '0')) {
-      const shopFriendlyId = message.args[0]
-      fetchShop(client, message.guild, shopFriendlyId, (shopData) => {
-        // View product directly, no category module
-        if (Object.prototype.hasOwnProperty.call(message.args, '1')) {
-          const productFriendlyId = message.args[1]
-          fetchShopProduct(client, productFriendlyId, (productData) => {
-            if (productData) {
-              const productEmbed = new MessageEmbed()
-                .setColor(shopData.shopColor || '#633bdf')
-                .setAuthor(shopData.shopName, shopData.shopLogo)
-                .setTitle(productData.productName)
-                .setImage(productData.productImage)
-                .addField(`:coin: ${getLocales(locale, 'SHOP_PRODUCTPRICE')}`, `\`${productData.productPrice} ${message.database.economyCurrency}\``, true)
-                .addField(`:robot: ${getLocales(locale, 'SHOP_COMMANDTOBUY')}`, `\`${message.database.guildPrefix}buyproduct ${productData.productId}\``, true)
-                .setFooter('Powered by Pingu', 'https://cdn.discordapp.com/attachments/907917245567598592/907917308620587059/Instagram_Profiles1.png')
+    if (message.database.economyEnabled !== 0) {
+      if (Object.prototype.hasOwnProperty.call(message.args, '0')) {
+        const shopFriendlyId = message.args[0]
+        fetchShop(client, message.guild, shopFriendlyId, (shopData) => {
+          // View product directly, no category module
+          if (Object.prototype.hasOwnProperty.call(message.args, '1')) {
+            const productFriendlyId = message.args[1]
+            fetchShopProduct(client, productFriendlyId, (productData) => {
+              if (productData) {
+                const productEmbed = new MessageEmbed()
+                  .setColor(shopData.shopColor || '#633bdf')
+                  .setAuthor(shopData.shopName, shopData.shopLogo)
+                  .setTitle(productData.productName)
+                  .setImage(productData.productImage)
+                  .addField(`:coin: ${getLocales(locale, 'SHOP_PRODUCTPRICE')}`, `\`${productData.productPrice} ${message.database.economyCurrency}\``, true)
+                  .addField(`:robot: ${getLocales(locale, 'SHOP_COMMANDTOBUY')}`, `\`${message.database.guildPrefix}buyproduct ${productData.productId}\``, true)
+                  .setFooter('Powered by Pingu', 'https://cdn.discordapp.com/attachments/907917245567598592/907917308620587059/Instagram_Profiles1.png')
 
-              if (productData.productDescription) productEmbed.setDescription(productData.productDescription)
+                if (productData.productDescription) productEmbed.setDescription(productData.productDescription)
 
-              message.channel.send({ embeds: [productEmbed] })
-            } else {
-              genericMessages.Error.customerror(message, getLocales(locale, 'SHOP_PRODUCTNOTFOUND'))
-            }
-          })
-        } else {
-          if (shopData) {
-            const shopEmbed = new MessageEmbed()
-              .setTitle(shopData.shopName)
-              .setThumbnail(shopData.shopLogo)
-              .setColor(shopData.shopColor || '#633bdf')
-              .setFooter('Powered by Pingu', 'https://cdn.discordapp.com/attachments/907917245567598592/907917308620587059/Instagram_Profiles1.png')
-            if (shopData.shopDescription) shopEmbed.setDescription(shopData.shopDescription)
-            fetchShopProducts(client, shopData.shopId, (shopProductsData) => {
-              if (shopProductsData) {
-                shopProductsData.forEach(shopProduct => {
-                  shopEmbed.addField(`${shopProduct.productName} (${shopProduct.productId})`, `:coin: \`${shopProduct.productPrice} ${message.database.economyCurrency}\`\n:frame_photo: [\`${getLocales(locale, 'SHOP_VIEWIMAGE')}\`](${shopProduct.productImage || 'about:blank'})\n\n` + (shopProduct.productDescription || getLocales(locale, 'SHOP_NODESCRIPTION')))
-                })
-                message.channel.send({ embeds: [shopEmbed] })
+                message.channel.send({ embeds: [productEmbed] })
               } else {
-                message.channel.send({ embeds: [shopEmbed] })
+                genericMessages.Error.customerror(message, getLocales(locale, 'SHOP_PRODUCTNOTFOUND'))
               }
             })
           } else {
-            genericMessages.Error.customerror(message, getLocales(locale, 'SHOP_NOTFOUND'))
+            if (shopData) {
+              const shopEmbed = new MessageEmbed()
+                .setTitle(shopData.shopName)
+                .setThumbnail(shopData.shopLogo)
+                .setColor(shopData.shopColor || '#633bdf')
+                .setFooter('Powered by Pingu', 'https://cdn.discordapp.com/attachments/907917245567598592/907917308620587059/Instagram_Profiles1.png')
+              if (shopData.shopDescription) shopEmbed.setDescription(shopData.shopDescription)
+              fetchShopProducts(client, shopData.shopId, (shopProductsData) => {
+                if (shopProductsData) {
+                  shopProductsData.forEach(shopProduct => {
+                    shopEmbed.addField(`${shopProduct.productName} (${shopProduct.productId})`, `:coin: \`${shopProduct.productPrice} ${message.database.economyCurrency}\`\n:frame_photo: [\`${getLocales(locale, 'SHOP_VIEWIMAGE')}\`](${shopProduct.productImage || 'about:blank'})\n\n` + (shopProduct.productDescription || getLocales(locale, 'SHOP_NODESCRIPTION')))
+                  })
+                  message.channel.send({ embeds: [shopEmbed] })
+                } else {
+                  message.channel.send({ embeds: [shopEmbed] })
+                }
+              })
+            } else {
+              genericMessages.Error.customerror(message, getLocales(locale, 'SHOP_NOTFOUND'))
+            }
           }
-        }
-      })
-    } else {
-      fetchShops(client, message.guild, (shopsData) => {
-        if (shopsData) {
-          let shopList
-          shopsData.forEach(shop => { shopList = `${shopList || ''}・${shop.shopName} (\`${shop.shopFriendlyId}\`)\n` })
-          const shopsAvaliable = new MessageEmbed()
-            .setAuthor(message.guild.name, message.guild.iconURL())
-            .setColor('#633bdf')
-            .setTitle(getLocales(locale, 'SHOP_AVALIABLE_TITLE'))
-            .setDescription(shopList)
-            .setFooter('Powered by Pingu', 'https://cdn.discordapp.com/attachments/907917245567598592/907917308620587059/Instagram_Profiles1.png')
+        })
+      } else {
+        fetchShops(client, message.guild, (shopsData) => {
+          if (shopsData) {
+            let shopList
+            shopsData.forEach(shop => { shopList = `${shopList || ''}・${shop.shopName} (\`${shop.shopFriendlyId}\`)\n` })
+            const shopsAvaliable = new MessageEmbed()
+              .setAuthor(message.guild.name, message.guild.iconURL())
+              .setColor('#633bdf')
+              .setTitle(getLocales(locale, 'SHOP_AVALIABLE_TITLE'))
+              .setDescription(shopList)
+              .setFooter('Powered by Pingu', 'https://cdn.discordapp.com/attachments/907917245567598592/907917308620587059/Instagram_Profiles1.png')
 
-          message.channel.send({ embeds: [shopsAvaliable] })
-        } else {
-          genericMessages.Error.customerror(message, getLocales(locale, 'SHOP_NOEXIST'))
-        }
-      })
+            message.channel.send({ embeds: [shopsAvaliable] })
+          } else {
+            genericMessages.Error.customerror(message, getLocales(locale, 'SHOP_NOEXIST'))
+          }
+        })
+      }
+    } else {
+      genericMessages.Error.no_avaliable(message, locale)
     }
   }
 }
