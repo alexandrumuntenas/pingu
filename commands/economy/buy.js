ba385fde06218ca6ebf909f52ae477587f7bda23
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-17 16:58:28 +0100

    Economía: Ejecutar el comando `shop` si no hay argumentos en el comando `buy`.

--- commands/economy/buy.js
+++ commands/economy/buy.js
@@ -7,7 +7,8 @@ module.exports = {
   execute (client, locale, message) {
     if (message.database.economyEnabled !== 0) {
       if (message.args && Object.prototype.hasOwnProperty.call(message.args, 0)) {
-        fetchShopProduct(client, message.args[0], (productData) => {
+        const product = message.content.replace(`${message.database.guildPrefix}buy `, '').trim()
+        fetchShopProduct(client, message.guild, product, (productData) => {
           if (productData) {
             buyItem(client, message.member, message.guild, productData, (status) => {
               if (status) {
@@ -17,9 +18,11 @@ module.exports = {
               }
             })
           } else {
-            genericMessages.error(message, getLocales(locale, 'BUYPRODUCT_NOTFOUND', { PRODUCT_NAME: message.args[0] }))
+            genericMessages.error(message, getLocales(locale, 'BUYPRODUCT_NOTFOUND', { PRODUCT_NAME: product }))
           }
         })
+      } else {
+        client.commands.get('shop').execute(client, locale, message)
       }
     } else {
       genericMessages.error.noavaliable(message, locale)
