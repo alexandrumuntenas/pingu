7bf26017cd06a238593735b2ccf370399ceb022d
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-17 18:56:57 +0100

    Economía: Crear comando `daily` para ganar dinero.

---
+++ commands/economy/daily.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require('discord.js')
+const { getMoney } = require('../../modules/economyModule')
+const genericMessages = require('../../modules/genericMessages')
+const getLocales = require('../../modules/getLocales')
+
+module.exports = {
+  name: 'daily',
+  execute (client, locale, message) {
+    if (message.database.economyEnabled !== 0) {
+      getMoney(client, message.member, message.guild, (money) => {
+        const inventoryEmbed = new MessageEmbed()
+          .setAuthor(message.member.displayName, message.author.displayAvatarURL())
+          .setColor('#633bdf')
+          .setDescription(getLocales(locale, 'DAILY', { DAILY: `${money} ${message.database.economyCurrencyIcon}` }))
+          .setFooter('Powered by Pingu', 'https://cdn.discordapp.com/attachments/907917245567598592/907917308620587059/Instagram_Profiles1.png')
+        message.reply({ embeds: [inventoryEmbed] })
+      })
+    } else {
+      genericMessages.error.noavaliable(message, locale)
+    }
+  }
+}
