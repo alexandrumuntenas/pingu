a9bea47e92d50d5eb00303bb4e1307635d873d52
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-19 09:50:58 +0100

    messageCreate: Separar la función autoresponder del evento.

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,5 +1,6 @@
 const { MessageEmbed, Collection } = require('discord.js')
 const genericMessages = require('../functions/genericMessages')
+const autoresponder = require('../modules/autoresponder')
 const guildFetchData = require('../modules/guildFetchData')
 const { rankUp } = require('../modules/levels')
 const cooldown = new Collection()
@@ -76,44 +77,6 @@ module.exports = async (client, message) => {
       rankUp(client, message)
     }
 
-    const mCgAR = client.Sentry.startTransaction({
-      op: 'messageCreate/guildAutoResponder',
-      name: 'Auto Responder'
-    })
-    client.pool.query('SELECT * FROM `guildAutoResponder` WHERE `guild` = ?', [message.guild.id], (err, result) => {
-      if (err) {
-        client.Sentry.captureException(err)
-        client.log.error(err)
-      }
-      if (result) {
-        try {
-          if (Object.prototype.hasOwnProperty.call(result, 0)) {
-            client.pool.query('SELECT * FROM `guildAutoResponder` WHERE `guild` = ? AND `autoresponderTrigger` = ?', [message.guild.id, message.content], (err, result) => {
-              if (err) {
-                client.Sentry.captureException(err)
-                client.log.error(err)
-              }
-              if (result) {
-                if (Object.prototype.hasOwnProperty.call(result, 0)) {
-                  const messageSent = new MessageEmbed()
-                    .setFooter('Powered by Pingu', 'https://cdn.discordapp.com/attachments/907917245567598592/907917308620587059/Instagram_Profiles1.png')
-                    .setDescription(result[0].autoresponderResponse)
-                    .setColor('BLURPLE')
-                  message.channel.send({ embeds: [messageSent] }).catch((err) => {
-                    client.log.error(err)
-                    client.Sentry.captureException(err)
-                  })
-                }
-              }
-            })
-          }
-        } catch (err) {
-          client.Sentry.captureException(err)
-          client.log.error(err)
-        } finally {
-          mCgAR.finish()
-        }
-      }
-    })
+    autoresponder(client, message)
   })
 }
