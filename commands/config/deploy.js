a0dec5ffbd42dbbd2ecfab236ac363ce07f42afe
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-20 22:17:33 +0100

    Adaptar comando `deploy`.

--- commands/config/deploy.js
+++ commands/config/deploy.js
@@ -6,8 +6,18 @@ const rest = new REST({ version: '9' }).setToken(process.env.INSIDER_TOKEN)
 
 module.exports = {
   name: 'deploy',
+  description: 'Deploys the Pingu\'s Slash Commands to the server.',
+  executeInteraction (client, locale, interaction) {
+    if (interaction.user.id === interaction.guild.ownerId) {
+      rest.put(Routes.applicationGuildCommands(client.user.id, interaction.guild.id), { body: client.interactions.map(command => command.toJSON()) })
+        .then(() => genericMessages.success(interaction, 'Successfully registered application commands.'))
+        .catch(console.error)
+    } else {
+      genericMessages.error.permissionerror(interaction, locale)
+    }
+  },
   executeLegacy (client, locale, message) {
-    if (message.author.id === '722810818823192629') {
+    if (message.member.id === message.guild.ownerId) {
       rest.put(Routes.applicationGuildCommands(client.user.id, message.guild.id), { body: client.interactions.map(command => command.toJSON()) })
         .then(() => genericMessages.legacy.success(message, 'Successfully registered application commands.'))
         .catch(console.error)
