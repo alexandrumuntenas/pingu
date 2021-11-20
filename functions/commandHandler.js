bd27f7e236e28311aa7f28aa562f29139fbbae13
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-20 21:10:06 +0100

    CommandHandler: Añadir soporte para las interacciones. Registarlas en un array para mapearlas a través de `deploy`.

--- functions/commandHandler.js
+++ functions/commandHandler.js
@@ -1,4 +1,6 @@
 const { Collection } = require('discord.js')
+const { SlashCommandBuilder } = require('@discordjs/builders')
+
 const fs = require('fs')
 const Ascii = require('ascii-table')
 
@@ -40,5 +42,40 @@ module.exports = {
     console.log(table.toString())
 
     return commands
+  },
+  loadInteractions: (client) => {
+    const interactions = []
+
+    load('./commands')
+
+    /**
+     * Load Pingu Interactions
+     * @param {collection} collection Discord Collection for Interactions
+     * @param {directory} directory The Directory Where Interactions are stored
+     */
+    async function load (directory) {
+      const files = fs.readdirSync(directory)
+
+      for (const file of files) {
+        const path = `${directory}/${file}`
+
+        if (file.endsWith('.js')) {
+          const interaction = require(`.${path}`)
+          if (interaction.name) {
+            interactions.push(interaction.data || new SlashCommandBuilder().setName(interaction.name).setDescription(interaction.description || 'Description not set'))
+            table.addRow(file, '✅')
+          } else {
+            table.addRow(file, '❌  -> missing a help.name, or help.name is not a string.')
+            continue
+          }
+        } else if (fs.lstatSync(path).isDirectory()) {
+          load(path)
+        }
+      }
+    }
+
+    console.log(table.toString())
+
+    return interactions
   }
 }
