f20e8474551b3595417e13e41126aa7565e56846
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-20 21:55:34 +0100

    Adaptar comando `levelstop`.

--- commands/leveling/levelstop.js
+++ commands/leveling/levelstop.js
@@ -5,6 +5,40 @@ const genericMessages = require('../../functions/genericMessages')
 module.exports = {
   cooldown: 10000,
   name: 'levelstop',
+  description: 'Get the leaderboard',
+  executeInteraction (client, locale, interaction) {
+    if (interaction.database.levelsEnabled !== 0) {
+      client.pool.query('SELECT * FROM `guildLevelsData` WHERE guild = ? ORDER BY memberLevel DESC, memberExperience DESC LIMIT 10', [interaction.guild.id], (err, rows, result) => {
+        if (err) {
+          client.Sentry.captureException(err)
+          client.log.error(err)
+        }
+        if (rows) {
+          if (Object.prototype.hasOwnProperty.call(rows, 0)) {
+            const embed = new MessageEmbed()
+              .setTitle(`:trophy: ${getLocales(locale, 'LEVELSTOP_TITLE')}`)
+              .setFooter(interaction.guild.name)
+              .setColor('#FFD700')
+
+            let count = 0
+            rows.forEach(function (row) {
+              client.users.fetch(row.member).then((user) => {
+                count++
+                embed.addFields({ name: `${user.username}#${user.discriminator}`, value: `${getLocales(locale, 'LEVELSTOP_ENTRY', { LEVEL: row.memberLevel, XP: row.memberExperience })}` })
+                if (count === rows.length) {
+                  interaction.editReply({ embeds: [embed] })
+                }
+              })
+            })
+          } else {
+            genericMessages.error(interaction, locale, 'LEVELSTOP_NODATA')
+          };
+        }
+      })
+    } else {
+      genericMessages.error.noavaliable(interaction, locale)
+    }
+  },
   executeLegacy (client, locale, message) {
     if (message.database.levelsEnabled !== 0) {
       client.pool.query('SELECT * FROM `guildLevelsData` WHERE guild = ? ORDER BY memberLevel DESC, memberExperience DESC LIMIT 10', [message.guild.id], (err, rows, result) => {
