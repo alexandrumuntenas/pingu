25daf2cfbac335de6ecdcda54272bbd7461045ac
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-20 23:38:16 +0100

    Adaptar comando `farewell`.

--- commands/config/farewell.js
+++ commands/config/farewell.js
@@ -1,4 +1,5 @@
 const { Permissions, MessageEmbed } = require('discord.js')
+const { SlashCommandBuilder } = require('@discordjs/builders')
 const genericMessages = require('../../functions/genericMessages')
 const getLocales = require('../../i18n/getLocales')
 const guildMemberRemove = require('../../events/guildMemberRemove')
@@ -6,6 +7,60 @@ const guildMemberRemove = require('../../events/guildMemberRemove')
 module.exports = {
   cooldown: 0,
   name: 'farewell',
+  description: 'Configure the farewell settings for your server.',
+  data: new SlashCommandBuilder()
+    .setName('farewell')
+    .setDescription('Configure the farewell settings for your server.')
+    .addSubcommand(subcommand => subcommand.setName('viewconfig').setDescription('View the current farewell configuration'))
+    .addSubcommand(subcommand => subcommand.setName('channel').setDescription('Set the farewell channel').addChannelOption(option => option.setName('farewellchannel').setDescription('Select a channel')))
+    .addSubcommand(subcommand => subcommand.setName('message').setDescription('Set the farewell message'))
+    .addSubcommand(subcommand => subcommand.setName('simulate').setDescription('Simulate the farewell message')),
+  executeInteraction (client, locale, interaction) {
+    if (interaction.guild.ownerId === interaction.member.id || interaction.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
+      switch (interaction.options.getSubcommand()) {
+        case 'viewconfig': {
+          const sentEmbed = new MessageEmbed()
+            .setColor('BLURPLE')
+            .setTitle(getLocales(locale, 'FAREWELL_VIEWCONFIG_TITLE'))
+            .setDescription(getLocales(locale, 'FAREWELL_VIEWCONFIG_DESCRIPTION'))
+            .addField(`<:blurple_announcements:892441292909469726> ${getLocales(locale, 'WELCOMER_VIEWCONFIG_CHANNEL')}`, `${interaction.guild.channels.cache.find(c => c.id === interaction.database.farewellChannel) || getLocales(locale, 'WELCOMER_VIEWCONFIG_NOCHANNEL')}`, true)
+            .addField(`<:blurple_chat:892441341827616859> ${getLocales(locale, 'WELCOMER_VIEWCONFIG_MESSAGE')}`, `${interaction.database.farewellMessage || getLocales(locale, 'WELCOMER_VIEWCONFIG_NOMESSAGE')}`, true)
+
+          interaction.editReply({ embeds: [sentEmbed] })
+          break
+        }
+        case 'channel': {
+          if (interaction.options.getChannel('farewellchannel')) {
+            client.pool.query('UPDATE `guildData` SET `farewellChannel` = ? WHERE `guild` = ?', [interaction.options.getChannel('farewellchannel').id, interaction.guild.id], (err) => {
+              if (err) client.Sentry.captureException(err)
+              genericMessages.success(interaction, getLocales(locale, 'FAREWELL_CHANNEL_SUCCESS', { FAREWELL_CHANNEL: interaction.options.getChannel('farewellchannel') }))
+            })
+          } else {
+            genericMessages.info.status(interaction, getLocales(locale, 'FAREWELL_CHANNEL_MISSING_ARGS', { FAREWELL_CHANNEL: interaction.guild.channels.cache.find(c => c.id === interaction.database.farewellChannel) }))
+          }
+          break
+        }
+        case 'message': {
+          const filter = m => m.member.id === interaction.member.id
+          genericMessages.info.status(interaction, getLocales(locale, 'FAREWELL_MESSAGE_PREUPDATE'))
+          interaction.channel.awaitMessages({ filter, max: 1 }).then(collected => {
+            client.pool.query('UPDATE `guildData` SET `farewellMessage` = ? WHERE `guild` = ?', [collected.first().content, interaction.guild.id], (err) => {
+              if (err) client.Sentry.captureException(err)
+              genericMessages.success(interaction, getLocales(locale, 'FAREWELL_MESSAGE_SUCCESS', { FAREWELL_MESSAGE: `\`${collected.first().content}\`` }))
+            })
+          })
+          break
+        }
+        case 'simulate': {
+          genericMessages.info.status(interaction, getLocales(locale, 'FAREWELL_SIMULATE_SUCCESS'))
+          guildMemberRemove(client, interaction.member)
+          break
+        }
+      }
+    } else {
+      genericMessages.error.permissionerror(interaction, locale)
+    }
+  },
   executeLegacy (client, locale, message) {
     if (message.guild.ownerId === message.member.id || message.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
       if (message.args[0]) {
