5260ac22c5cb48a02c3ec091c39e27c8b19b93a2
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-20 21:26:47 +0100

    Adaptar comando `docs`.

--- commands/tools/docs.js
+++ commands/tools/docs.js
@@ -3,6 +3,11 @@ const { MessageEmbed } = require('discord.js')
 module.exports = {
   cooldown: 0,
   name: 'docs',
+  description: 'Get documentation of the bot',
+  executeInteraction (client, locale, interaction) {
+    const messageToSend = new MessageEmbed().setColor('#F8F9FA').setDescription(':point_right: https://pingu.duoestudios.com')
+    interaction.editReply({ embeds: [messageToSend] })
+  },
   executeLegacy (client, locale, message) {
     const messageToSend = new MessageEmbed().setColor('#F8F9FA').setDescription(':point_right: https://pingu.duoestudios.com')
     message.reply({ embeds: [messageToSend] })
