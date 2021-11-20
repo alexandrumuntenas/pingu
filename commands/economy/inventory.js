a2a489900f953a75200556deedeeabbe75eb05f6
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-20 22:06:16 +0100

    Adaptar comando `inventory` y solucionar problema en la que se edita el mensaje enriquecido por cada producto del inventario.

--- commands/economy/inventory.js
+++ commands/economy/inventory.js
@@ -6,12 +6,52 @@ const getLocales = require('../../i18n/getLocales')
 module.exports = {
   cooldown: 1000,
   name: 'inventory',
-  executeLegacy (client, locale, message) {
-    if (message.database.economyEnabled !== 0) {
-      message.channel.send(`<a:loader:871389840904695838> ${getLocales(locale, 'INVENTORY_PRELOADER')}`).then((inventoryMessage) => {
-        fetchInventory(client, message.member, message.guild, (inventory) => {
+  description: 'Check your inventory.',
+  executeInteraction (client, locale, interaction) {
+    if (interaction.database.economyEnabled !== 0) {
+      fetchInventory(client, interaction.member, interaction.guild, (inventory) => {
+        const inventoryEmbed = new MessageEmbed()
+          .setAuthor(interaction.member.displayName, interaction.user.displayAvatarURL())
+          .setTitle(getLocales(locale, 'INVENTORY_TITLE'))
+          .setColor('#633bdf')
+          .setFooter('Powered by Pingu', 'https://cdn.discordapp.com/attachments/907917245567598592/907917308620587059/Instagram_Profiles1.png')
+        if (inventory) {
+          let inventoryString = ''
+          const inventoryData = JSON.parse(inventory.data)
+          const inventoryDataProducts = Object.keys(inventoryData)
+          if (inventoryDataProducts.length > 0) {
+            try {
+              let inventoryDataProductsIndex = 0
+              inventoryDataProducts.forEach((productId) => {
+                const product = inventoryData[productId]
+                fetchShopProduct(client, interaction.guild, product.productId, (productData) => {
+                  inventoryDataProductsIndex++
+                  inventoryString += `${productData.productName} - ${productData.productId} (x${product.productQuantity})\n`
+                  if (inventoryDataProductsIndex === inventoryDataProducts.length) {
+                    inventoryEmbed.setDescription(inventoryString)
+                    interaction.editReply({ embeds: [inventoryEmbed] })
+                  }
+                })
+              })
+            } catch (error) {
+              client.Sentry.captureException(error)
+            }
+          } else {
+            inventoryEmbed.setDescription(getLocales(locale, 'INVENTORY_EMPTY')).setImage('https://cdn.discordapp.com/attachments/908413370665938975/908414689451597824/empty_inventory.png')
+            interaction.editReply({ content: 'Loaded!', embeds: [inventoryEmbed] })
+          }
+        }
+      })
+    } else {
+      genericMessages.error.noavaliable(interaction, locale)
+    }
+  },
+  executeLegacy (client, locale, interaction) {
+    if (interaction.database.economyEnabled !== 0) {
+      interaction.channel.send(`<a:loader:871389840904695838> ${getLocales(locale, 'INVENTORY_PRELOADER')}`).then((inventoryMessage) => {
+        fetchInventory(client, interaction.member, interaction.guild, (inventory) => {
           const inventoryEmbed = new MessageEmbed()
-            .setAuthor(message.member.displayName, message.author.displayAvatarURL())
+            .setAuthor(interaction.member.displayName, interaction.author.displayAvatarURL())
             .setTitle(getLocales(locale, 'INVENTORY_TITLE'))
             .setColor('#633bdf')
             .setFooter('Powered by Pingu', 'https://cdn.discordapp.com/attachments/907917245567598592/907917308620587059/Instagram_Profiles1.png')
@@ -23,9 +63,9 @@ module.exports = {
               try {
                 let inventoryDataProductsIndex = 0
                 inventoryDataProducts.forEach((productId) => {
-                  inventoryDataProductsIndex++
                   const product = inventoryData[productId]
-                  fetchShopProduct(client, message.guild, product.productId, (productData) => {
+                  fetchShopProduct(client, interaction.guild, product.productId, (productData) => {
+                    inventoryDataProductsIndex++
                     inventoryString += `${productData.productName} - ${productData.productId} (x${product.productQuantity})\n`
                     if (inventoryDataProductsIndex === inventoryDataProducts.length) {
                       inventoryEmbed.setDescription(inventoryString)
@@ -44,7 +84,7 @@ module.exports = {
         })
       })
     } else {
-      genericMessages.legacy.error.noavaliable(message, locale)
+      genericMessages.legacy.error.noavaliable(interaction, locale)
     }
   }
 }
