fb22079724d716c8972daa97c9c8e41464704fb8
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-20 21:19:41 +0100

    Comando ID: Añadir propiedad executeInteraction.
    A diferencia de executeLegacy, se ha empleado genericMessages para el mensaje respondido, simplificando el código y el funcionamiento.

--- commands/tools/id.js
+++ commands/tools/id.js
@@ -1,9 +1,13 @@
 const { MessageEmbed } = require('discord.js')
+const genericMessages = require('../../functions/genericMessages')
 const getLocales = require('../../i18n/getLocales')
 
 module.exports = {
   cooldown: 0,
   name: 'id',
+  executeInteraction (client, locale, interaction) {
+    genericMessages.info.status(interaction, getLocales(locale, 'ID', { ID: `\`${interaction.guild.id}\`` }))
+  },
   executeLegacy (client, locale, message) {
     const messageSent = new MessageEmbed().setColor('#3984BD').setDescription(`<:win_information:876119543968305233> ${getLocales(locale, 'ID', { ID: `\`${message.guild.id}\`` })}`)
     message.reply({ embeds: [messageSent] })
