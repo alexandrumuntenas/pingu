e7ab044d0ebe6234aa87538ed5757db0f0fd4d95
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-20 21:30:36 +0100

    Adaptar comando `about`.

--- commands/tools/about.js
+++ commands/tools/about.js
@@ -3,6 +3,30 @@ const { MessageEmbed } = require('discord.js')
 module.exports = {
   cooldown: 0,
   name: 'about',
+  description: 'About the bot',
+  executeInteraction (client, locale, interaction) {
+    const embed = new MessageEmbed()
+      .setColor('#FFFFFF')
+      .addFields(
+        {
+          name: '<:reddit_upvote:876106253355585627> Vote us in TopGG',
+          value: 'https://rebrand.ly/pinguTopGG'
+        },
+        {
+          name: ':tools: Report issues',
+          value: 'https://rebrand.ly/pinguIssues'
+        },
+        {
+          name: ':satellite: Add Pingu to a new guild',
+          value: 'https://rebrand.ly/pingu'
+        },
+        {
+          name: ':speech_balloon: Pingu\'s support server',
+          value: 'https://rebrand.ly/pinguSupport'
+        }
+      ).setFooter(`👪 ${client.guilds.cache.size} guilds`)
+    interaction.editReply({ embeds: [embed] })
+  },
   executeLegacy (client, locale, message) {
     const embed = new MessageEmbed()
       .setColor('#FFFFFF')
