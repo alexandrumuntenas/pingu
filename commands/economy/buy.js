06457ba068027ac5a98d223b1db78b219be7e56f
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-20 22:14:30 +0100

    Adaptar comando `buy`.

--- commands/economy/buy.js
+++ commands/economy/buy.js
@@ -1,3 +1,4 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
 const { buyItem, fetchShopProduct } = require('../../modules/economy')
 const genericMessages = require('../../functions/genericMessages')
 const getLocales = require('../../i18n/getLocales')
@@ -5,28 +6,55 @@ const getLocales = require('../../i18n/getLocales')
 module.exports = {
   cooldown: 5000,
   name: 'buy',
-  executeLegacy (client, locale, message) {
-    if (message.database.economyEnabled !== 0) {
-      if (message.args && Object.prototype.hasOwnProperty.call(message.args, 0)) {
-        const product = message.content.replace(`${message.database.guildPrefix}buy `, '').trim()
-        fetchShopProduct(client, message.guild, product, (productData) => {
+  description: 'Buy a shop product',
+  data: new SlashCommandBuilder()
+    .setName('buy').setDescription('Buy a shop product')
+    .addStringOption(option => option.setName('productname').setDescription('Enter the product name you want to buy')),
+  executeInteraction (client, locale, interaction) {
+    if (interaction.database.economyEnabled !== 0) {
+      if (interaction.options.getString('productname')) {
+        fetchShopProduct(client, interaction.guild, interaction.options.getString('productname'), (productData) => {
           if (productData) {
-            buyItem(client, message.member, message.guild, productData, (status) => {
+            buyItem(client, interaction.member, interaction.guild, productData, (status) => {
               if (status) {
-                genericMessages.legacy.success(message, getLocales(locale, 'BUYPRODUCT_SUCCESS', { PRODUCT_NAME: productData.productName }))
+                genericMessages.success(interaction, getLocales(locale, 'BUYPRODUCT_SUCCESS', { PRODUCT_NAME: productData.productName }))
               } else {
-                genericMessages.legacy.error(message, getLocales(locale, 'BUYPRODUCT_NOMONEY', { PRODUCT_NAME: productData.productName }))
+                genericMessages.error(interaction, getLocales(locale, 'BUYPRODUCT_NOMONEY', { PRODUCT_NAME: productData.productName }))
               }
             })
           } else {
-            genericMessages.legacy.error(message, getLocales(locale, 'BUYPRODUCT_NOTFOUND', { PRODUCT_NAME: product }))
+            genericMessages.error(interaction, getLocales(locale, 'BUYPRODUCT_NOTFOUND', { PRODUCT_NAME: interaction.options.getString('productname') }))
           }
         })
       } else {
-        client.commands.get('shop').executeLegacy(client, locale, message)
+        client.commands.get('shop').executeInteraction(client, locale, interaction)
       }
     } else {
-      genericMessages.legacy.error.noavaliable(message, locale)
+      genericMessages.error.noavaliable(interaction, locale)
+    }
+  },
+  executeLegacy (client, locale, interaction) {
+    if (interaction.database.economyEnabled !== 0) {
+      if (interaction.args && Object.prototype.hasOwnProperty.call(interaction.args, 0)) {
+        const product = interaction.content.replace(`${interaction.database.guildPrefix}buy `, '').trim()
+        fetchShopProduct(client, interaction.guild, product, (productData) => {
+          if (productData) {
+            buyItem(client, interaction.member, interaction.guild, productData, (status) => {
+              if (status) {
+                genericMessages.legacy.success(interaction, getLocales(locale, 'BUYPRODUCT_SUCCESS', { PRODUCT_NAME: productData.productName }))
+              } else {
+                genericMessages.legacy.error(interaction, getLocales(locale, 'BUYPRODUCT_NOMONEY', { PRODUCT_NAME: productData.productName }))
+              }
+            })
+          } else {
+            genericMessages.legacy.error(interaction, getLocales(locale, 'BUYPRODUCT_NOTFOUND', { PRODUCT_NAME: product }))
+          }
+        })
+      } else {
+        client.commands.get('shop').executeLegacy(client, locale, interaction)
+      }
+    } else {
+      genericMessages.legacy.error.noavaliable(interaction, locale)
     }
   }
 }
