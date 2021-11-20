e17c0318ab4e131fcc01055e121777fea348da7b
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-20 21:21:56 +0100

    Comando Ping: Crear su interacción.

--- commands/tools/ping.js
+++ commands/tools/ping.js
@@ -4,6 +4,10 @@ const Math = require('mathjs')
 module.exports = {
   cooldown: 0,
   name: 'ping',
+  executeInteraction (client, locale, interaction) {
+    const embed = new MessageEmbed().setColor('#9DF63F').setTitle('🏓 Pong!').setDescription(`🕑 Bot: **${Date.now() - interaction.createdTimestamp}ms** \n📨 API: **${Math.round(client.ws.ping)}ms**\n ⌛ Total: **${Math.round(client.ws.ping + (Date.now() - interaction.createdTimestamp))}ms**`).setTimestamp().setFooter('Hey!')
+    interaction.editReply({ embeds: [embed] })
+  },
   executeLegacy (client, locale, message) {
     const embed = new MessageEmbed().setColor('#9DF63F').setTitle('🏓 Pong!').setDescription(`🕑 Bot: **${Date.now() - message.createdTimestamp}ms** \n📨 API: **${Math.round(client.ws.ping)}ms**\n ⌛ Total: **${Math.round(client.ws.ping + (Date.now() - message.createdTimestamp))}ms**`).setTimestamp().setFooter('Hey!')
     message.channel.send({ embeds: [embed] })
