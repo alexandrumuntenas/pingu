6e2d85a8653a6dd97d445085e91e123ffb77b107
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-20 21:52:28 +0100

    Adaptar comando `level`

--- commands/leveling/level.js
+++ commands/leveling/level.js
@@ -7,6 +7,27 @@ const tempFileRemover = require('../../functions/tempFileRemover')
 module.exports = {
   cooldown: 10000,
   name: 'level',
+  description: 'Check your level',
+  executeInteraction (client, locale, interaction) {
+    if (interaction.database.levelsEnabled !== 0) {
+      fetchMember(client, interaction.member, (data) => {
+        if (data) {
+          interaction.member.levelData = data
+          interaction.member.tag = `${interaction.user.username}#${interaction.user.discriminator}`
+          rankCard(client, interaction.member, locale, interaction.database).then((paths) => {
+            const attachmentSent = new MessageAttachment(paths.attachmentSent)
+            interaction.editReply({ files: [attachmentSent] }).then(() => {
+              tempFileRemover(paths)
+            })
+          })
+        } else {
+          genericMessages.error(interaction, locale, 'RANK_NO_CLASSIFIED')
+        }
+      })
+    } else {
+      genericMessages.error.noavaliable(interaction, locale)
+    }
+  },
   executeLegacy (client, locale, message) {
     if (message.database.levelsEnabled !== 0) {
       fetchMember(client, message.member, (data) => {
