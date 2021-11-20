bac5389dd865665624bd51d5bc2ba6586e9ae9ad
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-20 21:36:02 +0100

    Adaptar comando `flip`.

--- commands/misc/flip.js
+++ commands/misc/flip.js
@@ -5,6 +5,16 @@ const getLocales = require('../../i18n/getLocales')
 module.exports = {
   cooldown: 0,
   name: 'flip',
+  description: 'Flip a coin',
+  executeInteraction (client, locale, interaction) {
+    const embed = new MessageEmbed().setColor('#007BFF')
+    if (flip() === 'head') {
+      embed.setDescription(`:coin: ${getLocales(locale, 'FLIP_HEAD')}`)
+    } else {
+      embed.setDescription(`:coin: ${getLocales(locale, 'FLIP_CROSS')}`)
+    }
+    interaction.editReply({ embeds: [embed] })
+  },
   executeLegacy (client, locale, message) {
     const embed = new MessageEmbed().setColor('#007BFF')
     if (flip() === 'head') {
