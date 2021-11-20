664b7d4d7dceacb59fe8255fd0e76815963bfe71
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-20 22:05:37 +0100

    Adaptar comando `daily`.

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -6,6 +6,21 @@ const getLocales = require('../../i18n/getLocales')
 module.exports = {
   cooldown: 86400000,
   name: 'daily',
+  description: 'Get your daily salary.',
+  executeInteraction (client, locale, interaction) {
+    if (interaction.database.economyEnabled !== 0) {
+      getMoney(client, interaction.member, interaction.guild, (money) => {
+        const inventoryEmbed = new MessageEmbed()
+          .setAuthor(interaction.member.displayName, interaction.user.displayAvatarURL())
+          .setColor('#633bdf')
+          .setDescription(getLocales(locale, 'DAILY', { DAILY: `${money} ${interaction.database.economyCurrencyIcon}` }))
+          .setFooter('Powered by Pingu', 'https://cdn.discordapp.com/attachments/907917245567598592/907917308620587059/Instagram_Profiles1.png')
+        interaction.editReply({ embeds: [inventoryEmbed] })
+      })
+    } else {
+      genericMessages.error.noavaliable(interaction, locale)
+    }
+  },
   executeLegacy (client, locale, message) {
     if (message.database.economyEnabled !== 0) {
       getMoney(client, message.member, message.guild, (money) => {
