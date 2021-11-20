160979aa75b8c61e936639d9fc670c5ff8c78097
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-20 23:08:15 +0100

    Adaptar comando `setlanguage`.

--- commands/config/setlanguage.js
+++ commands/config/setlanguage.js
@@ -1,3 +1,4 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
 const { Permissions } = require('discord.js')
 const genericMessages = require('../../functions/genericMessages')
 const getLocales = require('../../i18n/getLocales')
@@ -7,18 +8,39 @@ const avaliableLanguages = ['en', 'es']
 module.exports = {
   cooldown: 0,
   name: 'setlanguage',
-  executeLegacy (client, locale, message) {
-    if (message.guild.ownerId === message.author.id || message.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
-      if (message.args[0] && avaliableLanguages.includes(message.args[0])) {
-        client.pool.query('UPDATE `guildData` SET `guildLanguage` = ? WHERE `guild` = ?', [message.args[0], message.guild.id], (err) => {
+  description: 'Set the language for the bot',
+  data: new SlashCommandBuilder()
+    .setName('setlanguage')
+    .setDescription('Set the language for the bot')
+    .addStringOption(option =>
+      option.setName('language')
+        .setDescription('The language to set')
+        .setRequired(true)
+        .addChoice('Spanish', 'es')
+        .addChoice('English', 'en')
+        .setRequired(true)),
+  executeInteraction (client, locale, interaction) {
+    if (interaction.guild.ownerId === interaction.member.id || interaction.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
+      client.pool.query('UPDATE `guildData` SET `guildLanguage` = ? WHERE `guild` = ?', [interaction.options.getString('language'), interaction.guild.id], (err) => {
+        if (err) client.Sentry.captureException(err)
+      })
+      genericMessages.success(interaction, getLocales(interaction.options.getString('language'), 'SETLANGUAGE_SUCCESS', { guildLanguage: `\`${interaction.options.getString('language')}\`` }))
+    } else {
+      genericMessages.legacy.error.permissionerror(interaction, locale)
+    }
+  },
+  executeLegacy (client, locale, interaction) {
+    if (interaction.guild.ownerId === interaction.author.id || interaction.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
+      if (interaction.args[0] && avaliableLanguages.includes(interaction.args[0])) {
+        client.pool.query('UPDATE `guildData` SET `guildLanguage` = ? WHERE `guild` = ?', [interaction.args[0], interaction.guild.id], (err) => {
           if (err) client.Sentry.captureException(err)
         })
-        genericMessages.legacy.success(message, getLocales(message.args[0], 'SETLANGUAGE_SUCCESS', { guildLanguage: `\`${message.args[0]}\`` }))
+        genericMessages.legacy.success(interaction, getLocales(interaction.args[0], 'SETLANGUAGE_SUCCESS', { guildLanguage: `\`${interaction.args[0]}\`` }))
       } else {
-        genericMessages.legacy.Info.help(message, locale, `${message.database.guildPrefix}setlanguage <language>`, ['en', 'es'])
+        genericMessages.legacy.Info.help(interaction, locale, `${interaction.database.guildPrefix}setlanguage <language>`, ['en', 'es'])
       }
     } else {
-      genericMessages.legacy.error.permissionerror(message, locale)
+      genericMessages.legacy.error.permissionerror(interaction, locale)
     }
   }
 }
