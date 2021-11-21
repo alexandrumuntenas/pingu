625807d46605ea3dd2d77aed664b6043d4a2e1f4
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-21 12:23:50 +0100

    Adaptar comandos `p2dismod` y  `p2enmod`.

--- commands/config/p2enmod.js
+++ commands/config/p2enmod.js
@@ -1,10 +1,75 @@
 const { Permissions } = require('discord.js')
+const { SlashCommandBuilder } = require('@discordjs/builders')
 const genericMessages = require('../../functions/genericMessages')
 const getLocales = require('../../i18n/getLocales')
 
 module.exports = {
   cooldown: 0,
   name: 'p2enmod',
+  description: 'Enable Pingu modules',
+  data: new SlashCommandBuilder()
+    .setName('p2enmod')
+    .setDescription('Enable Pingu modules')
+    .addStringOption(option => option.setName('module').setDescription('The module to enable')),
+  executeInteraction (client, locale, interaction) {
+    if (interaction.guild.ownerId === interaction.member.id || interaction.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
+      if (interaction.options.getString('module')) {
+        switch (interaction.options.getString('module')) {
+          case 'welcomer': {
+            client.pool.query('UPDATE `guildData` SET `welcomeEnabled` = 1 WHERE `guild` = ?', [interaction.guild.id], (err) => {
+              if (err) client.Sentry.captureException(err)
+            })
+            break
+          }
+          case 'joinroles': {
+            client.pool.query('UPDATE `guildData` SET `joinRolesEnabled` = 1 WHERE `guild` = ?', [interaction.guild.id], (err) => {
+              if (err) client.Sentry.captureException(err)
+            })
+            break
+          }
+          case 'farewell': {
+            client.pool.query('UPDATE `guildData` SET `farewellEnabled` = 1 WHERE `guild` = ?', [interaction.guild.id], (err) => {
+              if (err) client.Sentry.captureException(err)
+            })
+            break
+          }
+          case 'moderation': {
+            client.pool.query('UPDATE `guildData` SET `moderationEnabled` = 1 WHERE `guild` = ?', [interaction.guild.id], (err) => {
+              if (err) client.Sentry.captureException(err)
+            })
+            break
+          }
+          case 'levels': {
+            client.pool.query('UPDATE `guildData` SET `levelsEnabled` = 1 WHERE `guild` = ?', [interaction.guild.id], (err) => {
+              if (err) client.Sentry.captureException(err)
+            })
+            break
+          }
+          case 'economy': {
+            client.pool.query('UPDATE `guildData` SET `economyEnabled` = 1 WHERE `guild` = ?', [interaction.guild.id], (err) => {
+              if (err) client.Sentry.captureException(err)
+            })
+            break
+          }
+          case 'autoresponder': {
+            client.pool.query('UPDATE `guildData` SET `autoresponderEnabled` = 1 WHERE `guild` = ?', [interaction.guild.id], (err) => {
+              if (err) client.Sentry.captureException(err)
+            })
+            break
+          }
+          default: {
+            helpInteraction(interaction, locale)
+            return
+          }
+        }
+        genericMessages.success(interaction, getLocales(locale, 'P2ENMOD', { PMODULE: `\`${interaction.options.getString('module')}\`` }))
+      } else {
+        helpInteraction(interaction, locale)
+      }
+    } else {
+      genericMessages.error.permissionerror(interaction, locale)
+    }
+  },
   executeLegacy (client, locale, message) {
     if (message.guild.ownerId === message.member.id || message.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
       if (message.args[0]) {
@@ -68,4 +133,8 @@ module.exports = {
 
 function helpTray (message, locale) {
   genericMessages.legacy.Info.help(message, locale, `${message.database.guildPrefix}p2enmod <module>`, ['welcomer', 'joinroles', 'farewell', 'moderation', 'levels', 'economy', 'autoresponder'])
+}
+
+function helpInteraction (message, locale) {
+  genericMessages.info.help(message, locale, '/p2dismod <module>', ['welcomer', 'joinroles', 'farewell', 'moderation', 'levels', 'economy', 'autoresponder'])
 }
