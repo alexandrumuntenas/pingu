77c211bb8aa5ce82dc5c6413e8eedfa57c43eb53
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-11-24 12:02:38 +0100

    Adaptar comando `embed`.

--- commands/utils/embed.js
+++ commands/utils/embed.js
@@ -1,135 +1,162 @@
 const { MessageEmbed, Permissions } = require('discord.js')
-const genericMessages = require('../../functions/genericMessages')
+const { SlashCommandBuilder } = require('@discordjs/builders')
 const getLocales = require('../../i18n/getLocales')
+
 module.exports = {
-  cooldown: 0,
   name: 'embed',
-  executeLegacy (client, locale, message, result) {
-    if (message.member.permissions.has([Permissions.FLAGS.MANAGE_MESSAGES, Permissions.FLAGS.KICK_MEMBERS, Permissions.FLAGS.BAN_MEMBERS]) || message.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
-      message.channel.send(`<a:loader:871389840904695838> ${getLocales(locale, 'EMBED_PRELOADER')}`).then((embedMenu) => {
-        const embedCreated = new MessageEmbed()
-        const filter = m => m.member.id === message.member.id
-        embedCreated.setAuthor(message.author.username, message.author.avatarURL())
-        embedCreated.setTimestamp()
-        embedCreated.setFooter('Powered by Pingu')
-        embedCreated.setColor('#000000'.replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16) }))
-        let addfields = 0
-        function indice () {
-          embedMenu.edit({ content: `${getLocales(locale, 'EMBED_MENU_WELCOME')} \n\n ****${getLocales(locale, 'EMBED_MENU_AVALIABLE_OPTIONS')}** \n **1.**${getLocales(locale, 'EMBED_MENU_OPTIONS_1')}\n **2.**${getLocales(locale, 'EMBED_MENU_OPTIONS_2')}\n **3.**${getLocales(locale, 'EMBED_MENU_OPTIONS_3')}\n **4.**${getLocales(locale, 'EMBED_MENU_OPTIONS_4')}\n **5.**${getLocales(locale, 'EMBED_MENU_OPTIONS_5')}\n **6.**${getLocales(locale, 'EMBED_MENU_OPTIONS_6')}\n **7.**${getLocales(locale, 'EMBED_MENU_OPTIONS_7')}\n **8.**${getLocales(locale, 'EMBED_MENU_OPTIONS_8')}`, embeds: [embedCreated] })
-          message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
-            switch (collected.first().content) {
-              case '1':
-                collected.first().delete().then(() => editarTitulo())
-                break
-              case '2':
-                collected.first().delete().then(() => editarDescripcion())
-                break
-              case '3':
-                collected.first().delete().then(() => editarMiniatura())
-                break
-              case '4':
-                collected.first().delete().then(() => addField())
-                break
-              case '5':
-                collected.first().delete().then(() => editarImagen())
-                break
-              case '6':
-                collected.first().delete().then(() => editarColorBorde())
-                break
-              case '7':
-                collected.first().delete().then(() => enviar())
-                break
-              case '8':
-                collected.first().delete().then(() => embedMenu.edit({ content: '<:win_information:876119543968305233> Se ha descartado el mensaje enriquecido.' }))
-                break
-              default:
-                collected.first().delete().then(() => indice())
-                break
-            }
-          })
-        }
+  description: '📝 Create an embed message',
+  permissions: [Permissions.FLAGS.MANAGE_MESSAGES, Permissions.FLAGS.KICK_MEMBERS, Permissions.FLAGS.BAN_MEMBERS],
+  cooldown: 0,
+  ephemeral: true,
+  interactionData: new SlashCommandBuilder()
+    .setName('embed')
+    .setDescription('📝 Create an embed message')
+    .addStringOption(option => option.setName('title').setDescription('Sets the embed title. (256 characters max)').setRequired(true))
+    .addStringOption(option => option.setName('description').setDescription('Sets the embed description. (4096 characters max)').setRequired(true))
+    .addStringOption(option => option.setName('footer').setDescription('Sets the embed footer. (2048 characters max)'))
+    .addStringOption(option => option.setName('thumbnail').setDescription('Adds an image/gif to the upper-right corner of your embed. (Use a direct media link.)'))
+    .addStringOption(option => option.setName('image').setDescription('Adds an image/gif to the bottom of your embed. (Use a direct media link.)')),
+  executeInteraction (client, locale, interaction) {
+    const title = interaction.options.getString('title')
+    const description = interaction.options.getString('description')
+    const footer = interaction.options.getString('footer')
+    const thumbnail = interaction.options.getString('thumbnail')
+    const image = interaction.options.getString('image')
 
-        function editarTitulo () {
-          embedMenu.edit({ content: `:arrow_right: ${getLocales(locale, 'EMBED_CHANGETITLE')}` })
-          message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
-            embedCreated.setTitle(collected.first().content)
+    const embed = new MessageEmbed()
+      .setAuthor(interaction.user.username, interaction.user.avatarURL())
+      .setTitle(title)
+      .setDescription(description)
+      .setColor('#000000'.replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16) }))
+    if (footer) embed.setFooter(footer)
+    if (thumbnail) embed.setThumbnail(thumbnail)
+    if (image) embed.setImage(image)
+    interaction.editReply({ embeds: [embed] })
 
-            collected.first().delete().then(() => indice())
-          })
-        }
-
-        function editarDescripcion () {
-          embedMenu.edit({ content: `:arrow_right: ${getLocales(locale, 'EMBED_CHANGEBODY')}` })
-          message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
-            embedCreated.setDescription(collected.first().content)
-            collected.first().delete().then(() => indice())
-          })
-        }
-
-        function addField () {
-          ++addfields
-          if (addfields === 0 || addfields <= 25) {
-            embedMenu.edit({ content: `:arrow_right: ${getLocales(locale, 'EMBED_ADDFIELD_INSERTTITLE', { ADDFIELD_FIELD: addfields })}` })
-            message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
-              const titulo = collected.first().content
-              collected.first().delete()
-              embedMenu.edit({ content: `:arrow_right: ${getLocales(locale, 'EMBED_ADDFIELD_INSERTBODY', { ADDFIELD_FIELD: addfields })}` })
-              message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
-                const descrip = collected.first().content
-                embedCreated.addField(titulo, descrip)
-                collected.first().delete().then(() => indice())
-              })
-            })
-          } else {
-            embedMenu.edit({ content: `<:pingu_cross:876104109256769546> ${getLocales(locale, 'EMBED_ADDFIELD_NOMOREFIELDS')}` })
+    interaction.followUp({ embeds: [embed] })
+  },
+  executeLegacy (client, locale, message) {
+    message.channel.send(`<a:loader:871389840904695838> ${getLocales(locale, 'EMBED_PRELOADER')}`).then((embedMenu) => {
+      const embedCreated = new MessageEmbed()
+      const filter = m => m.member.id === message.member.id
+      embedCreated.setAuthor(message.author.username, message.author.avatarURL())
+      embedCreated.setTimestamp()
+      embedCreated.setFooter('Powered by Pingu')
+      embedCreated.setColor('#000000'.replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16) }))
+      let addfields = 0
+      function indice () {
+        embedMenu.edit({ content: `${getLocales(locale, 'EMBED_MENU_WELCOME')} \n\n ****${getLocales(locale, 'EMBED_MENU_AVALIABLE_OPTIONS')}** \n **1.**${getLocales(locale, 'EMBED_MENU_OPTIONS_1')}\n **2.**${getLocales(locale, 'EMBED_MENU_OPTIONS_2')}\n **3.**${getLocales(locale, 'EMBED_MENU_OPTIONS_3')}\n **4.**${getLocales(locale, 'EMBED_MENU_OPTIONS_4')}\n **5.**${getLocales(locale, 'EMBED_MENU_OPTIONS_5')}\n **6.**${getLocales(locale, 'EMBED_MENU_OPTIONS_6')}\n **7.**${getLocales(locale, 'EMBED_MENU_OPTIONS_7')}\n **8.**${getLocales(locale, 'EMBED_MENU_OPTIONS_8')}`, embeds: [embedCreated] })
+        message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
+          switch (collected.first().content) {
+            case '1':
+              collected.first().delete().then(() => editarTitulo())
+              break
+            case '2':
+              collected.first().delete().then(() => editarDescripcion())
+              break
+            case '3':
+              collected.first().delete().then(() => editarMiniatura())
+              break
+            case '4':
+              collected.first().delete().then(() => addField())
+              break
+            case '5':
+              collected.first().delete().then(() => editarImagen())
+              break
+            case '6':
+              collected.first().delete().then(() => editarColorBorde())
+              break
+            case '7':
+              collected.first().delete().then(() => enviar())
+              break
+            case '8':
+              collected.first().delete().then(() => embedMenu.edit({ content: '<:win_information:876119543968305233> Se ha descartado el mensaje enriquecido.' }))
+              break
+            default:
+              collected.first().delete().then(() => indice())
+              break
           }
-        }
+        })
+      }
 
-        function editarImagen () {
-          embedMenu.edit({ content: `:arrow_right: ${getLocales(locale, 'EMBED_EDITIMAGE')}` })
-          message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
-            embedCreated.setImage(collected.first().content.content)
-            collected.first().delete().then(() => indice())
-          })
-        }
+      function editarTitulo () {
+        embedMenu.edit({ content: `:arrow_right: ${getLocales(locale, 'EMBED_CHANGETITLE')}` })
+        message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
+          embedCreated.setTitle(collected.first().content)
 
-        function editarMiniatura () {
-          embedMenu.edit({ content: `:arrow_right: ${getLocales(locale, 'EMBED_EDITTHUMBNAIL')}` })
-          message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
-            embedCreated.setThumbnail(collected.first().content.content)
-            collected.first().delete().then(() => indice())
-          })
-        }
+          collected.first().delete().then(() => indice())
+        })
+      }
 
-        function editarColorBorde () {
-          embedMenu.edit({
-            content: `:arrow_right: ${getLocales(locale, 'EMBED_EDITCOLOR')}`
-          })
-          message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
-            embedCreated.setColor(collected.first().content.content)
-            collected.first().delete().then(() => indice())
-          })
-        }
+      function editarDescripcion () {
+        embedMenu.edit({ content: `:arrow_right: ${getLocales(locale, 'EMBED_CHANGEBODY')}` })
+        message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
+          embedCreated.setDescription(collected.first().content)
+          collected.first().delete().then(() => indice())
+        })
+      }
 
-        function enviar () {
-          embedMenu.edit({
-            content: `:arrow_right: ${getLocales(locale, 'EMBED_SEND')}`
-          })
+      function addField () {
+        ++addfields
+        if (addfields === 0 || addfields <= 25) {
+          embedMenu.edit({ content: `:arrow_right: ${getLocales(locale, 'EMBED_ADDFIELD_INSERTTITLE', { ADDFIELD_FIELD: addfields })}` })
           message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
-            const canal = collected.first().mentions.channels.first()
-            const mensaje = client.channels.cache.find(channel => channel.id === canal.id)
-            mensaje.send({ embeds: [embedCreated] })
-            embedMenu.edit({
-              content: `<:pingu_check:876104161794596964> ${getLocales(locale, 'EMBED_SENT_SUCCESS', { EMBED_CHANNEL: canal })}`, embeds: [embedCreated]
-            })
+            const titulo = collected.first().content
             collected.first().delete()
+            embedMenu.edit({ content: `:arrow_right: ${getLocales(locale, 'EMBED_ADDFIELD_INSERTBODY', { ADDFIELD_FIELD: addfields })}` })
+            message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
+              const descrip = collected.first().content
+              embedCreated.addField(titulo, descrip)
+              collected.first().delete().then(() => indice())
+            })
           })
+        } else {
+          embedMenu.edit({ content: `<:pingu_cross:876104109256769546> ${getLocales(locale, 'EMBED_ADDFIELD_NOMOREFIELDS')}` })
         }
+      }
 
-        indice()
-      })
-    } else {
-      genericMessages.legacy.error.permissionerror(message, locale)
-    }
+      function editarImagen () {
+        embedMenu.edit({ content: `:arrow_right: ${getLocales(locale, 'EMBED_EDITIMAGE')}` })
+        message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
+          embedCreated.setImage(collected.first().content.content)
+          collected.first().delete().then(() => indice())
+        })
+      }
+
+      function editarMiniatura () {
+        embedMenu.edit({ content: `:arrow_right: ${getLocales(locale, 'EMBED_EDITTHUMBNAIL')}` })
+        message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
+          embedCreated.setThumbnail(collected.first().content.content)
+          collected.first().delete().then(() => indice())
+        })
+      }
+
+      function editarColorBorde () {
+        embedMenu.edit({
+          content: `:arrow_right: ${getLocales(locale, 'EMBED_EDITCOLOR')}`
+        })
+        message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
+          embedCreated.setColor(collected.first().content.content)
+          collected.first().delete().then(() => indice())
+        })
+      }
+
+      function enviar () {
+        embedMenu.edit({
+          content: `:arrow_right: ${getLocales(locale, 'EMBED_SEND')}`
+        })
+        message.channel.awaitMessages({ filter, max: 1 }).then(collected => {
+          const canal = collected.first().mentions.channels.first()
+          const mensaje = client.channels.cache.find(channel => channel.id === canal.id)
+          mensaje.send({ embeds: [embedCreated] })
+          embedMenu.edit({
+            content: `<:pingu_check:876104161794596964> ${getLocales(locale, 'EMBED_SENT_SUCCESS', { EMBED_CHANNEL: canal })}`, embeds: [embedCreated]
+          })
+          collected.first().delete()
+        })
+      }
+
+      indice()
+    })
   }
 }
