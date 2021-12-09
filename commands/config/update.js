639f7a422d598d7c00fe42b94e0b8820d2395318
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-12-09 19:18:47 +0100

    Comando Update: Mostrar ID de servidor y no nombre.

--- commands/config/update.js
+++ commands/config/update.js
@@ -15,12 +15,12 @@ module.exports = {
   description: '⚙️ Deploys and updates the Pingu\'s Slash Commands of all the servers.',
   permissions: [Permissions.FLAGS.MANAGE_GUILD],
   cooldown: 0,
-  executeInteraction(client, locale, interaction) {
+  executeInteraction (client, locale, interaction) {
     if (interaction.member.id === '722810818823192629') {
       genericMessages.info.loader(interaction, 'Deploying commands...')
       client.guilds.cache.forEach(async (guild) => {
         rest.put(Routes.applicationGuildCommands(client.user.id, guild.id), { body: client.interactions.map(command => command.toJSON()) })
-          .then(() => client.log.success(`Commands deployed to ${guild.name}`))
+          .then(() => client.log.success(`Commands deployed to ${guild.id}`))
           .catch(console.error)
       })
       genericMessages.success(interaction, 'Successfully deployed commands!')
