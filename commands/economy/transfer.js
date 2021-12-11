935a82a1a7e349040396a4df161d5b29faafc16c
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-12-11 23:24:03 +0100

    Economía: Comprobar si el módulo está activo antes de ejecutar el comando tranfer.

--- commands/economy/transfer.js
+++ commands/economy/transfer.js
@@ -15,22 +15,26 @@ module.exports = {
     .addUserOption(option => option.setName('user').setDescription('The user to send money to.').setRequired(true))
     .addNumberOption(option => option.setName('amount').setDescription('The amount of money to send.').setRequired(true)),
   executeInteraction (client, locale, interaction) {
-    const userToSendMoney = interaction.options.getUser('user')
-    const amount = interaction.options.getNumber('amount')
-    if (!userToSendMoney.bot) {
-      if (parseInt(amount) > 0) {
-        makeMoneyTransferToUser(client, interaction.guild, interaction.member, userToSendMoney, amount, (status) => {
-          if (status) {
-            genericMessages.success(interaction, getLocales(locale, 'TRANSFER_SUCCESS', { USER: userToSendMoney }))
-          } else {
-            genericMessages.error(interaction, getLocales(locale, 'TRANSFER_NOENOUGHMONEY'))
-          }
-        })
+    if (interaction.database.economyEnabled !== 0) {
+      const userToSendMoney = interaction.options.getUser('user')
+      const amount = interaction.options.getNumber('amount')
+      if (!userToSendMoney.bot) {
+        if (parseInt(amount) > 0) {
+          makeMoneyTransferToUser(client, interaction.guild, interaction.member, userToSendMoney, amount, (status) => {
+            if (status) {
+              genericMessages.success(interaction, getLocales(locale, 'TRANSFER_SUCCESS', { USER: userToSendMoney }))
+            } else {
+              genericMessages.error(interaction, getLocales(locale, 'TRANSFER_NOENOUGHMONEY'))
+            }
+          })
+        } else {
+          genericMessages.error(interaction, getLocales(locale, 'TRANSFER_INVALIDAMOUNT'))
+        }
       } else {
-        genericMessages.error(interaction, getLocales(locale, 'TRANSFER_INVALIDAMOUNT'))
+        genericMessages.error(interaction, getLocales(locale, 'TRANSFER_ISBOT'))
       }
     } else {
-      genericMessages.error(interaction, getLocales(locale, 'TRANSFER_ISBOT'))
+      genericMessages.error.noavaliable(interaction, locale)
     }
   }
 }
