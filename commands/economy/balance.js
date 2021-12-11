3a91a6276164f713bfa2cbda6e46cb0498fc8cc4
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-12-11 23:08:25 +0100

    Economía: Retirar versión legacy del comando `balance`.

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -20,19 +20,5 @@ module.exports = {
     } else {
       genericMessages.error.noavaliable(interaction, locale)
     }
-  },
-  executeLegacy (client, locale, message) {
-    if (message.database.economyEnabled !== 0) {
-      fetchUserAccount(client, message.member, message.guild, (user) => {
-        const firstMessageSent = new MessageEmbed()
-          .setAuthor(message.member.displayName, message.author.displayAvatarURL())
-          .setColor('#009FE3')
-          .setDescription(`${user.amount || 0} ${message.database.economyCurrency} ${message.database.economyCurrencyIcon}`)
-
-        message.reply({ embeds: [firstMessageSent] })
-      })
-    } else {
-      genericMessages.legacy.error.noavaliable(message, locale)
-    }
   }
 }
