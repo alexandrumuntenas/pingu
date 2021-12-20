993ee59d198d7a28cab2dd72dcffffe3ace85943
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-12-20 19:54:45 +0100

    MessageBuilder: Crear nuevos métodos para satisfacer las necesidades del bot. Crear métodos Status, Loader, Success y Error.

--- modules/constructor/messageBuilder.js
+++ modules/constructor/messageBuilder.js
@@ -1,82 +1,28 @@
 const { MessageEmbed } = require('discord.js')
-const humanizeduration = require('humanize-duration')
 const getLocales = require('../../i18n/getLocales')
 
-module.exports.legacy = {}
-
-module.exports.legacy.Info = {}
-
-module.exports.legacy.Info.help = (message, locale, syntax, options, nsfw, nsfwOptions) => {
-  const sentEmbed = new MessageEmbed().setTitle('Help Tray')
-  if (syntax) {
-    sentEmbed.addField(getLocales(locale, 'COMMAND_HELP_SYNTAX'), `\`${syntax}\``)
-  }
-
-  if (options) {
-    let avaliableOptions = ''
-    options.forEach((object) => {
-      avaliableOptions = avaliableOptions + ` \`${object.option || object}\``
-    })
-    sentEmbed.addField(getLocales(locale, 'COMMAND_HELP_SUBCATEGORIES'), avaliableOptions)
-  }
-
-  if (!nsfw) {
-    sentEmbed.setColor('BLURPLE')
-  } else {
-    sentEmbed.setColor('#B23CFD')
-    if (nsfwOptions) {
-      let avaliableOptions = ''
-      nsfwOptions.forEach((object) => {
-        avaliableOptions = avaliableOptions + ` \`${object.option || object}\``
-      })
-      sentEmbed.addField(getLocales(locale, 'COMMAND_HELP_NSFW_OPTIONS'), avaliableOptions)
-    }
-  }
-
-  sentEmbed.setFooter(`<> => ${getLocales(locale, 'PARAMETER_NECESSARY')} | () => ${getLocales(locale, 'PAREMETER_OPTIONAL')}`)
-  message.reply({ embeds: [sentEmbed] })
+module.exports.Status = (message) => {
+  return new MessageEmbed()
+    .setColor('#2F3136')
+    .setDescription(`<:pingu_null:876103457860370442> ${message}`)
 }
 
-module.exports.legacy.Info.status = (message, text) => {
-  const sentEmbed = new MessageEmbed()
-    .setColor('BLURPLE')
-    .setDescription(`<:pingu_null:876103457860370442> ${text}`)
-  message.reply({ embeds: [sentEmbed] })
+module.exports.Loader = (message) => {
+  return new MessageEmbed()
+    .setColor('#2F3136')
+    .setDescription(`<a:loader:871389840904695838> ${message}`)
 }
 
-module.exports.legacy.success = (message, text) => {
-  const sentEmbed = new MessageEmbed()
-    .setColor('GREEN')
-    .setDescription(`<:pingu_on:876103503561502730> ${text}`)
-  message.reply({ embeds: [sentEmbed] })
+module.exports.Success = (message) => {
+  return new MessageEmbed()
+    .setColor('#2F3136')
+    .setDescription(`<:pingu_on:876103503561502730> ${message}`)
 }
 
-module.exports.legacy.error = (message, text) => {
-  const sent = new MessageEmbed()
-    .setColor('RED')
-    .setDescription(`<:pingu_false:876103413526564924> ${text}`)
-  message.reply({ embeds: [sent] })
-}
-
-module.exports.legacy.error.cooldown = (message, locale, cooldown) => {
-  const sent = new MessageEmbed()
-    .setColor('RED')
-    .setDescription(`<:pingu_false:876103413526564924> ${getLocales(locale, 'COOLDOWN', { COOLDOWN: humanizeduration(cooldown, { round: true, language: locale, fallbacks: ['en'] }) })}`)
-  message.reply({ embeds: [sent] })
-}
-
-module.exports.legacy.error.noavaliable = (message, locale) => {
-  const sent = new MessageEmbed()
-    .setColor('RED')
-    .setDescription(`<:pingu_false:876103413526564924> ${getLocales(locale, 'COMMAND_NO_AVALIABLE')}`)
-  message.reply({ embeds: [sent] })
-}
-
-module.exports.legacy.error.permissionerror = (message, locale) => {
-  const sent = new MessageEmbed()
-    .setColor('RED')
-    .setDescription(`<:pingu_false:876103413526564924> ${getLocales(locale, 'COMMAND_PERMISSION_ERROR')}`)
-  message.reply({ embeds: [sent] })
+module.exports.Error = (message) => {
+  return new MessageEmbed()
+    .setColor('#2F3136')
+    .setDescription(`<:pingu_false:876103413526564924> ${message}`)
 }
 
 module.exports.info = {}
@@ -110,53 +56,4 @@ module.exports.info.help = (interaction, locale, syntax, options, nsfw, nsfwOpti
 
   sentEmbed.setFooter(`<> => ${getLocales(locale, 'PARAMETER_NECESSARY')} | () => ${getLocales(locale, 'PAREMETER_OPTIONAL')}`)
   interaction.editReply({ embeds: [sentEmbed] })
-}
-
-module.exports.info.status = (interaction, text) => {
-  const sentEmbed = new MessageEmbed()
-    .setColor('#2F3136')
-    .setDescription(`<:pingu_null:876103457860370442> ${text}`)
-  interaction.editReply({ embeds: [sentEmbed] })
-}
-
-module.exports.info.loader = (interaction, text) => {
-  const sentEmbed = new MessageEmbed()
-    .setColor('#2F3136')
-    .setDescription(`<a:loader:871389840904695838> ${text}`)
-  interaction.editReply({ embeds: [sentEmbed] })
-}
-
-module.exports.success = (interaction, text) => {
-  const sentEmbed = new MessageEmbed()
-    .setColor('#2F3136')
-    .setDescription(`<:pingu_on:876103503561502730> ${text}`)
-  interaction.editReply({ embeds: [sentEmbed] })
-}
-
-module.exports.error = (interaction, text) => {
-  const sent = new MessageEmbed()
-    .setColor('#2F3136')
-    .setDescription(`<:pingu_false:876103413526564924> ${text}`)
-  interaction.editReply({ embeds: [sent] })
-}
-
-module.exports.error.cooldown = (interaction, locale, cooldown) => {
-  const sent = new MessageEmbed()
-    .setColor('#2F3136')
-    .setDescription(`<:pingu_false:876103413526564924> ${getLocales(locale, 'COOLDOWN', { COOLDOWN: humanizeduration(cooldown, { round: true, language: locale, fallbacks: ['en'] }) })}`)
-  interaction.editReply({ embeds: [sent] })
-}
-
-module.exports.error.noavaliable = (interaction, locale) => {
-  const sent = new MessageEmbed()
-    .setColor('#2F3136')
-    .setDescription(`<:pingu_false:876103413526564924> ${getLocales(locale, 'COMMAND_NO_AVALIABLE')}`)
-  interaction.editReply({ embeds: [sent] })
-}
-
-module.exports.error.permissionerror = (interaction, locale) => {
-  const sent = new MessageEmbed()
-    .setColor('#2F3136')
-    .setDescription(`<:pingu_false:876103413526564924> ${getLocales(locale, 'COMMAND_PERMISSION_ERROR')}`)
-  interaction.editReply({ embeds: [sent] })
 }
