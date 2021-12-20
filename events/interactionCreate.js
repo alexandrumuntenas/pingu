e61701a743a07bbad4a0f24aa2eb2ad759a07a4e
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-12-20 20:07:04 +0100

    EVENTO interactionCreate: Adaptar el envío de los mensajes para emplear los nuevos métodos de messageBuilder.
    En #142 es mencionado la necesidad de que cada función gestione de manera independiente el envío del mensaje.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,8 @@
 const { cooldown } = require('../functions/commands')
-const messageBuilder = require('../modules/constructor/messageBuilder')
+const { Error } = require('../modules/constructor/messageBuilder')
 const guildFetchData = require('../functions/guildFetchData')
+const getLocales = require('../i18n/getLocales')
+const humanizeduration = require('humanize-duration')
 
 module.exports.isCommand = async (client, interaction) => {
   const { commandName } = interaction
@@ -14,7 +16,7 @@ module.exports.isCommand = async (client, interaction) => {
     if (client.commands.has(commandName)) {
       const commandToExecute = client.commands.get(commandName)
       if (commandToExecute.permissions && !interaction.member.permissions.has(commandToExecute.permissions)) {
-        messageBuilder.error.permissionerror(interaction, interaction.database.guildLanguage || 'en')
+        interaction.editReply({ embeds: [Error(getLocales(interaction.database.guildLanguage || 'en', 'COMMAND_PERMISSION_ERROR'))] })
         return
       }
       if (cooldown.check(interaction.member, interaction.guild, commandToExecute)) {
@@ -22,7 +24,7 @@ module.exports.isCommand = async (client, interaction) => {
         if (client.statcord) client.statcord.postCommand(commandToExecute.name, '000000000000000')
         await commandToExecute.executeInteraction(client, interaction.database.guildLanguage || 'en', interaction)
       } else {
-        messageBuilder.error.cooldown(interaction, interaction.database.guildLanguage || 'en', (parseInt(cooldown.ttl(interaction.member, interaction.guild, commandToExecute)) - Date.now()))
+        interaction.editReply({ embeds: [Error(getLocales(interaction.database.guildLanguage || 'en', 'COOLDOWN', { COOLDOWN: humanizeduration(cooldown, { round: true, language: interaction.database.guildLanguage || 'en', fallbacks: ['en'] }) }))] })
       }
     } else {
       interaction.editReply({ content: 'This command is not longer working on Pingu. To remove this command from the list, please redeploy the commands using `deploy`.' })
