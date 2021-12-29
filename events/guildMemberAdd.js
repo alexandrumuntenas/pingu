19eab6fc39ae1099f851f6fd09034d48fe50db16
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-12-29 14:38:02 +0100

    Adapt events to work with the event handler.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,40 +4,43 @@ const { welcomeCard } = require('../modules/canvasProcessing')
 const guildFetchData = require('../functions/guildFetchData')
 const { fetchJoinRoles } = require('../modules/joinroles')
 
-module.exports = async (client, member) => {
-  const gMA = client.Sentry.startTransaction({
-    op: 'guildMemberAdd',
-    name: 'Guild Member Add'
-  })
-  guildFetchData(client, member.guild, (data) => {
-    if (data) {
-      if (data.welcomeEnabled !== 0) {
-        const mensaje = client.channels.cache.get(data.welcomeChannel)
-        if (mensaje) {
-          if (data.welcomeImage !== 0) {
-            welcomeCard(client, member, data.guildLanguage || 'en', data).then((paths) => {
-              const attachmentSent = new MessageAttachment(paths.attachmentSent)
-              mensaje.send({ content: data.welcomeMessage.replace('{member}', `<@${member.user.id}>`).replace('{guild}', `${member.guild.name}`), files: [attachmentSent] }).then(() => {
-                tempFileRemover(paths)
+module.exports = {
+  name: 'guildMemberAdd',
+  execute: async (client, member) => {
+    const gMA = client.Sentry.startTransaction({
+      op: 'guildMemberAdd',
+      name: 'Guild Member Add'
+    })
+    guildFetchData(client, member.guild, (data) => {
+      if (data) {
+        if (data.welcomeEnabled !== 0) {
+          const mensaje = client.channels.cache.get(data.welcomeChannel)
+          if (mensaje) {
+            if (data.welcomeImage !== 0) {
+              welcomeCard(client, member, data.guildLanguage || 'en', data).then((paths) => {
+                const attachmentSent = new MessageAttachment(paths.attachmentSent)
+                mensaje.send({ content: data.welcomeMessage.replace('{member}', `<@${member.user.id}>`).replace('{guild}', `${member.guild.name}`), files: [attachmentSent] }).then(() => {
+                  tempFileRemover(paths)
+                })
               })
-            })
-          } else {
-            mensaje.send({ content: data.welcomeMessage.replace('{member}', `<@${member.user.id}>`).replace('{guild}', `${member.guild.name}`) })
-          }
-        }
-      }
-      if (data.joinRolesEnabled !== 0) {
-        fetchJoinRoles(client, member.guild, (roles) => {
-          if (roles && Array.isArray(roles)) {
-            if (roles.length > 0) {
-              roles.forEach(role => {
-                member.roles.add(member.guild.roles.resolveId(role.roleID))
-              })
+            } else {
+              mensaje.send({ content: data.welcomeMessage.replace('{member}', `<@${member.user.id}>`).replace('{guild}', `${member.guild.name}`) })
             }
           }
-        })
+        }
+        if (data.joinRolesEnabled !== 0) {
+          fetchJoinRoles(client, member.guild, (roles) => {
+            if (roles && Array.isArray(roles)) {
+              if (roles.length > 0) {
+                roles.forEach(role => {
+                  member.roles.add(member.guild.roles.resolveId(role.roleID))
+                })
+              }
+            }
+          })
+        }
       }
-    }
-  })
-  gMA.finish()
+    })
+    gMA.finish()
+  }
 }
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,21 +1,24 @@
 const guildFetchData = require('../functions/guildFetchData')
 
-module.exports = async (client, member) => {
-  const gMR = client.Sentry.startTransaction({
-    op: 'guildMemberRemove',
-    name: 'Guild Member Remove'
-  })
-  if (member.user.id !== client.user.id) {
-    guildFetchData(client, member.guild, (data) => {
-      if (data.farewellEnabled !== 0) {
-        const mensaje = client.channels.cache.find(channel => channel.id === data.farewellChannel)
-        if (mensaje) {
-          mensaje.send(data.farewellMessage.replace('{member}', `${member.user.tag}`).replace('{guild}', `${member.guild.name}`))
-        }
-      }
+module.exports = {
+  name: 'guildMemberRemove',
+  execute: async (client, member) => {
+    const gMR = client.Sentry.startTransaction({
+      op: 'guildMemberRemove',
+      name: 'Guild Member Remove'
     })
-    client.pool.query('DELETE FROM `guildLevelsData` WHERE member = ? AND guild = ?', [member.user.id, member.guild.id])
-    client.pool.query('DELETE FROM `guildEconomyUserBank` WHERE member = ? AND guild = ?', [member.user.id, member.guild.id])
+    if (member.user.id !== client.user.id) {
+      guildFetchData(client, member.guild, (data) => {
+        if (data.farewellEnabled !== 0) {
+          const mensaje = client.channels.cache.find(channel => channel.id === data.farewellChannel)
+          if (mensaje) {
+            mensaje.send(data.farewellMessage.replace('{member}', `${member.user.tag}`).replace('{guild}', `${member.guild.name}`))
+          }
+        }
+      })
+      client.pool.query('DELETE FROM `guildLevelsData` WHERE member = ? AND guild = ?', [member.user.id, member.guild.id])
+      client.pool.query('DELETE FROM `guildEconomyUserBank` WHERE member = ? AND guild = ?', [member.user.id, member.guild.id])
+    }
+    gMR.finish()
   }
-  gMR.finish()
 }
