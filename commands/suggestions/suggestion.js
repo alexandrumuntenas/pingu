0d04122e51e2caaf7beeab50a0a93af0b7fff8af
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-12-30 14:14:28 +0100

    Crear comando suggestion para gestionar las sugerencias.

---
+++ commands/suggestions/suggestion.js
@@ -0,0 +1,45 @@
+const { Permissions, MessageEmbed } = require('discord.js')
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { Error, Success } = require('../../modules/constructor/messageBuilder')
+const { approveSuggestion, rejectSuggestion } = require('../../modules/suggestions')
+const i18n = require('../../i18n/i18n')
+
+module.exports = {
+  name: 'suggestion',
+  description: 'Command to handle suggestions',
+  permissions: [Permissions.FLAGS.MANAGE_GUILD],
+  cooldown: 0,
+  interactionData: new SlashCommandBuilder().setName('suggestion').setDescription('Command to handle suggestions')
+    .addSubcommand(sc => sc.setName('approve').setDescription('Approve a suggestion.').addStringOption(option => option.setName('id').setDescription('ID of the suggestion to approve.').setRequired(true)))
+    .addSubcommand(sc => sc.setName('reject').setDescription('Reject a suggestion.').addStringOption(option => option.setName('id').setDescription('ID of the suggestion to reject.').setRequired(true)).addStringOption(option => option.setName('reason').setDescription('Reason for rejection.'))),
+  executeInteraction: async (client, locale, interaction) => {
+    switch (interaction.options.getSubcommand()) {
+      case 'approve': {
+        approveSuggestion(client, { suggestionId: interaction.options.getString('id'), suggestionGuild: interaction.guild.id, suggestionSupervisor: interaction.member.id }, (status) => {
+          if (status === 500) return interaction.editReply({ embeds: [Error(i18n(locale, 'ERROR'))] })
+          if (status === 404) return interaction.editReply({ embeds: [Error(i18n(locale, 'SUGGESTIONS_APPROVE_NOEXIST', { SUGGESTION_ID: interaction.options.getString('id') }))] })
+          const suggestionAuthor = interaction.guild.members.resolve(status.suggestionAuthor)
+          const suggestionEmbed = new MessageEmbed().addField(i18n(locale, 'SUGGESTION_SUBMITTER'), `${suggestionAuthor.user.tag || 'Somebody :eyes:'} (${status.suggestionAuthor})`).addField(i18n(locale, 'SUGGESTION_SUGGESTION'), status.suggestionContent).setThumbnail(interaction.member.displayAvatarURL()).setColor('GREEN').setFooter(`${i18n(locale, 'SUGGESTION_STATUS')}: ${i18n(locale, 'SUGGESTION_APPROVED')}\n${i18n(locale, 'SUGGESTION_REVISOR')}: ${interaction.member.user.tag} (${interaction.member.id})\nSuggestion ID: ${status.suggestionId}\nServer ID: ${interaction.guild.id}\nDate`).setTimestamp()
+
+          if (interaction.database.suggestionsRevChannel && interaction.guild.channels.cache.get(interaction.database.suggestionsRevChannel)) interaction.guild.channels.cache.get(interaction.database.suggestionsRevChannel).send({ embeds: [suggestionEmbed] })
+          else interaction.guild.channels.cache.get(interaction.database.suggestionsChannel).messages.fetch(status.suggestionMessageId).then(message => message.edit({ embeds: [suggestionEmbed] }))
+          return interaction.editReply({ embeds: [Success(i18n(locale, 'SUGGESTIONS_APPROVE_SUCCESS', { SUGGESTION_ID: interaction.options.getString('id') }))] })
+        })
+        break
+      }
+      case 'reject': {
+        rejectSuggestion(client, { suggestionId: interaction.options.getString('id'), suggestionGuild: interaction.guild.id, suggestionSupervisor: interaction.member.id }, (status) => {
+          if (status === 500) return interaction.editReply({ embeds: [Error(i18n(locale, 'ERROR'))] })
+          if (status === 404) return interaction.editReply({ embeds: [Error(i18n(locale, 'SUGGESTIONS_REJECT_NOEXIST', { SUGGESTION_ID: interaction.options.getString('id') }))] })
+          const suggestionAuthor = interaction.guild.members.resolve(status.suggestionAuthor)
+          const suggestionEmbed = new MessageEmbed().addField(i18n(locale, 'SUGGESTION_SUBMITTER'), `${suggestionAuthor.user.tag || 'Somebody :eyes:'} (${status.suggestionAuthor})`).addField(i18n(locale, 'SUGGESTION_SUGGESTION'), status.suggestionContent).setThumbnail(interaction.member.displayAvatarURL()).setColor('RED').setFooter(`${i18n(locale, 'SUGGESTION_STATUS')}: ${i18n(locale, 'SUGGESTION_REJECTED')}\n${i18n(locale, 'SUGGESTION_REVISOR')}: ${interaction.member.user.tag} (${interaction.member.id})\nSuggestion ID: ${status.suggestionId}\nServer ID: ${interaction.guild.id}\nDate`).setTimestamp()
+
+          if (interaction.database.suggestionsRevChannel && interaction.guild.channels.cache.get(interaction.database.suggestionsRevChannel)) interaction.guild.channels.cache.get(interaction.database.suggestionsRevChannel).send({ embeds: [suggestionEmbed] })
+          else interaction.guild.channels.cache.get(interaction.database.suggestionsChannel).messages.fetch(status.suggestionMessageId).then(message => message.edit({ embeds: [suggestionEmbed] }))
+          return interaction.editReply({ embeds: [Success(i18n(locale, 'SUGGESTIONS_REJECT_SUCCESS', { SUGGESTION_ID: interaction.options.getString('id') }))] })
+        })
+        break
+      }
+    }
+  }
+}
