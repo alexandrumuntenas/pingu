4e91ff6638537081f16058a5db6de7aa9bf1f3be
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-12-30 14:13:55 +0100

    El Id enviado a createSuggestion no era el mismo que el reportado en el mensaje de confirmación

--- commands/suggestions/suggest.js
+++ commands/suggestions/suggest.js
@@ -19,7 +19,7 @@ module.exports = {
       const suggestionEmbed = new MessageEmbed().addField(i18n(locale, 'SUGGESTION_SUBMITTER'), `${interaction.member.user.tag} (${interaction.member.id})`).addField(i18n(locale, 'SUGGESTION_SUGGESTION'), suggestion).setThumbnail(interaction.member.displayAvatarURL()).setColor('#2F3136').setFooter(`${i18n(locale, 'SUGGESTION_STATUS')}: ${i18n(locale, 'SUGGESTION_PENDING')}\nSuggestion ID: ${suggestionId}\nServer ID: ${interaction.guild.id}\nDate`).setTimestamp()
       await interaction.guild.channels.fetch(interaction.database.suggestionsChannel).then(channel => {
         channel.send({ embeds: [suggestionEmbed] }).then((_reply) => {
-          createSuggestion(client, { suggestionId: makeId(5), suggestionContent: suggestion, suggestionAuthor: interaction.member.id, suggestionGuild: interaction.guild.id, suggestionMessage: _reply.id }, (status) => {
+          createSuggestion(client, { suggestionId: suggestionId, suggestionContent: suggestion, suggestionAuthor: interaction.member.id, suggestionGuild: interaction.guild.id, suggestionMessage: _reply.id }, (status) => {
             if (status === 500) return interaction.editReply({ embeds: [Error(i18n(locale, 'SUGGESTION_CREATION_ERROR'))] })
             return interaction.editReply({ embeds: [Success(i18n(locale, 'SUGGESTION_CREATED', { SUGGESTION_ID: suggestionId }))] })
           })
