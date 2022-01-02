fdb17664db95faf60646247e72eacab98a68b422
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-01-02 16:51:57 +0100

    Actualizar traducciones suggest, suggestion, suggestions.

--- commands/tools/server.js
+++ commands/tools/server.js
@@ -3,15 +3,15 @@ const i18n = require('../../i18n/i18n')
 const unixTime = require('unix-time')
 
 module.exports = {
-  name: 'server-info',
+  name: 'server',
   description: '👑 Shows information about the server',
   cooldown: 0,
   executeInteraction (client, locale, interaction) {
     const embed = new MessageEmbed()
-      .setTitle(i18n(locale, 'SERVER_INFO_EMBED_TITLE'))
+      .setTitle(i18n(locale, 'SERVER::EMBED:TITLE'))
       .setColor('#FFFFFF')
       .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
-      .setDescription(`:credit_card: ${i18n(locale, 'SERVER_INFO_EMBED_GUILD_NAME', { GUILDNAME: interaction.guild.name })}\n:calendar: ${i18n(locale, 'SERVER_INFO_EMBED_GUILD_CREATED_TIMESTAMP', { GUILDCREATEDTIMESTAMP: `<t:${unixTime(interaction.guild.createdTimestamp)}>` })}\n:crown: ${i18n(locale, 'SERVER_INFO_EMBED_GUILD_OWNER', { USER: client.users.cache.get(interaction.guild.ownerId).tag })}\n<a:nitro_boost:868214436178046976> ${i18n(locale, 'SERVER_INFO_EMBED_GUILD_BOOST', { GUILDBOOST: `${interaction.guild.premiumTier ? `Tier ${interaction.guild.premiumTier}` : i18n(locale, 'SERVER_INFO_BOOST_NONE')} (${interaction.guild.premiumSubscriptionCount || '0'})` })}`)
+      .setDescription(`:medal: **${i18n(locale, 'SERVER::EMBED:SERVERNAME')}**: ${interaction.guild.name}\n:calendar: **${i18n(locale, 'SERVER::EMBED:SERVERCREATIONDATE')}**: <t:${unixTime(interaction.guild.createdTimestamp)}>\n:crown: **${i18n(locale, 'SERVER::EMBED:SERVEROWNER')}**: ${client.users.cache.get(interaction.guild.ownerId).tag}\n<a:nitro:927222194034053161> **${i18n(locale, 'SERVER::EMBED:NITROSTATUS')}**: ${interaction.guild.premiumTier} (${interaction.guild.premiumSubscriptionCount || '0'})`)
       .setFooter('Powered by Pingu', 'https://cdn.discordapp.com/attachments/907917245567598592/907917308620587059/Instagram_Profiles1.png')
       .setTimestamp()
     interaction.editReply({ embeds: [embed] })
