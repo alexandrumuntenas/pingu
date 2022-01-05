25ecb1920e9807ddb0b743311b74d67cf14a9e69
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-01-05 21:18:11 +0100

    Reorganización del código: Separar del módulo canvasProcessing la generación de las tarjetas de rango e integrarlo directamente en el módulo de levels. Se ha resuelto el problema por el cual la barra de rango salía de los límites establecidos.

--- commands/leveling/rank.js
+++ commands/leveling/rank.js
@@ -1,7 +1,6 @@
 const { Error } = require('../../modules/constructor/messageBuilder')
-const { fetchMember } = require('../../modules/levels')
+const { getMember, generateRankCard } = require('../../modules/levels')
 const { MessageAttachment } = require('discord.js')
-const { rankCard } = require('../../modules/canvasProcessing')
 const tempFileRemover = require('../../functions/tempFileRemover')
 
 module.exports = {
@@ -11,11 +10,11 @@ module.exports = {
   description: '⭐ Check your level',
   executeInteraction (client, locale, interaction) {
     if (interaction.database.levelsEnabled !== 0) {
-      fetchMember(client, interaction.member, (data) => {
+      getMember(client, interaction.member, (data) => {
         if (data) {
           interaction.member.levelData = data
           interaction.member.tag = `${interaction.user.username}#${interaction.user.discriminator}`
-          rankCard(client, interaction.member, locale, interaction.database).then((paths) => {
+          generateRankCard(client, interaction.member, locale, interaction.database).then((paths) => {
             const attachmentSent = new MessageAttachment(paths.attachmentSent)
             interaction.editReply({ files: [attachmentSent] }).then(() => {
               tempFileRemover(paths)
@@ -31,11 +30,11 @@ module.exports = {
   },
   executeLegacy (client, locale, message) {
     if (message.database.levelsEnabled !== 0) {
-      fetchMember(client, message.member, (data) => {
+      getMember(client, message.member, (data) => {
         if (data) {
           message.member.levelData = data
           message.member.tag = message.author.tag
-          rankCard(client, message.member, locale, message.database).then((paths) => {
+          generateRankCard(client, message.member, locale, message.database).then((paths) => {
             const attachmentSent = new MessageAttachment(paths.attachmentSent)
             message.channel.send({ files: [attachmentSent] }).then(() => {
               tempFileRemover(paths)
--- modules/levels.js
+++ modules/levels.js
@@ -1,10 +1,20 @@
 const talkedRecently = new Set()
+const { registerFont, createCanvas, loadImage } = require('canvas')
+const { writeFileSync } = require('fs')
+const StackBlur = require('stackblur-canvas')
+const randomstring = require('randomstring')
+const isValidUrl = require('is-valid-http-url')
+const isImageUrl = require('is-image-url')
+const { millify } = require('millify')
+const hexToRgba = require('hex-to-rgba')
+
+registerFont('./modules/sources/fonts/Montserrat/Montserrat-SemiBold.ttf', { family: 'Montserrat' })
 
 module.exports = {
-  fetchMember: (client, member, callback) => {
+  getMember: (client, member, callback) => {
     const lfM = client.Sentry.startTransaction({
-      op: 'levels.fetchMember',
-      name: 'levels (Fetch Member)'
+      op: 'levels.getMember',
+      name: 'levels (Get Member)'
     })
     client.pool.query('SELECT * FROM `guildLevelsData` WHERE guild = ? AND member = ?', [member.guild.id, member.id], (err, result) => {
       if (err) client.logError(err)
@@ -21,7 +31,7 @@ module.exports = {
                 client.logError(err)
                 client.log.error(err)
               }
-              module.exports.fetchMember(client, member, callback)
+              module.exports.getMember(client, member, callback)
             })
           }
         })
@@ -31,7 +41,7 @@ module.exports = {
             client.logError(err)
             client.log.error(err)
           }
-          module.exports.fetchMember(client, member, callback)
+          module.exports.getMember(client, member, callback)
         })
       }
     })
@@ -65,7 +75,7 @@ module.exports = {
         setTimeout(() => {
           talkedRecently.delete(`${message.member.id}_${message.guild.id}`)
         }, 60000)
-        module.exports.fetchMember(client, message.member, (userData) => {
+        module.exports.getMember(client, message.member, (userData) => {
           if (userData) {
             let exp = parseInt(userData.memberExperience) + Math.round(Math.random() * (25 - 15) + 15)
             let niv = parseInt(userData.memberLevel)
@@ -91,5 +101,105 @@ module.exports = {
       }
     }
     lRU.finish()
+  },
+  generateRankCard: async (client, member, locale, database) => {
+    const uniqueIdentifiers = {
+      userAvatar: randomstring.generate({ charset: 'alphabetic' }),
+      attachmentSent: randomstring.generate({ charset: 'alphabetic' })
+    }
+
+    const paths = {
+      attachmentSent: `./modules/temp/${uniqueIdentifiers.attachmentSent}.png`
+    }
+
+    const canvas = createCanvas(1100, 320)
+    const ctx = canvas.getContext('2d')
+
+    // Establecer fondo del canvas
+    let imgPath = ''
+    if (database.levelsImageCustomBackground && isValidUrl(database.levelsImageCustomBackground) && isImageUrl(database.levelsImageCustomBackground)) {
+      imgPath = database.levelsImageCustomBackground
+      const background = await loadImage(imgPath)
+      const scale = Math.max(canvas.width / background.width, canvas.height / background.height)
+      ctx.drawImage(background, (canvas.width / 2) - (background.width / 2) * scale, (canvas.height / 2) - (background.height / 2) * scale, background.width * scale, background.height * scale)
+
+      // Establecer blured overlay
+      ctx.fillStyle = hexToRgba(database.levelsImageCustomOverlayColor || '#272934', (database.levelsImageCustomOpacity / 100))
+      ctx.fillRect(25, 25, 1050, 270)
+      StackBlur.canvasRGBA(canvas, 25, 25, 1050, 270, database.levelsImageCustomBlur)
+      /* Next Release: 22T2
+      ctx.fillRect(25, 25, 1050, 250)
+      StackBlur.canvasRGBA(canvas, 25, 25, 1050, 250, database.levelsImageCustomBlur)
+      */
+    } else {
+      ctx.fillStyle = '#272934'
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
+    }
+
+    // Escribir usuario
+    ctx.font = applyText(canvas, member.tag, 40)
+    ctx.textAlign = 'left'
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.8)'
+    ctx.fillText(`${member.tag}`, 295, 180, 500)
+
+    // Escribir nivel, experiencia y rango
+
+    ctx.font = '50px "Montserrat SemiBold"'
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.5)'
+    ctx.textAlign = 'right'
+    ctx.fillText(`Rank #${member.levelData.rank}  Level ${millify(member.levelData.memberLevel)}`, 1050, 100)
+
+    // Escribir progreso actual (actual/necesario)
+    const actualVSrequired = `${millify(member.levelData.memberExperience)} / ${millify(((member.levelData.memberLevel * member.levelData.memberLevel) * database.levelsDifficulty) * 100)} XP`
+
+    ctx.font = '30px "Montserrat SemiBold"'
+    ctx.textAlign = 'right'
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.8)'
+    ctx.fillText(actualVSrequired, 1050, 180)
+
+    // Añadir barra de progreso (backdrop)
+
+    ctx.fillStyle = 'rgba(255,255,255, 0.3)'
+    ctx.fillRect(295, 200, 755, 70)
+
+    // Añadir barra de progreso
+
+    ctx.fillStyle = 'rgb(255,255,255)'
+    ctx.fillRect(295, 200, (Math.abs((member.levelData.memberExperience) / (((member.levelData.memberLevel * member.levelData.memberLevel) * database.levelsDifficulty) * 100)) * 555), 70)
+
+    // Escribir progreso actual (porcentaje)
+
+    /* Next Release: 22T2
+    ctx.fillStyle = 'rgba(255,255,255, 0.4)'
+    ctx.fillRect(0, 300, 1100, 20)
+
+    ctx.fillStyle = 'rgb(255,255,255)'
+    ctx.fillRect(0, 300, (Math.abs((member.levelData.memberExperience) / (((member.levelData.memberLevel * member.levelData.memberLevel) * database.levelsDifficulty) * 100)) * 1100), 20)
+
+    */
+
+    // Añadir avatar de usuario
+
+    const avatar = await loadImage(member.user.displayAvatarURL({ format: 'png', size: 512 }))
+    ctx.drawImage(avatar, 50, 50, 220, 220)
+    /* Next Release: 22T2
+    ctx.drawImage(avatar, 50, 50, 200, 200)
+    */
+
+    const buffer = canvas.toBuffer('image/png')
+    writeFileSync(paths.attachmentSent, buffer)
+
+    return paths
   }
+}
+
+const applyText = (canvas, text, maxlimit) => {
+  const ctx = canvas.getContext('2d')
+  let fontSize = maxlimit || 100
+
+  do {
+    ctx.font = `${fontSize -= 1}px "Montserrat SemiBold"`
+  } while (ctx.measureText(text).width > canvas.width - 125)
+
+  return ctx.font
 }
