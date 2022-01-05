fb7cebc9fb43de23ec2492a627f5f575aeeb72f0
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-01-05 19:25:56 +0100

    Actualizar código: En help se ha actualizado las propiedades pasadas a MessageEmbed.setAuthor().

--- commands/utils/help.js
+++ commands/utils/help.js
@@ -5,7 +5,7 @@ module.exports = {
   description: 'Feeling lost? This command will help you out!',
   permissions: [],
   executeInteraction (client, locale, interaction) {
-    const helpMessage = new MessageEmbed().setAuthor(client.user.username, client.user.displayAvatarURL())
+    const helpMessage = new MessageEmbed().setAuthor({ name: client.user.username, iconUrl: client.user.displayAvatarURL() })
       .setColor('#2F3136')
       .addField('Help Us Grow', '[Vote and leave a rating in Top.GG](https://top.gg/bot/827199539185975417/)')
       .addField('Links', '[Docs](https://alexandrumuntenas.github.io/pingu/ ) • [Support Server](https://discord.com/invite/q55kCfekyy) • [Invite](https://discord.com/oauth2/authorize?client_id=827199539185975417&permissions=1933044831&scope=bot%20applications.commands) • [Source Code](https://github.com/alexandrumuntenas/pingu)')
@@ -13,7 +13,7 @@ module.exports = {
     interaction.editReply({ embeds: [helpMessage] })
   },
   executeLegacy (client, locale, message) {
-    const helpMessage = new MessageEmbed().setAuthor(client.user.username, client.user.displayAvatarURL())
+    const helpMessage = new MessageEmbed().setAuthor({ name: client.user.username, iconUrl: client.user.displayAvatarURL() })
       .setColor('#2F3136')
       .addField('Help Us Grow', '[Vote and leave a rating in Top.GG](https://top.gg/bot/827199539185975417/)')
       .addField('Links', '[Docs](https://alexandrumuntenas.github.io/pingu/) • [Support Server](https://discord.com/invite/q55kCfekyy) • [Invite](https://discord.com/oauth2/authorize?client_id=827199539185975417&permissions=1933044831&scope=bot%20applications.commands) • [Source Code](https://github.com/alexandrumuntenas/pingu)')
