360f66b4ee6d176f93aa8284cf1a0080482a9b6e
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-01-07 00:15:48 +0100

    MessageCreate: Eliminar la comprobación de la funcionalidad de los comandos legacy en un servidor. Activados en todos los servidores de manera predeterminada.

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,4 +1,4 @@
-const { Status, Error, Timer } = require('../modules/constructor/messageBuilder')
+const { Error, Timer } = require('../modules/constructor/messageBuilder')
 const i18n = require('../i18n/i18n')
 const autoresponder = require('../modules/autoresponder')
 const getGuildConfig = require('../functions/getGuildConfig')
@@ -24,26 +24,22 @@ module.exports = {
         message.args.shift()
 
         if (client.commands.has(commandToExecute)) {
-          if (message.database.legacyCMD !== 0) {
-            commandToExecute = client.commands.get(commandToExecute)
-            if (commandToExecute.permissions && !message.member.permissions.has(commandToExecute.permissions)) {
-              message.reply({ embeds: [Error(i18n(message.database.guildLanguage || 'en', 'COMMAND::PERMERROR'))] })
-              return
-            }
-            if (client.cooldownManager.check(message.member, message.guild, commandToExecute)) {
-              client.cooldownManager.add(message.member, message.guild, commandToExecute)
-              if (Object.prototype.hasOwnProperty.call(commandToExecute, 'executeLegacy')) {
-                if (client.statcord) client.statcord.postCommand(commandToExecute.name, message.member.id)
-                await commandToExecute.executeLegacy(client, message.database.guildLanguage || 'en', message)
-              } else {
-                message.reply({ embeds: [Error(i18n(message.database.guildLanguage || 'en', 'COMMAND::LEGACYNOAVALIABLE'))] })
-              }
+          commandToExecute = client.commands.get(commandToExecute)
+          if (commandToExecute.permissions && !message.member.permissions.has(commandToExecute.permissions)) {
+            message.reply({ embeds: [Error(i18n(message.database.guildLanguage || 'en', 'COMMAND::PERMERROR'))] })
+            return
+          }
+          if (client.cooldownManager.check(message.member, message.guild, commandToExecute)) {
+            client.cooldownManager.add(message.member, message.guild, commandToExecute)
+            if (Object.prototype.hasOwnProperty.call(commandToExecute, 'executeLegacy')) {
+              if (client.statcord) client.statcord.postCommand(commandToExecute.name, message.member.id)
+              await commandToExecute.executeLegacy(client, message.database.guildLanguage || 'en', message)
             } else {
-              message.reply({ embeds: [Timer(i18n(message.database.guildLanguage || 'en', 'COOLDOWN', { COOLDOWN: humanizeduration(client.cooldownManager.ttl(message.member, message.guild, commandToExecute), { round: true, language: message.database.guildLanguage || 'en', fallbacks: ['en'] }) }))] })
-              return
+              message.reply({ embeds: [Error(i18n(message.database.guildLanguage || 'en', 'COMMAND::LEGACYNOAVALIABLE'))] })
             }
           } else {
-            message.reply({ embeds: [Status(i18n(message.database.guildLanguage || 'en', 'LEGACY_DISABLED'))] })
+            message.reply({ embeds: [Timer(i18n(message.database.guildLanguage || 'en', 'COOLDOWN', { COOLDOWN: humanizeduration(client.cooldownManager.ttl(message.member, message.guild, commandToExecute), { round: true, language: message.database.guildLanguage || 'en', fallbacks: ['en'] }) }))] })
+            return
           }
         } else {
           if (client.cooldownManager.check(message.member, message.guild, commandToExecute)) {
