7fd55d4c2608a3cf177d474dd8f9b8b3761d793a
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-01-06 21:14:31 +0100

    Nacimiento del cooldown manager. Creación de cooldowns persistentes.

---
+++ modules/cooldownManager.js
@@ -0,0 +1,53 @@
+const fs = require('fs')
+const console = require('./console')
+
+let cooldownJson
+
+if (fs.existsSync('./cooldowns.json')) {
+  cooldownJson = require('../cooldowns.json')
+} else {
+  fs.writeFile('./cooldowns.json', '{}', (err) => {
+    if (err) console.log(err)
+    console.debug('CooldownManager: Cooldowns file has been created.')
+    cooldownJson = require('../cooldowns.json')
+  })
+}
+
+const cooldown = Object.assign({}, cooldownJson)
+
+module.exports = {}
+
+module.exports.add = (member, guild, command) => {
+  cooldown[`${command.name}${member.id}${guild.id}`] = (Date.now() + (parseInt(command.cooldown) || 10000))
+  setTimeout(() => {
+    delete cooldown[`${command.name}${member.id}${guild.id}`]
+  }, command.cooldown || 10000)
+}
+
+module.exports.check = (member, guild, command) => {
+  if (cooldown[`${command.name}${member.id}${guild.id}`]) return false
+  return true
+}
+
+module.exports.ttl = (member, guild, command) => {
+  return (cooldown[`${command.name}${member.id}${guild.id}`] - Date.now())
+}
+
+module.exports.saveCooldownCollectionIntoJsonFile = () => {
+  fs.writeFile('./cooldowns.json', JSON.stringify(cooldown), (err) => {
+    if (err) throw err
+    console.debug('CooldownManager: Cooldowns have been saved.')
+  })
+}
+
+setInterval(async () => {
+  module.exports.saveCooldownCollectionIntoJsonFile()
+}, 30000)
+
+process.on('unhandledRejection', () => {
+  module.exports.saveCooldownCollectionIntoJsonFile()
+  process.exit(1)
+}).on('uncaughtException', () => {
+  module.exports.saveCooldownCollectionIntoJsonFile()
+  process.exit(1)
+})
