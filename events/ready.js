769ea60d0ce36a1640fd560ddd1e13548a51cf47
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-01-08 22:28:23 +0100

    Simplificar comando update. Crear función generateTheCommandListOfTheGuild y añadir la actualización de los comandos Slash al evento ready.

--- events/ready.js
+++ events/ready.js
@@ -1,21 +1,35 @@
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v9')
+const getGuildConfig = require('../functions/getGuildConfig')
+const generateTheCommandListOfTheGuild = require('../functions/generateTheCommandListOfTheGuild')
+
+const rest = new REST({ version: '9' })
+if (process.env.ENTORNO === 'desarrollo') {
+  rest.setToken(process.env.INSIDER_TOKEN)
+} else {
+  rest.setToken(process.env.PUBLIC_TOKEN)
+}
+
 module.exports = {
   name: 'ready',
-  execute: async (client) => {
+  execute: (client) => {
     client.console.info(`Conectado como ${client.user.tag}!`)
     if (client.statcord) client.statcord.autopost()
 
     // TODO: Autoupdate the guilds slash commands.
-    /* setInterval(() => {
-      const { REST } = require('@discordjs/rest')
-      const { Routes } = require('discord-api-types/v9')
-      const getGuildConfig = require('../functions/getGuildConfig')
-
-      const rest = new REST({ version: '9' })
-      if (process.env.ENTORNO === 'desarrollo') {
-        rest.setToken(process.env.INSIDER_TOKEN)
-      } else {
-        rest.setToken(process.env.PUBLIC_TOKEN)
-      }
-    }, 86400000) */
+    setInterval(() => {
+      client.guilds.fetch().forEach(guild => {
+        client.console.info(`Deploying commands to ${guild.id}`)
+        getGuildConfig(client, guild, (guildConfig) => {
+          generateTheCommandListOfTheGuild(client, guildConfig, (commandListOfTheGuild) => {
+            rest.put(Routes.applicationGuildCommands(client.user.id, guild.id), { body: commandListOfTheGuild })
+              .then(() => {
+                client.console.success(`Commands deployed succesfully to ${guild.id}`)
+              })
+              .catch(console.error)
+          })
+        })
+      })
+    }, 86400000)
   }
 }
