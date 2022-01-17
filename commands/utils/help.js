9a8d1a50633c1fd38081a05bb319d5b51ab69043
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-01-17 23:09:43 +0100

    Mostrar en el comando help en el footer el avatar del bot.

--- commands/utils/help.js
+++ commands/utils/help.js
@@ -5,19 +5,19 @@ module.exports = {
   description: 'Feeling lost? This command will help you out!',
   permissions: [],
   executeInteraction (client, locale, interaction) {
-    const helpMessage = new MessageEmbed().setAuthor({ name: client.user.username, iconUrl: client.user.displayAvatarURL() })
+    const helpMessage = new MessageEmbed()
       .setColor('#2F3136')
       .addField('Help Us Grow', '[Vote and leave a rating in Top.GG](https://top.gg/bot/827199539185975417/)')
       .addField('Links', '[Docs](https://alexandrumuntenas.github.io/pingu/ ) • [Support Server](https://discord.com/invite/q55kCfekyy) • [Invite](https://discord.com/oauth2/authorize?client_id=827199539185975417&permissions=1933044831&scope=bot%20applications.commands) • [Source Code](https://github.com/alexandrumuntenas/pingu)')
-      .setFooter({ text: `Helping ${client.guilds.cache.size} guilds`, iconUrl: client.user.displayAvatarURL() })
+      .setFooter({ text: `Helping ${client.guilds.cache.size} guilds`, icon: client.user.displayAvatarURL() })
     interaction.editReply({ embeds: [helpMessage] })
   },
   executeLegacy (client, locale, message) {
-    const helpMessage = new MessageEmbed().setAuthor({ name: client.user.username, iconUrl: client.user.displayAvatarURL() })
+    const helpMessage = new MessageEmbed()
       .setColor('#2F3136')
       .addField('Help Us Grow', '[Vote and leave a rating in Top.GG](https://top.gg/bot/827199539185975417/)')
       .addField('Links', '[Docs](https://alexandrumuntenas.github.io/pingu/) • [Support Server](https://discord.com/invite/q55kCfekyy) • [Invite](https://discord.com/oauth2/authorize?client_id=827199539185975417&permissions=1933044831&scope=bot%20applications.commands) • [Source Code](https://github.com/alexandrumuntenas/pingu)')
-      .setFooter({ text: `Helping ${client.guilds.cache.size} guilds`, iconUrl: client.user.displayAvatarURL() })
+      .setFooter({ text: `Helping ${client.guilds.cache.size} guilds`, iconURL: client.user.displayAvatarURL() })
     message.reply({ embeds: [helpMessage] })
   }
 }
