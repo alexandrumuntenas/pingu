8a5768401f0eda6acc3b0fc7b05ae09842e3829e
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-01-20 22:41:11 +0100

    Documentar generateTheCommandListOfTheGuild

--- functions/generateTheCommandListOfTheGuild.js
+++ functions/generateTheCommandListOfTheGuild.js
@@ -1,18 +1,26 @@
 const { Collection } = require('discord.js')
 
-module.exports = (client, database, callback) => {
+/**
+ * Generate the command list of the guild
+ * @param {Client} client - The Bot Client
+ * @param {Array<GuildConfig>} guildConfig - The Guild Config
+ * @param {Function} callback - The callback function
+ * @returns Object - The command list
+ */
+
+module.exports = (client, guildConfig, callback) => {
   let welcome, joinroles, farewell, levels, economy, suggestions, bodyToSend
-  if (database.welcomeEnabled !== 0) welcome = client.commands.filter(command => command.module === 'welcome') || []
-  if (database.farewellEnabled !== 0) farewell = client.commands.filter(command => command.module === 'farewell') || []
-  if (database.joinRolesEnabled !== 0) joinroles = client.commands.filter(command => command.module === 'joinroles') || []
-  if (database.levelsEnabled !== 0) levels = client.commands.filter(command => command.module === 'levels') || []
-  if (database.suggestionsEnabled !== 0) suggestions = client.commands.filter(command => command.module === 'suggestions') || []
-  if (database.economyEnabled !== 0) economy = client.commands.filter(command => command.module === 'economy') || []
+  if (guildConfig.welcomeEnabled !== 0) welcome = client.commands.filter(command => command.module === 'welcome') || []
+  if (guildConfig.farewellEnabled !== 0) farewell = client.commands.filter(command => command.module === 'farewell') || []
+  if (guildConfig.joinRolesEnabled !== 0) joinroles = client.commands.filter(command => command.module === 'joinroles') || []
+  if (guildConfig.levelsEnabled !== 0) levels = client.commands.filter(command => command.module === 'levels') || []
+  if (guildConfig.suggestionsEnabled !== 0) suggestions = client.commands.filter(command => command.module === 'suggestions') || []
+  if (guildConfig.economyEnabled !== 0) economy = client.commands.filter(command => command.module === 'economy') || []
   const nomodule = client.commands.filter(command => !command.module)
 
   bodyToSend = new Collection().concat(welcome || [], joinroles || [], farewell || [], levels || [], economy || [], suggestions || [], nomodule || [])
 
-  if (database.guildViewCnfCmdsEnabled === 0) {
+  if (guildConfig.guildViewCnfCmdsEnabled === 0) {
     bodyToSend = bodyToSend.filter(command => command.isConfigCommand === false)
   }
 
