ffecba195ea04d97303844220c3d46b85cb7f335
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-01-27 14:12:50 +0100

    Aplicar cambios de eslint a guildDelete

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,16 +1,20 @@
 module.exports = {
-  name: 'guildDelete',
-  execute: async (client, guild) => {
-    const gD = client.console.sentry.startTransaction({
-      op: 'guildDelete',
-      name: 'Guild Delete'
-    })
-    const databaseTables = ['guildData', 'guildAutoResponder', 'guildEconomyProducts', 'guildJoinRoles', 'guildJoinRoles', 'memberData', 'guildLevelsRankupRoles', 'guildReactionRoles']
-    databaseTables.forEach(table => {
-      client.pool.query(`DELETE FROM ${table} WHERE guild = ?`, [guild.id], (err) => {
-        if (err) client.logError(err)
-      })
-    })
-    gD.finish()
-  }
-}
+	name: 'guildDelete',
+	execute: async (client, guild) => {
+		const gD = client.console.sentry.startTransaction({
+			op: 'guildDelete',
+			name: 'Guild Delete',
+		});
+
+		//! THIS HAS TO BE MOVED TO GUILDDATAMANAGER
+		const databaseTables = ['guildData', 'guildAutoResponder', 'guildEconomyProducts', 'guildJoinRoles', 'guildJoinRoles', 'memberData', 'guildLevelsRankupRoles', 'guildReactionRoles'];
+		databaseTables.forEach(table => {
+			client.pool.query(`DELETE FROM ${table} WHERE guild = ?`, [guild.id], err => {
+				if (err) {
+					client.logError(err);
+				}
+			});
+		});
+		gD.finish();
+	},
+};
