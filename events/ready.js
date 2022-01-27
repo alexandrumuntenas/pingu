c52d69e4ccae8be42dc7e3c67eeb395d192a829a
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-01-27 14:15:13 +0100

    Aplicar cambios de eslint a ready.

--- events/ready.js
+++ events/ready.js
@@ -1,40 +1,42 @@
-const { REST } = require('@discordjs/rest')
-const { Routes } = require('discord-api-types/v9')
-const { getGuildConfigNext } = require('../modules/guildDataManager.js')
-const generateTheCommandListOfTheGuild = require('../functions/generateTheCommandListOfTheGuild')
+const {REST} = require('@discordjs/rest');
+const {Routes} = require('discord-api-types/v9');
+const {getGuildConfigNext} = require('../modules/guildDataManager.js');
+const generateTheCommandListOfTheGuild = require('../functions/generateTheCommandListOfTheGuild');
 
-const rest = new REST({ version: '9' })
+const rest = new REST({version: '9'});
 if (process.env.ENTORNO === 'desarrollo') {
-  rest.setToken(process.env.INSIDER_TOKEN)
+	rest.setToken(process.env.INSIDER_TOKEN);
 } else {
-  rest.setToken(process.env.PUBLIC_TOKEN)
+	rest.setToken(process.env.PUBLIC_TOKEN);
 }
 
 module.exports = {
-  name: 'ready',
-  execute: (client) => {
-    client.console.info(`Conectado como ${client.user.tag}!`)
-    if (client.statcord) client.statcord.autopost()
-    updateGuildCommands(client)
-    setInterval(() => {
-      updateGuildCommands(client)
-    }, 86400000)
-  }
-}
+	name: 'ready',
+	execute: client => {
+		client.console.info(`Conectado como ${client.user.tag}!`);
+		if (client.statcord) {
+			client.statcord.autopost();
+		}
 
-function updateGuildCommands (client) {
-  client.guilds.fetch().then((guilds) => {
-    guilds.forEach(guild => {
-      client.console.info(`Deploying commands to ${guild.id}`)
-      getGuildConfigNext(client, guild, (guildConfig) => {
-        generateTheCommandListOfTheGuild(client, guildConfig, (commandListOfTheGuild) => {
-          rest.put(Routes.applicationGuildCommands(client.user.id, guild.id), { body: commandListOfTheGuild })
-            .then(() => {
-              client.console.success(`Commands deployed succesfully to ${guild.id}`)
-            })
-            .catch(console.error)
-        })
-      })
-    })
-  })
+		updateGuildCommands(client);
+		setInterval(() => {
+			updateGuildCommands(client);
+		}, 86400000);
+	},
+};
+
+async function updateGuildCommands(client) {
+	const guilds = await client.guilds.fetch();
+	guilds.forEach(guild => {
+		client.console.info(`Deploying commands to ${guild.id}`);
+		getGuildConfigNext(client, guild, guildConfig => {
+			generateTheCommandListOfTheGuild(client, guildConfig, commandListOfTheGuild => {
+				rest.put(Routes.applicationGuildCommands(client.user.id, guild.id), {body: commandListOfTheGuild})
+					.then(() => {
+						client.console.success(`Commands deployed succesfully to ${guild.id}`);
+					})
+					.catch(console.error);
+			});
+		});
+	});
 }
