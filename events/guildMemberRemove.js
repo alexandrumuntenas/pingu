f7322fa5450c86b327d4c91960a8524b3937175a
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-01-27 14:14:03 +0100

    Aplicar cambios de eslint a guildMemberRemove.

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,17 +1,19 @@
-const farewell = require('../modules/farewell.js')
+const farewell = require('../modules/farewell.js');
 
 module.exports = {
-  name: 'guildMemberRemove',
-  execute: async (client, member) => {
-    const gMR = client.console.sentry.startTransaction({
-      op: 'guildMemberRemove',
-      name: 'Guild Member Remove'
-    })
-    if (member.user.id !== client.user.id) {
-      farewell.doGuildMemberRemove(member)
-      client.pool.query('DELETE FROM `memberData` WHERE member = ? AND guild = ?', [member.user.id, member.guild.id]) // Mover esto a memberManager
-    }
+	name: 'guildMemberRemove',
+	execute: async (client, member) => {
+		const gMR = client.console.sentry.startTransaction({
+			op: 'guildMemberRemove',
+			name: 'Guild Member Remove',
+		});
 
-    gMR.finish()
-  }
-}
+		//! THIS HAS TO BE MOVED TO GUILDDATAMANAGER
+		if (member.user.id !== client.user.id) {
+			farewell.doGuildMemberRemove(member);
+			client.pool.query('DELETE FROM `memberData` WHERE member = ? AND guild = ?', [member.user.id, member.guild.id]); // Mover esto a memberManager
+		}
+
+		gMR.finish();
+	},
+};
