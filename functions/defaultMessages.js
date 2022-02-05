aedd7f26937f60245f9d04988a42183ea6386f56
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-02-05 22:12:18 +0100

    Actualizar colores utilizados en los mensajes enriquecidos en los mensajes predeterminados para que coincidan con los colores de Discord.

--- functions/defaultMessages.js
+++ functions/defaultMessages.js
@@ -8,25 +8,25 @@ module.exports.status = message => new MessageEmbed()
 	.setTimestamp();
 
 module.exports.loader = message => new MessageEmbed()
-	.setColor('#2F3136')
+	.setColor('#F3375C')
 	.setDescription(`<a:loader:927223896330084412> ${message}`)
 	.setFooter({text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL()})
 	.setTimestamp();
 
 module.exports.success = message => new MessageEmbed()
-	.setColor('#2F3136')
+	.setColor('#57F287')
 	.setDescription(`<:Blurple_verified_plain:938094790132764682> ${message}`)
 	.setFooter({text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL()})
 	.setTimestamp();
 
 module.exports.error = message => new MessageEmbed()
-	.setColor('#2F3136')
+	.setColor('#ED4245')
 	.setDescription(`<:blurple_employee:939096196801257472> ${message}`)
 	.setFooter({text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL()})
 	.setTimestamp();
 
 module.exports.info = message => new MessageEmbed()
-	.setColor('#2F3136')
+	.setColor('#5865F2')
 	.setDescription(`:information_source: ${message}`)
 	.setFooter({text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL()})
 	.setTimestamp();
@@ -38,7 +38,7 @@ module.exports.image = (imageURL, imageProvider) => new MessageEmbed()
 	.setTimestamp();
 
 module.exports.timer = message => new MessageEmbed()
-	.setColor('#2F3136')
+	.setColor('#F3375C')
 	.setImage('https://cdn.discordapp.com/attachments/908413370665938975/939097943036809247/hearties-daniel-lissing.gif')
 	.setDescription(`<:timeout_clock:937404313901359114> ${message}`)
 	.setFooter({text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL()})
@@ -51,7 +51,9 @@ module.exports.timer = message => new MessageEmbed()
  * @param {String} command.description - Command description.
  * @param {String} command.cooldown - Command cooldown.
  * @param {String} command.module - Command module.
+ * @param {String} command.parameters - Command parameters.
  * @param {Array.<{name:string,description:string,parameters:string,isNSFW:boolean}>} command.subcommands - Command subcommands.
+ * @returns {Array.<MessageEmbed>}
  */
 
 module.exports.help = command => {
@@ -60,9 +62,9 @@ module.exports.help = command => {
 	}
 
 	const embedOptions = new MessageEmbed()
-		.setColor('#2F3136')
+		.setColor('#5865F2')
 		.setTitle(`${command.name} • Options`)
-		.setDescription(`${command.description || 'No description'}\n\n<:timeout_clock:937404313901359114> Cooldown: ${command.cooldown || '10'}\n<:blurple_guide:937404928706617445> Module: ${command.module || 'No category'}`)
+		.setDescription(`${command.description || 'No description'}\n\n<:timeout_clock:937404313901359114> Cooldown: ${command.cooldown || '10'}\n<:blurple_guide:937404928706617445> Module: ${command.module || 'No category'}\n${command.parameters ? codeBlock(`${command.name} ${command.parameters}`) : ''}`)
 		.setFooter({text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL()})
 		.setTimestamp();
 
