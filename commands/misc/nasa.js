ca37b730aad258fb8116282cf16aa1529b588c7e
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-02-06 12:40:35 +0100

    Realizar ligeros cambios en el comando nasa y añadir soporte para los vídeos.

--- commands/misc/nasa.js
+++ commands/misc/nasa.js
@@ -10,25 +10,32 @@ module.exports = {
 	runInteraction(locale, interaction) {
 		fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_KEY}`)
 			.then(response => response.body)
-			.then(quote => {
-				if (quote.media_type === 'image') {
-					const embed = new MessageEmbed()
-						.setTitle(quote.title)
-						.setDescription(quote.explanation)
-						.setImage(quote.hdurl)
-						.setColor('#0B3D91')
+			.then(resource => {
+				const embed = new MessageEmbed()
+					.setTitle('Astronomy Picture of The Day')
+					.setDescription(resource.explanation)
+					.setAuthor({name: 'NASA', url: 'https://nasa.gov', iconURL: 'https://cdn.discordapp.com/attachments/908413370665938975/939841209629822986/nasa-logo.png'})
+					.setFooter({text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL()})
+					.setTimestamp(resource.date)
+					.setURL(`https://apod.nasa.gov/apod/ap${resource.date.slice(2).replace('-', '').replace('-', '')}.html`)
+					.setColor('#0B3D91');
+
+				if (resource.media_type === 'image') {
+					embed
+						.setTitle(resource.title)
+						.setImage(resource.hdurl)
 						.addField(
 							'+ Info',
-							`:camera: ${quote.copyright || 'We don\'t have that information'
+							`<:blurple_image:892443053359517696> ${resource.copyright || i18n(locale, 'IMAGEAPI::NOCOPYRIGHT')
 							}\n<a:ultimahora:876105976573472778> ${i18n(
 								locale,
 								'IMAGEAPI::PROVIDER',
-								{API: 'Nasa.gov'},
+								{PROVIDER: 'Nasa.gov'},
 							)}`,
 						);
 					interaction.editReply({embeds: [embed]});
 				} else {
-					interaction.editReply('We are working on video media type.');
+					interaction.editReply({embeds: [embed]});
 				}
 			});
 	},
@@ -36,29 +43,34 @@ module.exports = {
 		message
 			.reply({embeds: [loader(i18n(locale, 'FETCHINGDATA'))]})
 			.then(msg => {
-				fetch(
-					`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_KEY}`,
-				)
+				fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_KEY}`)
 					.then(response => response.body)
-					.then(quote => {
-						if (quote.media_type === 'image') {
-							const embed = new MessageEmbed()
-								.setTitle(quote.title)
-								.setDescription(quote.explanation)
-								.setImage(quote.hdurl)
-								.setColor('#0B3D91')
+					.then(resource => {
+						const embed = new MessageEmbed()
+							.setTitle('Astronomy Picture of The Day')
+							.setDescription(resource.explanation)
+							.setAuthor({name: 'NASA', url: 'https://nasa.gov', iconURL: 'https://cdn.discordapp.com/attachments/908413370665938975/939841209629822986/nasa-logo.png'})
+							.setFooter({text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL()})
+							.setTimestamp(resource.date)
+							.setURL(`https://apod.nasa.gov/apod/ap${resource.date.slice(2).replace('-', '').replace('-', '')}.html`)
+							.setColor('#0B3D91');
+
+						if (resource.media_type === 'image') {
+							embed
+								.setTitle(resource.title)
+								.setImage(resource.hdurl)
 								.addField(
 									'+ Info',
-									`:camera: ${quote.copyright || 'We don\'t have that information'
+									`<:blurple_image:892443053359517696> ${resource.copyright || i18n(locale, 'IMAGEAPI::NOCOPYRIGHT')
 									}\n<a:ultimahora:876105976573472778> ${i18n(
 										locale,
 										'IMAGEAPI::PROVIDER',
 										{PROVIDER: 'Nasa.gov'},
 									)}`,
 								);
-							msg.edit({content: 'Done!', embeds: [embed]});
+							msg.edit({embeds: [embed]});
 						} else {
-							message.channel.send('We are working on video media type.');
+							msg.edit({embeds: [embed]});
 						}
 					});
 			});
