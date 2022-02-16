99e839b5369904390b26c6a37b4c2b24a7205c7c
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-02-16 10:16:14 +0100

    Añadir fallback en caso de no existir los ficheros de traducción necesarios o la traducción necesaria solicitada.

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,4 +1,6 @@
 const stringPlaceholder = require('string-placeholder');
+const {handleError} = require('../functions/consolex');
+const {existsSync} = require('fs');
 
 /**
  * Solicitar la traducción para la i18n a través de string-placeholder
@@ -7,10 +9,26 @@ const stringPlaceholder = require('string-placeholder');
  * @param {Array<Object>} placeholders Los datos para reemplazar los placeholders
  */
 module.exports = (language, key, placeholders) => {
-	const locale = require(`./locales/${language}.json`)[key];
-	if (placeholders) {
-		return stringPlaceholder(locale, placeholders, {before: '%', after: '%'});
+	if (!existsSync(`./locales/${language}.json`)) {
+		handleError(`No se encontró el archivo de idioma ${language}.json`);
+		language = 'es';
 	}
 
-	return locale;
+	let translation = require(`./locales/${language}.json`)[key];
+	if (placeholders) {
+		try {
+			translation = stringPlaceholder(translation, placeholders, {before: '%', after: '%'});
+		} catch (err) {
+			if (err) {
+				translation = 'Error al intentar obtener la traducción para este mensaje (╯°□°）╯︵ ┻━┻';
+				handleError(err);
+			}
+		}
+	}
+
+	if (!translation) {
+		translation = 'Error al intentar obtener la traducción para este mensaje (╯°□°）╯︵ ┻━┻';
+	}
+
+	return translation;
 };
