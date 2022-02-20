4ff0adaa13be0f31c8d1a2880d118b242fda8ca9
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-02-20 18:21:23 +0100

    ✨ Reducir líneas de código en el comando help

--- commands/utilities/help.js
+++ commands/utilities/help.js
@@ -3,8 +3,7 @@ const i18n = require('../../i18n/i18n')
 
 module.exports = {
   name: 'help',
-  description:
-    'Feeling lost? 👀',
+  description: 'Feeling lost? 👀',
   cooldown: 1,
   runInteraction (locale, interaction) {
     const helpMessage = new MessageEmbed()
@@ -12,16 +11,11 @@ module.exports = {
       .setThumbnail(process.Client.user.displayAvatarURL())
       .setTitle(process.Client.user.username)
       .setDescription(`${i18n(locale, 'HELP::HELPINGGUILDS', { GUILDS: Math.floor(process.Client.guilds.cache.size) })}`)
+      .setFooter({ text: `©️ ${new Date().getFullYear()} Alexandru Muntenas`, iconURL: 'https://avatars.githubusercontent.com/u/59341776' })
 
     helpMessage.addField('Vote us on', `[Top.GG](https://top.gg/bot/${process.Client.user.id}/vote)`)
-    helpMessage.addField(
-      'Links',
-			`[Docs](https://alexandrumuntenas.github.io/pingu/ ) • [Support Server](${process.env.GUILDSUPPORTINVITE || null}) • [Invite](https://discord.com/oauth2/authorize?client_id=${process.Client.user.id}&permissions=388627950679&scope=bot%20applications.commands) • [Source Code](https://github.com/alexandrumuntenas/pingu)`
-    )
-      .setFooter({
-        text: `©️ ${new Date().getFullYear()} Alexandru Muntenas`,
-        iconURL: 'https://avatars.githubusercontent.com/u/59341776'
-      })
+    helpMessage.addField('Links', `[Docs](https://alexandrumuntenas.github.io/pingu/ ) • [Support Server](${process.env.GUILDSUPPORTINVITE || null}) • [Invite](https://discord.com/oauth2/authorize?client_id=${process.Client.user.id}&permissions=388627950679&scope=bot%20applications.commands) • [Source Code](https://github.com/alexandrumuntenas/pingu)`)
+
     interaction.editReply({ embeds: [helpMessage] })
   },
   runCommand (locale, message) {
@@ -30,16 +24,10 @@ module.exports = {
       .setThumbnail(process.Client.user.displayAvatarURL())
       .setTitle(process.Client.user.username)
       .setDescription(`${i18n(locale, 'HELP::HELPINGGUILDS', { GUILDS: Math.floor(process.Client.guilds.cache.size) })}`)
+      .setFooter({ text: `©️ ${new Date().getFullYear()} Alexandru Muntenas`, iconURL: 'https://avatars.githubusercontent.com/u/59341776' })
 
     helpMessage.addField('Vote us on', `[Top.GG](https://top.gg/bot/${process.Client.user.id}/vote)`)
-    helpMessage.addField(
-      'Links',
-			`[Docs](https://alexandrumuntenas.github.io/pingu/ ) • [Support Server](${process.env.GUILDSUPPORTINVITE || null}) • [Invite](https://discord.com/oauth2/authorize?client_id=${process.Client.user.id}&permissions=388627950679&scope=bot%20applications.commands) • [Source Code](https://github.com/alexandrumuntenas/pingu)`
-    )
-      .setFooter({
-        text: `©️ ${new Date().getFullYear()} Alexandru Muntenas`,
-        iconURL: 'https://avatars.githubusercontent.com/u/59341776'
-      })
+    helpMessage.addField('Links', `[Docs](https://alexandrumuntenas.github.io/pingu/ ) • [Support Server](${process.env.GUILDSUPPORTINVITE || null}) • [Invite](https://discord.com/oauth2/authorize?client_id=${process.Client.user.id}&permissions=388627950679&scope=bot%20applications.commands) • [Source Code](https://github.com/alexandrumuntenas/pingu)`)
 
     message.reply({ embeds: [helpMessage] })
   }
