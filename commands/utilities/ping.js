ef82def4cf71cff51ecb8110530b4e1515ed1c50
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-02-20 12:53:20 +0100

    Solucionar pings negativos. Se ha aplicado el valor absoluto a algunas operaciones para prevenir valores negativos y no acordes con la realidad.

--- commands/utilities/ping.js
+++ commands/utilities/ping.js
@@ -10,9 +10,9 @@ module.exports = {
 		const embed = new MessageEmbed()
 			.setColor('#57F287')
 			.setTitle('🏓 Pong!')
-			.addField(`<:blurple_bot:938094998283501569> ${i18n(locale, 'PING::BOTPING')}`, `${Date.now() - interaction.createdTimestamp}ms`, true)
+			.addField(`<:blurple_bot:938094998283501569> ${i18n(locale, 'PING::BOTPING')}`, `${Math.abs(Date.now() - interaction.createdTimestamp)}ms`, true)
 			.addField(`<:the_connection_is_excellent:939550716555583508> ${i18n(locale, 'PING::GATEWAYPING')}`, `${Math.round(process.Client.ws.ping)}ms`, true)
-			.addField(`<:timeout_clock:937404313901359114> ${i18n(locale, 'PING::TOTALPING')}`, `${Math.round(process.Client.ws.ping + (Date.now() - interaction.createdTimestamp))}ms`, true)
+			.addField(`<:timeout_clock:937404313901359114> ${i18n(locale, 'PING::TOTALPING')}`, `${Math.round(process.Client.ws.ping + (Math.abs(Date.now() - interaction.createdTimestamp)))}ms`, true)
 			.setFooter({text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL()})
 			.setTimestamp();
 		interaction.editReply({embeds: [embed]});
@@ -21,9 +21,9 @@ module.exports = {
 		const embed = new MessageEmbed()
 			.setColor('#57F287')
 			.setTitle('🏓 Pong!')
-			.addField(`<:blurple_bot:938094998283501569> ${i18n(locale, 'PING::BOTPING')}`, `${Date.now() - message.createdTimestamp}ms`, true)
+			.addField(`<:blurple_bot:938094998283501569> ${i18n(locale, 'PING::BOTPING')}`, `${Math.abs(Date.now() - message.createdTimestamp)}ms`, true)
 			.addField(`<:the_connection_is_excellent:939550716555583508> ${i18n(locale, 'PING::GATEWAYPING')}`, `${Math.round(process.Client.ws.ping)}ms`, true)
-			.addField(`<:timeout_clock:937404313901359114> ${i18n(locale, 'PING::TOTALPING')}`, `${Math.round(process.Client.ws.ping + (Date.now() - message.createdTimestamp))}ms`, true)
+			.addField(`<:timeout_clock:937404313901359114> ${i18n(locale, 'PING::TOTALPING')}`, `${Math.round(process.Client.ws.ping + (Math.abs(Date.now() - message.createdTimestamp)))}ms`, true)
 			.setFooter({text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL()})
 			.setTimestamp();
 		message.channel.send({embeds: [embed]});
