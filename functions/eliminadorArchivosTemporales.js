9ce1145391aead1d0ab50cbcd2cb5b12287d3584
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-02-20 19:29:56 +0100

    ✨ Simplificar eliminadorArchivosTemporales.

--- functions/eliminadorArchivosTemporales.js
+++ functions/eliminadorArchivosTemporales.js
@@ -10,14 +10,12 @@ module.exports = () => {
 
   for (const file of files) {
     stat(`./modules/temp/${file}`, (err, stats) => {
-      if (err) {
-        consolex.handleError(err)
-      }
+      if (err) consolex.handleError(err)
 
       const fileDate = new Date(stats.birthtime)
       const now = new Date()
 
-      if (now - fileDate > 600000) {
+      if (now - fileDate >= 600000) {
         consolex.info(`Eliminador de Archivos temporales ha eliminado ${file}`)
         unlinkSync(`./modules/temp/${file}`)
       }
