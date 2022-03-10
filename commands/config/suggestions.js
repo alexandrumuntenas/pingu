08fe4efd0e133bab3e5da380ba3cb539c2bba2bf
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-03-10 22:32:41 +0100

    ✨Añadir más posibles funciones a suggestions y crear la plantilla del comando mysuggestions para comprobar tus sugerencias.

--- commands/config/suggestions.js
+++ commands/config/suggestions.js
@@ -11,7 +11,12 @@ module.exports = {
   interactionData: new SlashCommandBuilder()
     .addSubcommand(sc => sc.setName('dmupdates').setDescription('Send suggestion status updates to it\'s author?').addBooleanOption(input => input.setName('enable').setDescription('Send suggestion status updates to it\'s author?')))
     .addSubcommand(sc => sc.setName('setlogs').setDescription('Set the channel where the bot will send the suggestion logs').addChannelOption(input => input.setName('channel').setDescription('Set the channel where the bot will send the suggestion logs').addChannelType(ChannelType.GuildText)))
-    .addSubcommand(sc => sc.setName('setchannel').setDescription('Set the channel where the bot will send the suggestions').addChannelOption(input => input.setName('channel').setDescription('Set the channel where the bot will send the suggestions').addChannelType(ChannelType.GuildText))),
+    .addSubcommand(sc => sc.setName('setchannel').setDescription('Set the channel where the bot will send the suggestions').addChannelOption(input => input.setName('channel').setDescription('Set the channel where the bot will send the suggestions').addChannelType(ChannelType.GuildText)))
+    .addSubcommand(sc => sc.setName('setreviewer').setDescription('Set the role that can review suggestions').addRoleOption(input => input.setName('role').setDescription('Set the role that can review suggestions')))
+    .addSubcommand(sc => sc.setName('setcooldown').setDescription('Set the cooldown between suggestions').addIntegerOption(input => input.setName('cooldown').setDescription('Set the cooldown between suggestions')))
+    .addSubcommand(sc => sc.setName('communitybefore').setDescription('Before sending the suggestion to the staff, make the community review it.').addBooleanOption(input => input.setName('enable').setDescription('Before sending the suggestion to the staff, make the community review it.')))
+    .addSubcommand(sc => sc.setName('communityafter').setDescription('After sending the suggestion to the staff, make the community review it.').addBooleanOption(input => input.setName('enable').setDescription('After sending the suggestion to the staff, make the community review it.')))
+    .addSubcommand(sc => sc.setName('votingtimelimity').setDescription('Set the time limit for the voting phase').addIntegerOption(input => input.setName('time').setDescription('Set the time limit for the voting phase in minutes'))),
   runInteraction () {
   }
 }
---
+++ commands/suggestions/mysuggestions.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'mysuggestions',
+  module: 'suggestions',
+  description: '👀 View your suggestions',
+  isConfigurationCommand: false,
+  runInteraction () {
+  }
+}
