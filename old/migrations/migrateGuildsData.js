7413805c2b295635f261783dce81e9f875b728c1
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-03-13 19:39:21 +0100

    🧭 Crear actualizador de datos para poder proceder a eliminar las tablas antiguas. Categorizado en old.

---
+++ old/migrations/migrateGuildsData.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const { getGuildConfigNext } = require('../../functions/guildDataManager')
+const Discord = require('discord.js')
+// Eliminar comentario a la siguiente línea para que funcione el actualizador.
+// const Database = require('../../functions/databaseConnection')
+
+const Client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.GUILD_BANS, Discord.Intents.FLAGS.GUILD_INVITES, Discord.Intents.FLAGS.GUILD_VOICE_STATES, Discord.Intents.FLAGS.GUILD_VOICE_STATES, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING], partials: ['REACTION', 'MESSAGE', 'USER'] })
+
+Client.login(process.env.PUBLIC_TOKEN)
+
+Client.on('ready', () => {
+  console.log('Bot is ready!')
+  Client.guilds.fetch().then(guilds => {
+    for (const guild of guilds.values()) {
+      setTimeout(() => {
+        getGuildConfigNext(guild, () => console.log(`[${guild.id}] Updated`))
+      }, 2500)
+    }
+  })
+})
