9acba6397a746606499ab711cc8ba7260e964edf
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-03-24 20:52:41 +0100

    🚨Primeros cambios en la función messageManager.  Todas las opciones existentes de función "plantilla" han sido categorizados y documentados bajo "plantillas".

--- functions/messageManager.js
+++ functions/messageManager.js
@@ -1,44 +1,97 @@
 const { MessageEmbed } = require('discord.js')
 const { codeBlock } = require('@discordjs/builders')
 
-module.exports.status = message => new MessageEmbed()
+module.exports.plantillas = {}
+
+/**
+ * Plantilla para los estados. Se utiliza para mostrar
+ * la situación en el cual se encuentra, por ejemplo,
+ * la configuración de un módulo,
+ * @param {String} message
+ * @returns MessageEmbed
+ */
+
+module.exports.plantillas.status = message => new MessageEmbed()
   .setColor('#2F3136')
   .setDescription(`<:pingu_null:876103457860370442> ${message}`)
   .setFooter({ text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL() })
   .setTimestamp()
 
-module.exports.loader = message => new MessageEmbed()
+/**
+ * Plantilla para mostrar un mensaje temporal en las
+ * situaciones en el cual se requiere de más de 2 segundos
+ * para devolver la respuesta final al usuario. Así se da
+ * la sensación de que el bot está trabajando en su respuesta
+ * y que el usuario no ha sido ignorado.
+ * @param {String} message
+ * @returns MessageEmbed
+ */
+
+module.exports.plantillas.loader = message => new MessageEmbed()
   .setColor('#FEE75C')
   .setDescription(`<a:loader:927223896330084412> ${message}`)
   .setFooter({ text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL() })
   .setTimestamp()
 
-module.exports.success = message => new MessageEmbed()
+/**
+ * Plantilla para los mensajes que indican que una acción
+ * se ha realizado correctamente.
+ * @param {String} message
+ * @returns MessageEmbed
+ */
+
+module.exports.plantillas.success = message => new MessageEmbed()
   .setColor('#57F287')
   .setDescription(`<:Blurple_verified_plain:938094790132764682> ${message}`)
   .setFooter({ text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL() })
   .setTimestamp()
 
-module.exports.error = message => new MessageEmbed()
+/**
+ * Plantilla para los mensajes que indican que una acción
+ * no ha podido ser realizada debido a un error.
+ * @param {String} message
+ * @returns MessageEmbed
+ */
+
+module.exports.plantillas.error = message => new MessageEmbed()
   .setColor('#ED4245')
   .setDescription(`<:blurple_employee:939096196801257472> ${message}`)
   .setFooter({ text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL() })
   .setTimestamp()
 
-module.exports.info = message => new MessageEmbed()
+/**
+ * Plantilla para los mensajes que solo devuelven una info.
+ * @param {String} message
+ * @returns MessageEmbed
+ */
+
+module.exports.plantillas.info = message => new MessageEmbed()
   .setColor('#5865F2')
   .setDescription(`:information_source: ${message}`)
   .setFooter({ text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL() })
   .setTimestamp()
 
-module.exports.image = (imageURL, imageProvider) => new MessageEmbed()
+/**
+ * Plantilla para los mensajes que solo devuelven una imagen.
+ * @param {URL} imageURL
+ * @param {String} imageProvider
+ * @returns MessageEmbed
+ */
+
+module.exports.plantillas.image = (imageURL, imageProvider) => new MessageEmbed()
   .setColor('#2F3136')
   .setImage(imageURL)
   .setDescription(`<:blurple_image:892443053359517696> Image via ${imageProvider} API.`)
   .setFooter({ text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL() })
   .setTimestamp()
 
-module.exports.timer = message => new MessageEmbed()
+/**
+ * Plantilla para los mensajes relacionados con la espera para una acción.
+ * @param {String} message
+ * @returns MessageEmbed
+ */
+
+module.exports.plantillas.timer = message => new MessageEmbed()
   .setColor('#F3375C')
   .setImage('https://cdn.discordapp.com/attachments/908413370665938975/939097943036809247/hearties-daniel-lissing.gif')
   .setDescription(`<:timeout_clock:937404313901359114> ${message}`)
@@ -46,18 +99,18 @@ module.exports.timer = message => new MessageEmbed()
   .setTimestamp()
 
 /**
- * Generate a help message for a command
- * @param {Object} command - Object with information about the command.
- * @param {String} command.name - Command name.
- * @param {String} command.description - Command description.
- * @param {String} command.cooldown - Command cooldown.
- * @param {String} command.module - Command module.
- * @param {String} command.parameters - Command parameters.
- * @param {Array.<{name:string,description:string,parameters:string,isNSFW:boolean}>} command.subcommands - Command subcommands.
+ * Plantilla para los mensajes de ayuda del bot
+ * @param {Object} command - Objeto con la información del comando
+ * @param {String} command.name - Nombre del comando
+ * @param {String} command.description - Descripción del comandp
+ * @param {String} command.cooldown - Cooldown del comando
+ * @param {String} command.module - Módulo del comando
+ * @param {String} command.parameters - Parámetros del comando.
+ * @param {Array.<{name:string,description:string,parameters:string,isNSFW:boolean}>} command.subcommands - Subcomandos del comando
  * @returns {Array.<MessageEmbed>}
  */
 
-module.exports.help = command => {
+module.exports.plantillas.help = command => {
   if (!command) {
     throw new Error('No command provided')
   }
