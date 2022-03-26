ca2dc88a1d637958af6a39fc6ffd97f93c9fac50
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-03-26 22:03:57 +0100

    ❌ Eliminar un subcomando inexistente de suggestions.

--- commands/config/suggestions.js
+++ commands/config/suggestions.js
@@ -16,7 +16,6 @@ module.exports = {
     .addSubcommand(sc => sc.setName('dmupdates').setDescription('Send suggestion status updates to it\'s author?').addBooleanOption(input => input.setName('enable').setDescription('Send suggestion status updates to it\'s author?').setRequired(true)))
     .addSubcommand(sc => sc.setName('setlogs').setDescription('Set the channel where the bot will send the suggestion logs').addChannelOption(input => input.setName('channel').setDescription('Set the channel where the bot will send the suggestion logs').addChannelType(ChannelType.GuildText).setRequired(true)))
     .addSubcommand(sc => sc.setName('setchannel').setDescription('Set the channel where the bot will send the suggestions').addChannelOption(input => input.setName('channel').setDescription('Set the channel where the bot will send the suggestions').addChannelType(ChannelType.GuildText).setRequired(true)))
-    .addSubcommand(sc => sc.setName('setreviewer').setDescription('Set the role that can review suggestions').addRoleOption(input => input.setName('role').setDescription('Set the role that can review suggestions')))
     .addSubcommand(sc => sc.setName('setcooldown').setDescription('Set the cooldown between suggestions').addIntegerOption(input => input.setName('cooldown').setDescription('Set the cooldown between suggestions'))),
   runInteraction (locale, interaction) {
     switch (interaction.options.getSubcommand()) {
