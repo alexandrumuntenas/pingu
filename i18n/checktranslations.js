1535e63e65593fd9e21ee23c7e886c0695b70953
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-03-26 18:56:54 +0100

    🛠️ Añadir algunas reglas de skipcq para deepsource.io

--- i18n/checktranslations.js
+++ i18n/checktranslations.js
@@ -8,12 +8,12 @@ const clavesAComprobar = Object.keys(traduccionPrincipal)
 const clavesNoExistentes = []
 
 readdirSync('i18n/locales').forEach(file => {
-  console.warn(chalk.yellowBright(`Comprobando traducciones de ${file}`))
+  console.warn(chalk.yellowBright(`Comprobando traducciones de ${file}`)) // skipqc: JS-0002
   const traduccion = require(`./locales/${file}`)
   clavesNoExistentes[file] = []
   clavesAComprobar.forEach(clave => {
     if (!traduccion[clave]) {
-      console.info(`❌ ${file} | ${clave}`)
+      console.info(`❌ ${file} | ${clave}`) // skipqc: JS-0002
       clavesNoExistentes[file].push(clave)
     }
   })
