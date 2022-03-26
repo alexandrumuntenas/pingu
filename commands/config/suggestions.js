84884417fbd401c7c73d8416fc66eb49736a36e5
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-03-26 22:06:06 +0100

    🫧 Declarar comando de suggestions como un comando de configuración.

--- commands/config/suggestions.js
+++ commands/config/suggestions.js
@@ -11,7 +11,7 @@ module.exports = {
   cooldown: 1000,
   description: '⚙️ Configure the suggestions module',
   permissions: [Permissions.FLAGS.MANAGE_GUILD],
-  isConfigurationCommand: false,
+  isConfigurationCommand: true,
   interactionData: new SlashCommandBuilder()
     .addSubcommand(sc => sc.setName('dmupdates').setDescription('Send suggestion status updates to it\'s author?').addBooleanOption(input => input.setName('enable').setDescription('Send suggestion status updates to it\'s author?').setRequired(true)))
     .addSubcommand(sc => sc.setName('setlogs').setDescription('Set the channel where the bot will send the suggestion logs').addChannelOption(input => input.setName('channel').setDescription('Set the channel where the bot will send the suggestion logs').addChannelType(ChannelType.GuildText).setRequired(true)))
