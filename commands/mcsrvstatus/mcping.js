6547838a0cf79d4ccf4c12706345fe62b8b9da51
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-03-27 17:45:41 +0200

    🛠️ Ajustar mensajes de error, corregir ruta de acceso al módulo de internacionalización, corregir descripción de la interación y añadir "publicidad".

--- commands/mcsrvstatus/mcping.js
+++ commands/mcsrvstatus/mcping.js
@@ -1,15 +1,16 @@
 const Gamedig = require('gamedig')
 const { convertirMOTDaImagen, pingAEmoji } = require('../../modules/mcsrvstatus')
 const { MessageAttachment, MessageEmbed } = require('discord.js')
-const i18n = require('../../i18n')
+const i18n = require('../../i18n/i18n')
+const { plantillas } = require('../../functions/messageManager')
 
 module.exports = {
   name: 'mcping',
-  // module: 'mcsrvstatus',
-  description: '🏅 Get your rank',
+  module: 'mcsrvstatus',
+  description: '🖥️ Ping the configured Minecraft server',
   cooldown: 1,
   runInteraction (locale, interaction) {
-    if (!interaction.guild.configuration.mcsrvstatus.host) return interaction.editReply(i18n(locale, 'MCPING::NO_HOST'))
+    if (!interaction.guild.configuration.mcsrvstatus.host) return interaction.editReply({ embeds: [plantillas.error(i18n(locale, 'MCPING::NO_HOST'))] })
     Gamedig.query({ type: 'minecraft', host: interaction.guild.configuration.mcsrvstatus.host, port: interaction.guild.configuration.mcsrvstatus.port ? interaction.guild.configuration.mcsrvstatus.port : 25565 }).then((state) => {
       convertirMOTDaImagen(state.raw.vanilla.raw.description.extra, motd => {
         const attachment = new MessageAttachment(motd, 'motd.png')
@@ -19,15 +20,16 @@ module.exports = {
           .addField(`${pingAEmoji(state.raw.vanilla.ping)} Ping`, `${state.raw.vanilla.ping}ms` || 'Failed to fetch server ping', true)
           .addField(':desktop: Address', state.connect, false)
           .setImage('attachment://motd.png')
+          .setFooter({ text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL() }).setTimestamp()
 
         interaction.editReply({ files: [attachment], embeds: [embed] })
       })
     }).catch(() => {
-      interaction.editReply(i18n(locale, 'MCPING::ERROR'))
+      interaction.editReply({ embeds: [plantillas.conexito(i18n(locale, 'MCPING::ERROR'))] })
     })
   },
   runCommand (locale, message) {
-    if (!message.guild.configuration.mcsrvstatus.host) return message.reply(i18n(locale, 'MCPING::NO_HOST'))
+    if (!message.guild.configuration.mcsrvstatus.host) return message.reply({ embeds: [plantillas.error(i18n(locale, 'MCPING::NO_HOST'))] })
     Gamedig.query({ type: 'minecraft', host: message.guild.configuration.mcsrvstatus.host, port: message.guild.configuration.mcsrvstatus.port ? message.guild.configuration.mcsrvstatus.port : 25565 }).then((state) => {
       convertirMOTDaImagen(state.raw.vanilla.raw.description.extra, motd => {
         const attachment = new MessageAttachment(motd, 'motd.png')
@@ -37,11 +39,12 @@ module.exports = {
           .addField(`${pingAEmoji(state.raw.vanilla.ping)} Ping`, `${state.raw.vanilla.ping}ms` || 'Failed to fetch server ping', true)
           .addField(':desktop: Address', state.connect, false)
           .setImage('attachment://motd.png')
+          .setFooter({ text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL() }).setTimestamp()
 
         message.reply({ files: [attachment], embeds: [embed] })
       })
     }).catch(() => {
-      message.reply(i18n(locale, 'MCPING::ERROR'))
+      message.reply({ embeds: [plantillas.conexito(i18n(locale, 'MCPING::ERROR'))] })
     })
   }
 }
