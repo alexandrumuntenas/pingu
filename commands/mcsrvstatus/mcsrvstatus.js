2010c79f1572f39beffff20a720913f5964f3609
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-04-10 15:40:34 +0200

    ✨ Añadir nueva opción al comando mcsrvstatus

--- commands/mcsrvstatus/mcsrvstatus.js
+++ commands/mcsrvstatus/mcsrvstatus.js
@@ -11,13 +11,21 @@ module.exports = {
   permissions: [Permissions.FLAGS.MANAGE_GUILD],
   isConfigurationCommand: false,
   interactionData: new SlashCommandBuilder()
-    .addSubcommand(sc => sc.setName('setdefaulthost').setDescription('Set the default host for the Minecraft Server Status module').addStringOption(input => input.setName('host').setDescription('The host to use').setRequired(true)).addNumberOption(input => input.setName('port').setDescription('The port to use'))),
+    .addSubcommand(sc => sc.setName('setdefaulthost').setDescription('Set the default host for the Minecraft Server Status module').addStringOption(input => input.setName('host').setDescription('The host to use').setRequired(true)).addNumberOption(input => input.setName('port').setDescription('The port to use')))
+    .addSubcommand(sc => sc.setName('setpanelchannel').setDescription('Set the channel where the Minecraft Server Status panel will be sent').addChannelOption(input => input.setName('channel').setDescription('The channel where to post updates').setRequired(true))),
   runInteraction (locale, interaction) {
     switch (interaction.options.getSubcommand()) {
       case 'setdefaulthost': {
         actualizarConfiguracionDelServidor(interaction.guild, { column: 'mcsrvstatus', newconfig: { host: interaction.options.getString('host'), port: interaction.options.getNumber('port') } }, err => {
           if (err) return interaction.editReply({ embeds: [plantillas.error(i18n(locale, 'MCSRVSTATUS::SETDEFAULTHOST:ERROR'))] })
           return interaction.editReply({ embeds: [plantillas.conexito(i18n(locale, 'MCSRVSTATUS::SETDEFAULTHOST:SUCCESS', { HOST: interaction.options.getString('host') }))] })
+        })
+        break
+      }
+      case 'setpanelchannel': {
+        actualizarConfiguracionDelServidor(interaction.guild, { column: 'mcsrvstatus', newconfig: { messagePanelChannel: interaction.options.getChannel('channel') } }, err => {
+          if (err) return interaction.editReply({ embeds: [plantillas.error(i18n(locale, 'MCSRVSTATUS::SETPANELCHANNEL:ERROR'))] })
+          return interaction.editReply({ embeds: [plantillas.conexito(i18n(locale, 'MCSRVSTATUS::SETPANELCHANNEL:SUCCESS', { CHANNEL: interaction.options.getChannel('channel').toString() }))] })
         })
         break
       }
