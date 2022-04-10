a6b1c7be4a390978a2c7452732fa9ad3d5095742
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-04-10 13:19:32 +0200

    ✨ Finalizar comando mcserver!

--- commands/mcsrvstatus/mcserver.js
+++ commands/mcsrvstatus/mcserver.js
@@ -9,8 +9,8 @@ module.exports = {
   module: 'mcsrvstatus',
   description: '🖥️ Ping a specified Minecraft server',
   interactionData: new SlashCommandBuilder().addStringOption(input => input.setName('ip_or_address').setRequired(true).setDescription('The IP or address of the server')).addStringOption(input => input.setName('port').setDescription('The port of the server')),
-  cooldown: 1,
-  runInteraction (locale, interaction) {
+  cooldown: 10000,
+  runInteraction(locale, interaction) {
     obtenerDatosDelServidor({ ip: interaction.options.getString('ip_or_address'), port: interaction.options.getString('port') }, datosDelServidor => {
       if (datosDelServidor) {
         const attachment = new MessageAttachment(datosDelServidor.motd, 'motd.png')
@@ -27,11 +27,38 @@ module.exports = {
       }
     })
   },
-  runCommand (locale, message) {
-    try {
-      obtenerDatosDelServidor({})
-    } catch {
-      message.reply({ embeds: [plantillas.error(i18n(locale, 'MCPING::ERROR'))] })
+  runCommand(locale, message) {
+    if (!Object.prototype.hasOwnProperty.call(message.parameters, 0)) {
+      return message.reply({
+        embeds: plantillas.ayuda({
+          name: module.exports.name,
+          description: module.exports.description,
+          parameters: '<ip_or_address> [port]',
+          module: 'mcsrvstatus'
+        })
+      })
     }
+
+    message.reply({ embeds: [plantillas.precargador(i18n(locale, 'OBTAININGDATA'))] }).then(_message => {
+      try {
+        obtenerDatosDelServidor({ ip: message.parameters[0], port: message.parameters[1] }, datosDelServidor => {
+          if (datosDelServidor) {
+            const attachment = new MessageAttachment(datosDelServidor.motd, 'motd.png')
+            const embed = new MessageEmbed()
+              .addField(':radio_button: Version', datosDelServidor.version, true)
+              .addField(':busts_in_silhouette: Players', datosDelServidor.jugadores, true)
+              .addField(`${datosDelServidor.ping.emoji} Ping`, `${datosDelServidor.ping.ms}ms` || 'Failed to fetch server ping', true)
+              .addField(':desktop: Address', datosDelServidor.direccion, false)
+              .setImage('attachment://motd.png')
+              .setFooter({ text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL() }).setTimestamp()
+            _message.edit({ files: [attachment], embeds: [embed] })
+          } else {
+            return _message.edit({ embeds: [plantillas.error(i18n(locale, 'MCPING::ERROR'))] })
+          }
+        })
+      } catch {
+        _message.edit({ embeds: [plantillas.error(i18n(locale, 'MCPING::ERROR'))] })
+      }
+    })
   }
 }
