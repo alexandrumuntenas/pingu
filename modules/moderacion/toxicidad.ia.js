e4ca90e9e756c66045043271318e78db820e76c5
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-04-18 23:40:44 +0200

    💾 Checkpoint.

--- modules/moderacion/toxicidad.ia.js
+++ modules/moderacion/toxicidad.ia.js
@@ -1,12 +1,10 @@
 const modeloToxicidad = require('@tensorflow-models/toxicity')
 
-const threshold = 0.9
-
-const modelo = modeloToxicidad.load(threshold)
-
 module.exports.analizarMensaje = (message, callback) => {
-  modelo.classify(message.content).then(predictions => {
-    console.log(predictions)
+  modeloToxicidad.load(0.9).then(modelo => modelo.classify(message.content)).then(prediccion => {
+    prediccion.forEach(prediccion => {
+      console.log(prediccion)
+      console.log('\n\n\n')
+    })
   })
-  callback()
 }
--- modules/moderation.js
+++ modules/moderation.js
@@ -1,5 +1,7 @@
-module.exports.GestorIncializadorDeAccionesEnmessageCreate = (message) => {
+const { analizarMensaje } = require("./moderacion/toxicidad.ia")
 
+module.exports.GestorIncializadorDeAccionesEnmessageCreate = (message) => {
+  analizarMensaje(message)
 }
 
 module.exports.aplicarAcciones = (accion, motivo, callback) => {
