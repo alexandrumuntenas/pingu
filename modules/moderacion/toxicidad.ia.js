6d0666fa03f3d8144c2dba91a81695890862262e
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-04-22 13:33:35 +0200

    🔔Eliminar librerías de tensorflow... Buscando alternativas.

--- modules/moderacion/toxicidad.ia.js
+++ modules/moderacion/toxicidad.ia.js
@@ -1,10 +1,3 @@
-const modeloToxicidad = require('@tensorflow-models/toxicity')
-
 module.exports.analizarMensaje = (message, callback) => {
-  modeloToxicidad.load(0.9).then(modelo => modelo.classify(message.content)).then(prediccion => {
-    prediccion.forEach(prediccion => {
-      console.log(prediccion)
-      console.log('\n\n\n')
-    })
-  })
+
 }
--- modules/moderation.js
+++ modules/moderation.js
@@ -1,7 +1,5 @@
-const { analizarMensaje } = require("./moderacion/toxicidad.ia")
-
 module.exports.GestorIncializadorDeAccionesEnmessageCreate = (message) => {
-  analizarMensaje(message)
+
 }
 
 module.exports.aplicarAcciones = (accion, motivo, callback) => {
