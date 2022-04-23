ea37963a2ccd59951f4ce94fb11c5c6adddb865f
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-04-23 13:17:35 +0200

    👀 Eliminar toxicidad.ia y dar bienvenida a perspective.ia

---
+++ modules/moderacion/perspective.ia.js
@@ -0,0 +1,8 @@
+const Perspective = require('perspective-api-client')
+const perspective = new Perspective({ apiKey: process.env.PERSPECTIVE_API_KEY })
+
+module.exports.analizarMensaje = (message, callback) => {
+  perspective.analyze(message.content).then(resultado => {
+    callback(resultado)
+  })
+}
--- modules/moderacion/toxicidad.ia.js
+++
@@ -1,3 +0,0 @@
-module.exports.analizarMensaje = (message, callback) => {
-
-}
