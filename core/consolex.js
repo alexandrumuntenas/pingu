500cce9b0c435be1a0f0534fa7d0acad8f72be65
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-05 14:07:23 +0200

    Corregir identación en consolex.

--- core/consolex.js
+++ core/consolex.js
@@ -17,7 +17,7 @@ function getCurrentTime () {
 
 module.exports = {
   debug: message => {
-    process.stdout.write(`${getCurrentTime()} [${chalk.whiteBright.bold('DEBUG')}]    ${message}\n`)
+    process.stdout.write(`${getCurrentTime()} [${chalk.whiteBright.bold('DEBUG')}]     ${message}\n`)
   },
   info: message => {
     process.stdout.write(`${getCurrentTime()} [${chalk.cyanBright.bold('INFORMA')}]   ${message}\n`)
