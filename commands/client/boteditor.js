38562e035c611e4eea3086a0b34334c03cf5878b
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-05 12:22:15 +0200

    Preparar boteditor

--- commands/client/boteditor.js
+++ commands/client/boteditor.js
@@ -1,19 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
-const { Permissions, MessageEmbed } = require('discord.js')
-const { subirInteraccionesDelServidor, actualizarConfiguracionDelServidor } = require('../../functions/guildManager')
-const { plantillas } = require('../../functions/messageManager')
-const { construirHelpDelComando } = require('../../functions/commandsManager')
-
-const avaliableModules = ['suggestions', 'farewell', 'welcome', 'autoreplies', 'customcommands', 'leveling', 'mcsrvstatus']
-const i18n = require('../../i18n/i18n')
-const Consolex = require('../../functions/consolex')
+const { Permissions } = require('discord.js')
 
 module.exports = {
   name: 'boteditor',
-  description: 'BOT::HELP:DESCRIPTION',
-  cooldown: 1000,
   permissions: [Permissions.FLAGS.MANAGE_GUILD],
-  isConfigurationCommand: true,
+  isconfigurationcommand: true,
   interaction: new SlashCommandBuilder(),
   runInteraction (interaction) {
 
