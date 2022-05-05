19aad578006e64870a0f139a46acf9895cbd3088
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-05 12:26:29 +0200

    Eliminar carga de eventos de client.js. Parece que alguien lo ha arrastrado en el merge.

--- client.js
+++ client.js
@@ -11,8 +11,6 @@ require('dotenv').config()
 const { GatewayIntentBits } = require('discord-api-types/v10')
 const Discord = require('discord.js')
 
-const fs = require('fs')
-
 process.Client = new Discord.Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildBans, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping], partials: ['REACTION', 'MESSAGE', 'USER'], ws: { properties: { $browser: 'Discord iOS' } } })
 
 const Consolex = require('./functions/consolex')
@@ -31,12 +29,6 @@ require('./functions/eventManager').cargarEventosDeProceso()
 require('./functions/eventManager').cargarEventos()
 
 process.Client.modulos = require('./functions/moduleManager').registrarModulos()
-
-for (const file of fs.readdirSync('./events').filter(files => files.endsWith('.js'))) {
-  const event = require(`./events/${file}`)
-  Consolex.success(`Evento ${file} cargado`)
-  process.Client.on(event.name, (...args) => event.execute(...args))
-}
 
 process.on('exit', () => {
   process.Client.destroy()
