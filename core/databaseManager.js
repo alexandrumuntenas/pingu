9d12a1fbbff3855f51b2cbe82f6a5b4e1d1d2490
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-13 14:46:37 +0200

    Checkpoint

--- core/databaseManager.js
+++ core/databaseManager.js
@@ -12,3 +12,20 @@ const Database = require('mysql2').createPool({
 Database.config.namedPlaceholders = true
 
 module.exports = Database
+
+const { readdirSync, readFileSync } = require('fs')
+
+module.exports.comprobarSiExistenTodasLasTablasNecesarias = () => {
+  // abrir cada sql de la carpeta database y ejecutar el query
+
+  const consultas = readdirSync('./database/')
+  const tablasYConsultas = {}
+
+  consultas.forEach(file => {
+    if (file.endsWith('.sql')) {
+      tablasYConsultas[file] = readFileSync(`./database/${file}`, 'utf8')
+    }
+  })
+
+  const tablas = Object.keys(tablasYConsultas)
+}
