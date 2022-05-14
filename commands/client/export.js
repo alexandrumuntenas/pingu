017cf16001a78a31ce7cbf810843a23f45c9d80a
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-15 00:30:35 +0200

    Split bot into two commands: export and import.

---
+++ commands/client/export.js
@@ -0,0 +1,26 @@
+const { BitField, Attachment } = require('discord.js')
+const { exportarDatosDelServidorEnFormatoYAML } = require('../../core/guildManager')
+const { plantillas } = require('../../core/messageManager')
+const { modulosDisponibles } = require('../../core/moduleManager')
+
+const i18n = require('../../core/i18nManager')
+const avaliableModules = []
+
+modulosDisponibles.forEach(modulo => avaliableModules.push({ name: modulo, value: modulo }))
+
+module.exports = {
+  name: 'export',
+  cooldown: 1000,
+  permissions: [BitField.Flags.ManageGuild],
+  isConfigurationCommand: true,
+  runInteraction (interaction) {
+    exportarDatosDelServidorEnFormatoYAML(interaction.guild, rutaLocalDelArchivo => {
+      interaction.editReply({ embeds: [plantillas.informacion(i18n.getTranslation(interaction.preferredLocale, 'YAMLCONFIGURATION_EXPORT_INFORMATION'))], files: [new Attachment(rutaLocalDelArchivo, `${interaction.guild}_${interaction.guild.id}.yaml`)] })
+    })
+  },
+  runCommand (message) {
+    exportarDatosDelServidorEnFormatoYAML(message.guild, rutaLocalDelArchivo => {
+      message.reply({ embeds: [plantillas.informacion(i18n.getTranslation(message.preferredLocale, 'YAMLCONFIGURATION_EXPORT_INFORMATION'))], files: [new Attachment(rutaLocalDelArchivo, `${message.guild}_${message.guild.id}.yaml`)] })
+    })
+  }
+}
