ddc29fb481e586d5fa071683699284a7d6c8ed1a
Alexandru Theodor Muntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2022-05-15 16:41:44 +0200

    Comenzar a recrear mcsrvstatus
    Mandar hacer puñetas la librería gamedig y reemplazarlo por mcsrvstat-wrapper. Utilizamos ya su API.

--- modules/mcsrvstatus.js
+++ modules/mcsrvstatus.js
@@ -135,27 +135,8 @@ function limpiarTextosDeEspaciosInicialesFinalesVaciosManteniendoElSaltodeLinea 
 }
 
 const comprobarSiTextoEsUnColorHexadecimal = /^#(?<hex>[0-9a-f]{3}){1,2}$/i
-/**
- * Procesa el MOTD de un servidor de Minecraft para convertirlo
- * a una matriz procesable por convertirMOTDaImagen()
- * @param {String || Array} motd - MOTD de un servidor de Minecraft. Resultado del ping del servidor de Minecraft (exactamente la propiedad: state.raw.vanilla.raw.description.extra)
- */
 
 module.exports.procesarMOTD = motd => {
-  const motdProcesado = []
-  if (typeof motd === 'string') {
-    return motdParser.textToJSON(limpiarTextosDeEspaciosInicialesFinalesVaciosManteniendoElSaltodeLinea(motd)).extra
-  } else if (Array.isArray(motd)) {
-    motd.forEach(trozoDeTexto => {
-      if (Object.prototype.hasOwnProperty.call(trozoDeTexto, 'text') && Object.prototype.hasOwnProperty.call(trozoDeTexto, 'color') && comprobarSiTextoEsUnColorHexadecimal.test(trozoDeTexto.color)) motdProcesado.push({ text: limpiarTextosDeEspaciosInicialesFinalesVaciosManteniendoElSaltodeLinea(trozoDeTexto.text), color: trozoDeTexto.color })
-      else if (Object.prototype.hasOwnProperty.call(trozoDeTexto, 'text') && Object.prototype.hasOwnProperty.call(trozoDeTexto, 'color')) motdProcesado.push({ text: limpiarTextosDeEspaciosInicialesFinalesVaciosManteniendoElSaltodeLinea(trozoDeTexto.text), color: coloresDeMinecraft[trozoDeTexto.color] || coloresDeMinecraft.f })
-      else if (Object.prototype.hasOwnProperty.call(trozoDeTexto, 'text')) motdProcesado.push({ text: limpiarTextosDeEspaciosInicialesFinalesVaciosManteniendoElSaltodeLinea(trozoDeTexto.text) })
-      else motdProcesado.push({ text: limpiarTextosDeEspaciosInicialesFinalesVaciosManteniendoElSaltodeLinea(trozoDeTexto) })
-    })
-
-    return motdProcesado
-  }
-  return [{ text: limpiarTextosDeEspaciosInicialesFinalesVaciosManteniendoElSaltodeLinea(motd) }]
 }
 
 /**
@@ -174,119 +155,24 @@ module.exports.limpiarFormatoDeLosTextos = (texto) => {
   return textoProcesado.join(' ')
 }
 
-const Gamedig = require('gamedig')
 const { obtenerConfiguracionDelServidor, actualizarConfiguracionDelServidor } = require('../core/guildManager')
 const consolex = require('../core/consolex')
 
-/**
- * Conectar con el servidor de minecraft y procesar toda su información
- * para que pueda ser usado por el bot.
- * @param {Object} host - Host de un servidor de Minecraft
- * @param {String} host.ip - IP del servidor de Minecraft
- * @param {Number} host.port - Puerto del servidor de Minecraft
- * @param {Function} callback
- * @returns {Object} - Objeto con la información del servidor de Minecraft
- */
-
 module.exports.obtenerDatosDelServidor = (host, callback) => {
-  if (!callback) throw new Error('Debe proporcionar un callback')
-  Gamedig.query({ type: 'minecraft', host: host.ip.trim(), port: host.port ? host.port : 25565 }).then((state) => {
-    const servidor = {}
-    module.exports.convertirMOTDaImagen(state.raw.vanilla.raw.description.extra || state.raw.vanilla.raw.description || state.name, motd => {
-      servidor.motd = motd
-    })
-    servidor.version = module.exports.limpiarFormatoDeLosTextos(state.raw.vanilla.raw.version.name) || 'Unknown version'
-    servidor.jugadores = `${state.raw.vanilla.raw.players.online}/${state.raw.vanilla.raw.players.max}` || 'Unknown players'
-    servidor.ping = { emoji: module.exports.pingAEmoji(state.raw.vanilla.ping), ms: state.raw.vanilla.ping || 'Unknown ping' }
-    servidor.direccion = `${host.ip}${host.port ? `:${host.port}` : ''}` || host.ip
-    return callback(servidor)
-  }).catch(() => {
-    return callback()
-  })
+
 }
 
-/* A partir de aquí solo habrá código relacionado con las tareas de actualización de datos */
-
 function actualizarNumeroDeJugadoresDelSidebar (guild) {
-  obtenerConfiguracionDelServidor(guild, config => {
-    const sidebarPlayercount = process.Client.guilds.resolve(guild.id).channels.resolve(config.mcsrvstatus.sidebarPlayercount)
-    if (config.mcsrvstatus.enabled && config.mcsrvstatus.sidebarPlayercount && sidebarPlayercount) {
-      module.exports.obtenerDatosDelServidor({ ip: config.mcsrvstatus.host, port: config.mcsrvstatus.port }, servidor => {
-        sidebarPlayercount.edit({ name: `🎭 Players: ${servidor.jugadores}` })
-      })
-    }
-  })
 }
 
 const { MessageAttachment, EmbedBuilder } = require('discord.js')
 const { createHash } = require('crypto')
 
 function actualizarDatosDelPanel (guild) {
-  obtenerConfiguracionDelServidor(guild, config => {
-    if (config.mcsrvstatus.enabled && config.mcsrvstatus.messagePanelChannel) {
-      module.exports.obtenerDatosDelServidor({ ip: config.mcsrvstatus.host, port: config.mcsrvstatus.port }, servidor => {
-        const attachment = new MessageAttachment(servidor.motd || './setup/defaultresourcesforguilds/emptymotd.png', 'motd.png')
-        const embed = new EmbedBuilder()
-        if (servidor) {
-          embed
-            .addFields([
-              { name: ':radio_button: Version', value: servidor.version, inline: true },
-              { name: ':busts_in_silhouette: Players', value: servidor.jugadores, inline: true },
-              { name: `${servidor.ping.emoji} Ping`, value: `${servidor.ping.ms}ms` || 'Failed to fetch server ping', inline: true },
-              { name: ':desktop: Address', value: servidor.direccion, inline: true }
-            ])
-            .setImage('attachment://motd.png')
-            .setFooter({ text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL() }).setTimestamp()
-        } else {
-          embed
-            .setTitle(':x: Error')
-            .setDescription('Failed to fetch server data')
-            .setFooter({ text: 'Powered by Pingu', iconURL: process.Client.user.displayAvatarURL() }).setTimestamp()
-        }
-
-        function fallback () {
-          try {
-            process.Client.channels.resolve(config.mcsrvstatus.messagePanelChannel).send({ embeds: [embed], files: [attachment] }).then(newMessage => {
-              actualizarConfiguracionDelServidor(guild, { column: 'mcsrvstatus', newconfig: { messagePanelId: newMessage.id } })
-            })
-          } catch {
-            consolex.error(`No se pudo actualizar el panel del servidor ${createHash('sha256').update(guild.id).digest('hex')}`)
-          }
-        }
-
-        try {
-          process.Client.channels.resolve(config.mcsrvstatus.messagePanelChannel).messages.fetch(config.mcsrvstatus.messagePanelId).then(message => {
-            message.edit({ embeds: [embed], files: [attachment] }).catch(() => {
-              fallback()
-            })
-          }).catch(() => {
-            fallback()
-          })
-        } catch {
-          consolex.error(`No se pudo actualizar el panel del servidor ${createHash('sha256').update(guild.id).digest('hex')}`)
-        }
-      })
-    }
-  })
 }
 
 module.exports.comenzarActualizarDatosDeLosServidores = () => {
-  consolex.info('Actualizando datos de los de minecraft configurados...')
-  process.Client.guilds.fetch().then(guilds => {
-    guilds.forEach(guild => {
-      actualizarNumeroDeJugadoresDelSidebar(guild)
-      actualizarDatosDelPanel(guild)
-    })
-  })
-
   setInterval(() => {
-    consolex.info('Actualizando datos de los de minecraft configurados...')
-    process.Client.guilds.fetch().then(guilds => {
-      guilds.forEach(guild => {
-        actualizarNumeroDeJugadoresDelSidebar(guild)
-        actualizarDatosDelPanel(guild)
-      })
-    })
   }, 300000)
 }
 
