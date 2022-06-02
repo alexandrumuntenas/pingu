0168cc8ae0454d3125183b0db0e19907ad13f640
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-06-02 09:34:22 +0000

    Terminar de adaptar Client.

--- src/client.ts
+++ src/client.ts
@@ -11,11 +11,13 @@ import 'dotenv/config'
 import * as Discord from 'discord.js'
 import { GatewayIntentBits } from 'discord-api-types/v10'
 
-interface PinguClient extends NodeJS.Process {
-  client: Discord.Client
+declare global {
+  interface Window {
+    client: Discord.Client
+  }
 }
 
-process.Client = new Discord.Client({
+globalThis.Client = new Discord.Client({
   intents: [
     GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildBans, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates,
     GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.MessageContent],
@@ -27,21 +29,21 @@ const consolex = require('./core/consolex')
 
 if (process.env.ENTORNO === 'publico') {
   consolex.warn('Iniciando sesión como el bot público.')
-  process.Client.login(process.env.PUBLIC_TOKEN)
+  globalThis.Client.login(process.env.PUBLIC_TOKEN);
 } else {
   consolex.warn('Iniciando sesión como el bot de desarrollo.')
-  process.Client.login(process.env.INSIDER_TOKEN)
+  globalThis.Client.login(process.env.INSIDER_TOKEN);
 }
 
 require('./core/databaseManager').comprobarSiExistenTodasLasTablasNecesarias()
 
-process.Client.comandos = require('./core/commandsManager').cargarComandoseInteracciones()
+globalThis.Client.comandos = require('./core/commandsManager').cargarComandoseInteracciones()
 
 require('./core/eventManager').cargarEventosDeProceso()
 require('./core/eventManager').cargarEventos()
 
-process.Client.modulos = require('./core/moduleManager').registrarModulos()
+globalThis.Client.modulos = require("./core/moduleManager").registrarModulos();
 
 process.on('exit', () => {
-  process.Client.destroy()
+  globalThis.Client.destroy();
 })
