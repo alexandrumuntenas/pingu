422a3712acb70706668622b71e49495c8a92d3db
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-06-06 08:38:09 +0000

    UPDATE eliminadorArchivosTemporales.ts
    Finalizarlo.

--- src/core/utils/eliminadorArchivosTemporales.ts
+++ src/core/utils/eliminadorArchivosTemporales.ts
@@ -1,38 +1,32 @@
-import { readdirSync, mkdirSync, unlinkSync, stat } from 'fs' 
-import Consolex from '../consolex' 
- 
-function eliminarArchivos (files: string[]) { 
-  for (const file of files) { 
-    stat(`./temp/${file}`, (err, stats) => { 
-      if (err) Consolex.gestionarError(err) 
- 
-      const fileDate = new Date(stats.birthtime) 
-      const now = new Date() 
- 
-      if (now - fileDate >= 600000) { 
-        Consolex.debug( 
-          `ClientManager: Eliminador de Archivos temporales ha eliminado ${file}` 
-        ) 
-        unlinkSync(`./temp/${file}`) 
-      } 
-    }) 
-  } 
-} 
- 
-function eliminadorArchivosTemporales (): void  { 
-  try { 
-    eliminarArchivos(readdirSync('./temp')) 
-  } catch { 
-    mkdirSync('./temp') 
-    Consolex.info( 
-      'Eliminador de Archivos temporales ha creado el directorio de archivos temporales' 
-    ) 
-    eliminarArchivos(readdirSync('./temp')) 
-  } 
- 
-  setInterval(() => { 
-    eliminadorArchivosTemporales() 
-  }, 300000) 
-} 
- 
-export default eliminadorArchivosTemporales 
+import { readdirSync, mkdirSync, unlinkSync, stat } from 'fs'
+import Consolex from '../consolex'
+
+function eliminarArchivos (files: string[]) {
+  for (const file of files) {
+    stat(`./temp/${file}`, (err, stats) => {
+      if (err) Consolex.gestionarError(err)
+      if (Number(new Date()) - Number(new Date(stats.birthtime)) >= 600000) {
+        Consolex.debug(`ClientManager: Eliminador de Archivos temporales ha eliminado ${file}`)
+        unlinkSync(`./temp/${file}`)
+      }
+    })
+  }
+}
+
+function eliminadorArchivosTemporales (): void {
+  try {
+    eliminarArchivos(readdirSync('./temp'))
+  } catch {
+    mkdirSync('./temp')
+    Consolex.info(
+      'Eliminador de Archivos temporales ha creado el directorio de archivos temporales'
+    )
+    eliminarArchivos(readdirSync('./temp'))
+  }
+
+  setInterval(() => {
+    eliminadorArchivosTemporales()
+  }, 300000)
+}
+
+export default eliminadorArchivosTemporales
