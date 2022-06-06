1f6c11a49398d6dd86094cb5208dc7936bbe8db2
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-06-06 09:28:23 +0000

    UPDATE moduleManager.ts
    Adaptar a TS.

--- src/core/moduleManager.ts
+++ src/core/moduleManager.ts
@@ -1,61 +1,46 @@
-const consolex = require('./consolex')
-const { inyectarEnEventoFuncionesDeTerceros } = require('./eventManager')
+import Module from '../classes/Module'
+import Consolex from './consolex'
 
-const modulos = []
+import { ClientCommandsManager } from '../client'
+import { inyectarEnEventoFuncionesDeTerceros } from './eventManager'
+import { readdirSync } from 'fs'
 
-/**
- * Registra un módulo en el listado de módulos
- * @param {Object} modulo - Objeto con la información del módulo
- * @param {String} modulo.nombre - Nombre del módulo
- * @param {?String} modulo.descripcion - Descripción del módulo
- * @param {Array} modulo.comandos - Comandos del módulo
- * @param {Array} modulo.hooks - Array de objetos con la información de los hooks
- */
+const modulosDisponibles = []
 
-module.exports.registrarModulo = (modulo) => {
-  // eslint-disable-next-line no-constant-condition
-  if (!typeof modulo === 'object') throw new Error('El módulo debe ser un objeto')
+function registrarModulo (modulo: Module) {
+  if (!ClientCommandsManager) throw new Error('Debe ejecutar esta función después de que el cliente haya cargado los comandos.')
 
-  if (!Object.prototype.hasOwnProperty.call(modulo, 'nombre') &&
-  !Object.prototype.hasOwnProperty.call(modulo, 'descripcion') &&
-  !Object.prototype.hasOwnProperty.call(modulo, 'comandos')) throw new Error('El módulo no tiene nombre, descripción o comandos')
+  modulo.asignarComandos(ClientCommandsManager.toArray().filter(command => command.module === modulo.nombre) || [])
+  Consolex.info(`ModuleManager: El módulo ${modulo.nombre} acoge los comandos ${modulo.comandos.map(command => command.name).join(', ')}`)
 
-  if (!Client.comandos) throw new Error('Debe ejecutar esta función después de que el cliente haya cargado los comandos.')
-
-  modulo.comandos = Client.comandos.filter(command => command.module === modulo.nombre) || []
-  consolex.info(`ModuleManager: El módulo ${modulo.nombre} acoge los comandos ${modulo.comandos.map(command => command.name).join(', ')}`)
-
-  if (!modulos.find(m => m.nombre === modulo.nombre)) {
-    consolex.success(`ModuleManager: Módulo ${modulo.nombre} registrado`)
-    modulos.push(modulo)
+  if (!modulosDisponibles.find((m) => m.nombre === modulo.nombre)) {
+    Consolex.success(`ModuleManager: Módulo ${modulo.nombre} registrado`)
+    modulosDisponibles.push(modulo)
     if (modulo.hooks) {
-      modulo.hooks.forEach(hook => {
-        consolex.info(`ModuleManager: Registrando hook en evento ${hook.evento} para módulo ${modulo.nombre}`)
-        inyectarEnEventoFuncionesDeTerceros(hook.event, hook.function, hook.type)
+      modulo.hooks.forEach((hook) => {
+        Consolex.info(`ModuleManager: Registrando hook en evento ${hook.evento} para módulo ${modulo.nombre}`)
+        inyectarEnEventoFuncionesDeTerceros(hook)
       })
     }
   } else {
-    consolex.error(`ModuleManager: Módulo ${modulo.nombre} ya registrado`)
+    Consolex.error(`ModuleManager: Módulo ${modulo.nombre} ya registrado`)
   }
 }
 
-const { readdirSync } = require('fs')
-
-module.exports.registrarModulos = () => {
+function registrarModulos () {
   const directorioDeModulos = readdirSync('./modules')
   directorioDeModulos.forEach(modulo => {
     if (modulo.endsWith('.js') && !modulo.endsWith('dev.js')) {
-      const { nombre, descripcion, hooks, modeloDeConfiguracion } = require(`../modules/${modulo}`)
-      module.exports.registrarModulo({ nombre: nombre || modulo.replace('.js', ''), descripcion, hooks, modeloDeConfiguracion })
+      registrarModulo(require(`../modules/${modulo}`))
     }
   })
 
-  return modulos
+  return modulosDisponibles
 }
 
-module.exports.modulosDisponibles = modulos
-
-module.exports.comprobarSiElModuloExiste = (modulo) => {
-  if (!modulos.find(m => m.nombre === modulo)) return false
+function comprobarSiElModuloExiste (modulo: string) {
+  if (!modulosDisponibles.find((m) => m.nombre === modulo)) return false
   return true
 }
+
+export { registrarModulo, registrarModulos, comprobarSiElModuloExiste, modulosDisponibles }
