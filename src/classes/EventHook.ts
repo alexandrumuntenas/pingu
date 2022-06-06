e5951a7a4658e2d61da50844a9c79666977dd291
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-06-06 09:00:46 +0000

    CREATE EventHook.ts

---
+++ src/classes/EventHook.ts
@@ -0,0 +1,19 @@
+// evento: string, funcion: Function, tipo: string | number
+
+class EventHook {
+  evento: string
+  funcion: Function
+  tipo: string | number
+
+  constructor (evento: string, funcion: Function, tipo?: string | number) {
+    this.evento = evento
+    this.funcion = funcion
+    this.tipo = tipo
+  }
+
+  execute (...args: Array<any>) {
+    this.funcion(...args)
+  }
+}
+
+export default EventHook
