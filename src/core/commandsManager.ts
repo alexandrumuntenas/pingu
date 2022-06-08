ef1d42d0373fc101430a4cffad6d578df144efe3
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-06-08 08:13:45 +0000

    UPDATE commandsManager.ts
    Añadir función para subir las interacciones.

--- src/core/commandsManager.ts
+++ src/core/commandsManager.ts
@@ -1,48 +1,55 @@
-import Consolex from './consolex'
-import Command from '../classes/Command'
+import Consolex from "./consolex";
+import Command from "../classes/Command";
 
-import { Collection, SlashCommandBuilder } from 'discord.js'
-import { lstatSync, readdirSync } from 'fs'
-import { avaliableLocales, obtenerTraduccion } from './i18nManager'
+import { Collection, Guild, SlashCommandBuilder } from "discord.js";
+import { lstatSync, readdirSync } from "fs";
+import { avaliableLocales, obtenerTraduccion } from "./i18nManager";
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v10";
+import { ClientGuildManager, ClientUser } from "../client";
 
+const rest = new REST({ version: "9" });
+
+if (process.env.ENTORNO === "publico") rest.setToken(process.env.PUBLIC_TOKEN);
+else rest.setToken(process.env.INSIDER_TOKEN);
 class CommandsManager {
-  commands: Collection<string, Command>
+  commands: Collection<string, Command>;
 
-  constructor (commandsDirectory: string) {
-    this.commands = new Collection()
-    this.loadCommands(commandsDirectory)
+  constructor(commandsDirectory: string) {
+    this.commands = new Collection();
+    this.loadCommands(commandsDirectory);
   }
 
-  add (command: Command): void {
-    this.commands.set(command.name, command)
+  add(command: Command): void {
+    this.commands.set(command.name, command);
   }
 
-  remove (command: Command): void {
-    this.commands.delete(command.name)
+  remove(command: Command): void {
+    this.commands.delete(command.name);
   }
 
-  loadCommands (directory): void {
+  loadCommands(directory): void {
     readdirSync(directory).forEach((file) => {
-      const path = `${directory}/${file}`
+      const path = `${directory}/${file}`;
 
-      if (file.endsWith('.js') && !file.endsWith('dev.js')) {
-        const command = require(`.${path}`)
+      if (file.endsWith(".js") && !file.endsWith("dev.js")) {
+        const command = require(`.${path}`);
 
-        if (Object.prototype.hasOwnProperty.call(command, 'name')) {
-          if (Object.prototype.hasOwnProperty.call(command, 'interaction')) {
-            command.interaction.setName(command.name)
+        if (Object.prototype.hasOwnProperty.call(command, "name")) {
+          if (Object.prototype.hasOwnProperty.call(command, "interaction")) {
+            command.interaction.setName(command.name);
           } else {
             command.interaction = new SlashCommandBuilder().setName(
               command.name
-            )
+            );
           }
 
           avaliableLocales.forEach((locale) => {
             command.interaction.setDescriptionLocalized(
               locale,
               obtenerTraduccion(locale, command.name)
-            )
-          })
+            );
+          });
 
           this.add(
             new Command({
@@ -53,57 +60,79 @@ class CommandsManager {
               parameters: command.parameters,
               interaction: command.interaction,
               runInteraction: command.runInteraction,
-              runCommand: command.runCommand
+              runCommand: command.runCommand,
             })
-          )
+          );
 
-          Consolex.success(`CommandsManager: Comando ${file} cargado`)
+          Consolex.success(`CommandsManager: Comando ${file} cargado`);
         } else {
           Consolex.warn(
             `CommandsManager: ${file} no se ha cargado porque no tiene una propiedad "name"`
-          )
+          );
         }
-      } else if (lstatSync(path).isDirectory()) this.loadCommands(path)
-    })
+      } else if (lstatSync(path).isDirectory()) this.loadCommands(path);
+    });
   }
 
-  getCommand (name: string) {
-    return this.commands.get(name)
+  getCommand(name: string) {
+    return this.commands.get(name);
   }
 
-  getCommands (): Collection<string, Command> {
-    return this.commands
+  getCommands(): Collection<string, Command> {
+    return this.commands;
   }
 
-  getCommandsByModule (module: string) {
-    return this.commands.filter((command) => command.module === module)
+  getCommandsByModule(module: string) {
+    return this.commands.filter((command) => command.module === module);
   }
 
-  getCommandsByModuleAndName (module: string, name: string) {
-    return this.commands.filter((command) => command.module === module && command.name === name)
+  getCommandsByModuleAndName(module: string, name: string) {
+    return this.commands.filter(
+      (command) => command.module === module && command.name === name
+    );
   }
 
-  has (name: string) {
-    return this.commands.has(name)
+  has(name: string) {
+    return this.commands.has(name);
   }
 
-  toArray () {
-    return [...this.commands.values()]
+  toArray() {
+    return [...this.commands.values()];
   }
 
-  createInteractionList (configuration: Object): Array<Object> {
-    const interactionList = []
+  createInteractionList(configuration: Object): Array<Object> {
+    const interactionList = [];
 
     this.commands.forEach((command) => {
-      if (Object.prototype.hasOwnProperty.call(configuration, command.module) && configuration[command.module].enabled) {
-        interactionList.push(command.interaction.toJSON())
+      if (
+        Object.prototype.hasOwnProperty.call(configuration, command.module) &&
+        configuration[command.module].enabled
+      ) {
+        interactionList.push(command.interaction.toJSON());
       } else {
-        interactionList.push(command.interaction.toJSON())
+        interactionList.push(command.interaction.toJSON());
       }
-    })
+    });
 
-    return interactionList
+    return interactionList;
+  }
+
+  uploadInteractions(guild: Guild): void {
+    ClientGuildManager.obtenerConfiguracionDelServidor(guild).then(
+      (configuracion) => {
+        rest
+          .put(Routes.applicationGuildCommands(ClientUser.user.id, guild.id), {
+            body: this.createInteractionList(configuracion),
+          })
+          .catch((err) => {
+            return Consolex.gestionarError(err);
+          })
+          .then(() => {
+            return null;
+          });
+      }
+    );
   }
 }
 
-export default CommandsManager
+export default CommandsManager;
