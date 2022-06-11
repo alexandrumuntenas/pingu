8523c9cb72e2287426ca8f2383fbd8cc8c16a631
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-06-11 20:26:25 +0000

    UPDATE ajustarDatosDelArchivoYAML...

--- src/core/utils/ajustarDatosDelArchivoYAMLparaQueCoincidaConElModeloDeConfiguracion.ts
+++ src/core/utils/ajustarDatosDelArchivoYAMLparaQueCoincidaConElModeloDeConfiguracion.ts
@@ -39,7 +39,7 @@ function ajustarDatosDelArchivoYAMLparaQueCoincidaConElModeloDeConfiguracion (co
     }
   })
 
-  return configuracionProcesada
+  return { configuracionProcesada, errores }
 }
 
 export default ajustarDatosDelArchivoYAMLparaQueCoincidaConElModeloDeConfiguracion
