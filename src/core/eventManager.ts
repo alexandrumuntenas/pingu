d205615692ec59f519d71d7629f78650eca62470
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-06-11 20:48:08 +0000

    EventManager a class

--- src/core/eventManager.ts
+++ src/core/eventManager.ts
@@ -5,48 +5,79 @@ import Consolex from './consolex'
 
 import { ClientUser } from '../client'
 
-const eventosDisponibles = []
+class EventManager {
+  eventosDisponibles: Array<Event>
+  eventosDisponiblesProceso: Array<Event>
+  funcionesDeTerceros: Object
 
-function cargarEventos (): Array <Event> {
-  fs.readdirSync('./events/').filter(files => files.endsWith('.js')).forEach(archivo => {
-    const event = require(`../events/${archivo}`)
-    Consolex.success(`EventManager: Evento ${archivo} cargado`)
-    eventosDisponibles.push(event)
-    ClientUser.on(event.name, async (...args) => event.execute(...args))
-  })
+  constructor () {
+    fs.readdirSync('./events/')
+      .filter((files) => files.endsWith('.js'))
+      .forEach((archivo) => {
+        const event = require(`../events/${archivo}`)
+        Consolex.success(`EventManager: Evento ${archivo} cargado`)
+        this.eventosDisponibles.push(event)
+        ClientUser.on(event.name, async (...args) => event.execute(...args))
+      })
 
-  return eventosDisponibles
+    fs.readdirSync('./events/proceso')
+      .filter((files) => files.endsWith('.js'))
+      .forEach((archivo) => {
+        const evento = require(`../events/proceso/${archivo}`)
+        Consolex.success(
+          `ProcessEventManager: Evento de proceso ${archivo} cargado`
+        )
+        this.eventosDisponiblesProceso.push(evento)
+        process.on(evento.name, async (...args) => evento.execute(...args))
+      })
+  }
+
+  inyectarEnEventoFuncionDeTercero (funcionDeTercero: EventHook): Object {
+    if (!this.funcionesDeTerceros[funcionDeTercero.evento]) {
+      this.funcionesDeTerceros[funcionDeTercero.evento] = { notype: [] }
+    }
+
+    if (
+      funcionDeTercero.tipo &&
+      !this.funcionesDeTerceros[funcionDeTercero.evento][funcionDeTercero.tipo]
+    ) {
+      this.funcionesDeTerceros[funcionDeTercero.evento][funcionDeTercero.tipo] =
+        [funcionDeTercero.funcion]
+    } else if (funcionDeTercero.tipo) {
+      this.funcionesDeTerceros[funcionDeTercero.evento][
+        funcionDeTercero.tipo
+      ].push(funcionDeTercero.funcion)
+    } else {
+      this.funcionesDeTerceros[funcionDeTercero.evento].notype.push(
+        funcionDeTercero.funcion
+      )
+    }
+    Consolex.warn(
+      `EventManager: Funciones de terceros inyectadas en evento ${funcionDeTercero.evento}`
+    )
+
+    return this.funcionesDeTerceros
+  }
+
+  ejecutarFuncionesDeTerceros (
+    evento: string,
+    tipoDeFuncion?: string | number,
+    ...argumentos: Array<any>
+  ): void {
+    // skipcq: JS-0323
+    if (
+      this.funcionesDeTerceros[evento] &&
+      this.funcionesDeTerceros[evento][tipoDeFuncion]
+    ) {
+      return this.funcionesDeTerceros[evento][tipoDeFuncion].forEach(
+        (funcion) => funcion(...argumentos)
+      )
+    } else {
+      return this.funcionesDeTerceros[evento].notype.forEach((funcion) =>
+        funcion(...argumentos)
+      )
+    }
+  }
 }
 
-const eventosDisponiblesProceso = []
-
-function cargarEventosDeProceso (): Array<Event> {
-  fs.readdirSync('./events/proceso').filter(files => files.endsWith('.js')).forEach(archivo => {
-    const evento = require(`../events/proceso/${archivo}`)
-    Consolex.success(`ProcessEventManager: Evento de proceso ${archivo} cargado`)
-    eventosDisponiblesProceso.push(evento)
-    process.on(evento.name, async (...args) => evento.execute(...args))
-  })
-
-  return eventosDisponiblesProceso
-}
-
-const funcionesDeTerceros = {}
-
-function inyectarEnEventoFuncionDeTercero (funcionDeTercero: EventHook): Object {
-  if (!funcionesDeTerceros[funcionDeTercero.evento]) funcionesDeTerceros[funcionDeTercero.evento] = { notype: [] }
-
-  if (funcionDeTercero.tipo && !funcionesDeTerceros[funcionDeTercero.evento][funcionDeTercero.tipo]) funcionesDeTerceros[funcionDeTercero.evento][funcionDeTercero.tipo] = [funcionDeTercero.funcion]
-  else if (funcionDeTercero.tipo) funcionesDeTerceros[funcionDeTercero.evento][funcionDeTercero.tipo].push(funcionDeTercero.funcion)
-  else funcionesDeTerceros[funcionDeTercero.evento].notype.push(funcionDeTercero.funcion)
-  Consolex.warn(`EventManager: Funciones de terceros inyectadas en evento ${funcionDeTercero.evento}`)
-
-  return funcionesDeTerceros
-}
-
-function ejecutarFuncionesDeTerceros (evento: string, tipoDeFuncion?: string | number, ...argumentos: Array<any>): void { // skipcq: JS-0323
-  if (funcionesDeTerceros[evento] && funcionesDeTerceros[evento][tipoDeFuncion]) return funcionesDeTerceros[evento][tipoDeFuncion].forEach(funcion => funcion(...argumentos))
-  else return funcionesDeTerceros[evento].notype.forEach(funcion => funcion(...argumentos))
-}
-
-export { cargarEventos, cargarEventosDeProceso, inyectarEnEventoFuncionDeTercero, ejecutarFuncionesDeTerceros, funcionesDeTerceros }
+export default EventManager
