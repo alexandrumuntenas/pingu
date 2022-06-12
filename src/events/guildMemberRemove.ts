05a76c809806031d05034a4739941164134deef8
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-06-12 15:26:35 +0000

    SOLVE JS-0359 in GuildMemberRemove

--- src/events/guildMemberRemove.ts
+++ src/events/guildMemberRemove.ts
@@ -1,13 +1,11 @@
 import Event from '../classes/Event'
 
-import { ClientUser } from '../client'
+import { ClientEventManager, ClientMemberManager, ClientUser } from '../client'
 import { GuildMember } from 'discord.js'
-const { ejecutarFuncionesDeTerceros } = require('../core/eventManager')
-const { eliminarDatosDelUsuario } = require('../core/memberManager')
 
 export default new Event('guildMemberRemove', (member: GuildMember) => {
   if (member.user.id !== ClientUser.user.id) {
-    ejecutarFuncionesDeTerceros('guildMemberRemove', null, member)
-    eliminarDatosDelUsuario(member)
+    ClientEventManager.ejecutarFuncionesDeTerceros('guildMemberRemove', null, member)
+    ClientMemberManager.eliminarDatosDelUsuario(member)
   }
 })
