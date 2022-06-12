494c8218b83b74933fb379f6179a425844bbcd4a
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-06-12 14:41:38 +0000

    UPDATE guildMemberRemove

--- src/events/guildMemberRemove.ts
+++ src/events/guildMemberRemove.ts
@@ -1,12 +1,13 @@
+import Event from '../classes/Event'
+
+import { ClientUser } from '../client'
+import { GuildMember } from 'discord.js'
 const { ejecutarFuncionesDeTerceros } = require('../core/eventManager')
 const { eliminarDatosDelUsuario } = require('../core/memberManager')
 
-module.exports = {
-  name: 'guildMemberRemove',
-  execute: member => { // skipcq: JS-0116
-    if (member.user.id !== Client.user.id) {
-      ejecutarFuncionesDeTerceros('guildMemberRemove', null, member)
-      eliminarDatosDelUsuario(member)
-    }
+export default new Event('guildMemberRemove', (member: GuildMember) => {
+  if (member.user.id !== ClientUser.user.id) {
+    ejecutarFuncionesDeTerceros('guildMemberRemove', null, member)
+    eliminarDatosDelUsuario(member)
   }
-}
+})
