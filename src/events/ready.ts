511f51a4c14c9b2e81ed033f095b8b80b68296a7
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-06-12 14:39:34 +0000

    UPDATE ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,21 +1,19 @@
+import Event from '../classes/Event'
 import Consolex from '../core/consolex'
-import { ClientUser } from '../client'
 import eliminadorArchivosTemporales from '../core/utils/eliminadorArchivosTemporales'
-import { ejecutarFuncionesDeTerceros } from '../core/eventManager'
+
 import { ActivityType } from 'discord.js'
+import { ClientEventManager, ClientUser } from '../client'
 
-module.exports = {
-  name: 'ready',
-  execute: () => {
-    Consolex.info(`Conectado como ${ClientUser.user.tag}!`)
+export default new Event('ready', () => {
+  Consolex.info(`Conectado como ${ClientUser.user.tag}!`)
 
-    eliminadorArchivosTemporales()
-    ClientUser.user.setActivity('new update TS2203', { type: ActivityType.Watching })
+  eliminadorArchivosTemporales()
+  ClientUser.user.setActivity('new update TS2203', { type: ActivityType.Watching })
 
-    ejecutarFuncionesDeTerceros('guildMemberAdd')
+  ClientEventManager.ejecutarFuncionesDeTerceros('guildMemberAdd')
 
-    setInterval(() => {
-      ClientUser.user.setActivity(`${ClientUser.guilds.cache.size} guilds`, { type: ActivityType.Watching })
-    }, 600000)
-  }
-}
+  setInterval(() => {
+    ClientUser.user.setActivity(`${ClientUser.guilds.cache.size} guilds`, { type: ActivityType.Watching })
+  }, 600000)
+})
