3f6a303383aeebdb3acfe953c389c100f13a93b4
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-06-12 14:41:26 +0000

    UPDATE guildMemberAdd

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,6 +1,7 @@
 import Event from '../classes/Event'
+import { GuildMember } from 'discord.js'
 import { ClientEventManager } from '../client'
 
-export default new Event('guildMemberAdd', (member) => {
+export default new Event('guildMemberAdd', (member: GuildMember) => {
   ClientEventManager.ejecutarFuncionesDeTerceros('guildMemberAdd', null, member)
 })
