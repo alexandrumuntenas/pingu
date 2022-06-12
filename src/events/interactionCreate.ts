cff68ac89512d70140c6152be8d78213b49f72cf
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-06-12 15:02:26 +0000

    UPDATE interactionCreate.ts

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,46 +1,49 @@
-const consolex = require('../core/consolex')
-const CooldownManager = require('../core/cooldownManager')
+import { ChatInputCommandInteraction, Interaction, InteractionDeferReplyOptions } from 'discord.js'
+import * as humanizeDuration from 'humanize-duration'
+import Consolex from '../core/consolex'
+import { obtenerTraduccion } from '../core/i18nManager'
+import {
+  ClientCommandsManager,
+  ClientCooldownManager,
+  ClientGuildManager,
+  ClientMessageTemplate
+} from '../client'
+import Event from '../classes/Event'
 
-const { plantillas } = require('../core/messageManager')
-const { obtenerConfiguracionDelServidor } = require('../core/guildManager.js')
-const i18n = require('../core/i18nManager')
-const humanizeduration = require('humanize-duration')
+class PinguChatInputCommandInteraction extends ChatInputCommandInteraction {
+  deferredReply?: InteractionDeferReplyOptions
+  guildConfiguration?: { [key: string]: any }
+}
 
-async function isChatInputCommand (interaction) {
-  if (interaction.channel.type === 'dm' || interaction.author === Client.user) return
-
-  interaction.deferredReply = await interaction.deferReply({ fetchReply: true }) // skipcq: JS-0040
-  obtenerConfiguracionDelServidor(interaction.guild).then(async configuracionDelServidor => {
-    interaction.guild.configuration = configuracionDelServidor
-    if (Client.comandos.has(interaction.commandName)) {
-      const interactionToRun = Client.comandos.get(interaction.commandName)
+async function isChatInputCommand (interaction: PinguChatInputCommandInteraction) {
+  ClientGuildManager.obtenerConfiguracionDelServidor(interaction.guild).then(async configuracionDelServidor => {
+    interaction.guildConfiguration = configuracionDelServidor
+    if (ClientCommandsManager.has(interaction.commandName)) {
+      const interactionToRun = ClientCommandsManager.getCommand(interaction.commandName)
 
       if (interactionToRun.module && !configuracionDelServidor[interactionToRun.module].enabled) {
-        return interaction.editReply({ embeds: [plantillas.error(i18n.obtenerTraduccion(interaction.guild.preferredLocale, 'COMMAND::NOT_ENABLED'))] })
+        return interaction.editReply({ embeds: [ClientMessageTemplate.error(obtenerTraduccion(interaction.guild.preferredLocale, 'COMMAND::NOT_ENABLED'))] })
       }
 
-      if (interactionToRun.permissions && !interaction.member.permissions.has(interactionToRun.permissions)) {
-        return interaction.editReply({ embeds: [plantillas.error(i18n.obtenerTraduccion(interaction.guild.preferredLocale, 'COMMAND::PERMISSION_ERROR'))] })
+      if (interactionToRun.permissions && !interaction.memberPermissions.has(interactionToRun.permissions)) {
+        return interaction.editReply({ embeds: [ClientMessageTemplate.error(obtenerTraduccion(interaction.guild.preferredLocale, 'COMMAND::PERMISSION_ERROR'))] })
       }
 
-      if (CooldownManager.check(interaction.member, interaction.guild, interactionToRun.name)) {
-        CooldownManager.add(interaction.member, interaction.guild, interactionToRun)
+      if (ClientCooldownManager.check(interaction.member, interaction.guild, interactionToRun)) {
+        ClientCooldownManager.add(interaction.member, interaction.guild, interactionToRun)
 
         await interactionToRun.runInteraction(interaction)
       } else {
-        return interaction.editReply({ embeds: [plantillas.contador(i18n.obtenerTraduccion(interaction.guild.preferredLocale, 'COOLDOWN', { COOLDOWN: humanizeduration(CooldownManager.ttl(interaction.member, interaction.guild, interactionToRun.name), { round: true, language: interaction.guild.configuration.common.language || 'en-US', fallbacks: ['en-US'] }) }))] })
+        return interaction.editReply({ embeds: [ClientMessageTemplate.timeout(obtenerTraduccion(interaction.guild.preferredLocale, 'COOLDOWN', { COOLDOWN: humanizeDuration(ClientCooldownManager.ttl(interaction.member, interaction.guild, interactionToRun), { round: true, language: interaction.guildConfiguration.common.language || 'en-US', fallbacks: ['en-US'] }) }))] })
       }
     } else {
-      return interaction.editReply({ content: i18n.obtenerTraduccion(interaction.guild.preferredLocale, 'COMMAND::NOT_FOUND') })
+      return interaction.editReply({ content: obtenerTraduccion(interaction.guild.preferredLocale, 'COMMAND::NOT_FOUND') })
     }
   })
 }
 
-module.exports = {
-  name: 'interactionCreate',
-  execute: async interaction => { // skipcq: JS-0116
-    if (interaction.isCommand()) {
-      isChatInputCommand(interaction).catch(consolex.gestionarError)
-    }
+export default new Event('interactionCreate', (interaction: Interaction) => {
+  if (interaction.isChatInputCommand()) {
+    isChatInputCommand(interaction).catch(Consolex.gestionarError)
   }
-}
+})
