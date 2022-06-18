b6e38dd232fc78fa10f8e9a9c9d2e0354c401fad
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-06-18 18:36:49 +0000

    Terminar de ajustar customCommands.

--- src/modules/customcommands.ts
+++ src/modules/customcommands.ts
@@ -1,10 +1,14 @@
 import { Guild } from 'discord.js'
+import { ClientUser } from '../client'
+import EventHook from '../core/classes/EventHook'
+import Module from '../core/classes/Module'
 import { PinguMessage } from '../events/messageCreate'
+import CustomCommand from './classes/CustomCommand'
 
 const Database = require('../core/databaseManager')
 const consolex = require('../core/consolex')
 
-async function getCustomCommand (guild: Guild, command: string) {
+async function getCustomCommand (guild: Guild, command: string): Promise<CustomCommand> {
   try {
     const [customCommand] = await Database.execute(
       'SELECT * FROM `guildCustomCommands` WHERE `guild` = ? AND `customCommand` = ? LIMIT 1',
@@ -13,22 +17,28 @@ async function getCustomCommand (guild: Guild, command: string) {
       Object.prototype.hasOwnProperty.call(result, 0) ? result : {}
     )
 
-    return JSON.parse(customCommand.customcommandproperties)
+    const customCommandProperties = JSON.parse(customCommand.customCommandProperties)
+
+    return new CustomCommand(ClientUser.guilds.resolve(customCommand.guild), customCommand.customCommand, customCommandProperties)
   } catch (err) {
     consolex.gestionarError(err)
   }
 }
 
-module.exports.hooks = [
-  {
-    event: 'messageCreate',
-    type: 'withPrefix',
-    function: (message: PinguMessage) => {
+export default new Module(
+  'CustomCommands',
+  'Base module for custom commands',
+  [
+    new EventHook('messageCreate', (message: PinguMessage) => {
       if (message.guildConfiguration.customcommands.enabled) {
-        getCustomCommand(message.guild, message.content).then((customCommand) => {
-
-        })
+        getCustomCommand(message.guild, message.content).then(
+          (customCommand) => { customCommand.executeCommand(message) }
+        )
       }
-    }
-  }
-]
+    })
+  ],
+  {
+    enabled: 'boolean'
+  },
+  { enabled: false }
+)
