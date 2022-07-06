b8dd36e5ddd6bc45a0a67178369b1d26b9b05b16
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-07-06 21:03:18 +0000

    Revisar consolex

--- src/core/consolex.ts
+++ src/core/consolex.ts
@@ -1,34 +1,36 @@
-import chalk from 'chalk';
-import * as moment from 'moment';
-import * as Sentry from '@sentry/node';
+import chalk from 'chalk'
+import * as moment from 'moment'
+import * as Sentry from '@sentry/node'
 
-if (process.env.SENTRY_DSN) Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  tracesSampleRate: 1.0,
-  environment: process.env.NODE_ENV,
-});
+if (process.env.SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 1.0,
+    environment: process.env.NODE_ENV
+  })
+}
 
 function getCurrentTime () {
   return moment().format('MMM Do YY H:mm:ss')
 }
 
 const Consolex = {
-  debug: message => {
+  debug: (message: string | unknown) => {
     process.stdout.write(`${getCurrentTime()} [${chalk.whiteBright.bold(' DEBUG ')}]   ${message}\n`)
   },
-  info: message => {
+  info: (message: string | unknown) => {
     process.stdout.write(`${getCurrentTime()} [${chalk.cyanBright.bold('INFORMA')}]   ${message}\n`)
   },
-  success: message => {
+  success: (message: string | unknown) => {
     process.stdout.write(`${getCurrentTime()} [${chalk.greenBright.bold('SUCCESS')}]   ${message}\n`)
   },
-  warn: message => {
+  warn: (message: string | unknown) => {
     process.stdout.write(`${getCurrentTime()} [${chalk.yellowBright.bold('WARNING')}]   ${message}\n`)
   },
-  error: message => {
+  error: (message: string | unknown) => {
     process.stdout.write(`${getCurrentTime()} [${chalk.redBright.bold('ERROR')}]     ${message}\n`)
   },
-  gestionarError: err => {
+  gestionarError: (err: string | unknown) => {
     module.exports.error(err)
     module.exports.Sentry.captureException(err)
     throw err
@@ -36,4 +38,4 @@ const Consolex = {
   Sentry
 }
 
-export default Consolex;
+export default Consolex
