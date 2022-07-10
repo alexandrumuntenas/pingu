25221ec21e77076c13fc166fd1af8e1907bf7820
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-07-10 22:00:58 +0000

    Revisar procesarObjetosDeConfiguracion.ts

--- src/core/utils/procesarObjetosdeConfiguracion.ts
+++ src/core/utils/procesarObjetosdeConfiguracion.ts
@@ -1,12 +1,10 @@
-function procesarObjetosdeConfiguracion (config, newconfig) {
-  if (newconfig instanceof Object === false) return newconfig
-
+function procesarObjetosdeConfiguracion (config: { [index: string]: any }, newconfig: { [index: string]: any }) {
   let count = 0
 
   const newConfigProperties = Object.keys(newconfig)
   newConfigProperties.forEach(property => {
     if (Object.prototype.hasOwnProperty.call(config, property) && typeof newconfig[property] === 'object') {
-      config[property] = module.exports(config[property], newconfig[property])
+      config[property] = procesarObjetosdeConfiguracion(config[property], newconfig[property])
       count += 1
     } else {
       config[property] = newconfig[property]
