f0880dc0e8915ff92a324aae46193dc239096d88
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-07-10 21:02:46 +0000

    Fusionado también claveTraduccionSolicitada y parametrosTraduccionSolicitada en un solo argumento. Clave es el sustituto de claveTraduccionSolicitada en el objeto de traduccionSolicitada.

--- src/core/i18nManager.ts
+++ src/core/i18nManager.ts
@@ -2,20 +2,20 @@ import stringPlaceholder from 'string-placeholder'
 import { existsSync } from 'fs'
 import Consolex from './consolex'
 
-function obtenerTraduccion (claveTraduccionSolicitada: string, parametrosTraduccionSolicitada?: { idioma?: string, placeholders?: Array<string> }): string {
-  if (parametrosTraduccionSolicitada?.idioma && !existsSync(`-/core/locales/${parametrosTraduccionSolicitada?.idioma}.json`)) {
-    Consolex.gestionarError(`[i18n Utils] INE001: The requested translation file ${parametrosTraduccionSolicitada?.idioma} has not been found. Using es-ES as fallback.`)
+function obtenerTraduccion (traduccionSolicitada: { clave: string, idioma?: string, placeholders?: Array<string> }): string {
+  if (traduccionSolicitada?.idioma && !existsSync(`-/core/locales/${traduccionSolicitada?.idioma}.json`)) {
+    Consolex.gestionarError(`[i18n Utils] INE001: The requested translation file ${traduccionSolicitada?.idioma} has not been found. Using es-ES as fallback.`)
   }
 
-  let textoTraducido = require(`./locales/${parametrosTraduccionSolicitada?.idioma || 'es-ES'}.json`)[claveTraduccionSolicitada]
+  let textoTraducido = require(`./locales/${traduccionSolicitada?.idioma || 'es-ES'}.json`)[traduccionSolicitada.clave]
 
   if (!textoTraducido) {
-    Consolex.gestionarError(`[i18n Utils] INE002: The key specified "${claveTraduccionSolicitada}" to obtain your translation does not exist. Returning error to the requester.`)
-    return `INE002: The key specified "${claveTraduccionSolicitada}" to obtain your translation does not exist. Returning error to the requester.`
+    Consolex.gestionarError(`[i18n Utils] INE002: The key specified "${traduccionSolicitada.clave}" to obtain your translation does not exist. Returning error to the requester.`)
+    return `INE002: The key specified "${traduccionSolicitada.clave}" to obtain your translation does not exist. Returning error to the requester.`
   }
 
-  if (parametrosTraduccionSolicitada?.placeholders) {
-    parametrosTraduccionSolicitada?.placeholders.forEach((parametro, posicion) => {
+  if (traduccionSolicitada?.placeholders) {
+    traduccionSolicitada?.placeholders.forEach((parametro, posicion) => {
       const placeholder: { [index: string]: string } = {}
       placeholder[posicion.toString()] = parametro
 
