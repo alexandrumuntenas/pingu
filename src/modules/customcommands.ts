f92a3c18fc1c4be00b316a1f3d1f854c992bad5e
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-07-10 13:07:38 +0000

    Revisar customcommands.ts

--- src/modules/customcommands.ts
+++ src/modules/customcommands.ts
@@ -2,27 +2,19 @@ import { Guild } from 'discord.js'
 import { ClientUser } from '../client'
 import EventHook from '../core/classes/EventHook'
 import Module from '../core/classes/Module'
+import Consolex from '../core/consolex'
+import { PoolConnection } from '../core/databaseManager'
 import { PinguMessage } from '../events/messageCreate'
 import CustomCommand from './classes/CustomCommand'
 
-const Database = require('../core/databaseManager')
-const consolex = require('../core/consolex')
+async function getCustomCommand (guild: Guild | null, command: string): Promise<CustomCommand> {
+  if (!(guild instanceof Guild)) throw new Error('El "Guild" especificado no existe.')
 
-async function getCustomCommand (guild: Guild, command: string): Promise<CustomCommand> {
-  try {
-    const [customCommand] = await Database.execute(
-      'SELECT * FROM `guildCustomCommands` WHERE `guild` = ? AND `customCommand` = ? LIMIT 1',
-      [guild.id, command]
-    ).then((result) =>
-      Object.prototype.hasOwnProperty.call(result, 0) ? result : {}
-    )
+  const [customCommand] = await PoolConnection.execute('SELECT * FROM `guildCustomCommands` WHERE `guild` = ? AND `customCommand` = ? LIMIT 1', [guild.id, command]).then((result: any) => Object.prototype.hasOwnProperty.call(result, 0) ? result : {}).catch((error) => Consolex.gestionarError(error))
 
-    const customCommandProperties = JSON.parse(customCommand.customCommandProperties)
+  const customCommandProperties = JSON.parse(customCommand.customCommandProperties)
 
-    return new CustomCommand(ClientUser.guilds.resolve(customCommand.guild), customCommand.customCommand, customCommandProperties)
-  } catch (err) {
-    consolex.gestionarError(err)
-  }
+  return new CustomCommand(ClientUser.guilds.resolve(customCommand.guild), customCommand.customCommand, customCommandProperties)
 }
 
 export default new Module(
