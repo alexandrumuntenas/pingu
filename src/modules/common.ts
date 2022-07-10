bea8d0dec94588de06db32df3d7adf0d67f52047
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-07-10 13:05:10 +0000

    Revisar common.ts

--- src/modules/common.ts
+++ src/modules/common.ts
@@ -1,5 +1,5 @@
 import Module from '../core/classes/Module'
 
-export default new Module('Common', 'Base module for common functions', [], {}, {
+export default new Module('Common', 'Módulo base para todas las configuraciones necesarias para el funcionamiento básico de Pingu en los servidores de Discord.', [], {}, {
   prefix: 'string'
 })
