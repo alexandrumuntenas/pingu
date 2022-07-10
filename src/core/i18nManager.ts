c9cadd6732592c85076d75347a7c5b7042676509
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-07-10 20:52:31 +0000

    Crear nueva función para obtener traducciones.

--- src/core/i18nManager.ts
+++ src/core/i18nManager.ts
@@ -2,6 +2,33 @@ import stringPlaceholder from 'string-placeholder'
 import { existsSync } from 'fs'
 import Consolex from './consolex'
 
+function obtenerTraduccion (claveTraduccionSolicitada: string, parametrosTraduccionSolicitada: Array<string>, idiomaTraduccionSolicitada?: string): string {
+  if (idiomaTraduccionSolicitada && !existsSync(`-/core/locales/${idiomaTraduccionSolicitada}.json`)) {
+    Consolex.gestionarError(`[i18n Utils] INE001: The requested translation file ${idiomaTraduccionSolicitada} has not been found. Using es-ES as fallback.`)
+  }
+
+  let textoTraducido = require(`./locales/${idiomaTraduccionSolicitada || 'es-ES'}.json`)[claveTraduccionSolicitada]
+
+  if (!textoTraducido) {
+    Consolex.gestionarError(`[i18n Utils] INE002: The key specified "${claveTraduccionSolicitada}" to obtain your translation does not exist. Returning error to the requester.`)
+  }
+
+  if (parametrosTraduccionSolicitada) {
+    parametrosTraduccionSolicitada.forEach((parametro, posicion) => {
+      const placeholder: { [index: string]: string } = {}
+      placeholder[posicion.toString()] = parametro
+
+      try {
+        textoTraducido = stringPlaceholder(textoTraducido, placeholder, { before: '{', after: '}' })
+      } catch (error) {
+        Consolex.gestionarError('[i18n Utils] INE003: Error when trying to adjust the translation.')
+      }
+    })
+  }
+
+  return textoTraducido
+}
+
 function deprecatedObtenerTraduccion (traduccion: string, parametros?: Object, idioma?: string): string {
   let idiomaAUsar = idioma || 'es-ES'
   if (!existsSync(`./core/locales/${idiomaAUsar}.json`)) {
@@ -29,4 +56,4 @@ function deprecatedObtenerTraduccion (traduccion: string, parametros?: Object, i
 
 const avaliableLocales: string[] = []
 
-export { deprecatedObtenerTraduccion, avaliableLocales }
+export { obtenerTraduccion, deprecatedObtenerTraduccion, avaliableLocales }
