bfe00bfb79a0f514fa18ce619afe5aa4aabe9143
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-07-10 21:59:41 +0000

    Revisar EventHook.

--- src/core/classes/EventHook.ts
+++ src/core/classes/EventHook.ts
@@ -3,7 +3,7 @@
 class EventHook {
   evento: string
   funcion: Function
-  tipo: string | number
+  tipo?: string | number
 
   constructor (evento: string, funcion: Function, tipo?: string | number) {
     this.evento = evento
