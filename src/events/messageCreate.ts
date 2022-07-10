5cb052961f3141834349395824cd2fc99dde7564
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-07-10 21:22:38 +0000

    Descontinuar los prefijos personalizados en messageCreate. Se motivará a los usuarios a realizar un ping a Pingu. Pequeña "salvación" en caso de que se nos retire el messageContent.

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -30,97 +30,78 @@ interface PinguMessage extends Message {
 export default new Event('messageCreate', (message: PinguMessage) => {
   if (message.channel.type === ChannelType.DM || message.author.bot || message.author === ClientUser.user) { return }
 
-  ClientGuildManager.obtenerConfiguracionDelServidor(message.guild).then(
-    (configuracionDelServidor) => {
-      message.guildConfiguration = configuracionDelServidor
+  ClientGuildManager.obtenerConfiguracionDelServidor(message.guild).then((configuracionDelServidor) => {
+    message.guildConfiguration = configuracionDelServidor
 
-      ClientEventManager.ejecutarFuncionesDeTerceros('messageCreate', null, message)
+    ClientEventManager.ejecutarFuncionesDeTerceros('messageCreate', null, message)
 
-      // TODO: ELIMINAR LA PARTE DEL USO DEL PREFIJO PERSONALIZADO.
-      if (
-        (message.content.startsWith(message.guildConfiguration.common.prefix) &&
-          message.content !== message.guildConfiguration.common.prefix) ||
-        message.content.startsWith(`<@${ClientUser.user?.id}>`) ||
-        message.content.startsWith(`<@!${ClientUser.user?.id}>`)
-      ) {
-        if (message.content.startsWith(`<@${ClientUser.user?.id}>`)) {
-          message.args = message.content
-            .slice(`<@${ClientUser.user?.id}>`.length)
-            .trim()
-            .split(/ +/)
-        } else if (message.content.startsWith(`<@!${ClientUser.user?.id}>`)) {
-          message.args = message.content
-            .slice(`<@!${ClientUser.user?.id}>`.length)
-            .trim()
-            .split(/ +/)
-        } else {
-          message.args = message.content
-            .slice(message.guildConfiguration.common.prefix.length)
-            .trim()
-            .split(/ +/)
-        }
+    if (message.content.startsWith(ClientUser.user?.toString() || `<@!${ClientUser.user?.id}>`)) {
+      message.args = message.content
+        .slice(ClientUser.user?.toString().length)
+        .trim()
+        .split(/ +/)
 
-        [message.rawCommand] = message.args
-        message.args.shift()
+      [message.rawCommand] = message.args
+      message.args.shift()
 
-        // TODO: ELIMINAR ESTO. SE DESCONTINUA LA OBLIGACIÓN DE USAR INTERACCIONES. EL PREFIJO PREDETERMINADO SERÁ EL PING DEL BOT.
-        if (message.guildConfiguration.interactions && message.guildConfiguration.interactions.enforceusage) {
-          return message.reply({
-            embeds: [
-              ClientMessageTemplate.error(obtenerTraduccion('INTERACTION-ENFORCEUSAGE'))
-            ]
-          })
-        }
-
-        message.command = ClientCommandsManager.getCommand(message.rawCommand)
-
-        if (ClientCooldownManager.check(message.member, message.command || { name: message.rawCommand })) {
-          if (ClientCommandsManager.has(message.command?.name || message.rawCommand)) {
-            if (message.command && message.command.module && ClientModuleManager.nombresModulosDisponibles.includes(message.command.module) && !message.guildConfiguration[message.command.module].enabled) {
-              return message.reply({
-                embeds: [
-                  ClientMessageTemplate.error(obtenerTraduccion({ clave: 'EL_MODULO_AL_QUE_PERTENECE_EL_COMANDO_NO_ESTA_ACTIVO', idioma: message.guild?.preferredLocale }))
-                ]
-              })
-            }
-
-            if (message.command?.permissions && !message.member?.permissions.has(message.command.permissions, false)) {
-              return message.reply({
-                embeds: [
-                  ClientMessageTemplate.error(obtenerTraduccion({ clave: 'NO_DISPONES_DE_LOS_SUFICIENTES_PERMISOS_PARA_EJECUTAR_ESTE_COMANDO', idioma: message.guild?.preferredLocale }))
-                ]
-              })
-            }
-
-            ClientCooldownManager.add(message.member, message.command || { name: message.rawCommand })
-
-            if (message.command && Object.prototype.hasOwnProperty.call(message.command, 'runCommand') && message.command.runCommand instanceof Function) {
-              return message.command?.runCommand(message)
-            } else if (message.command) {
-              return message.reply({
-                embeds: [
-                  ClientMessageTemplate.error(
-                    obtenerTraduccion({ clave: 'EL_COMANDO_SOLICITADO_SOLO_ESTA_DISPONIBLE_COMO_INTERACCION', idioma: message.guild?.preferredLocale })
-                  )
-                ]
-              })
-            }
-          }
-          return ClientEventManager.ejecutarFuncionesDeTerceros(
-            'messageCreate',
-            'withPrefix',
-            message
-          )
-        }
-        return message.reply({embeds: [ClientMessageTemplate.timeout(obtenerTraduccion({ clave: 'DEBE_ESPERAR_X_TIEMPO_PARA_PODER_EJECUTAR_ESTE_COMANDO', idioma: message.guild?.preferredLocale, placeholders: [humanizarTiempo(ClientCooldownManager.ttl(message.member, message.command || { name: message.rawCommand }), { round: true, language: message.guild?.preferredLocale, fallbacks: ['en'] })] }))]})
+      // TODO: ELIMINAR ESTO. SE DESCONTINUA LA OBLIGACIÓN DE USAR INTERACCIONES. EL PREFIJO PREDETERMINADO SERÁ EL PING DEL BOT.
+      if (message.guildConfiguration.interactions && message.guildConfiguration.interactions.enforceusage) {
+        return message.reply({
+          embeds: [
+            ClientMessageTemplate.error(obtenerTraduccion('INTERACTION-ENFORCEUSAGE'))
+          ]
+        })
       }
 
-      return ClientEventManager.ejecutarFuncionesDeTerceros(
-        'messageCreate',
-        'noPrefix',
-        message
-      )
+      message.command = ClientCommandsManager.getCommand(message.rawCommand)
+
+      if (ClientCooldownManager.check(message.member, message.command || { name: message.rawCommand })) {
+        if (ClientCommandsManager.has(message.command?.name || message.rawCommand)) {
+          if (message.command && message.command.module && ClientModuleManager.nombresModulosDisponibles.includes(message.command.module) && !message.guildConfiguration[message.command.module].enabled) {
+            return message.reply({
+              embeds: [
+                ClientMessageTemplate.error(obtenerTraduccion({ clave: 'EL_MODULO_AL_QUE_PERTENECE_EL_COMANDO_NO_ESTA_ACTIVO', idioma: message.guild?.preferredLocale }))
+              ]
+            })
+          }
+
+          if (message.command?.permissions && !message.member?.permissions.has(message.command.permissions, false)) {
+            return message.reply({
+              embeds: [
+                ClientMessageTemplate.error(obtenerTraduccion({ clave: 'NO_DISPONES_DE_LOS_SUFICIENTES_PERMISOS_PARA_EJECUTAR_ESTE_COMANDO', idioma: message.guild?.preferredLocale }))
+              ]
+            })
+          }
+
+          ClientCooldownManager.add(message.member, message.command || { name: message.rawCommand })
+
+          if (message.command && Object.prototype.hasOwnProperty.call(message.command, 'runCommand') && message.command.runCommand instanceof Function) {
+            return message.command?.runCommand(message)
+          } else if (message.command) {
+            return message.reply({
+              embeds: [
+                ClientMessageTemplate.error(
+                  obtenerTraduccion({ clave: 'EL_COMANDO_SOLICITADO_SOLO_ESTA_DISPONIBLE_COMO_INTERACCION', idioma: message.guild?.preferredLocale })
+                )
+              ]
+            })
+          }
+        }
+        return ClientEventManager.ejecutarFuncionesDeTerceros(
+          'messageCreate',
+          'withPrefix',
+          message
+        )
+      }
+      return message.reply({ embeds: [ClientMessageTemplate.timeout(obtenerTraduccion({ clave: 'DEBE_ESPERAR_X_TIEMPO_PARA_PODER_EJECUTAR_ESTE_COMANDO', idioma: message.guild?.preferredLocale, placeholders: [humanizarTiempo(ClientCooldownManager.ttl(message.member, message.command || { name: message.rawCommand }), { round: true, language: message.guild?.preferredLocale, fallbacks: ['en'] })] }))] })
     }
+
+    return ClientEventManager.ejecutarFuncionesDeTerceros(
+      'messageCreate',
+      'noPrefix',
+      message
+    )
+  }
   ).catch((err) => {
     Consolex.gestionarError(err)
   })
