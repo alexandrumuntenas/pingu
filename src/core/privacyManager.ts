202ab0e33d0ba93db4965503be064ae4ca90fa88
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-07-16 20:17:41 +0000

    Crear función dataUserTakeout

--- src/core/privacyManager.ts
+++ src/core/privacyManager.ts
@@ -1,16 +1,40 @@
-import { Guild, GuildMember } from "discord.js"
-import Consolex from "./consolex"
-import { PoolConnection, tablasDisponibles } from "./databaseManager"
+import { Guild, GuildMember, User } from 'discord.js'
+import { writeFileSync } from 'fs'
+import { PoolConnection, tablasDisponibles } from './databaseManager'
+
+import Consolex from './consolex'
+import * as randomstring from 'randomstring'
 
 class PrivacyManager {
-  dataTakeout () {
-    return 'Not implemented'
+  tablasConDatosDeUsuario: string[]
+  tablasConDatosDeServidor: string[]
+
+  constructor () {
+    this.tablasConDatosDeServidor = tablasDisponibles.filter((tabla) => tabla.startsWith('guild'))
+    this.tablasConDatosDeUsuario = tablasDisponibles.filter((tabla) => tabla.startsWith('member'))
+  }
+
+  dataUserDataTakeout (user: User): string {
+    const documentoConTodosLosDatos: { [index: string]: any } = {}
+    const rutaDocumento = `./temp/${randomstring.generate({ charset: 'alphabetic' })}.png`
+
+    this.tablasConDatosDeUsuario.forEach((tabla) => {
+      PoolConnection.execute('SELECT * FROM ?? WHERE member ?', [tabla, user.id]).catch((error) => {
+        Consolex.gestionarError(error)
+      }).then((datos: any) => {
+        if (Object.prototype.hasOwnProperty.call(datos, 0)) {
+          documentoConTodosLosDatos[tabla] = datos[0]
+        }
+      })
+    })
+
+    writeFileSync(rutaDocumento, JSON.stringify(documentoConTodosLosDatos))
+
+    return rutaDocumento
   }
 
   eliminarGuildMemberData (member: GuildMember): void {
-    const tablasConDatosDeUsuario = tablasDisponibles.filter((tabla) => tabla.startsWith('member'))
-
-    tablasConDatosDeUsuario.forEach((tabla) => {
+    this.tablasConDatosDeUsuario.forEach((tabla) => {
       PoolConnection.execute('DELETE FROM ?? WHERE member = ? AND guild = ?', [tabla, member.id, member.guild.id]).catch((error) => {
         Consolex.gestionarError(error)
       })
@@ -18,9 +42,7 @@ class PrivacyManager {
   }
 
   eliminarGuildData (guild: Guild) {
-    const tablasConDatosDeServidor = tablasDisponibles.filter((tabla) => tabla.startsWith('guild'))
-
-    tablasConDatosDeServidor.forEach((tabla) => {
+    this.tablasConDatosDeServidor.forEach((tabla) => {
       PoolConnection.execute('DELETE FROM ?? WHERE guild = ?', [tabla, guild.id]).catch((error) => {
         Consolex.gestionarError(error)
       })
