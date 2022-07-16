092e493d81334e02c31f57ae296fbe179be792f1
#59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2022-07-16 20:35:15 +0000

    Revert "Feat: Crear la clase BaseClient. (#320)"
    This reverts commit 5cd68a531b832207a9d8f7ca80bb95d64858ff3e.

--- src/client.ts
+++ src/client.ts
@@ -10,37 +10,76 @@
 import 'dotenv/config'
 import * as Discord from 'discord.js'
 import { GatewayIntentBits } from 'discord-api-types/v10'
-class BaseClient {
-  BaseClient = Discord.Client
 
-  constructor () {
-    const ClientUser: Discord.Client = new Discord.Client({
-      intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildBans,
-        GatewayIntentBits.GuildInvites,
-        GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMessageReactions,
-        GatewayIntentBits.GuildMessageTyping,
-        GatewayIntentBits.MessageContent
-      ],
-      partials: [
-        Discord.Partials.Reaction,
-        Discord.Partials.Message,
-        Discord.Partials.User
-      ],
-      ws: { properties: { $browser: 'Discord iOS' } }
-    })
+import CommandsManager from './core/commandsManager'
+import CooldownMananger from './core/cooldownManager'
+import ModuleManager from './core/moduleManager'
+import GuildManager from './core/guildManager'
+import EventManager from './core/eventManager'
+import MessageTemplate from './templates/messageTemplate'
+import InternationalizationManager from './core/internationalizationManager'
+import PrivacyManager from './core/privacyManager'
 
-    if (!process.env.CLIENT_TOKEN) throw new Error('GTW000: CLIENT_TOKEN not declared.')
-    ClientUser.login(process.env.CLIENT_TOKEN)
+const ClientUser: Discord.Client = new Discord.Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [
+    Discord.Partials.Reaction,
+    Discord.Partials.Message,
+    Discord.Partials.User
+  ],
+  ws: { properties: { $browser: 'Discord iOS' } }
+})
 
-    process.on('exit', () => {
-      ClientUser.destroy()
-    })
-  }
+if (!process.env.CLIENT_TOKEN) throw new Error('GTW000: CLIENT_TOKEN not declared.')
+ClientUser.login(process.env.CLIENT_TOKEN)
+
+process.on('exit', () => {
+  ClientUser.destroy()
+})
+
+// Managers
+
+const ClientInternationalizationManager = new InternationalizationManager()
+const ClientCommandsManager = new CommandsManager('src/client/commands')
+const ClientCooldownManager = new CooldownMananger()
+const ClientModuleManager = new ModuleManager()
+const ClientGuildManager = new GuildManager()
+const ClientEventManager = new EventManager()
+const ClientPrivacyManager = new PrivacyManager()
+
+// Templates
+
+const ClientMessageTemplate = new MessageTemplate({
+  status: { color: Discord.Colors.Blurple, emoji: '📝' },
+  success: { color: Discord.Colors.Green, emoji: '✅' },
+  error: { color: Discord.Colors.Red, emoji: '❌' },
+  warning: { color: Discord.Colors.Orange, emoji: '⚠' },
+  info: { color: Discord.Colors.Blue, emoji: 'ℹ' },
+  debug: { color: Discord.Colors.LuminousVividPink, emoji: '🔧' },
+  question: { color: Discord.Colors.Gold, emoji: '❓' },
+  loading: { color: Discord.Colors.Blurple, emoji: '<a:core_loading:970712845429903461>' },
+  help: { color: Discord.Colors.Gold, emoji: '❓' },
+  timeout: { color: Discord.Colors.Red, emoji: '<:system_timeout:970715618938617856>' }
+})
+
+export {
+  ClientUser,
+  ClientCommandsManager,
+  ClientCooldownManager,
+  ClientModuleManager,
+  ClientGuildManager,
+  ClientEventManager,
+  ClientMessageTemplate,
+  ClientInternationalizationManager,
+  ClientPrivacyManager
 }
-
-export default BaseClient
